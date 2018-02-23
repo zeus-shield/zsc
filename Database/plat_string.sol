dae57314b04b79a581734ba1cb0e4cbe60a90307
Eric-Yao <norhustar@hotmail.com>
2018-02-23 22:00:54 +0100

    added function bytes32ToString()

--- Database/plat_string.sol
+++ Database/plat_string.sol
@@ -133,4 +133,22 @@ library PlatString {
     //function addIntToBuff(string buff, d32 value, du32 offset);
     //function addstringToBuff(string dst, string src, d32 len, du32 offset);
     //function addCharToBuff(string buff, d8 value, du32 offset);
+
+
+    function bytes32ToString(bytes32 x) internal pure returns (string) {
+        bytes memory bytesString = new bytes(32);
+        uint charCount = 0;
+        for (uint j = 0; j < 32; j++) {
+            byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
+            if (char != 0) {
+                bytesString[charCount] = char;
+                charCount++;
+            }
+        }
+        bytes memory bytesStringTrimmed = new bytes(charCount);
+        for (j = 0; j < charCount; j++) {
+            bytesStringTrimmed[j] = bytesString[j];
+        }
+        return string(bytesStringTrimmed);
+    }
 }
