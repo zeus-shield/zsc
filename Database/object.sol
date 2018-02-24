db7a52faf6277fd92e4f23f52f1e4dcc55e015e3
Eric (Yong) Yao <36345092+Eric-Yao@users.noreply.github.com>
2018-02-24 19:16:27 +0100

    added contract ERC20Interface{}; added function transferAnyERC20Token()

--- Database/object.sol
+++ Database/object.sol
@@ -7,7 +7,21 @@ pragma solidity ^0.4.18;
 
 import "./plat_string.sol";
 
+// ----------------------------------------------------------------------------
+// ERC Token Standard #20 Interface
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+// ----------------------------------------------------------------------------
+contract ERC20Interface {
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
 
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
 
 contract Owned {
     address public owner;
@@ -92,9 +106,11 @@ contract Object is Delegated {
     function writeLog(string str) internal {
         LogRecorder(logFile_).recordLog(str);
     }
+
+    // ------------------------------------------------------------------------
+    // Owner can transfer out any accidentally sent ERC20 tokens
+    // ------------------------------------------------------------------------
+    function transferAnyERC20Token(address tokenAddress, uint tokens) public only_owner returns (bool success) {
+        return ERC20Interface(tokenAddress).transfer(owner, tokens);
+    }
 }
-
-
-/*
-
-*/
