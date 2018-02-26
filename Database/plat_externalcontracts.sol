d3d2983069f0f2c0889e2e51ad2ea83afd1f50ae
Eric-Yao <norhustar@hotmail.com>
2018-02-26 16:55:08 +0100

    added file plat_externalcontracts.sol

---
+++ Database/plat_externalcontracts.sol
@@ -0,0 +1,16 @@
+/*
+Copyright (c) 2018, ZSC Dev Team
+*/
+
+pragma solidity ^0.4.18;
+
+import "./plat_string.sol";
+
+// ----------------------------------------------------------------------------
+// ERC Token Standard #20 Interface
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+// ----------------------------------------------------------------------------
+contract ERC20Interface {
+    function transfer(address to, uint tokens) public returns (bool success);
+}
+
