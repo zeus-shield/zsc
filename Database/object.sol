6c05c38e0db5d52aca5fac70a3e0f1b28794c508
Eric-Yao <norhustar@hotmail.com>
2018-02-26 16:55:26 +0100

    removed contract ERC20Interface

--- Database/object.sol
+++ Database/object.sol
@@ -6,14 +6,7 @@ Copyright (c) 2018, ZSC Dev Team
 pragma solidity ^0.4.18;
 
 import "./plat_string.sol";
-
-// ----------------------------------------------------------------------------
-// ERC Token Standard #20 Interface
-// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
-// ----------------------------------------------------------------------------
-contract ERC20Interface {
-    function transfer(address to, uint tokens) public returns (bool success);
-}
+import "./plat_externalcontracts.sol";
 
 contract Owned {
     address public owner;
