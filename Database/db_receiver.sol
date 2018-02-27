a21edd6f8ef10aaf2d2210ea440fdf776c4fec5a
Eric-Yao <norhustar@hotmail.com>
2018-02-27 19:03:39 +0100

    temporarily removed version control

--- Database/db_receiver.sol
+++ Database/db_receiver.sol
@@ -1,9 +1,5 @@
 /*
 Copyright (c) 2018 ZSC Dev, Zeusshield Blockchain Technology Development Co., Ltd
-2017-12-18: v0.01
-2018-02-07: v0.02
-2018-02-09: v0.03
-2018-02-10: v0.04
 */
 
 pragma solidity ^0.4.18;
