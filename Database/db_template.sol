a09a04eb554fbc085232b2643aefcd6cb403c607
Eric-Yao <norhustar@hotmail.com>
2018-02-27 19:04:00 +0100

    updated constructor and function addItem()

--- Database/db_template.sol
+++ Database/db_template.sol
@@ -4,25 +4,24 @@ Copyright (c) 2018, ZSC Dev Team, Eric Yao
 */
 
 pragma solidity ^0.4.18;
-import "./plat_string.sol";
 import "./db_entity.sol";
 import "./db_item.sol";
-import "./db_idmanager.sol";
 
 contract DBTemplate is DBEntity {
-    DBIDManager itemIDs_;
     address itemRoot_;
     
     function DBTemplate(bytes32 _name) public DBEntity(_name) {
     	setEntityType("template");
-
-    	itemRoot_ = new DBNode(PlatString.tobytes32(PlatString.append(_name, "_it_")));
-        addChild(address(itemRoot_));
+        itemRoot_ = address(0);
     }
 
     function addItem(bytes32 _name) public only_delegate returns(address) {
-        DBItem temp = new DBItem(_name);
-        return DBNode(itemRoot_).addChild(address(temp));
+        if (itemRoot_ == address(0)) {
+            itemRoot_ = addSubRootNode();
+        }
+        DBItem nd = new DBItem(_name);
+        nd.setDelegate(itemRoot_, true);
+        return DBNode(itemRoot_).addChild(address(nd));
     }
 
     function numItems() public only_delegate constant returns (uint) {
