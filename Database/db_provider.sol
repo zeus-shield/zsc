b4441b4f154c5a51fd49addfc95d2acdc8e18233
Eric-Yao <norhustar@hotmail.com>
2018-03-01 09:20:58 +0100

    removed variable templateRoot_, and  two functions addTemplate() and numTemplates()

--- Database/db_provider.sol
+++ Database/db_provider.sol
@@ -4,20 +4,17 @@ Copyright (c) 2018 ZSC Dev.
 
 pragma solidity ^0.4.18;
 
+import "./plat_string.sol";
 import "./db_user.sol";
-import "./db_item.sol";
-import "./db_template.sol";
+//import "./db_item.sol";
+//import "./db_template.sol";
 
 contract DBProvider is DBUser {
-    address templateRoot_;
-
     // Constructor
     function DBProvider(bytes32 _name) public DBUser(_name) {
-        templateRoot_ = address(0);
         setEntityType("proiver"); 
         initParameters();
     }
-
     function initParameters() internal {
         addParameter("assurerType");
         addParameter("assurerName");
@@ -35,17 +32,5 @@ contract DBProvider is DBUser {
         addParameter("claimEmail");
         addParameter("claimPhone");
     }
-
-    function addTemplate(bytes32 _templateName) public only_delegate returns (address) {
-        if (templateRoot_ == address(0)) {
-            templateRoot_ = addSubRootNode();
-        }
-        DBTemplate nd = new DBTemplate(_templateName);
-        nd.setDelegate(templateRoot_, true);
-        return DBNode(templateRoot_).addChild(address(nd));
-    }
-
-    function numTemplates() public only_delegate constant returns (uint) {
-        return DBNode(templateRoot_).numChildren();
-    }
+  
 }
