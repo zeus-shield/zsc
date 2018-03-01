8fdd5e02954412a97f03ad6e35f5762df3928404
Eric-Yao <norhustar@hotmail.com>
2018-03-01 09:23:51 +0100

    comment out two functions addItem() and numItems()

--- Database/db_template.sol
+++ Database/db_template.sol
@@ -5,7 +5,7 @@ Copyright (c) 2018, ZSC Dev Team, Eric Yao
 
 pragma solidity ^0.4.18;
 import "./db_entity.sol";
-import "./db_item.sol";
+//import "./db_item.sol";
 
 contract DBTemplate is DBEntity {
     address itemRoot_;
@@ -15,16 +15,20 @@ contract DBTemplate is DBEntity {
         itemRoot_ = address(0);
     }
 
+/*
     function addItem(bytes32 _name) public only_delegate returns(address) {
         if (itemRoot_ == address(0)) {
-            itemRoot_ = addSubRootNode();
+            itemRoot_ = new DBNode(PlatString.tobytes32(PlatString.append(name(), "_1")));
+            DBNode(itemRoot_).setDelegate(this, true);
+            addChild(itemRoot_);
         }
-        DBItem nd = new DBItem(_name);
-        nd.setDelegate(itemRoot_, true);
-        return DBNode(itemRoot_).addChild(address(nd));
+        address nd = new DBNode(_name);
+        DBNode(nd).setDelegate(itemRoot_, true);
+        return DBNode(itemRoot_).addChild(nd);
     }
 
     function numItems() public only_delegate constant returns (uint) {
     	return DBNode(itemRoot_).numChildren();
     }
+    */
 }
