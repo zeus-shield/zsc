ac8004c05539fbafc38272670d2f7a4158072837
Eric-Yao <norhustar@hotmail.com>
2018-03-05 17:43:08 +0100

    added new file factory_basic.sol

---
+++ Database/factory_basic.sol
@@ -0,0 +1,27 @@
+/*
+Copyright (c) 2018 ZSC Dev Team
+*/
+
+pragma solidity ^0.4.18;
+
+import "./object.sol";
+
+contract ZSCDatabase is Object { 
+    function getRootNode() public only_delegate constant returns (address);
+    function getNode(bytes32 _name) public only_delegate constant returns (address);
+    function destroyNode(bytes32 _name) public only_delegate returns (bool);
+    function destroyNode(address _node) public only_delegate returns (bool);
+}
+
+contract FactoryBasic is Object {
+    address db_;
+    bytes32 temp_nm_;
+    bytes32 temp_adr_;
+
+    function FactoryBasic(bytes32 _name, address _db) public Object(_name) {
+        require(_db != address(0));
+        db_ = _db;
+    }
+
+    function getDB() public only_delegate constant returns (address) { return db_;}
+}
