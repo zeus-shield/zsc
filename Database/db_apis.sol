af5d7feb43973c8c92dcaddc4dafb26928811a73
Eric-Yao <norhustar@hotmail.com>
2018-03-08 08:54:09 +0100

    re-construct contract DBApis

--- Database/db_apis.sol
+++ Database/db_apis.sol
@@ -5,45 +5,33 @@ Copyright (c) 2018 ZSC Dev Team
 pragma solidity ^0.4.18;
 
 import "./plat_string.sol";
-import "./object.sol";
+import "./db_factory.sol";
 
-/**
- * The ZSDatabase contract does this and that...
- */
-contract ZSCDatabase is Object { 
+contract DBFactory is Object { 
     function createNode(bytes32 _name) public only_delegate returns (address);
+    function getBindedDB() public only_delegate constant returns (address);
     function getNode(bytes32 _name) public only_delegate constant returns (address);
 }
 
-contract ZSCNode is Object { 
-    function addChild(address _node) public only_delegate returns (address);
-}
-
 contract DBApis is Object {
-    mapping(bytes32 => address) databases_;
+    mapping(bytes32 => address) factories_;
 
     function DBApis(bytes32 _name) public Object(_name) {
     }
 
-    function setDatabase(bytes32 _db, address _adr) public only_delegate {
-        databases_[_db] = _adr;
+    function addFactory(bytes32 _name, address _adr) public only_delegate returns (bool) {
+        if (factories_[_mame] != 0) return false;
+        factories_[_name] = _adr;
+        return true;
     }
 
-    function createReceiver(bytes32 _name) public only_delegate returns (bool) {
-        address db = databases_["receiver"];
-        if (db == 0) return false;
-        return (ZSCDatabase(db).createNode(_name) != 0);
+    function getDatabase(bytes32 _name) internal only_delegate constant returns (address) {
+        if (factories_[_name] == 0) return false;
+        return DBFactory(factories_[_name]).getBindedDB();
     }
-    
+
     function createProvider(bytes32 _name) public only_delegate returns (bool) {
-        address db = databases_["provider"];
-        if (db == 0) return false;
-        return (ZSCDatabase(db).createNode(_name) != 0);
-    }
-
-    function createAgreement(bytes32 _name) public only_delegate returns (bool) {
-        address db = databases_["agreement"];
-        if (db == 0) return false;
-        return (ZSCDatabase(db).createNode(_name) != 0);
+        if (factories_["provider"] == 0) return false;
+        return DBFactory(factories_["provider"]).createNode(_name);
     }
 }
