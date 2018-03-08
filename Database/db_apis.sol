a5e7c17f9a328e34f09f5b0ddb68ee72e2102e07
Eric-Yao <norhustar@hotmail.com>
2018-03-08 12:02:58 +0100

    updated three functions addFactory(), getDatabase() and createProvider()

--- Database/db_apis.sol
+++ Database/db_apis.sol
@@ -5,7 +5,7 @@ Copyright (c) 2018 ZSC Dev Team
 pragma solidity ^0.4.18;
 
 import "./plat_string.sol";
-import "./db_factory.sol";
+import "./object.sol";
 
 contract DBFactory is Object { 
     function createNode(bytes32 _name) public only_delegate returns (address);
@@ -20,18 +20,18 @@ contract DBApis is Object {
     }
 
     function addFactory(bytes32 _name, address _adr) public only_delegate returns (bool) {
-        if (factories_[_mame] != 0) return false;
+        if (factories_[_name] != 0) return false;
         factories_[_name] = _adr;
         return true;
     }
 
     function getDatabase(bytes32 _name) internal only_delegate constant returns (address) {
-        if (factories_[_name] == 0) return false;
+        if (factories_[_name] == 0) return 0;
         return DBFactory(factories_[_name]).getBindedDB();
     }
 
-    function createProvider(bytes32 _name) public only_delegate returns (bool) {
-        if (factories_["provider"] == 0) return false;
+    function createProvider(bytes32 _name) public only_delegate returns (address) {
+        if (factories_["provider"] == 0) return 0;
         return DBFactory(factories_["provider"]).createNode(_name);
     }
 }
