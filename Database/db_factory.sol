031f4e62a2d4c67e5202f38c4d8af11794769185
Eric-Yao <norhustar@hotmail.com>
2018-03-08 18:29:34 +0100

    removed virtual contract ZSCDBNode; changed the variables to be private

--- Database/db_factory.sol
+++ Database/db_factory.sol
@@ -4,7 +4,7 @@ Copyright (c) 2018 ZSC Dev Team
 
 pragma solidity ^0.4.18;
 
-import "./object.sol";
+import "./db_entity.sol";
 
 contract ZSCDatabase is Object { 
     function getNode(bytes32 _name) public only_delegate constant returns (address);
@@ -12,46 +12,33 @@ contract ZSCDatabase is Object {
     function destroyNode(address _node) public only_delegate returns (bool);
 }
 
-contract ZSCDBNode is Object {
-    function getEntityType() public only_delegate constant returns (bytes32);
-    function setID(uint _id) only_delegate  public; 
-    function setActivated(bool _activated) only_delegate public;
-    function getID() public only_delegate constant returns (uint);
-    function getActivated() public only_delegate constant returns (bool);
-    function addParameter(bytes32 _parameter) public only_delegate returns (bool);
-    function removeParameter(bytes32 _parameter) public only_delegate returns (bool);
-    function setParameter(bytes32 _parameter, string _value) public only_delegate returns (bool);
-    function numParameters() public only_delegate constant returns (uint);
-    function getParameterNameByIndex(uint _index) public only_delegate constant returns (bytes32);
-}
-
 contract DBFactory is Object {
-    address bindedDB_;
-    address apiController_;
+    address private bindedDB_;
+    address private apiController_;
 
     function DBFactory(bytes32 _name) public Object(_name) {
     }
 
-    function initFactory(address _callbackApiController, address _database) public only_delegate  {
-        if (_callbackApiController != 0) {
-            if (apiController_ != 0) {
-                setDelegate(apiController_, false);
-            }
-            apiController_ = _callbackApiController;
-            setDelegate(_callbackApiController, true);
-        }
-
-        if (_database != 0) bindedDB_ = _database;
-    }
-
     function createNode(bytes32 _name) internal returns (address);
-
+    
     function getBindedDB() public only_delegate constant returns (address) { return bindedDB_;}
 
     function getBindedApiController() public only_delegate constant returns (address) { return apiController_;}
 
     function getNode(bytes32 _name) public only_delegate constant returns (address) {
         return ZSCDatabase(bindedDB_).getNode(_name);
+    }
+
+    function initFactory(address _callbackApiController, address _database) public only_delegate  {
+        if (_database != 0) bindedDB_ = _database;
+
+        if (_callbackApiController != 0 && _callbackApiController != apiController_) {
+            if (apiController_ != 0) {
+                setDelegate(apiController_, false);
+            }
+            apiController_ = _callbackApiController;
+            setDelegate(_callbackApiController, true);
+        }
     }
 
     function operateNode(bytes32 _operation, bytes32 _node) public only_delegate returns (address) {
@@ -63,7 +50,7 @@ contract DBFactory is Object {
     function operateParameter(bytes32 _operation, bytes32 _node, bytes32 _parameter, string _value) public only_delegate returns (bool) {
         bool tag = true;
         if (_operation == "set") {
-            tag = ZSCDBNode(ZSCDatabase(bindedDB_).getNode(_node)).setParameter(_parameter, _value);
+            tag = DBEntity(ZSCDatabase(bindedDB_).getNode(_node)).setParameter(_parameter, _value);
         } else if (_operation == "get") {
             //Solidity-0.4.18 does not support the return string among different contracts 
             tag = false;
