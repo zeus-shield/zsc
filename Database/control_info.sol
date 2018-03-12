761eada12b93b124f28b915d589af56c287eb282
Eric-Yao <norhustar@hotmail.com>
2018-03-12 09:24:12 +0100

    changed mapping name from nodeParameters_ to parameters_; removed modifier user_exist() and user_notexist(); added modifiers node_exist() and user_notregistered(); added function _recordString() and addParameterEmptyInfo()

--- Database/control_info.sol
+++ Database/control_info.sol
@@ -16,21 +16,31 @@ contract ControlInfo {
 
     struct ParameterInfo {
         mapping (bytes32 => string) value_;
+        bool tag_;
     }
     
     mapping(bytes32 => UserInfo) private users_;
-    mapping(bytes32 => ParameterInfo) private nodeParameters_;
+    mapping(bytes32 => ParameterInfo) private parameters_;
 
-    modifier user_exist(_name) {require (factories_[_name] == 0); _;}
-    modifier user_notexist(_name) {require (factories_[_name] != 0); _;}
+    modifier node_exist(bytes32 _name) {require(parameters_[_name].tag_ == true); _;}
+    modifier user_notregistered(bytes32 _name) {require(users_[_name].id_ == 0); _;}
 
-    function ControlInfo() {
+    function ControlInfo() public {
+    }
+ 
+    function _recordString(bytes32 _node, bytes32 _parameter, string _value) public node_exist(_node) {
+        parameters_[_node].value_[_parameter] = _value;
     }
 
-    function registerUser(bytes32 _type, bytes32 _name) user_notexist(_name) public {
+    function registerUser(NodeType _type, bytes32 _name) public user_notregistered(_name) {
         require(users_[_name].id_ != 0);
         users_[_name].id_ = msg.sender;
         users_[_name].type_ = _type;
         users_[_name].status_ = 1;
+    }
+
+    function addParameterEmptyInfo(bytes32 _node, bytes32 _parameter) internal {
+        parameters_[_node].tag_ = true;
+        parameters_[_node].value_[_parameter] = "";
     }
 }
