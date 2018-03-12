54d7d3d3c19bf86e15bdc464d1626f67c3501032
Eric-Yao <norhustar@hotmail.com>
2018-03-12 09:03:48 +0100

    added file control_apis.sol

---
+++ Database/control_apis.sol
@@ -0,0 +1,12 @@
+/*
+Copyright (c) 2018 ZSC Dev Team
+*/
+
+pragma solidity ^0.4.18;
+
+import "./control_base.sol";
+
+contract ControlApis is ControBase {
+    function ControlApis(bytes32 _name) public ControBase(_name) {
+    }
+}
