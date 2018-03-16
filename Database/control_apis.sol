102e91d0c207aa0b19767eac4602b29cd8bc83b1
Eric-Yao <norhustar@hotmail.com>
2018-03-16 11:52:34 +0100

    added functions doesProviderExist(), getProviderParameterNameByIndex(); changed function name numNodeParameters() to numProviderParameters(), from getProviderParameter() to getNodeParameter(); updated function addProviderParameter

--- Database/control_apis.sol
+++ Database/control_apis.sol
@@ -24,6 +24,12 @@ contract ControlApis is ControlBase {
         getFactory("provider");
     }
 
+    /// @dev Check the provider wheather or not existing
+    /// @param _node The name of the node to be checked
+    function doesProviderExist(bytes32 _node) public only_delegate constant returns (bool) {
+        return (getFactory("provider").getNode(_node) != 0);
+    }
+
     /// @dev Creat a provider node
     /// @param _node The name of the added provider
     function createProviderNode(bytes32 _node) public only_delegate returns (address) {
@@ -34,30 +40,42 @@ contract ControlApis is ControlBase {
     /// @param _node The name of the existing provider
     /// @param _parameter The name of the added parameter
     function addProviderParameter(bytes32 _node, bytes32 _parameter) public only_delegate returns (bool) {
+        require(checkAllowedUser(_node));
         return operateNodeParameter("provider", "add", _node, _parameter, "");
     }
 
-    /// @dev Set the value to a paramter of a provider node
+    /// @dev Set the value to a paramter of a provider 
     /// @param _node The name of the existing provider
     /// @param _parameter The name of the existing parameter
     /// @param _value The parameter value
     function setProviderParameter(bytes32 _node, bytes32 _parameter, string _value) public only_delegate returns (bool) {
+        require(checkAllowedUser(_node));
         return operateNodeParameter("provider", "set", _node, _parameter, _value);
     }
 
-    /// @dev Get the value to a paramter of a provider node
-    /// @param _node The name of the existing provider
+    /// @dev Get the value to a paramter of a node
+    /// @param _node The name of the provider
     /// @param _parameter The name of the existing parameter
-    function getProviderParameter(bytes32 _node, bytes32 _parameter) public only_delegate constant returns (string) {
+    function getNodeParameter(bytes32 _node, bytes32 _parameter) public only_delegate constant returns (string) {
+        require(checkAllowedUser(_node));
         return getControlBaseParameterValue(_node, _parameter);
     }
 
-    /// @dev Get the number of paramters of a node
-    /// @param _node The name of the existing node
-    /// @param _parameter The name of the existing parameter
-    function numNodeParameters(bytes32 _node, bytes32 _parameter) public only_delegate constant returns (string) {
+    /// @dev Get the number of paramters of a provider
+    /// @param _node The name of the existing provider
+    function numProviderParameters(bytes32 _node) public only_delegate constant returns (uint) {
         return  getFactory("provider").numNodeParameters(_node);
     }
 
-    
+    /// @dev Get the number of paramters of a provider
+    /// @param _node The name of the existing provider
+    /* Example:
+        var num = numNodeParameters("test");
+        if (num > 0) {
+            var para = getNodeParameterNameByIndex("test", 0);
+        }
+    */
+    function getProviderParameterNameByIndex(bytes32 _node, uint _index) public only_delegate constant returns (bytes32) {
+        return  getFactory("provider").getNodeParameterNameByIndex(_node, _index);
+    }
 }
