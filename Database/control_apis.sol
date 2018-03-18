f3e0c7275838071cbe1d9d2787dd8a8979e49523
Eric-Yao <norhustar@hotmail.com>
2018-03-18 17:19:29 +0100

    redefined the api functions

--- Database/control_apis.sol
+++ Database/control_apis.sol
@@ -7,7 +7,6 @@ pragma solidity ^0.4.18;
 import "./control_base.sol";
 
 contract ControlApis is ControlBase {
-
     /// @dev Constructor
     /// @param _name The name of the controller
     function ControlApis(bytes32 _name) public ControlBase(_name) {
@@ -15,56 +14,51 @@ contract ControlApis is ControlBase {
 
     /// @dev Add the database factory of managing the provider nodes
     /// @param _adr The address of the database factory
-    function addProviderFactory(address _adr) public only_owner {
-        addFactory("provider", _adr);
-    }
-
-    /// @dev Get the database factory of managing the provider nodes
-    function getProviderFactory() public only_owner constant {
-        getFactory("provider");
+    function addFactory(uint _type, address _adr) public only_owner {
+        addFactory(factoryType(_type), _adr);
     }
 
     /// @dev Check the provider wheather or not existing
     /// @param _node The name of the node to be checked
-    function doesProviderExist(bytes32 _node) public only_delegate constant returns (bool) {
-        return (getFactory("provider").getNode(_node) != 0);
+    function doesElementExist(uint _factroyType, bytes32 _node) public only_delegate constant returns (bool) {
+        return (getFactory(factoryType(_factroyType)).getNode(_node) != 0);
     }
 
     /// @dev Creat a provider node
     /// @param _node The name of the added provider
-    function createProviderNode(bytes32 _node) public only_delegate returns (address) {
-        return createFactoryNode("provider", _node);
+    function createElement(uint _factroyType, bytes32 _node) public only_delegate returns (address) {
+        return createFactoryNode(factoryType(_factroyType), _node);
     }
 
     /// @dev add a paramter to a provider node
     /// @param _node The name of the existing provider
     /// @param _parameter The name of the added parameter
-    function addProviderParameter(bytes32 _node, bytes32 _parameter) public only_delegate returns (bool) {
+    function addElementParameter(uint _factroyType, bytes32 _node, bytes32 _parameter) public only_delegate returns (bool) {
         require(checkAllowedUser(_node));
-        return operateNodeParameter("provider", "add", _node, _parameter, "");
+        return operateNodeParameter(factoryType(_factroyType), "add", _node, _parameter, "");
     }
 
     /// @dev Set the value to a paramter of a provider 
     /// @param _node The name of the existing provider
     /// @param _parameter The name of the existing parameter
     /// @param _value The parameter value
-    function setProviderParameter(bytes32 _node, bytes32 _parameter, string _value) public only_delegate returns (bool) {
+    function setElementParameter(uint _factroyType, bytes32 _node, bytes32 _parameter, string _value) public only_delegate returns (bool) {
         require(checkAllowedUser(_node));
-        return operateNodeParameter("provider", "set", _node, _parameter, _value);
+        return operateNodeParameter(factoryType(_factroyType), "set", _node, _parameter, _value);
     }
 
     /// @dev Get the value to a paramter of a node
     /// @param _node The name of the provider
     /// @param _parameter The name of the existing parameter
-    function getNodeParameter(bytes32 _node, bytes32 _parameter) public only_delegate constant returns (string) {
+    function getElementParameter(bytes32 _node, bytes32 _parameter) public only_delegate constant returns (string) {
         require(checkAllowedUser(_node));
         return getControlBaseParameterValue(_node, _parameter);
     }
 
     /// @dev Get the number of paramters of a provider
     /// @param _node The name of the existing provider
-    function numProviderParameters(bytes32 _node) public only_delegate constant returns (uint) {
-        return  getFactory("provider").numNodeParameters(_node);
+    function numElementParameters(uint _factroyType, bytes32 _node) public only_delegate constant returns (uint) {
+        return  getFactory(factoryType(_factroyType)).numNodeParameters(_node);
     }
 
     /// @dev Get the number of paramters of a provider
@@ -75,7 +69,7 @@ contract ControlApis is ControlBase {
             var para = getNodeParameterNameByIndex("test", 0);
         }
     */
-    function getProviderParameterNameByIndex(bytes32 _node, uint _index) public only_delegate constant returns (bytes32) {
-        return  getFactory("provider").getNodeParameterNameByIndex(_node, _index);
+    function getElementParameterNameByIndex(uint _factroyType, bytes32 _node, uint _index) public only_delegate constant returns (bytes32) {
+        return  getFactory(factoryType(_factroyType)).getNodeParameterNameByIndex(_node, _index);
     }
 }
