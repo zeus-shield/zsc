c160a2e42f3a0452daedc76afe113ca0e16d043f
Eric-Yao <norhustar@hotmail.com>
2018-03-26 08:50:45 +0200

    updated contract LogRecorder
    updated mapping variable and functions

--- Database/log_recorder.sol
+++ Database/log_recorder.sol
@@ -7,21 +7,29 @@ pragma solidity ^0.4.18;
 import "./Object.sol";
 
 contract LogRecorder is Delegated { 
+    struct LogInfo {
+        uint nos_;
+        mapping(uint => string) logs_;
+    }
     mapping(address => bool) listeners_;
-    mapping(address => string) print_log_;
+    mapping(address => LogInfo) print_log_;
 
     modifier only_listener(address _adr) {require(listeners_[_adr]); _; }
+
+    function LogRecorder() public Delegated() {}
 
     function registerListener(address _adr) public only_delegate {
         listeners_[_adr] = true;
     }
 
     function addLog(string _log) public only_delegate {
-        print_log_[msg.sender] = PlatString.append(print_log_[msg.sender], _log);
+        uint index = print_log_[msg.sender].nos_;
+        print_log_[msg.sender].nos_++;
+        print_log_[msg.sender].logs[index] = _log;
     }
 
-    function printLog(address _adr) public only_delegate only_listener(_adr) constant returns (string) {
-        return print_log_[_adr];
+    function printLog(address _adr, uint _index) public only_delegate only_listener(_adr) constant returns (string) {
+        return print_log_[_adr].logs_[_index];
     } 
 }
 
