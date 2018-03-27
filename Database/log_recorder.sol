e5f868c180050b912947356f047d837ed3120465
Eric-Yao <norhustar@hotmail.com>
2018-03-27 12:04:56 +0200

    updated functions registerListener(), addLog(), printLog()

--- Database/log_recorder.sol
+++ Database/log_recorder.sol
@@ -9,6 +9,7 @@ import "./Object.sol";
 contract LogRecorder is Delegated { 
     struct LogInfo {
         uint nos_;
+        bytes32 name_;
         mapping(uint => string) logs_;
     }
     mapping(address => bool) listeners_;
@@ -18,18 +19,33 @@ contract LogRecorder is Delegated {
 
     function LogRecorder() public Delegated() {}
 
-    function registerListener(address _adr) public only_delegate {
+    
+    function registerListener(address _adr, bytes32 _name) public only_delegate {
         listeners_[_adr] = true;
+        print_log_[_adr].name_ = _name;
     }
 
-    function addLog(string _log) public only_listener(msg.sender) {
+    function addLog(string _log, bool _newLine) public only_listener(msg.sender) {
         uint index = print_log_[msg.sender].nos_;
-        print_log_[msg.sender].nos_++;
-        print_log_[msg.sender].logs_[index] = _log;
+        if (index == 0) {
+                print_log_[msg.sender].logs_[index] = _log;
+        } else {
+            if (_newLine == false) {
+                print_log_[msg.sender].logs_[index] = PlatString.append(print_log_[msg.sender].logs_[index], _log);
+            } else {
+                print_log_[msg.sender].nos_++;
+                print_log_[msg.sender].logs_[index + 1] = _log;
+            }
+        }
     }
 
-    function printLog(address _adr, uint _index) public only_delegate() constant returns (string) {
-        return print_log_[_adr].logs_[_index];
+    function printLog(address _adr, uint _index) public only_listener(_adr) constant returns (string) {
+        if (isDelegate(_adr) == false || _index >= print_log_[msg.sender].nos_ ) 
+            return "null";
+
+        string memory str = PlatString.bytes32ToString(print_log_[_adr].name_);
+        str = PlatString.append(str, print_log_[_adr].logs_[_index]);
+        return str;
     } 
 }
 
