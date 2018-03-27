1085e7e081b855b50ea84c2acaf7ccb482360692
Eric-Yao <norhustar@hotmail.com>
2018-03-27 20:43:03 +0200

    updated three functions
    updated registerListener(), addLog(), printLog()

--- Database/log_recorder.sol
+++ Database/log_recorder.sol
@@ -13,7 +13,7 @@ contract LogRecorder is Delegated {
         mapping(uint => string) logs_;
     }
     mapping(address => bool) listeners_;
-    mapping(address => LogInfo) print_log_;
+    mapping(address => LogInfo) public print_log_;
 
     modifier only_listener(address _adr) {require(listeners_[_adr]); _; }
 
@@ -23,28 +23,26 @@ contract LogRecorder is Delegated {
     function registerListener(address _adr, bytes32 _name) public only_delegate {
         listeners_[_adr] = true;
         print_log_[_adr].name_ = _name;
+        print_log_[_adr].nos_ = 1;
+        print_log_[_adr].logs_[0] = "registered";
     }
 
     function addLog(string _log, bool _newLine) public only_listener(msg.sender) {
-        uint index = print_log_[msg.sender].nos_;
-        if (index == 0) {
-                print_log_[msg.sender].logs_[index] = _log;
+        uint index = print_log_[msg.sender].nos_ - 1;
+        if (_newLine == true) {
+            print_log_[msg.sender].nos_++;
+            print_log_[msg.sender].logs_[index + 1] = _log;
         } else {
-            if (_newLine == false) {
-                print_log_[msg.sender].logs_[index] = PlatString.append(print_log_[msg.sender].logs_[index], _log);
-            } else {
-                print_log_[msg.sender].nos_++;
-                print_log_[msg.sender].logs_[index + 1] = _log;
-            }
+            print_log_[msg.sender].logs_[index] = PlatString.append(print_log_[msg.sender].logs_[index], _log);
         }
     }
 
     function printLog(address _adr, uint _index) public only_listener(_adr) constant returns (string) {
-        if (isDelegate(_adr) == false || _index >= print_log_[msg.sender].nos_ ) 
+        if (isDelegate(msg.sender) == false || _index >= print_log_[_adr].nos_ ) 
             return "null";
 
         string memory str = PlatString.bytes32ToString(print_log_[_adr].name_);
-        str = PlatString.append(str, print_log_[_adr].logs_[_index]);
+        str = PlatString.append("[", str, "] ", print_log_[_adr].logs_[_index]);
         return str;
     } 
 }
