c421f7427c6194ab10ba7573174a88ec1ea750cc
Eric-Yao <norhustar@hotmail.com>
2018-03-28 21:54:05 +0200

    removed enum AgreementStatus and added three functions
    added functions setAgreementStatus(), getAgreementStatus(), setParameter()

--- Database/db_agreement.sol
+++ Database/db_agreement.sol
@@ -7,12 +7,12 @@ import "./db_entity.sol";
 import "./db_idmanager.sol";
 
 contract DBAgreement is DBEntity {
-    enum AgreementStatus { UNDEFINED, ONGOING, CANCLED, PAID, NOTPAID}
-    AgreementStatus agreementStatus_;
+    uint private status_; // 0: UNDEFINED; 1: ONGOING; 2: PAID; 3: NOTPAID
 
     // Constructor
     function DBAgreement(bytes32 _name) public DBEntity(_name) {
         setNodeType("agreement");
+        status_ = 0;
     }
 
     function initParameters() internal {
@@ -23,6 +23,15 @@ contract DBAgreement is DBEntity {
         addParameter("paymentAmount");
     }
 
+    function setAgreementStatus(uint _status) public only_delegate { status_ = _status; }
+
+    function getAgreementStatus() public only_delegate constant returns (uint) { status_ = _status; }
+
+    function setParameter(bytes32 _parameter, string _value) public only_delegate returns (bool) {
+        if (status_ == 0) {
+            super.setParameter(_parameter, _value);
+        }
+    }
  }
 
 
