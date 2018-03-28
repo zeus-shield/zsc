9105da1eebca2ded832d58a45ae989ebfd9f971f
Eric-Yao <norhustar@hotmail.com>
2018-03-28 18:13:02 +0200

    added 4 functions
    added functions uF_showHashResult(), uF_getSingleParameter(), uF_loadElementParameterValues(), uF_setElementParameter()

--- Api-example/userFunctions.js
+++ Api-example/userFunctions.js
@@ -10,6 +10,27 @@ var uF_controlApisAdvAbi = "";
 var uF_userName ;
 var uF_userNameHr ;
 
+function uF_showHashResult(elementID, hash) {
+    web3.eth.getTransactionReceipt(hash, 
+    function(error, result){ 
+        if(!error) {
+            var show;
+            if (result == null) {
+                show =  "(pending)" + hash ;
+                uF_showHashResult(elementID, hash);
+            } else {
+                if (result.status == 0) {
+                    show = "(failure)" + hash;
+                } else {
+                    show = "(succeeded)" + hash ;
+                }
+            }
+            document.getElementById(elementID).innerText = show;
+        } else {
+            console.log("error: " + error);
+        }
+    });
+}
 function uf_getEthAccount() {
     //console.log(web3.eth.accounts[0])
     var account = web3.eth.accounts[0];
@@ -17,11 +38,11 @@ function uf_getEthAccount() {
     return account;
 }
 
-function uf_getGasPrice() {
+function uf_getGasPrice(limit) {
     return limit * 1000000000; //limits * gwei
 }
 
-function uf_getGasLimit() {
+function uf_getGasLimit(limit) {
     return limit * 1000 * 1000; //limits * 1 million
 }
 
@@ -76,8 +97,55 @@ function uF_getUsernameHr() {
     return uF_userNameHr;
 }
 
+function uF_getSingleParameter(node, index, func){  
+    var myControlApi = uf_getControlApi();
+    myControlApi.getElementParameter(node, uF_parameters[index], {from: uf_getEthAccount()},
+         function(error, value){ 
+            if(!error) { 
+                func(index, value);
+            }  
+            else  console.log("error: " + error);
+        });
+}
+
+function uF_loadElementParameterValues(node, num, func) {
+    for (var i = 0; i < num; ++i) {
+        uF_getSingleParameter(node, i, function(index, value) {
+            uF_parameterValue[index] = value;
+            if (index == num - 1)
+                func(index);
+        });
+    } 
+} 
 
 
+function uF_setElementParameter(nodeId, logID) {
+    var myControlApi = uf_getControlApi();
+    var nodeName = document.getElementById(nodeId).innerText;
+    var hr = document.getElementById(nodeId + "Hr").innerText;
+
+    var info = "";
+    var count = 0;
+
+    for (var i = 0; i < uF_parameters.length; ++i) {
+        var value = document.getElementById(uF_parameters[i]).value;
+        if (value != uF_parameterValue[i]) {
+            count ++;
+            uF_parameterValue[i] = value;
+
+            info += "{<" + uF_parameters[i] + ">" + "<" + value + ">}";
+        }
+    }
+
+    if (count > 0) {
+        myControlApi.setElementMultipleParameters(1, nodeName, hr, info,  
+           {from: uf_getEthAccount(), gasPrice: uf_getGasPrice(1), gas : uf_getGasLimit(55000)}, 
+           function(error, result){ 
+            if(!error) uF_showHashResult(logID, result);
+            else console.log("error: " + error);
+        });
+    }
+} 
 
 
 
