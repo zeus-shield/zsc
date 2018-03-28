01ac598308319f1f9b336f9e1aa7a2ecc8219365
Eric-Yao <norhustar@hotmail.com>
2018-03-29 00:01:35 +0200

    updated three virtual contract and functions
    updated contracts DBFactory, DBDatabase and DBNode; removed mapping bindedDBs_; removed modifier db_exist; added function setDatabaseAdr(); updated functions addFactoryAdr(), getDBNode(), createFactoryNode(), operateNodeParameter(), duplicateNode() and getDBDatabase(); removed functions getDBNodeAddress(),

--- Database/control_base.sol
+++ Database/control_base.sol
@@ -9,19 +9,18 @@ import "./object.sol";
 import "./control_info.sol";
 
 contract DBFactory is Object { 
-    function getBindedDB() public only_delegate constant returns (address);
     function createNode(bytes32 _node) public returns (address);
 }
 
 contract DBDatabase is Object { 
     function getNode(bytes32 _name) public only_delegate constant returns (address);
-    function getNodeNameByAddress(address _adr) public only_delegate constant returns (bytes32);
+    function checkeNodeByAddress(address _adr) public only_delegate constant returns (bool);
     function numNodes() public only_delegate constant returns (uint);
-    function getNodeNameByIndex(uint _index) public only_delegate constant returns (bytes32);
+    function getNodeByIndex(uint _index) public only_delegate constant returns (address);
 }
 
 contract DBNode is Object {
-    function getEntityType() public only_delegate constant returns (bytes32);
+    function getNodeType() public only_delegate constant returns (bytes32);
     function getBlance(bytes32 _name, address _adr) public only_delegate constant returns (uint256);
 
     function setActivated(bool _activated) only_delegate public;
@@ -39,16 +38,18 @@ contract DBNode is Object {
     function bindEntity(address _adr) only_delegate public;
     function numBindedEntities(bytes32 _type) public only_delegate constant returns (uint);
     function getBindedEntityNameByIndex(bytes32 _type, uint _index) public only_delegate constant returns (bytes32);
+
+    function setAgreementStatus(uint _status) only_delegate public;
+    function getAgreementStatus() public only_delegate constant returns (uint);
 }
 
 contract ControlBase is Object, ControlInfo {   
     mapping(uint => bytes32) private factoryTypes_;
     mapping(bytes32 => address) private factories_;
-    mapping(bytes32 => address) private bindedDBs_;
+    address private bindedDB_;
 
     modifier factroy_exist(bytes32 _name) {require(factories_[_name] != 0); _;}
     modifier factroy_notexist(bytes32 _name) {require(factories_[_name] == 0); _;}
-    modifier db_exist(bytes32 _name) {require(bindedDBs_[_name] != 0); _;}
 
     function ControlBase(bytes32 _name) public Object(_name) {
         factoryTypes_[1] = "provider";
@@ -59,10 +60,16 @@ contract ControlBase is Object, ControlInfo {
 
     function mapType(uint _type) internal constant returns (bytes32) { return factoryTypes_[_type]; }
 
-    function addFactory(bytes32 _name, address _adr) internal factroy_notexist(_name) {
+    function setDatabaseAdr(address _db) internal only_delegate {
+        bindedDB_ = _db;
+
+        addLog("Added database: ", true);
+        addLog(PlatString.bytes32ToString(Object(_db).name()), false);
+    }
+
+    function addFactoryAdr(bytes32 _name, address _adr) internal factroy_notexist(_name) {
         require(_adr != 0);
         factories_[_name] = _adr;
-        bindedDBs_[_name] = DBFactory(_adr).getBindedDB();
 
         addLog("Added factory: ", true);
         addLog(PlatString.bytes32ToString(_name), false);
@@ -72,22 +79,12 @@ contract ControlBase is Object, ControlInfo {
         return DBFactory(factories_[_name]);
     }
 
-    function getDBDatabase(bytes32 _name) internal db_exist(_name) constant returns (DBDatabase) { 
-        return DBDatabase(bindedDBs_[_name]);
+    function getDBDatabase() internal constant returns (DBDatabase) { 
+        return DBDatabase(bindedDB_);
     }
 
-    function getDBNodeAddress(bytes32 _db, bytes32 _node) internal constant returns (address) {
-        return getDBDatabase(_db).getNode(_node);
-    }
-
-    function getDBNodeName(bytes32 _db, address _adr) internal constant returns (bytes32) {
-        return getDBDatabase(_db).getNodeNameByAddress(_adr);
-    }
-
-    function getDBNode(bytes32 _db, bytes32 _node) internal constant returns (DBNode) {
-        address nd = getDBNodeAddress(_db, _node);
-        require (nd != 0);        
-        return DBNode(nd);
+    function getDBNode(bytes32 _node) internal constant returns (DBNode) {      
+        return DBNode(getDBDatabase().getNode(_node));
     }
 
     function createFactoryNode(bytes32 _factory, bytes32 _user, bytes32 _node, bytes32 extra, address _sender) internal returns (address) {
@@ -104,30 +101,31 @@ contract ControlBase is Object, ControlInfo {
             registerHolder(_node, _sender);
         } else if (_factory == "agreement") {
             registerHolder(_node, _sender);
-            duplicateNode("template", extra, "agreement", _node);
+            duplicateNode(extra,  _node);
+            DBNode(adr).setAgreementStatus(1);
         }
         return adr;
     }
 
-    function operateNodeParameter(bytes32 _db, bytes32 _operation, bytes32 _node, bytes32 _parameter, string _value) internal returns (bool) {
+    function operateNodeParameter(bytes32 _operation, bytes32 _node, bytes32 _parameter, string _value) internal returns (bool) {
         bool ret;
         string memory str = ""; 
         str = PlatString.append(str, PlatString.bytes32ToString(_node), " : " );
         str = PlatString.append(str, PlatString.bytes32ToString(_parameter), " : " , _value);
         if (_operation == "add") {
             str = PlatString.append("addNodeParameter - ", str);
-            ret = getDBNode(_db, _node).addParameter(_parameter);
+            ret = getDBNode(_node).addParameter(_parameter);
         } else if (_operation == "set") {
             str = PlatString.append("setNodeParameter - ", str);
-            ret = getDBNode(_db, _node).setParameter(_parameter, _value);
+            ret = getDBNode(_node).setParameter(_parameter, _value);
         }        
         addLog(str, true);
         return ret;
     }
 
-    function duplicateNode(bytes32 _factorySrc, bytes32 _nodeSrc, bytes32 _factoryDst, bytes32 _nodeDst) internal factroy_exist(_factorySrc) factroy_exist(_factoryDst) returns (bool) {
-        address nodeSrc = address(getDBNode(_factorySrc, _nodeSrc));
-        address nodeDst = address(getDBNode(_factoryDst, _nodeDst));
+    function duplicateNode(bytes32 _nodeSrc, bytes32 _nodeDst) internal returns (bool) {
+        address nodeSrc = address(getDBNode( _nodeSrc));
+        address nodeDst = address(getDBNode( _nodeDst));
 
         if (nodeSrc == address(0)) return false;
         if (nodeDst == address(0)) return false;
