98bfff84f54f96bd13b65c55982da36398e7be80
Eric-Yao <norhustar@hotmail.com>
2018-03-29 14:07:18 +0200

    added a new file htmlFormat.js

---
+++ WebClient/htmlFormat.js
@@ -0,0 +1,13 @@
+/*
+Copyright (c) 2018 ZSC Dev Team
+*/
+
+function uF_loadButtonForEnablingElement(elementId) {
+    var functionInput = "uF_creatProvider('CreateProvider', 'CreateProviderHash')";
+    var text = '<text>Enable the user in ZSC blockchain system</text>'
+    text += '<div class="well">'
+    text += '    <button type="button" onClick="' + functionInput + '">Enable User</button>'
+    text += '    <text id="CreateProviderHash"></text>'
+    text += '</div>'
+    document.getElementById(elementId).innerHTML = text;  
+}
