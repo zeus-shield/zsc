810c5c9fa9392639d0252f08ce57440987d24d6c
Eric-Yao <norhustar@hotmail.com>
2018-03-29 20:09:25 +0200

    added variables and updated functions
    added variables bF_userName, bF_userNameHr, bF_controlApisAdr, bF_controlApisAbi; removed function bF_parserUrlRequest(); added functions bf_configureClient(), bF_getControlApisAdr(), bF_getControlApisAbi()

--- WebClient/basicFunctions.js
+++ WebClient/basicFunctions.js
@@ -2,8 +2,49 @@
 Copyright (c) 2018 ZSC Dev Team
 */
 
-var uF_userName ;
-var uF_userNameHr ;
+var bF_userName ;
+var bF_userNameHr ;
+var bF_controlApisAdr;
+var bF_controlApisAbi;
+
+function bf_getEthAccount() {
+    //console.log(web3.eth.accounts[0])
+    var account = web3.eth.accounts[0];
+    //if (account == undefined) alert("Need to login in MetaMask!!");
+    return account;
+}
+
+function bf_getGasPrice(limit) {
+    return limit * 1000000000; //limits * gwei
+}
+
+function bf_getGasLimit(limit) {
+    return limit * 1000 * 1000; //limits * 1 million
+}
+
+function bf_configureClient(user, hr, adr, abi) {
+    bF_userName = user;
+    bF_userNameHr = hr;
+    bF_controlApisAdr = adr;
+    bF_controlApisAbi = abi;
+}
+
+function bF_getUsername() {    
+    return uF_userName;
+}
+
+function bF_getUsernameHr() {    
+    return uF_userNameHr;
+}
+
+function bF_getControlApisAdr() {    
+    return bF_controlApisAdr;
+}
+
+function bF_getControlApisAbi() {    
+    return bF_controlApisAbi;
+}
+
 
 function bF_showHashResult(elementID, hash) {
     web3.eth.getTransactionReceipt(hash, 
@@ -27,52 +68,3 @@ function bF_showHashResult(elementID, hash) {
     });
 }
 
-function bf_getEthAccount() {
-    //console.log(web3.eth.accounts[0])
-    var account = web3.eth.accounts[0];
-    //if (account == undefined) alert("Need to login in MetaMask!!");
-    return account;
-}
-
-function bf_getGasPrice(limit) {
-    return limit * 1000000000; //limits * gwei
-}
-
-function bf_getGasLimit(limit) {
-    return limit * 1000 * 1000; //limits * 1 million
-}
-
-function bF_parserUrlRequest(nodeId) {    
-    var urlinfo=window.location.href; 
-
-    var found1 = urlinfo.indexOf("?");
-    var found2 = urlinfo.indexOf("=");
-
-    if (found1 == -1 || found2 == -1) return false;
-
-    var len=urlinfo.length;
-    var offset=urlinfo.indexOf("?");
-    var newsidinfo=urlinfo.substr(offset,len)
-    var newsids = newsidinfo.split("&");
-
-    var userInfo = newsids[0];
-    var hrInfo = newsids[1];
-
-    var user = userInfo.split("=")[1];
-    var hr = hrInfo.split("=")[1];
-
-    uF_userName = user;
-    uF_userNameHr = hr;
-
-    return true;
-}  
-
-
-function bF_getUsername() {    
-    return uF_userName;
-}
-
-
-function bF_getUsernameHr() {    
-    return uF_userNameHr;
-}
