34fb88ef83b46802a1e4f453fe71301728c6024d
Eric-Yao <norhustar@hotmail.com>
2018-03-30 18:42:08 +0200

    changed file name
    chang file name from app_controller.sol to app_aprox.sol; removed old codes and added new code

---
+++ Database/app_proxy.sol
@@ -0,0 +1,33 @@
+/*
+Copyright (c) 2018 ZSC Dev Team
+*/
+
+pragma solidity ^0.4.18;
+
+import "./object.sol";
+
+contract AppProxy is Object {
+    string private controlApisAbi_;
+    address private controlApisAdr_;
+
+    function AppProxy() Object("null") {
+    }
+    
+    function setControlApisAbi(string _abi) public only_owner {
+        controlApisAbi_ = _abi;
+    }
+
+    function setControlApisAdr(address _adr) public only_owner {
+        controlApisAdr_ = _adr;
+    }
+
+    function getControlApisAbi() public constant returns (string) {
+        return controlApisAbi_;
+    }
+
+    function getControlApisAdr() public constant returns (string) {
+        return controlApisAdr_;
+    }
+
+
+}
