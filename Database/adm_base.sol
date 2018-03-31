9ec9fd068aa9835827c8c7064ad33a4c64b2b2a1
Eric-Yao <norhustar@hotmail.com>
2018-03-31 19:43:12 +0200

    updated contract and functions
    updated virtual contract ControlApis; updated function setAdrs(); added functions getDBDatabaseAdr(), getFactoryProAdr(), getFactoryRecAdr(), getFactoryTmpAdr(), getFactoryAgrAdr()

--- Database/adm_base.sol
+++ Database/adm_base.sol
@@ -7,7 +7,7 @@ pragma solidity ^0.4.18;
 import "./object.sol";
 
 contract ControlApis is Object {
-    function createElement(uint _factroyType, bytes32 _user, bytes32 _node, address _userId) public only_delegate returns (address);
+    function createElement(uint _factroyType, bytes32 _node, bytes32 extraInfo, bytes32 extraAdr) public returns (address);
 }
 
 contract AdmBase is Object {
@@ -27,13 +27,28 @@ contract AdmBase is Object {
 
     function toHexx(bytes32 _value) internal returns (bytes32);
 
-    function setAdrs(address _controlApis) 
+    function setAdrs(address _controlApis,
+                     address _dbDatabase,
+                     address _factoryPro,
+                     address _factoryRec,
+                     address _factoryTmp,
+                     address _factoryAgr) 
         public only_delegate {
-        if (_controlApis != 0)systemAdrs_["controlApis"] = _controlApis;
+        if (_controlApis != 0) systemAdrs_["ControlApis"] = _controlApis;
+        if (_dbDatabase != 0)  systemAdrs_["DBDatabase"]  = _dbDatabase;
+        if (_factoryPro != 0)  systemAdrs_["FactoryPro"]  = _factoryPro;
+        if (_factoryRec != 0)  systemAdrs_["FactoryRec"]  = _factoryRec;
+        if (_factoryTmp != 0)  systemAdrs_["FactoryTmp"]  = _factoryTmp;
+        if (_factoryAgr != 0)  systemAdrs_["FactoryAgr"]  = _factoryAgr;
     }
 
-    function getControlApisAdr() public constant returns (address){ return systemAdrs_["controlApis"]; }
-
+    function getControlApisAdr() public constant returns (address){ return systemAdrs_["ControlApis"]; }
+    function getDBDatabaseAdr()  public only_delegate constant returns (address){ return  systemAdrs_["DBDatabase"]; }
+    function getFactoryProAdr()  public only_delegate constant returns (address) { return systemAdrs_["FactoryPro"] ; }
+    function getFactoryRecAdr()  public only_delegate constant returns (address) { return systemAdrs_["FactoryRec"] ; }
+    function getFactoryTmpAdr()  public only_delegate constant returns (address) { return systemAdrs_["FactoryTmp"] ; }
+    function getFactoryAgrAdr()  public only_delegate constant returns (address) { return systemAdrs_["FactoryAgr"] ; }
+    
     function addUser(bytes32 _user) public only_owner {
         var ret = toHexx(_user);
         require(testUsers_[ret].status_ ==0);
