8614940ac51e5453a3913b72c3e4251c7088e20b
Eric-Yao <norhustar@hotmail.com>
2018-03-31 18:05:28 +0200

    removed struct; update functions; added variables
    removed struct AgreementKeyInfo; added variable priceETH_, priceZSC_, startTime_, duration_; updated functions (), setParameter(),  addParameter, setAgreementStatus()

--- Database/db_agreement.sol
+++ Database/db_agreement.sol
@@ -7,15 +7,11 @@ import "./db_user.sol";
 import "./db_idmanager.sol";
 
 contract DBAgreement is DBUser {
-    uint private status_; // 0: READY; 1: PUBLISHED; 2: PAID; 3: NOTPAID
-    struct AgreementKeyInfo {
-        uint startTime_;
-        uint endTime_;
-        uint insuredAmount;
-        uint paymentAmount;
-    }
-
-    AgreementKeyInfo private keyInfo_;
+    uint private status_; // 0: CREATED; 1: READY; 2: PUBLISHED; 3: PAID; 4: NOTPAID
+    uint private priceETH_;
+    uint private priceZSC_;
+    uint private startTime_;
+    uint private duration_;
 
     // Constructor
     function DBAgreement(bytes32 _name) public DBUser(_name) {
@@ -27,32 +23,39 @@ contract DBAgreement is DBUser {
         addParameter("status");
         addParameter("startTime");
         addParameter("endTime");
-        addParameter("signedTime");
         addParameter("insuredAmount");
         addParameter("paymentAmount");
     }
 
     function setParameter(bytes32 _parameter, string _value) public only_delegate returns (bool) {
-        return false;
+        if (status_ > 0 )
+            return false;  
+
+        if (_parameter == "Price (TestETH)") {
+            priceETH_ = PlatString.stringToUint(_value);
+        } else if (_parameter == "Price (TestZSC)") {
+            priceZSC_ = PlatString.stringToUint(_value);
+        } else if (_parameter == "Duration") {
+            duration_ = PlatString.stringToUint(_value);
+        }
         super.setParameter(_parameter, _value);
     }
 
     function addParameter(bytes32 _parameter, string _value) public only_delegate returns (bool) {
-        return false;
+        if (status_ > 0 )
+            return false;  
         super.addParameter(_parameter, _value);
     }
 
     function setAgreementStatus(bytes32 _tag) public only_delegate returns (bool) {
-        if (status_ > 1) return false;
+        if (status_ > 2) return false;
 
-        if (status_ == 0 && _tag == "PUBLISHED") {
-            status_ = 1;
-            return super.setParameter("status",  _tag);
-        } else if (status_ == 1 && _tag == "READY") {
-            status_ = 0;
-            return super.setParameter("status",  _tag);
-        }
-        return false;
+        if(status_ == 0 && _tag == "READY") status_ = 1;
+        else if (status_ == 1 && _tag == "PUBLISHED") status_ = 2;
+        else if (status_ == 2 && _tag == "READY") status_ = 1;
+        else return false;
+       
+        return super.setParameter("status",  _tag);
     }
 
     function() public payable {
@@ -64,8 +67,14 @@ contract DBAgreement is DBUser {
         require(ret1 || ret2);
 
         if (ret2 == true) {
+            status_ = 3;
             executeEtherTransaction(getNode(getBindedEntityNameByIndex("provider", 0)), msg.value, "");
-            status_ = 2;
+            startTime_ = now;
+            endTime_ = startTime_ + duration_;
+
+            super.setParameter("status", "PAID");
+            super.setParameter("startTime", PlatString.uintToString(startTime_));
+            super.setParameter("endTime", PlatString.uintToString(endTime_));
         }
     }
 
