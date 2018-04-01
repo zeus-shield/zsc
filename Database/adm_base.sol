602797426c370f118ff8eb50685fdbe2b85fb0b0
Eric-Yao <norhustar@hotmail.com>
2018-04-01 15:38:49 +0200

    added virtual contract, variables, functions
    added virtual contract AbisForTesting; added array testUsers_ and mapping userIndex_; added functions setControlApbisFullAbi() and getControlApisFullAbi() and accordingly updated other functions

--- Database/adm_base.sol
+++ Database/adm_base.sol
@@ -6,6 +6,11 @@ pragma solidity ^0.4.18;
 
 import "./object.sol";
 
+contract AbisForTesting {
+    function tryLogin(bytes32 _user, bytes32 _pass) public constant returns (bytes32); 
+    function getFullAbi(bytes32 _user, bytes32 _hexx) public constant returns (string);
+}
+
 contract ControlApis is Object {
     function createElement(uint _factroyType, bytes32 _node, bytes32 extraInfo, bytes32 extraAdr) public returns (address);
 }
@@ -13,12 +18,13 @@ contract ControlApis is Object {
 contract AdmBase is Object {
     struct TestUserInfo {
         bytes32 name_ ;
-        uint status_ ; //0: not exist; 1: added; 2: applied; 3: approved
-        uint type_;    //1: provider; 2: receiver 
+        bytes32 status_ ; //0: not exist; 1: added; 2: applied; 3: approved
+        bytes32 type_;    //1: provider; 2: receiver 
         address id_   ;
         address node_ ;
     }
-    mapping(bytes32 => TestUserInfo) private testUsers_;
+    TestUserInfo[] testUsers_;
+    mapping(bytes32 => uint) private userIndex_;
     mapping(bytes32 => address) private systemAdrs_;
 
     modifier only_added(bytes32 _hexx) { require(testUsers_[_hexx].status_ == 1); _; }
@@ -26,6 +32,10 @@ contract AdmBase is Object {
     function AdmBase() public Object("zsc_adm") {}
 
     function toHexx(bytes32 _value) internal returns (bytes32);
+
+    function setControlApisFullAbi(string _fullAbi) public only_owner { fullAib_ = _fullAbi; }
+
+    function getControlApisFullAbi( bytes32 _hexx) public constant returns (string) { return fullAib_; }
 
     function setAdrs(address _controlApis,
                      address _dbDatabase,
@@ -53,30 +63,32 @@ contract AdmBase is Object {
         var ret = toHexx(_user);
         require(testUsers_[ret].status_ ==0);
 
-        testUsers_[ret] = TestUserInfo(_user, 1, 0, 0x0, 0x0);
+        userIndex_[ret] = testUsers_.length;
+        testUsers_.push(TestUserInfo(_user, "added", 0, 0x0, 0x0));
 
         addLog("add a User - ", true);
         addLog(PlatString.bytes32ToString(_user), false);
     }
 
-    function applyForUser(bytes32 _hexx, uint _type, address _id) internal {
+    function applyForUser(bytes32 _hexx, bytes32 _type, address _id) internal {
         testUsers_[_hexx].status_ = 2;
         testUsers_[_hexx].type_ = _type;
         testUsers_[_hexx].id_ = _id;
     }
 
     function applyForProvider(bytes32 _hexx) public only_added(_hexx) returns (bool) {
-        applyForUser(_hexx, 1, msg.sender);       
+        applyForUser(_hexx, "provider", msg.sender);       
     }
 
     function applyForReceiver(bytes32 _hexx) public only_added(_hexx) returns (bool) {
-        applyForUser(_hexx, 2, msg.sender);       
+        applyForUser(_hexx, "receiver", msg.sender);       
     }
 
-    function approveUser(bytes32 _hexx) public only_delegate {
-        require (testUsers_[_hexx].status_ == 2);
+    function approveUser(bytes32 _name) public only_delegate {
+        bytes32 hexx = toHexx(_name);
+        require (testUsers_[hexx].status_ == 2);
 
-        uint userType = testUsers_[_hexx].type_; 
+        uint userType = testUsers_[hexx].type_; 
         require(userType == 1 || userType == 2);
 
         address adr = ControlApis(systemAdrs_["controlApis"]).createElement(userType, testUsers_[_hexx].name_, testUsers_[_hexx].id_);
