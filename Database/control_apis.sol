ab5fe281e51cde765759e3174dfab015c980cf56
Eric-Yao <norhustar@hotmail.com>
2018-04-01 21:07:45 +0200

    updated api functions and descriptions

--- Database/control_apis.sol
+++ Database/control_apis.sol
@@ -12,53 +12,17 @@ contract ControlApis is ControlBase {
     function ControlApis(bytes32 _name) public ControlBase(_name) {
     }
 
-    /// @dev Add the database factory of managing the provider nodes
+    /// @dev Add the database factory of managing the elements
+    /// @param _type The type of the database factory
     /// @param _adr The address of the database factory
     function addFactory(uint _type, address _adr) public only_owner {
         addFactoryAdr(mapType(_type), _adr);
     }
 
-    /// @dev Add the database factory of managing the provider nodes
-    /// @param _adr The address of the database factory
+    /// @dev Set the database address
+    /// @param _adr The address of the database 
     function setDatabase(address _adr) public only_owner {
         setDatabaseAdr(_adr);
-    }
-
-    /// @dev Creat a user element
-    /// @param _factroyType The type of the factory for creating the element
-    /// @param _node The name of the element belonging to the user
-    function createElement(uint _factroyType, bytes32 _node, bytes32 extraInfo, address extraAdr) public only_registered(_node) returns (address) {
-        //require(checkAllowedUser(_node));
-        address adr = extraAdr;
-        if (adr == 0) adr = msg.sender;
-        return createFactoryNode(mapType(_factroyType), _node, extraInfo, adr);
-    }
-
-    /// @dev Creat a user element
-    /// @param _adr The address of the existing element
-    function getElementNameByAddress(address _adr) public only_delegate constant returns (bytes32) {
-        require (getDBDatabase().checkeNodeByAddress(_adr));
-        return Object(_adr).name();
-    }
-
-    /// @dev Get the type of an element
-    function getElementType(bytes32 _node) public only_delegate constant returns (bytes32) {
-        DBNode nd = getDBNode( _node);
-        require(nd != DBNode(0));
-        return nd.getNodeType();
-    }
-
-    /// @dev Get the number of elements of a database
-    function numElements() public only_delegate constant returns (uint) { 
-        return getDBDatabase().numNodes(); 
-    }
-    
-    /// @dev Get the number of elements of the database
-    /// @param _index The index of the element in the database
-    function getElementNameByIndex(uint _index) public only_delegate constant returns (bytes32) { 
-        address nd = getDBDatabase().getNodeByIndex(_index);
-        require(nd != address(0));
-        return Object(nd).name();
     }
 
     /// @dev Check the element wheather or not existing
@@ -67,7 +31,48 @@ contract ControlApis is ControlBase {
         return (getDBNode(_node) != DBNode(0));
     }
 
-    /// @dev add a paramter to an element
+    /// @dev Creat an element
+    /// @param _factroyType The type of the factory for creating the element
+    /// @param _node The name of the element belonging to the user
+    /// @param _extraInfo The extra information
+    /// @param _extraAdr The extra address
+    function createElement(uint _factroyType, bytes32 _node, bytes32 _extraInfo, address _extraAdr) public only_registered(_node) returns (address) {
+        //require(checkAllowedUser(_node));
+        address adr = _extraAdr;
+        if (adr == 0) adr = msg.sender;
+        return createFactoryNode(mapType(_factroyType), _node, _extraInfo, adr);
+    }
+
+    /// @dev Get the element by its address
+    /// @param _adr The address of the existing element
+    function getElementNameByAddress(address _adr) public only_delegate constant returns (bytes32) {
+        require (getDBDatabase().checkeNodeByAddress(_adr));
+        return Object(_adr).name();
+    }
+
+    /// @dev Get the type of an element
+    /// @param _node The name of the element belonging to the user
+    function getElementType(bytes32 _node) public only_delegate constant returns (bytes32) {
+        DBNode nd = getDBNode( _node);
+        require(nd != DBNode(0));
+        return nd.getNodeType();
+    }
+
+    /// @dev Get the number of elements of the database
+    function numElements() public only_delegate constant returns (uint) { 
+        return getDBDatabase().numNodes(); 
+    }
+    
+    /// @dev Get the element name by the index
+    /// @param _index The index of the element in the database
+    function getElementNameByIndex(uint _index) public only_delegate constant returns (bytes32) { 
+        address nd = getDBDatabase().getNodeByIndex(_index);
+        require(nd != address(0));
+        return Object(nd).name();
+    }
+
+
+    /// @dev Add a paramter to an element
     /// @param _node The name of the existing element
     /// @param _parameter The name of the added parameter
     function addElementParameter(bytes32 _node, bytes32 _parameter) public only_registered(_node) returns (bool) {
@@ -82,20 +87,20 @@ contract ControlApis is ControlBase {
         return operateNodeParameter("set", _node, _parameter, _value);
     }
 
-    /// @dev Get the value to a paramter of a node
+    /// @dev Get the value of a paramter of a element
     /// @param _node The name of the element
     /// @param _parameter The name of the existing parameter
     function getElementParameter(bytes32 _node, bytes32 _parameter) public only_registered(_node) constant returns (string) {
         return getControlInfoParameterValue(_node, _parameter);
     }
 
-    /// @dev Get the value to a paramter of a node
+    /// @dev Get the address of the element 
     /// @param _node The name of the element
     function getElementAddress(bytes32 _node) public only_registered(_node) constant returns (address) {
         return address(getDBNode(_node));
     }
 
-    /// @dev Get the value to a paramter of a node
+    /// @dev Get the eth balance of the element
     /// @param _node The name of the element
     function getElementEthBalance(bytes32 _node) public only_registered(_node) constant returns (uint256) {
         return getDBNode(_node).getBlance("ether", 0);
@@ -135,7 +140,7 @@ contract ControlApis is ControlBase {
         return getDBNode(_node).numBindedEntities(mapType(_elementType));
     }
 
-    /// @dev Get the name of the element binded to the node
+    /// @dev Get the name of the element binded to the element by index
     /// @param _node The name of the existing element
     /// @param _elementType The type of the element
     /// @param _index The index of the template
@@ -143,12 +148,19 @@ contract ControlApis is ControlBase {
         return getDBNode(_node).getBindedEntityNameByIndex(mapType(_elementType), _index);
     }
 
-    function publishInsurance(bytes32 _node, bool _tag) public only_registered(_node) returns (bool) {
-        if (_tag) return getDBNode(_node).setAgreementStatus("PUBLISHED");
-        else return getDBNode(_node).setAgreementStatus("READY");
+    /// @dev Announce an insurance agreement by a provider
+    /// @param _agreement The agreement name
+    /// @param _tag The announcement status
+    function announceInsurance(bytes32 _agreement, bool _tag) public only_registered(_node) returns (bool) {
+        if (_tag) return getDBNode(_agreement).setAgreementStatus("PUBLISHED");
+        else return getDBNode(_agreement).setAgreementStatus("READY");
     }
 
-    function buyInsurance(bytes32 _user, bytes32 _node, uint _amount) public only_registered(_user) returns (bool) {
-        return getDBNode(_user).executeEtherTransaction(address(getDBNode(_node)), _amount, "null");
+    /// @dev Buy an insurance agreement with Eth from a provider
+    /// @param _user The receiver name
+    /// @param _agreement The agreement name
+    /// @param _amount The eth amount
+    function buyInsuranceWithEth(bytes32 _user, bytes32 _agreement, uint _amount) public only_registered(_user) returns (bool) {
+        return getDBNode(_user).executeEtherTransaction(address(getDBNode(_agreement)), _amount, "null");
     }
 }
