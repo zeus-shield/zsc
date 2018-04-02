feacba62a626872a274d97e4d4c4fcbbcdbc38bc
Eric-Yao <norhustar@hotmail.com>
2018-04-02 21:41:02 +0200

    updated "class" ZSCUser
    change name from zscUser to ZSCUser; added method tryLogin(); updated method getFullAbi();

--- WebClient/zscUser.js
+++ WebClient/zscUser.js
@@ -1,35 +1,50 @@
 /*
 Copyright (c) 2018 ZSC Dev Team
 */
-function zscUser() {
+function ZSCUser() {
     this.userName;
     this.userNameHr;
     this.controlApisAdr;
     this.controlApisFullAbi;
 }
-zscUser.prototype.setUserName = function(name) { this.userName = name; }
-zscUser.prototype.setUserNameHr = function(hr) { this.userNameHr = hr; } 
-zscUser.prototype.setControlApisAdr = function(adr) { this.controlApisAdr = adr; } 
-zscUser.prototype.setControlApisFullAbi = function(abi) { this.controlApisFullAbi = abi; } 
-zscUser.prototype.getUserName = function() { return this.userName; }
-zscUser.prototype.getUserNameHr = function() { return this.userNameHr; }
-zscUser.prototype.getControlApisAdr = function() { return this.controlApisAdr; }
-zscUser.prototype.getControlApisFullAbi = function() { return this.controlApisFullAbi; }
-zscUser.prototype.getLoginAbi = function() { return [{"constant":true,"inputs":[{"name":"_user","type":"bytes32"},{"name":"_hexx","type":"bytes32"}],"name":"getFullAbi","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_user","type":"bytes32"},{"name":"_pass","type":"bytes32"}],"name":"tryLogin","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"}]; }
+ZSCUser.prototype.setControlApisAdr = function(adr) { this.controlApisAdr = adr; } 
+ZSCUser.prototype.setControlApisFullAbi = function(abi) { this.controlApisFullAbi = abi; } 
+ZSCUser.prototype.getUserName = function() { return this.userName; }
+ZSCUser.prototype.getUserNameHr = function() { return this.userNameHr; }
+ZSCUser.prototype.getControlApisAdr = function() { return this.controlApisAdr; }
+ZSCUser.prototype.getControlApisFullAbi = function() { return this.controlApisFullAbi; }
+ZSCUser.prototype.getLoginAbi = function() { return [{"constant":true,"inputs":[{"name":"_user","type":"bytes32"},{"name":"_hexx","type":"bytes32"}],"name":"getFullAbi","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_user","type":"bytes32"},{"name":"_pass","type":"bytes32"}],"name":"tryLogin","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"}]; }
 
-zscUser.prototype.getFullAbi = function (func){
+ZSCUser.prototype.tryLogin = function(user, pass, adr, func){
+    var myContract = web3.eth.contract(uF_controlApisAdvAbiLogin);
+    var myControlApi = myContract.at(adr);
+
+    myControlApi.tryLogin(user, pass, function(error, hexx) {
+        if(!error) {
+            if (hexx == 0x0) {
+                func(false);
+            } else {
+                this.getFullAbi(user, hexx, adr, func);
+            }
+        } else console.log("error: " + error);
+    } );
+}
+
+ZSCUser.prototype.getFullAbi = function (user, hexx, adr, func){
     var myContract = web3.eth.contract(this.getLoginAbi());
     var myControlApi = myContract.at(adr);
-    myControlApi.getFullAbi(this.userName, this.userNameHr, function(error, fullAbi) {
+    myControlApi.getFullAbi(user, hexx, adr, function(error, fullAbi) {
         if(!error) { 
-            uF_controlApisAdvFullAbi = fullAbi;
-            uF_userName = user;
-            uF_userNameHr = hex;
-            uF_controlApisAdr = adr;
+            this.userName = user;
+            this.userNameHr = hex;
+            this.controlApisAdr = adr;
+            this.controlApisFullAbi = fullAbi;
             func(true);
         } else {
             console.log("error: " + error);
         }
     } );
 }
+
+
 
