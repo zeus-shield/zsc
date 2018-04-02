401f9ca9ec0f1ded912f2fb657ce240b05fa0ab9
Eric-Yao <norhustar@hotmail.com>
2018-04-02 20:04:42 +0200

    added file adm_control_apis_adv.php

---
+++ WebAdm/adm_control_apis_adv.php
@@ -0,0 +1,44 @@
+
+
+<?php include("adm_header.php"); ?>
+
+<?php
+$urlSuffixTag = false;
+
+?>
+
+
+<html>
+<head>
+<?php echo includeScriptFiles(); ?>
+<script type="text/javascript">
+    var web3 = setupWeb3js(false);
+    
+    
+</script>
+</head>
+<body>
+
+<div class="well">
+        <text>Step - 1 </text>
+        <button type="button" onClick="initSystemModule('DBDatabase')">Init DBDatabase</button> 
+        <text id="DBDatabaseHash"></text><br>
+
+        <br><text>Step - 2 </text>
+        <button type="button" onClick="initSystemModule('FactoryPro')">Init FactoryPro</button>
+        <text id="FactoryProHash"></text><br>
+
+        <br><text>Step - 3 </text>
+        <button type="button" onClick="initSystemModule('ControlApisAdv')">Bind Factories</button> 
+        <text id="ControlApisAdvHash"></text> <br>
+
+        <br><text>Step - 4 </text>
+        <button type="button" onClick="setControlApisAdvAbi('ControlApisAdvAbiHash')">Set ControlApisAdv Abi</button> <br>
+        <text id="ControlApisAdvAbiHash"></text> <br>
+        <text id="ControlApisAdvAbi"> </text> 
+
+</div>
+
+</body>
+</html>
+
