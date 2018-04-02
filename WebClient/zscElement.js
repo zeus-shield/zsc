ef0dd56c4470d24f74b5fbf84d26ee57d9717b21
Eric-Yao <norhustar@hotmail.com>
2018-04-02 22:47:18 +0200

    changed "class" name form zscElement to ZSCElement; added methoed transferEth()

--- WebClient/zscElement.js
+++ WebClient/zscElement.js
@@ -3,10 +3,9 @@ Copyright (c) 2018 ZSC Dev Team
 */
 
 //class zscElement
-function zscElement(nm, abi, adr) {
+function ZSCElement(nm, abi, adr) {
     this.name = nm;
     this.parameNos = 0;
-    this.ethAccount = web3.eth.accounts[0];
     this.ethBalance = 0;
     this.nodeAddress = 0;
     this.parameterNames = [];
@@ -14,25 +13,25 @@ function zscElement(nm, abi, adr) {
     this.binedElements = [];
     this.myControlApi = web3.eth.contract(abi).at(adr);
 }
-zscElement.prototype.getName = function() { return this.name;}
+ZSCElement.prototype.getName = function() { return this.name;}
 
-zscElement.prototype.activeTid = function(index, func, time) { this.tid[index] = setInterval(func, time);}
+ZSCElement.prototype.activeTid = function(index, func, time) { this.tid[index] = setInterval(func, time);}
 
-zscElement.prototype.deactiveTid = function(index) { clearInterval(this.tid[index]);}
+ZSCElement.prototype.deactiveTid = function(index) { clearInterval(this.tid[index]);}
 
-zscElement.prototype.getEthBalance = function() { return this.ethBalance;}
+ZSCElement.prototype.getEthBalance = function() { return this.ethBalance;}
 
-zscElement.prototype.getAddress = function() { return this.nodeAddress;}
+ZSCElement.prototype.getAddress = function() { return this.nodeAddress;}
 
-zscElement.prototype.getParameterName = function(index) { return this.parameterNames[index]; }
+ZSCElement.prototype.getParameterName = function(index) { return this.parameterNames[index]; }
 
-zscElement.prototype.getParameterValue = function(index) { return this.parameterValues[index]; }
+ZSCElement.prototype.getParameterValue = function(index) { return this.parameterValues[index]; }
 
-zscElement.prototype.numBindedElements = function() { return this.binedElements.length; }
+ZSCElement.prototype.numBindedElements = function() { return this.binedElements.length; }
 
-zscElement.prototype.getBindedElementName = function() { return this.binedElements[index]; }
+ZSCElement.prototype.getBindedElementName = function() { return this.binedElements[index]; }
 
-zscElement.prototype.loadEthBalance = function(func) {
+ZSCElement.prototype.loadEthBalance = function(func) {
     this.myControlApi.getElementEthBalance(this.name, function(error, balance){ 
         if(!error) {
             this.ethBalance = balance;  
@@ -48,7 +47,7 @@ zscElement.prototype.loadEthBalance = function(func) {
     });
 }
 
-zscElement.prototype.loadParameterNamesAndvalues = function(func) {
+ZSCElement.prototype.loadParameterNamesAndvalues = function(func) {
     this.numParameters(function() {
         this.loadParameterNames(function() {
             this.loadParameterValues(function(index){
@@ -60,9 +59,9 @@ zscElement.prototype.loadParameterNamesAndvalues = function(func) {
     });
 }
 
-zscElement.prototype.numParameters = function(func) {
+ZSCElement.prototype.numParameters = function(func) {
     this.myControlApi.numElementParameters(this.name, 
-        {from: this.ethAccount},
+        {from: bF_getEthAccount()},
         function(error, num){ 
             if(!error) { 
                 this.parameNos = num.toString(10); 
@@ -73,7 +72,7 @@ zscElement.prototype.numParameters = function(func) {
          });
 }
 
-zscElement.prototype.loadParameterNames = function(func) {
+ZSCElement.prototype.loadParameterNames = function(func) {
     for (var i = 0; i < this.parameNos; ++i) {
         loadParameterNameByIndex(i, function(index, para) {
             uF_parameters[index] = para;
@@ -84,9 +83,9 @@ zscElement.prototype.loadParameterNames = function(func) {
     } 
 } 
 
-zscElement.prototype.loadParameterNameByIndex = function(index, func) {
+ZSCElement.prototype.loadParameterNameByIndex = function(index, func) {
     this.myControlApi.getElementParameterNameByIndex(this.name, index, 
-        {from: this.ethAccount},
+        {from: bF_getEthAccount()},
         function(error, para){ 
             if(!error) {
                 var ret = web3.toUtf8(para);
@@ -97,7 +96,7 @@ zscElement.prototype.loadParameterNameByIndex = function(index, func) {
         });
 }
 
-zscElement.prototype.loadParameterValues = function(func) {
+ZSCElement.prototype.loadParameterValues = function(func) {
     for (var i = 0; i < this.parameNos; ++i) {
         loadParameterValueByIndex(i, function(index, value) {
             if (index == this.parameNos - 1) {
@@ -107,9 +106,9 @@ zscElement.prototype.loadParameterValues = function(func) {
     } 
 } 
 
-function loadParameterValueByIndex(index, func){ 
+ZSCElement.prototype.loadParameterValueByIndex = function(index, func){ 
     this.myControlApi.getElementParameter(this.name, this.getParameterName(index), 
-        {from: this.ethAccount},
+        {from: bF_getEthAccount()},
         function(error, value){ 
             if(!error) {
                 this.parameterValues[index] = value;
@@ -120,6 +119,20 @@ function loadParameterValueByIndex(index, func){
         });
 }
 
+
+ZSCElement.prototype.transferEth = function(destAddressID, amountID, logID){  
+    var destAddress = document.getElementById(destAddressID).value;
+    var amount = document.getElementById(amountID).value;
+
+    if (destAddress != 0 && amount > 0) {
+        this.myControlApi.elementwithDrawEth(this.name, destAddress, web3.toWei(amount, 'ether') , 
+            {from: bF_getEthAccount(), gasPrice: bF_getGasPrice(1), gas : bF_getGasLimit(55000)}, 
+            function(error, result){ 
+            if(!error) bF_showHashResult(logID, result);
+            else console.log("error: " + error);
+        });
+    }
+}
 
 
 
