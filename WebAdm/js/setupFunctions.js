2362bd1f00f98b7874c3385d45e389bc0dd38d70
Eric-Yao <norhustar@hotmail.com>
2018-04-03 21:06:55 +0200

    updated functions
    updated functions sF_initSystemModule(), sF_initDatabase(), added function sF_initFactory(), sF_setDatabaseAdr(),

--- WebAdm/js/setupFunctions.js
+++ WebAdm/js/setupFunctions.js
@@ -1,3 +1,6 @@
+/*
+Copyright (c) 2018 ZSC Dev Team
+*/
 
 function sF_showHashResult(elementID, hash) {
     web3.eth.getTransactionReceipt(hash, 
@@ -47,24 +50,52 @@ function sF_setLogRecorderToListener(logRecorderAdr, listener,listenerName, hash
 
 
 function sF_initSystemModule(module, adrs, hashID) {
-    var DBDatabaseAdr = adrs[0];
-    var FactoryProAdr = adrs[1];
-    var ControlApisAdr = adrs[2];
-    if (module == "DBDatabase") sF_initDatabase(DBDatabaseAdr, FactoryProAdr, ControlApisAdr, hashID);
-    else if (module == "FactoryPro") sF_initFactoryPro(DBDatabaseAdr, FactoryProAdr, ControlApisAdr, hashID);
+    var AdmAdv = adrs[0];
+    var DBDatabaseAdr = adrs[1];
+    var FactoryProAdr = adrs[2];
+    var FactoryTmpAdr = adrs[3];
+    var FactoryAgrAdr = adrs[4];
+    var ControlApisAdr = adrs[5];
+
+    if (module == "DBDatabase") sF_initDatabase(AdmAdv, DBDatabaseAdr, FactoryProAdr, FactoryTmpAdr, FactoryAgrAdr, ControlApisAdr, hashID);
+    else if (module == "FactoryPro") sF_initFactory(module, FactoryProAdr, DBDatabaseAdr, ControlApisAdr, hashID);
+    else if (module == "FactoryTmp") sF_initFactory(module, FactoryTmpAdr, DBDatabaseAdr, ControlApisAdr, hashID);
+    else if (module == "FactoryAgr") sF_initFactory(module, FactoryAgrAdr, DBDatabaseAdr, ControlApisAdr, hashID);
     else if (module == "ControlApisAdv") sF_addFactory(DBDatabaseAdr, FactoryProAdr, ControlApisAdr, hashID);
 }
 
 
-function sF_initDatabase(DBDatabaseAdr, FactoryProAdr, ControlApisAdr, hashID) {
-    var myContract = web3.eth.contract(getContractAbi("DBDatabase"));
+function sF_initDatabase(DBDatabaseAdr, FactoryProAdr, FactoryTmpAdr, FactoryAgrAdr, ControlApisAdr, hashID) {
+    var myContract = web3.eth.contract(cC_getContractAbi("DBDatabase"));
     var myDatabase = myContract.at(DBDatabaseAdr);
-    myDatabase.initDatabase([FactoryProAdr], ControlApisAdr, {from:web3.eth.accounts[0], gas: 9000000},
+    myDatabase.initDatabase([FactoryProAdr, FactoryTmpAdr, FactoryAgrAdr], ControlApisAdr, {from:web3.eth.accounts[0], gas: 9000000},
     function(error, result){ 
         if(!error) sF_showHashResult(hashID, result);
         else console.log("error: " + error);
     });
 }  
+
+
+function sF_initFactory(factoryName, FactoryAdr, DBDatabaseAdr, ControlApisAdr, hashID) {
+    var myContract = web3.eth.contract(cC_getContractAbi("FactoryPro"));
+    var myFactoryPro= myContract.at(FactoryAdr);
+    myFactoryPro.initFactory(ControlApisAdr, DBDatabaseAdr, {from: web3.eth.accounts[0], gas: 9000000},
+    function(error, result){ 
+        if(!error) sF_showHashResult(hashID, result);
+        else console.log("error: " + error);
+    });
+}  
+
+function sF_setDatabaseAdr(DBDatabaseAdr, ControlApisAdr, hashID) {
+    var myContract = web3.eth.contract(getContractAbi("ControlApisAdv"));
+    var myControlApi= myContract.at(ControlApisAdr);
+    myControlApi.setDatabaseAdr(DBDatabaseAdr, {from: web3.eth.accounts[0], gas: 9000000},
+    function(error, result){ 
+        if(!error) sF_showHashResult(hashID, result);
+        else console.log("error: " + error);
+    });
+}  
+
 
 ////////////////
 var sF_logerModuleAdr;
