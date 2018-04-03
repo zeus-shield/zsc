8d5cfb9473174568a9cdde3ff940e9a2d96e2a33
Eric-Yao <norhustar@hotmail.com>
2018-04-03 15:58:27 +0200

    added file setupFunctions.js

---
+++ WebAdm/js/setupFunctions.js
@@ -0,0 +1,22 @@
+
+function sF_showHashResult(elementID, hash) {
+    web3.eth.getTransactionReceipt(hash, 
+    function(error, result){ 
+        if(!error) {
+            var show;
+            if (result == null) {
+                show =  "(pending)" + hash ;
+                sF_showHashResult(elementID, hash);
+            } else {
+                if (result.status == 0) {
+                    show = "(failure)" + hash;
+                } else {
+                    show = "(succeeded)" + hash ;
+                }
+            }
+            document.getElementById(elementID).innerText = show;
+        } else {
+            console.log("error: " + error);
+        }
+    });
+}
