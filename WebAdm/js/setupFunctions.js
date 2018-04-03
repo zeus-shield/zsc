24136940f6b5fd2ad09e719630c50e18082cb474
Eric-Yao <norhustar@hotmail.com>
2018-04-03 16:23:10 +0200

    added functions
    added functions sF_registerListenerToLogRecorder() and sF_setLogRecorderToListener();

--- WebAdm/js/setupFunctions.js
+++ WebAdm/js/setupFunctions.js
@@ -20,3 +20,27 @@ function sF_showHashResult(elementID, hash) {
         }
     });
 }
+
+function sF_registerListenerToLogRecorder(logRecorderAdr, listener, listenerName, hashID) {
+    var myContract = web3.eth.contract(cC_getContractAbi("LogRecorder"));
+    var myLogRecorder = myContract.at(logRecorderAdr);
+    var account = web3.eth.accounts[0];
+
+    myLogRecorder.registerListener(listener, listenerName, {from:account, gas: 9000000},
+    function(error, result){ 
+        if(!error) sF_showHashResult(hashID, result);
+        else console.log("error: " + error);
+    });
+}  
+
+function sF_setLogRecorderToListener(logRecorderAdr, listener,listenerName, hashID) {
+    var myContract = web3.eth.contract(cC_getContractAbi(listenerName));
+    var myListener = myContract.at(listener);
+    var account = web3.eth.accounts[0];
+
+    myListener.setLogRecorder(logRecorderAdr, {from:account, gas: 9000000},
+    function(error, result){ 
+        if(!error) sF_showHashResult(hashID, result);
+        else console.log("error: " + error);
+    });
+}  
