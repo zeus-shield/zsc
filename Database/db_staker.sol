04e09a6f68761e29af3d6d6696dfe19646887310
Eric-Yao <norhustar@hotmail.com>
2018-04-03 15:45:35 +0200

    added file db_staker.sol

---
+++ Database/db_staker.sol
@@ -0,0 +1,20 @@
+/*
+Copyright (c) 2018, ZSC Dev Team
+2018-02-12: v0.01
+*/
+
+pragma solidity ^0.4.18;
+import "./db_user.sol";
+
+contract DBStaker is DBEntity {
+    address private templates_ = 0;
+
+    // Constructor
+    function DBStaker(bytes32 _name) public DBUser(_name) {
+        setNodeType("staker"); 
+    }
+
+    function initParameters() internal {
+    }
+}
+
