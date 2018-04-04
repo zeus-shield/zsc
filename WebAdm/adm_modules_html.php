fb7c1eb4a9671398476fcb0ec5df3bd194ce2392
Eric-Yao <norhustar@hotmail.com>
2018-04-04 21:33:32 +0200

    added file adm_modules_html

---
+++ WebAdm/adm_modules_html.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Copyright (c) 2018, ZSC Dev Team
+*/
+?>
+
+<?php
+class AdmModulesHtml {
+    
+    function getHeader() {
+    }
+    
+    function getScriptFiles() {
+    }
+    
+    function getCreateContract($func) {
+    }
+    
+    function getRegisterLogRecordee($func) {
+    }
+    
+    function getSetLogRecorder($func) {
+    
+    }
+}
+
+?>
