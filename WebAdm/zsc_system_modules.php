3abf94c70e9bcc11ed72e20d86d28357d93eb42e
Eric-Yao <norhustar@hotmail.com>
2018-04-05 13:27:34 +0200

    added functions to class ZscSystemModules

--- WebAdm/zsc_system_modules.php
+++ WebAdm/zsc_system_modules.php
@@ -12,11 +12,16 @@ class ZscSystemModules {
     private $logedModuleNameArrayInString =  "['AdmAdv', 'DBDatabase', 'FactoryPro', 'FactoryTmp', 'FactoryAgr', 'ControlApisAdv']";
 
     private function readContent($file) {
-
+        $myfile = fopen($file, "r");
+        $text = fread($myfile,filesize($file));
+        fclose($myfile);
+        return $text;
     }
 
     private function writeContent($file, $text) {
-
+        $myfile = fopen($file, "wr") or die("Unable to open file!");
+        fwrite($myfile, $text);
+        fclose($myfile);
     }
 
     public function getModuleArray() { 
@@ -31,12 +36,38 @@ class ZscSystemModules {
         return $logedModuleNameArrayInString;
     }
     public function readModuleAddress($name) {
-
+        return readContent('./adrs/'.$name.'.txt');
     }
 
     public function writeModuleAddress($name, $adr) {
-
+        writeContent('./adrs/'.$name.'.txt', $adr);
     }
-} 
+
+    public function getLogedModuleAddressArrayInString() {
+        $logedModules = getModuleArray();
+        $num = count($logedModules);
+    
+        $text = "";
+    
+        for($x = 0; $x < $num; $x++) {
+            $name = $logedModules[$x];
+            $text .= "['".readModuleAddress($name);
+        }
+        return $text;
+    }
+
+    public function getUrlSuffixForAdrs() {
+        $system_modules = getModuleArray();
+        $num = count($system_modules);
+        $text = '?';
+        for($x = 0; $x < $num; $x++) {
+            $name = $system_modules[$x];
+            $text .= $name.'='.readModuleAddress($name).'&';
+        }
+        return $text;
+    }
+}
 
 ?>
+
+
