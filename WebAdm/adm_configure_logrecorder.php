a7ed806eb056848c5ebb169c33d3ce9b3f0324c1
Eric-Yao <norhustar@hotmail.com>
2018-04-05 14:07:55 +0200

    added class object htmlModules; updated php content

--- WebAdm/adm_configure_logrecorder.php
+++ WebAdm/adm_configure_logrecorder.php
@@ -1,11 +1,19 @@
+<?php 
+/*
+Copyright (c) 2018 ZSC Dev Team
+*/
+?>
 
-
-<?php include("adm_header.php"); ?>
 
 <?php
+include("adm_header.php");
+
 $urlSuffixTag = false;
+
+$htmlModules= new ZscHtmlModules();
+
 function recordSystemModuleAdrs() {
-    $system_modules = getModuleArray();
+    $system_modules = $htmlModules->getModuleArray();
     $num = count($system_modules);
     
     for($x = 0; $x < $num; $x++) {
@@ -14,7 +22,7 @@ function recordSystemModuleAdrs() {
 
         if(!empty($adr)){
             $urlSuffixTag = true;
-            writeModuleAddress($module, $adr);
+            $htmlModules->writeModuleAddress($module, $adr);
         }
     }
 }
@@ -23,47 +31,47 @@ recordSystemModuleAdrs();
 
 <html>
 <head>
-<?php echo includeScriptFiles(); ?>
+<?php echo $htmlModules->loadScriptFiles(); ?>
 <script type="text/javascript">
     var web3 = setupWeb3js(false);
     
     function getModuleAdr(module) {
         var adr;
-        if (module == "AdmAdv") adr = "<?php echo readModuleAddress('AdmAdv')?>";        
-        else if (module == "DBDatabase") adr = "<?php echo readModuleAddress('DBDatabase')?>";
-        else if (module == "FactoryPro") adr = "<?php echo readModuleAddress('FactoryPro')?>";
-        else if (module == "FactoryTmp") adr = "<?php echo readModuleAddress('FactoryTmp')?>";
-        else if (module == "FactoryAgr") adr = "<?php echo readModuleAddress('FactoryAgr')?>";
-        else if (module == "ControlApisAdv") adr = "<?php echo readModuleAddress('ControlApisAdv')?>";
+        if (module == "AdmAdv") adr = "<?php echo $htmlModules->readModuleAddress('AdmAdv')?>";        
+        else if (module == "DBDatabase") adr = "<?php echo $htmlModules->readModuleAddress('DBDatabase')?>";
+        else if (module == "FactoryPro") adr = "<?php echo $htmlModules->readModuleAddress('FactoryPro')?>";
+        else if (module == "FactoryTmp") adr = "<?php echo $htmlModules->readModuleAddress('FactoryTmp')?>";
+        else if (module == "FactoryAgr") adr = "<?php echo $htmlModules->readModuleAddress('FactoryAgr')?>";
+        else if (module == "ControlApisAdv") adr = "<?php echo $htmlModules->readModuleAddress('ControlApisAdv')?>";
         return adr;
     }
 
     function registerToLogRecorder(module, elementId) {
-        logAdr = "<?php echo readModuleAddress('LogRecorder')?>";
+        logAdr = "<?php echo $htmlModules->readModuleAddress('LogRecorder')?>";
         sF_registerListenerToLogRecorder(logAdr,  getModuleAdr(module), module, elementId);
     }
 
     function setLogRecorderToListener(module, elementId) {
-        logAdr = "<?php echo readModuleAddress('LogRecorder')?>";
+        logAdr = "<?php echo $htmlModules->readModuleAddress('LogRecorder')?>";
         sF_setLogRecorderToListener(logAdr,  getModuleAdr(module), module, elementId);
     }
 
 </script>
 </head>
 <body>
-<?php echo includeHeader();?>
+<?php echo $htmlModules->loadHeader();?>
 <script type="text/javascript">
     var web3 = setupWeb3js(false);
     
 </script>
 </head>
 <body>
-<?php echo includeHeader();
+<?php echo $htmlModules->loadAllAdrs();
     echo '<div class="page-header"> <font size="5" color="blue" >Setup ZSC system in the testing envrioment</font></div>';
 
-    echo includeRegisterLogRecorderHtml('registerToLogRecorder');
+    echo $htmlModules->loadRegisterLogRecorderHtml('registerToLogRecorder');
     
-    echo includeSetLogRecorderHtml('setLogRecorderToListener');
+    echo $htmlModules->loadSetLogRecorderHtml('setLogRecorderToListener');
 
     echo '<div class="well">
         <button type="button" onClick="window.location.href=\'./adm_control_apis_adv.php\'">Next: go to control page</button>
