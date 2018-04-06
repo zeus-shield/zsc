8fc62d6c128f00b200494ac3efe02ace4c47bd78
Eric-Yao <norhustar@hotmail.com>
2018-04-07 06:51:21 +0800

    updated class ZscUserMangement;
    added properties userInfo, userNos, account, myControlApi; updated methods addUser(), added methods loadUsers(), numUsers(), loadUserInfos(), loadUserInfoByIndex();

--- WebAdm/js/zsc_user_management.js
+++ WebAdm/js/zsc_user_management.js
@@ -4,20 +4,66 @@ Copyright (c) 2018 ZSC Dev Team
 function ZscUserMangement(adr, abi) {
     this.admAdvAdr = admAdr;
     this.admAdvAbi = abi;
+    this.userInfo = [];
+    this.userNos = 0;
+    this.account = web3.eth.accounts[0];
+    this.myControlApi = web3.eth.contract(abi).at(adr);
 }
 
 ZscUserMangement.prototype.addUser = function(userNameId, hashId, func){
     var userName = document.getElementById(userNameId).value; 
-    var myContract = web3.eth.contract(this.admAdvAbi );
-    var myControlApi= myContract.at(this.admAdvAdr);
-    var account = web3.eth.accounts[0];
-
-    myControlApi.addUser(userName, {from: account, gas: 9000000},
+    this.myControlApi.addUser(userName, {from: this.account, gas: 9000000},
     function(error, result){ 
         if(!error) sF_showHashResult(hashId, result);
         else console.log("error: " + error);
     });
 }  
 
+ZscUserMangement.prototype.loadUsers = function(func) {
+    this.numUsers(function() {
+        this.loadUserInfos(function(index){
+            if (index == this.parameNos - 1) {
+                func();
+            }
+        });
+    });
+}
+
+ZSCElement.prototype.numUsers = function(func) {
+    this.myControlApi.numUsers(
+        {from: this.account},
+        function(error, num){ 
+            if(!error) { 
+                this.userNos = num.toString(10); 
+                func();
+            } else {
+                console.log("error: " + error);
+            }
+         });
+}
+
+ZSCElement.prototype.loadUserInfos = function(func) {
+    for (var i = 0; i < this.userNos; ++i) {
+        loadUserNameByIndex(i, function(index, para) {
+                this.userInfo[index] = para;
+                if (index == this.userNos - 1) {
+                    func();
+                 }
+            });
+    } 
+} 
+
+ZSCElement.prototype.loadUserInfoByIndex = function(index, func) {
+    this.myControlApi.getUserInfoByIndex(index, 
+        {from: this.account},
+        function(error, para){ 
+            if(!error) {
+                var ret = web3.toUtf8(para);
+                func(index, ret);  
+            } else { 
+                console.log("error: " + error);
+            }
+        });
+}
 
 
