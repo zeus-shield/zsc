6442338ede9bf86a2d8eddc70bda2f96ab953250
Eric-Yao <norhustar@hotmail.com>
2018-04-06 20:45:24 +0800

    updated class ZscSystemModules

--- WebAdm/zsc_system_modules.php
+++ WebAdm/zsc_system_modules.php
@@ -8,34 +8,41 @@ Copyright (c) 2018, ZSC Dev Team
 include("zsc_base.php");
 
 class ZscSystemModules extends ZscBase {    
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public  function __destruct() {
+    }
+
     public function readModuleAddress($name) {
-        return readContent('./adrs/'.$name.'.txt');
+        return parent::readContent('./adrs/'.$name.'.txt');
     }
 
     public function writeModuleAddress($name, $adr) {
-        writeContent('./adrs/'.$name.'.txt', $adr);
+        parent::writeContent('./adrs/'.$name.'.txt', $adr);
     }
 
     public function getLogedModuleAddressArrayInString() {
-        $logedModules = getModuleArray();
+        $logedModules = parent::getModuleArray();
         $num = count($logedModules);
     
         $text = "";
     
         for($x = 0; $x < $num; $x++) {
             $name = $logedModules[$x];
-            $text .= "['".readModuleAddress($name);
+            $text .= "['".$this->readModuleAddress($name);
         }
         return $text;
     }
 
     public function getUrlSuffixForAdrs() {
-        $system_modules = getModuleArray();
+        $system_modules = parent::getModuleArray();
         $num = count($system_modules);
         $text = '?';
         for($x = 0; $x < $num; $x++) {
             $name = $system_modules[$x];
-            $text .= $name.'='.readModuleAddress($name).'&';
+            $text .= $name.'='.$this->readModuleAddress($name).'&';
         }
         return $text;
     }
