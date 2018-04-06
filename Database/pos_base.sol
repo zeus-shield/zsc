96940a4cbd793510f6e6265aef93a350304c2e55
Eric-Yao <norhustar@hotmail.com>
2018-04-07 03:26:56 +0800

    updated contract PosBase;
    added structs ZscStaker and ZscStakerPool; added variable zscStakerPool_; added functions setZscTokenAddress(), registerStaker()

--- Database/pos_base.sol
+++ Database/pos_base.sol
@@ -4,9 +4,10 @@ Copyright (c) 2018 ZSC Dev.
 
 pragma solidity ^0.4.18;
 
-import "./db_node.sol";
+import "./object.sol";
 
-contract PosBase is DBNode {
+//Proof of Stake for ZSC system
+contract PosBase is Object {
     enum TransactionStatus {NULL, PENDING, FAILED, SUCCEED}
     
     struct Transaction {
@@ -18,22 +19,66 @@ contract PosBase is DBNode {
         address sender_;
         address receiver_;
         uint256 amount_;
+        uint ethGasUsed_;
         bytes data_;
     }
 
     struct TransactionHistory {
         uint nos_;
-        uint256 total_;
         mapping(uint => Transaction) transactions_;
     }
     
+    struct ZscStaker {
+        bytes32 nodeName_;
+        address nodeAddress_;
+        address ethAddress_;
+        uint stakePoint_;
+        mapping(uint => uint) zscAmounts_;
+    }
+
+    struct ZscStakerPool {        
+        uint nos_;
+        mapping(address => bool) stakerExists_;
+        mapping(address => uint) stakerIndex_;
+        mapping(uint => ZscStaker) stakers_;
+    }
+
     TransactionHistory private transactionHistory_;
+    ZscStakerPool private zscStakerPool_;
+
+    address zscToken_;
 
     // Constructor
-    function PosBase(bytes32 _name) public DBNode(_name) {
+    function PosBase(bytes32 _name) public Object(_name) {
     } 
 
-    function registerTransaction() public only_delegate {
-        
+    function setZscTokenAddress(address _adr) public only_delegate {
+        zscToken_ = _adr;
     }
+
+    function registerTransaction() public constant only_delegate {
+    }
+
+    function registerStaker(bytes32 _nodeName, address _nodeAddress, address _ethAddress) public only_delegate {
+        require(!zscStakerPool_.stakerExists_[_ethAddress]);
+        uint index = zscStakerPool_.nos_;
+        zscStakerPool_.nos_++;
+        zscStakerPool_.stakerExists_[_ethAddress] = true;
+        zscStakerPool_.stakerIndex_[_ethAddress] = index;
+        zscStakerPool_.stakers_[index].nodeName_    = _nodeName;
+        zscStakerPool_.stakers_[index].nodeAddress_ = _nodeAddress;
+        zscStakerPool_.stakers_[index].ethAddress_  = _ethAddress;
+        zscStakerPool_.stakers_[index].stakePoint_  = 0;
+    }
+
+    function registerStaker(address _ethAddress) public only_delegate  {
+        require(zscStakerPool_.stakerExists_[_ethAddress]);
+        uint index = zscStakerPool_.stakerIndex_[_ethAddress];
+        zscStakerPool_.nos_--;
+
+        delete zscStakerPool_.stakerExists_[_ethAddress];
+        delete zscStakerPool_.stakerIndex_[_ethAddress];
+        delete zscStakerPool_.stakers_[index];
+    }
+
 }
