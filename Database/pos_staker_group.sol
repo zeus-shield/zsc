3d5c61d15dd40900374cf94480034f6072ab4d5b
Eric-Yao <norhustar@hotmail.com>
2018-04-08 22:08:36 +0800

    updated contract PosStakerGroup
    updated struct ZscStaker, ZscStakerGroup; changed variable name ZscStakerGroup_ -> zscStakerGroup_; updated functions constructor, registerStaker(), removeStaker(); added funtions claimStakerSPByIndex(), useStakerSPByIndex(), getTotalRemainingSP;

--- Database/pos_staker_group.sol
+++ Database/pos_staker_group.sol
@@ -11,16 +11,17 @@ contract PosStakerGroup is Object {
         bytes32 nodeName_;
         address nodeAddress_;
         address ethAddress_;
-        uint stakePoint_;
-        uint rewardedZsc_;
         uint dividenDuration_;
+        uint earnedStakePoint_;
+        uint currentstakePoint_;
+        uint rewardedZsc_;
         uint dividenTimes_;
-        mapping(uint => uint) zscAmounts_;
     }
 
     struct ZscStakerGroup {        
         uint nos_;
-        uint totalGasUsed_;
+        uint spUsed_;
+        uint spRemaining_;
         uint settledBlocks_;
         uint dividenDuration_;
         uint dividenTimes_;
@@ -29,40 +30,63 @@ contract PosStakerGroup is Object {
         mapping(uint => ZscStaker) stakers_;
     }
 
-    ZscStakerGroup ZscStakerGroup_;
+    ZscStakerGroup zscStakerGroup_;
 
     address zscToken_;
 
     // Constructor
     function PosStakerGroup() {
-        ZscStakerGroup_.nos_ = 0;
-        ZscStakerGroup_.settledBlocks_ = 0;
-        ZscStakerGroup_.totalGasUsed_ = 0;
+        zscStakerGroup_.nos_ = 0;
+        zscStakerGroup_.settledBlocks_ = 0;
+        zscStakerGroup_.totalGasUsed_ = 0;
     } 
 
     function setZscTokenAddress(address _adr) public only_delegate {
         zscToken_ = _adr;
     }
 
-    function registerStaker(bytes32 _nodeName, address _nodeAddress, address _ethAddress) public only_delegate {
-        require(!ZscStakerGroup_.stakerExists_[_ethAddress]);
-        uint index = ZscStakerGroup_.nos_;
-        ZscStakerGroup_.nos_++;
-        ZscStakerGroup_.stakerExists_[_ethAddress] = true;
-        ZscStakerGroup_.stakerIndex_[_ethAddress] = index;
-        ZscStakerGroup_.stakers_[index].nodeName_    = _nodeName;
-        ZscStakerGroup_.stakers_[index].nodeAddress_ = _nodeAddress;
-        ZscStakerGroup_.stakers_[index].ethAddress_  = _ethAddress;
-        ZscStakerGroup_.stakers_[index].stakePoint_  = 0;
+    function numStakers() internal constant returns (uint) {
+        return zscStakerGroup_.nos_;
+    }
+    
+    function registerStaker(bytes32 _nodeName, address _nodeAddress, address _ethAddress, uint _dividenDuration) public only_delegate {
+        require(!zscStakerGroup_.stakerExists_[_ethAddress]);
+        uint index = zscStakerGroup_.nos_;
+        zscStakerGroup_.nos_++;
+        zscStakerGroup_.stakerExists_[_ethAddress] = true;
+        zscStakerGroup_.stakerIndex_[_ethAddress] = index;
+        zscStakerGroup_.stakers_[index] = ZscStaker(_nodeName, _nodeAddress, _ethAddress, _dividenDuration, 0, 0, 0 ,0)
     }
 
     function removeStaker(address _ethAddress) public only_delegate  {
-        require(ZscStakerGroup_.stakerExists_[_ethAddress]);
-        uint index = ZscStakerGroup_.stakerIndex_[_ethAddress];
-        ZscStakerGroup_.nos_--;
+        require(zscStakerGroup_.stakerExists_[_ethAddress]);
+        uint index = zscStakerGroup_.stakerIndex_[_ethAddress];
+        zscStakerGroup_.nos_--;
 
-        delete ZscStakerGroup_.stakerExists_[_ethAddress];
-        delete ZscStakerGroup_.stakerIndex_[_ethAddress];
-        delete ZscStakerGroup_.stakers_[index];
+        delete zscStakerGroup_.stakerExists_[_ethAddress];
+        delete zscStakerGroup_.stakerIndex_[_ethAddress];
+        delete zscStakerGroup_.stakers_[index];
     }
+
+    function claimStakerSPByIndex(uint _index, uint _amount) public only_delegate {
+        zscStakerGroup_.stakers_[_index].spRemaining_ += _amount;
+        zscStakerGroup_.spRemaining_ += _amount;
+    }
+
+    function useStakerSPByIndex(uint _index, uint _amount) internal returns (uint) {
+        if (zscStakerGroup_.stakers_[_index].stakePoint_ > _amount) {
+            zscStakerGroup_.stakers_[_index].stakePoint_ -= _amount;
+            zscStakerGroup_.spRemaining_ -= _amount;
+            return 0;
+        } else {
+            uint delta = _amount - zscStakerGroup_.stakers_[_index].stakePoint_;
+            zscStakerGroup_.stakers_[_index].stakePoint_ = 0;
+            zscStakerGroup_.spRemaining_ -= delta;
+            return delta;
+        }
+    }
+
+    function getTotalRemainingSP() public only_delegate constant returns (uint) {
+        return zscStakerGroup_.spRemaining_;
+    } 
 }
