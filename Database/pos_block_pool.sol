4d44f29e78806b88d0a5b329f74fe79f3a719cbb
Eric-Yao <norhustar@hotmail.com>
2018-04-08 21:00:25 +0800

    updated contract PosBlockPool
    updated struct Block, BlockPool; changed variable name lastBlockedIndex_->lastPendingBlockIndex_; updated functions constructor, registerTransaction(); added functions getLastPendingBlockIndex(), getBlockNos(), getBlockSizeByIndex(), getUnminedGas(), setBlockMinedByIndex()

--- Database/pos_block_pool.sol
+++ Database/pos_block_pool.sol
@@ -22,18 +22,19 @@ contract PosBlockPool is Object {
         uint blockLimit_;
         uint nosTransaction_;
         uint currentSize_;
-        mapping(uint => Block) blocks_;
     }
 
     struct BlockPool {
         uint nosBlocks_;
+        uint minedGasUsage_;
+        uint remaingGasUsage_;
         mapping(uint => Block) blocks_;
     }
 
     BlockPool private blockPool_;
     Block private currentBlock_;
     uint private blockUnitLimit_;
-    uint private lastBlockedIndex_;
+    uint private lastPendingBlockIndex_;
 
     // Constructor
     function PosBlockPool() {
@@ -41,10 +42,14 @@ contract PosBlockPool is Object {
         currentBlock_.blockLimit_ = 0;
         currentBlock_.nosTransaction_ = 0;
         currentBlock_.currentBlock_ = 0;
-        lastBlockedIndex_ = 0;
+        lastPendingIndex_ = 0;
         blockUnitLimit_ = 0;
-    } 
 
+        //The first block is empty
+        blockPool_[blockPool_.nosBlocks_] = currentBlock_;
+        blockPool_.nosBlocks_ = 1;
+    } 
+    
     function setBlockUnitLimit(uint _limit /*in terms of gas usage*/) public only_delegate {
         blockUnitLimit_ = _limit;
         resetCurrentBlock();
@@ -63,6 +68,7 @@ contract PosBlockPool is Object {
         if (currentSize > blockUnitLimit_) {
             blockPool_[blockPool_.nosBlocks_] = currentBlock_;
             blockPool_.nosBlocks_++;
+            blockPool_.remaingGasUsage_ += currentBlock_.currentBlock_;
 
             resetCurrentBlock();
 
@@ -73,9 +79,28 @@ contract PosBlockPool is Object {
         currentBlock_.currentSize_ = currentSize;
     }
 
-    function minePendingBlock(uint) public constant only_delegate {
-        
+    function getLastPendingBlockIndex() internal constant returns (uint) { 
+        if (lastPendingBlockIndex_ == blockPool_.nosBlocks_) return 0;
+        return lastPendingBlockIndex_; 
     }
-    
+
+    function getBlockNos() internal constant returns (uint) { 
+        return blockPool_.nosBlocks_ ; 
+    }
+
+    function getBlockSizeByIndex(uint _index) internal constant returns (uint) { 
+        return blockPool_.blocks_[_index].currentSize_; 
+    }
+
+    function getUnminedGas() internal constant returns (uint) { 
+        return blockPool_.remaingGasUsage_;
+    }
+
+    function setBlockMinedByIndex(uint _index) internal constant {
+        if (_index < blockPool_.nosBlocks_) {
+            blockPool_.blocks_[_index].status_ = BlockStatus.PENDING;
+            blockPool_.remaingGasUsage_ -= blockPool_.blocks_[_index].currentSize_;
+        }
+    }
 
 }
