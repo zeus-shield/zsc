a91c282dba2155d3e003c0c1ab6abbfe3ae4cd63
Eric-Yao <norhustar@hotmail.com>
2018-04-11 19:40:48 +0800

    updated contract PosBlockPool
    added functions registerNewBlock(), getBlockSizeByIndex(); updated functions createPool(), registerNewTx(), numBlocks()

--- Database/pos_block_pool.sol
+++ Database/pos_block_pool.sol
@@ -5,6 +5,7 @@ Copyright (c) 2018 ZSC Dev.
 pragma solidity ^0.4.18;
 
 import "./pos_block.sol";
+import "./db_block.sol"
 
 contract PosBlockPool is Object {
     struct BlockPool {
@@ -28,7 +29,7 @@ contract PosBlockPool is Object {
 
     function createBlock() internal returns (address);
 
-    function createPool(bytes32 _name, uint _dividendDuration, uint _rewardRate /* x% */) public only_delegate(1) {
+    function createPool(bytes32 _name, uint _dividendDuration, uint _rewardRate /* x / 1000: x = 0, 1, 2, ..., 1000) */) public only_delegate(1) {
         require(poolExists_[_name]);
 
         poolIndice_[_name] = poolNos_;
@@ -50,9 +51,17 @@ contract PosBlockPool is Object {
         pools_[_poolIndex].blocks_ ++;
     }
 
+    function registerNewBlock(uint _poolIndex) internal returns (bool) {
+        uint blockIndex = pools_[_poolIndex].blockNos_;
+        address adr = createBlock();
+
+        pools_[_poolIndex].blocks_[blockIndex] = adr;
+        pools_[_poolIndex].blocks_ ++;
+    }
+
     function registerNewTx(uint _poolIndex, uint _blockIndex, bytes32 _tx, bytes32 _sender, bytes32 _receiver, uint _gasUsage) internal returns (bool) {
         pools_[_poolIndex].remaingGasUsage_ += _gasUsage;
-        return PosBloc(pools_[_poolIndex].blocks_[_blockIndex]).registerTx(bytes32 _tx, bytes32 _sender, bytes32 _receiver, uint _gasUsage);
+        return PosBlock(pools_[_poolIndex].blocks_[_blockIndex]).registerTx(bytes32 _tx, bytes32 _sender, bytes32 _receiver, uint _gasUsage);
     }
 
     function getLastPendingBlockIndex() internal constant returns (uint) { 
@@ -67,8 +76,11 @@ contract PosBlockPool is Object {
         return 0;
     }
 
-    function numBlock(uint _poolIndex) internal constant returns (uint) { 
+    function numBlocks(uint _poolIndex) internal constant returns (uint) { 
         return pools_[_poolIndex].blockNos_; 
     }
     
+    function getBlockSizeByIndex(uint _poolIndex, uint _blockIndex) internal constant returns (address) {
+        return PosBlock(pools_[_poolIndex].blocks_[_blockIndex]).getCurrentSize();
+    }
 }
