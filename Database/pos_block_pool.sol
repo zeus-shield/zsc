37ec58505d1a2b06e7de308721fdc7b90d8588ce
Eric-Yao <norhustar@hotmail.com>
2018-04-11 18:21:22 +0800

    updated contract PosBlockPool
    added functions registerNewBlock(), registerNewTx(), getLastPendingBlockIndex(), numBlock();

--- Database/pos_block_pool.sol
+++ Database/pos_block_pool.sol
@@ -41,4 +41,34 @@ contract PosBlockPool is Object {
         poolNos_ = 0;
         blockSizeLimit_ = _sizeLimit;
     }
+
+    function registerNewBlock(uint _poolIndex) internal returns (bool) {
+        uint blockIndex = pools_[_poolIndex].blockNos_;
+        address adr = createBlock();
+
+        pools_[_poolIndex].blocks_[blockIndex] = adr;
+        pools_[_poolIndex].blocks_ ++;
+    }
+
+    function registerNewTx(uint _poolIndex, uint _blockIndex, bytes32 _tx, bytes32 _sender, bytes32 _receiver, uint _gasUsage) internal returns (bool) {
+        pools_[_poolIndex].remaingGasUsage_ += _gasUsage;
+        return PosBloc(pools_[_poolIndex].blocks_[_blockIndex]).registerTx(bytes32 _tx, bytes32 _sender, bytes32 _receiver, uint _gasUsage);
+    }
+
+    function getLastPendingBlockIndex() internal constant returns (uint) { 
+        if (pools_ == 0) return 0;
+
+        for (uint i = poolNos_ - 1; i >= 0; --i) {
+            address block = pools_[i].
+            if (PosBlock(pools_[i]).doesMined() == true) {
+                return (i + 1);
+            }
+        }
+        return 0;
+    }
+
+    function numBlock(uint _poolIndex) internal constant returns (uint) { 
+        return pools_[_poolIndex].blockNos_; 
+    }
+    
 }
