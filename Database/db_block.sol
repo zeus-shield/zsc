79e6f1bc307ed219db6e63203c9610b0bec48eb6
Eric-Yao <norhustar@hotmail.com>
2018-04-11 12:36:17 +0800

    updated contract db_block
    changed type of variable status_; added funtions doesBlockMined(), getBlockLimit(), getTxNos(), getCurrentSize(), getTxInfoByIndex(); updated function registerTransaction();

--- Database/db_block.sol
+++ Database/db_block.sol
@@ -14,7 +14,7 @@ contract DBBlock is DBNode {
         uint reigsterTime_;
     }
     
-    BlockStatus status_;
+    bool minedStatus_;
     uint blockLimit_;
     uint nosTransaction_;
     uint currentSize_;
@@ -26,6 +26,8 @@ contract DBBlock is DBNode {
     }
 
     function registerTransaction(bytes32 _tx, bytes32 _sender, bytes32 _receiver, uint _gasUsage) public constant only_delegate(1) returns (bool) {
+        if (minedStatus_) return false;
+        
         uint size = currentSize_ + _gasUsage;
         if (size < blockUnitLimit_) {
             txIndice_[_tx] = txNos_;
@@ -33,9 +35,33 @@ contract DBBlock is DBNode {
             txNos_++;
             return true;
         } else {
+            minedStatus_ = true;
             return false;
         }
     }
+
+    function doesBlockMined() public only_delegate(1) constant returns (bool) {
+        return minedStatus_;
+    }
+    
+    function getBlockLimit() public only_delegate(1) constant return (uint) {
+        return blockLimit_;
+    }
+
+    function getTxNos() public only_delegate(1) constant return (uint) {
+        return txNos_;
+    }
+
+    function getCurrentSize public only_delegate(1) constant return (uint) {
+        return currentSize_
+    }
+
+    function getTxInfoByIndex(uint _index) public only_delegate(1) constant returns (address, address, uint, uint) {
+        require(_index < txNos_);
+        
+        return (txHashs_[_index].sender_, txHashs_[_index].receiver_, txHashs_[_index].gasUsage_, txHashs_[_index].reigsterTime_);
+    }
+    
   
 }
 
