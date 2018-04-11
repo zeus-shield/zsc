127fc0d62635e96f352ffbed1366212130486cfe
Eric-Yao <norhustar@hotmail.com>
2018-04-11 20:37:45 +0800

    updated contract WalletBase
    updated struct Payment; remove variable paymentHistory_; added variables inputHistory_ and outHistory_; updated functions constructor, initParameters(), recordInput(), recordOut()

--- Database/wallet_base.sol
+++ Database/wallet_base.sol
@@ -13,7 +13,6 @@ contract WalletBase is DBNode {
         address sender_;
         address receiver_;
         uint256 amount_;
-        bool isInput_;
         bytes data_;
     }
 
@@ -23,10 +22,30 @@ contract WalletBase is DBNode {
         mapping(uint => Payment) payments_;
     }
     
-    PaymentHistory private paymentHistory_;
+    PaymentHistory private inputHistory_;
+    PaymentHistory private outHistory_;
+
+    bool private isEthAccount_;
 
     // Constructor
     function WalletBase(bytes32 _name) public DBNode(_name) {
-    } 
+        setNodeType("wallet"); 
+        isEthAccount_ = false;
+    }
 
+    function recordInput(address _sender, bytes32 _tx, uint _amount, bytes _data) internal {
+        uint index = inputHistory_.nos_;
+        inputHistory_.nos_++;
+        inputHistory_.total_ += _amount;
+        paymentHistory_.payments_[index] = Payment(now, _tx, _sender, this, _amount, _data);
+    }
+
+    function recordOut(address _sender, bytes32 _tx, uint _amount, bytes _data) internal {
+        require(inputHistory_.total_ >= _amount);
+        uint index = outHistory_.nos_;
+        inputHistory_.total_ -= _amount;
+        outHistory_.nos_++;
+        outHistory_.total_ += _amount;
+        outHistory_.payments_[index] = Payment(now, _tx, _sender, this, _amount, _data);
+    }
 }
