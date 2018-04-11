2ff59b50fb71108556da31a679b6af1900f79e4b
Eric-Yao <norhustar@hotmail.com>
2018-04-11 20:42:32 +0800

    added file wallet_erc20.sol

---
+++ Database/wallet_erc20.sol
@@ -0,0 +1,32 @@
+/*
+Copyright (c) 2018 ZSC Dev.
+*/
+
+pragma solidity ^0.4.18;
+
+import "./wallet_base.sol";
+
+contract WalletErc20 is WalletBase {
+    address _erc20TokenAdr;
+
+    // Constructor
+    function WalletErc20(bytes32 _name) public WalletBase(_name) {
+    }
+
+    function() public payable {        
+        revert();
+    }
+
+    function setERC20TokenAddress(address _tokenAdr) public only_delegate(1) {
+        _erc20TokenAdr = _tokenAdr;
+    }
+
+    function executeTransaction(address _dest, uint256 _amount, bytes _data) public only_delegate(1) returns (bool) {
+        if (ERC20Interface(_erc20TokenAdr).transfer(_dest, _value)) {
+            recordOut(address(this), _dest, _amount, _data);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
