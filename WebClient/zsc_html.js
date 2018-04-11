c7ee916d245e6238b4e54353c3227c29bd7e0772
Eric-Yao <norhustar@hotmail.com>
2018-04-12 07:45:40 +0800

    updated methods

--- WebClient/zsc_html.js
+++ WebClient/zsc_html.js
@@ -18,8 +18,61 @@ ZSCHtml.prototype.setHtmlContent = function(elementId, text) {
     document.getElementById(elementId).innerHTML = text;  
 }
 
+ZSCHtml.prototype.loadPageHeader = function(funcName) {
+    var funcPrefix = funcName + "(";
+    var funcSuffix = ")";
+    var text = ''
+    text += '<div class="well">'
+    text += '   <button type="button" onClick="' + funcPrefix + "'apply'" + funcSuffix + '">Apply for user</button>'
+    text += '   <button type="button" onClick="' + funcPrefix + "'wallet'" + funcSuffix + '">Wallate</button>'
+    text += '   <button type="button" onClick="' + funcPrefix + "'profile'" + funcSuffix + '">Profile</button>'
+    text += '   <button type="button" onClick="' + funcPrefix + "'templates'" + funcSuffix + '">Templates</button>'
+    text += '   <button type="button" onClick="' + funcPrefix + "'agreements'" + funcSuffix + '">Agreements</button>'
+    text += '</div>'
+    this.setHtmlContent(this.hearderId, text);  
+}
+
+
+ZSCHtml.prototype.loadWaitingApproval = function(funcName) {
+    var func;
+    var hashLogId;
+    var text = '<text>Enable the user in ZSC blockchain system</text>'
+
+    functionInput = funcName + "()";
+    text += '<div class="well">'
+    text += '    <text value="Applied as' + this.type + '></text>'
+    text += '    <button type="button" onClick="' + functionInput + '">Refresh</button>'
+    text += '</div>'
+    
+    this.setHtmlContent(this.pageBodyId, text);  
+}
+
+
+//////////
+ZSCHtml.prototype.loadPageBody = function(tag, funcName) {
+    var text;
+    switch(tag) {
+        case "login": 
+            text = this.loadLogin(funcName); 
+            break;
+        case "welecom": 
+            text = this.loadWelcome(); 
+            break;
+        case "apply": 
+            text = this.loadButtonForEnablingElement(funcName); 
+            break;
+        case "wallet": 
+            text = this.loadWallet(funcName);
+            break;
+        case "profile": 
+            text = this.loadParameters("user");
+            break;
+    }
+    document.getElementById(this.pageBodyId).innerHTML = text; 
+} 
+
 ZSCHtml.prototype.loadLogin = function(funcName) {
-    var funcLoad = funcName + "('AdmAdvAdr', 'userName')";
+    var functionInput = funcName + "('AdmAdvAdr', 'userName')";
     var text = ''
     text += '<div class="well">'
     text += '   <text>Login ZSC system</text><br><br>'
@@ -27,9 +80,17 @@ ZSCHtml.prototype.loadLogin = function(funcName) {
     text += '   <input class="form-control"  type="text" id="AdmAdvAdr" value="0x1ac03ee2171d22aa7b7d68231018f7169ba2d8ac"></input> <br> <br>'
     text += '   <text>User Name </text> <br>' 
     text += '   <input type="text" id="userName" value="test"></input> <br>' 
-    text += '   <button type="button" onClick="' + funcLoad + '">Enter</button>'
+    text += '   <button type="button" onClick="' + functionInput + '">Enter</button>'
     text += '</div>'
     this.setHtmlContent(this.pageBodyId, text);  
+}
+
+ZSCHtml.prototype.loadWelcome = function() {
+    var text = ''
+    text += '<div class="well">'
+    text += '   <text>Welcome to the ZSC testing platform</text>'
+    text += '</div>'
+    return text; 
 }
 
 ZSCHtml.prototype.loadButtonForEnablingElement = function(funcName) {
@@ -50,61 +111,6 @@ ZSCHtml.prototype.loadButtonForEnablingElement = function(funcName) {
     text += '</div>'
     
     this.setHtmlContent(this.pageBodyId, text);  
-}
-
-ZSCHtml.prototype.loadWaitingApproval = function(funcName) {
-    var func;
-    var hashLogId;
-    var text = '<text>Enable the user in ZSC blockchain system</text>'
-
-    functionInput = funcName + "()";
-    text += '<div class="well">'
-    text += '    <text value="Applied as' + this.type + '></text>'
-    text += '    <button type="button" onClick="' + functionInput + '">Refresh</button>'
-    text += '</div>'
-    
-    this.setHtmlContent(this.pageBodyId, text);  
-}
-
-ZSCHtml.prototype.loadPageHeader = function(funcName) {
-    var funcPrefix = funcName + "(";
-    var funcSuffix = ")";
-    var text = ''
-    text += '<div class="well">'
-    text += '   <button type="button" onClick="' + funcPrefix + "'welecome'" + funcSuffix + '">Wallate</button>'
-    text += '   <button type="button" onClick="' + funcPrefix + "'apply'" + funcSuffix + '">Wallate</button>'
-    text += '   <button type="button" onClick="' + funcPrefix + "'wallet'" + funcSuffix + '">Wallate</button>'
-    text += '   <button type="button" onClick="' + funcPrefix + "'profile'" + funcSuffix + '">Profile</button>'
-    text += '   <button type="button" onClick="' + funcPrefix + "'templates'" + funcSuffix + '">Templates</button>'
-    text += '   <button type="button" onClick="' + funcPrefix + "'agreements'" + funcSuffix + '">Agreements</button>'
-    text += '</div>'
-    this.setHtmlContent(this.hearderId, text);  
-}
-
-
-//////////
-ZSCHtml.prototype.loadPageBody = function(tag, funcName) {
-    var text;
-    switch(tag) {
-        case "welecom": 
-            text = this.loadWelcome(); 
-            break;
-        case "wallet": 
-            text = this.loadWallet(funcName);
-            break;
-        case "profile": 
-            text = this.loadParameters("user");
-            break;
-    }
-    document.getElementById(this.pageBodyId).innerHTML = text; 
-} 
-
-ZSCHtml.prototype.loadWelcome = function() {
-    var text = ''
-    text += '<div class="well">'
-    text += '   <text>Welcome to the ZSC testing platform</text>'
-    text += '</div>'
-    return text; 
 }
 
 ZSCHtml.prototype.loadWallet = function(funcName)  {
