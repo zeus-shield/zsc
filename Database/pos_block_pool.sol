02644f119d06017aa800c76f1d016b4a0278e4ae
Eric-Yao <norhustar@hotmail.com>
2018-04-11 15:09:12 +0800

    updated contract PosBlockPool
    removed enum BlockStatus; removed structs Transaction and Block; updated struct BlockPool; updated variables; updated functions

--- Database/pos_block_pool.sol
+++ Database/pos_block_pool.sol
@@ -4,103 +4,41 @@ Copyright (c) 2018 ZSC Dev.
 
 pragma solidity ^0.4.18;
 
-import "./object.sol";
+import "./pos_block.sol";
 
 contract PosBlockPool is Object {
-    enum BlockStatus {NULL, PENDING, MINED}
-
-    struct Transaction {
-        bytes32 txhash_;
-        address sender_;
-        address receiver_;
-        uint gasUsage_;
-        uint reigsterTime_;
-    }
-    
-    struct Block {
-        BlockStatus status_;
-        uint blockLimit_;
-        uint nosTransaction_;
-        uint currentSize_;
-    }
-
     struct BlockPool {
-        uint nosBlocks_;
+        uint blockNos_;
         uint minedGasUsage_;
         uint remaingGasUsage_;
-        mapping(uint => Block) blocks_;
+        mapping(uint => address) blocks_;
     }
 
-    BlockPool private blockPool_;
-    Block private currentBlock_;
-    uint private blockUnitLimit_;
-    uint private lastPendingBlockIndex_;
+    uint private poolNos_;
+    mapping(uint => BlockPool) private pools_;
+    mapping(bytes32 => uint) private poolIndice_;
+    mapping(bytes32 => bool) private poolExists_;
+
+    ////////////////
+    uint blockSizeLimit_;
 
     // Constructor
-    function PosBlockPool() {
-        blockPool_.nosBlocks_ = 0;
-        currentBlock_.blockLimit_ = 0;
-        currentBlock_.nosTransaction_ = 0;
-        currentBlock_.currentBlock_ = 0;
-        lastPendingIndex_ = 0;
-        blockUnitLimit_ = 0;
-
-        //The first block is empty
-        blockPool_[blockPool_.nosBlocks_] = currentBlock_;
-        blockPool_.nosBlocks_ = 1;
+    function BlockPool() {
     } 
-    
-    function setBlockUnitLimit(uint _limit /*in terms of gas usage*/) public only_delegate(1) {
-        blockUnitLimit_ = _limit;
-        resetCurrentBlock();
+
+    function createBlock() internal returns (address);
+
+    function createPool(bytes32 _name, uint _dividendDuration, uint _rewardRate /* x% */) public only_delegate(1) {
+        require(poolExists_[_name]);
+
+        poolIndice_[_name] = poolNos_;
+        pools_[poolNos_].blockNos_ = 0;
+        pools_[poolNos_].minedGasUsage_ = 0;
+        pools_[poolNos_].remaingGasUsage_ = 0;
     }
 
-    function resetCurrentBlock() internal {
-        currentBlock_.status_ = BlockStatus.PENDING;
-        currentBlock_.blockLimit_ = blockUnitLimit_;
-        currentBlock_.nosTransaction_ = 0;
-        ucurrentBlock_.currentSize_ = 0;
+    function adjustBlockSize(uint _sizeLimit /*in terms of gas usage*/) internal {
+        poolNos_ = 0;
+        blockSizeLimit_ = _sizeLimit;
     }
-
-    function registerTransaction(bytes32 _tx, bytes32 _sender, bytes32 _receiver, uint _gasUsage) public constant only_delegate(1) {
-        uint currentSize = currentBlock_.currentBlock_ + _gasUsage;
-        uint index;
-        if (currentSize > blockUnitLimit_) {
-            blockPool_[blockPool_.nosBlocks_] = currentBlock_;
-            blockPool_.nosBlocks_++;
-            blockPool_.remaingGasUsage_ += currentBlock_.currentBlock_;
-
-            resetCurrentBlock();
-
-            currentSize = _gasUsage;
-        }
-        currentBlock_(currentBlock_.nosTransaction_) = Transaction(_tx, _sender, _receiver, _gasUsage, now);
-        currentBlock_.nosTransaction_++;
-        currentBlock_.currentSize_ = currentSize;
-    }
-
-    function getLastPendingBlockIndex() internal constant returns (uint) { 
-        if (lastPendingBlockIndex_ == blockPool_.nosBlocks_) return 0;
-        return lastPendingBlockIndex_; 
-    }
-
-    function numBlock() internal constant returns (uint) { 
-        return blockPool_.nosBlocks_ ; 
-    }
-
-    function getBlockSizeByIndex(uint _index) internal constant returns (uint) { 
-        return blockPool_.blocks_[_index].currentSize_; 
-    }
-
-    function getUnminedGas() internal constant returns (uint) { 
-        return blockPool_.remaingGasUsage_;
-    }
-
-    function setBlockMinedByIndex(uint _index) internal constant {
-        if (_index < blockPool_.nosBlocks_) {
-            blockPool_.blocks_[_index].status_ = BlockStatus.PENDING;
-            blockPool_.remaingGasUsage_ -= blockPool_.blocks_[_index].currentSize_;
-        }
-    }
-
 }
