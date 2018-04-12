96cf1b74bb25b934a594ec65d5f9e2f1fb6b6243
Eric-Yao <norhustar@hotmail.com>
2018-04-12 19:59:56 +0800

    updated contract WalletManager
    added strunct TokenHolder; added variabl hoderNos_; added mappings tokenHoders_, holderIndices_, holderExists_; added functions addTokenHolder(), enableErc20TokenByHolder()

--- Database/wallet_mangager.sol
+++ Database/wallet_mangager.sol
@@ -14,10 +14,21 @@ contract WalletManager is Object {
         address tokenAdr_;
     }
 
+    struct TokenHolder {
+        bytes32 name_;
+        address adr_;
+        mapping(uint => bool) private enabledTokens_; // by index
+    }
+
     uint private tokenNos_;
     mapping(uint => tokenNos_) private erc20Tokens_;
     mapping(bytes32 => uint) private erc20TokenIndice_;
     mapping(bytes32 => bool) private erc20TokenExists_;
+
+    uint private holderNos_;
+    mapping(uint => TokenHolder) private tokenHoders_;
+    mapping(bytes32 => uint) private holderIndices_;
+    mapping(bytes32 => bool) private holderExists_;
 
     // Constructor
     function WalletManager() public Object("zsc_wallet_manager") {
@@ -41,5 +52,22 @@ contract WalletManager is Object {
         delete erc20TokenExists_[_symbol];
         delete erc20Tokens_[index];
         tokenNos_--;
+    }
+
+    function addTokenHolder(bytes32 _nodeName, address _nodeAddress) public only_delegate returns (bool) {
+        if (holderExists_[_nodeName]) return false;
+
+        holderExists_[_nodeName] = true;
+        holderIndices_[_nodeName] = holderNos_;
+        tokenHoders_[holderNos_].name_ = _nodeName;
+        tokenHoders_[holderNos_].adr_ = _nodeAddress;
+    }
+
+    function enableErc20TokenByHolder(bytes32 _nodeName, bytes32 _tokenSymbol) public only_delegate returns (bool) {
+        require(erc20TokenExists_[_tokenSymbol] && holderExists_[_nodeName]);
+
+        uint tokenIndex = erc20TokenIndice_[_tokenSymbol];
+        uint holderIndex = holderIndices_[_nodeName];
+        tokenHoders_[holderIndex].enabledTokens_[tokenIndex] =true;   
     }
 }
