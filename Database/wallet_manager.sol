361a10422ade5e77a6219d9715440f567f083d59
Eric-Yao <norhustar@hotmail.com>
2018-04-13 19:10:02 +0800

    modified the file name

--- Database/wallet_manager.sol
+++ Database/wallet_manager.sol
@@ -17,11 +17,11 @@ contract WalletManager is Object {
     struct TokenHolder {
         bytes32 name_;
         address adr_;
-        mapping(uint => bool) private enabledTokens_; // by index
+        mapping(uint => bool) enabledTokens_; // by index
     }
 
     uint private tokenNos_;
-    mapping(uint => tokenNos_) private erc20Tokens_;
+    mapping(uint => Erc20Token) private erc20Tokens_;
     mapping(bytes32 => uint) private erc20TokenIndice_;
     mapping(bytes32 => bool) private erc20TokenExists_;
 
@@ -51,7 +51,7 @@ contract WalletManager is Object {
         }
     }
 
-    function addErc20Token(bytes32 _name, bytes32 _symbol, uint _decimals, address _tokenAdr) public only_delegate returns (bool) {
+    function addErc20Token(bytes32 _name, bytes32 _symbol, uint _decimals, address _tokenAdr) public only_delegate(1) returns (bool) {
         if (erc20TokenExists_[_symbol]) return false;
 
         erc20TokenIndice_[_symbol] = tokenNos_;
@@ -60,7 +60,7 @@ contract WalletManager is Object {
         return true;
     }
 
-    function removeErc20Token(bytes32 _symbol) public only_delegate returns (bool) {
+    function removeErc20Token(bytes32 _symbol) public only_delegate(1) returns (bool) {
         if (!erc20TokenExists_[_symbol]) return false;
         
         uint index = erc20TokenIndice_[_symbol];
@@ -70,7 +70,7 @@ contract WalletManager is Object {
         tokenNos_--;
     }
 
-    function addTokenHolder(bytes32 _nodeName, address _nodeAddress) public only_delegate returns (bool) {
+    function addTokenHolder(bytes32 _nodeName, address _nodeAddress) public only_delegate(1) returns (bool) {
         if (holderExists_[_nodeName]) return false;
 
         holderExists_[_nodeName] = true;
@@ -79,7 +79,7 @@ contract WalletManager is Object {
         tokenHoders_[holderNos_].adr_ = _nodeAddress;
     }
 
-    function enableErc20TokenByHolder(bytes32 _nodeName, bytes32 _tokenSymbol) public only_delegate returns (bool) {
+    function enableErc20TokenByHolder(bytes32 _nodeName, bytes32 _tokenSymbol) public only_delegate(1) returns (bool) {
         require(erc20TokenExists_[_tokenSymbol] && holderExists_[_nodeName]);
 
         uint tokenIndex = erc20TokenIndice_[_tokenSymbol];
