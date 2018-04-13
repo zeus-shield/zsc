8585eec3f093aae111569ac060c2c6e7c3a65535
Eric-Yao <norhustar@hotmail.com>
2018-04-14 05:52:56 +0800

    updated contract WalletBase
    added structs LockInfo and LockHistory; added variables lockHistory_, totalValue_, lockedValue_; added function lockValue();

--- Database/wallet_base.sol
+++ Database/wallet_base.sol
@@ -22,14 +22,33 @@ contract WalletBase is DBNode {
         mapping(uint => Payment) payments_;
     }
     
+    struct LockInfo {
+        bool locked_;
+        uint amount_;
+        uint time_;
+        uint duration_;
+        address agreementAdr_;
+    }
+
+    struct LockHistory {
+        uint nos_;
+        uint256 total_;
+        mapping(uint => LockInfo) locks_;;
+    }
+
     PaymentHistory private inputHistory_;
     PaymentHistory private outHistory_;
+    LockHistory private lockHistory_;
 
     bool private isEthAccount_;
+    uint private totalValue_;
+    uint private lockedValue_;
 
     // Constructor
     function WalletBase(bytes32 _name) public DBNode(_name) {
         isEthAccount_ = false;
+        totalValue_ = 0;
+        lockedValue_ = 0;
     }
 
     function executeTransaction(address _dest, uint256 _amount, bytes _data) public only_delegate(1) returns (bool);
@@ -60,5 +79,18 @@ contract WalletBase is DBNode {
         outHistory_.nos_++;
         outHistory_.total_ += _amount;
         outHistory_.payments_[index] = Payment(now, _tx, _sender, this, _amount, _data);
+    }
+
+    function lockValue(uint _amount, uint _duration, address _agreementAdr) public only_delegate(1) returns (bool) {
+        if (totalValue_ - lockedValue_ < _amount) {
+            return false;
+        }
+
+        uint index = lockHistory_.nos_;
+        lockHistory_.nos_++;
+        lockHistory_.total_ += _amount;
+        lockHistory_.locks_[index] = LockInfo(true, _amount, now, _duration, _agreementAdr);
+
+        lockedValue_ += _amount;
     }
 }
