b59711f0bc6f058663ee0e459ef5c6d4dcbd22c0
Eric-Yao <norhustar@hotmail.com>
2018-04-13 21:20:05 +0800

    added file zsc_setup.js

--- WebAdm/js/zsc_setup.js
+++ WebAdm/js/zsc_setup.js
@@ -1,7 +1,7 @@
 /*
 Copyright (c) 2018 ZSC Dev Team
 */
-function ZscSetup(logRecorderAdr, adrs, abi) {
+function ZscSetup(logRecorderAdr, zscTokenAdr, adrs, abi) {
     this.RecorderAdr = logRecorderAdr;
     this.AdmAdvAdr = adrs[0];
     this.PosAdvAdr = adrs[1];
@@ -12,6 +12,7 @@ function ZscSetup(logRecorderAdr, adrs, abi) {
     this.FactoryTmpAdr = adrs[6];
     this.FactoryAgrAdr = adrs[7];
     this.ControlApisAdr = adrs[8];
+    this.zscTokenAdr = zscTokenAdr;
     this.account = web3.eth.accounts[0];
     this.myControlApi = web3.eth.contract(abi).at(adr);
 }
@@ -45,9 +46,58 @@ ZscSetup.prototype.registerListenerToLogRecorder = function(listener, listenerNa
 
     myLogRecorder.registerListener(listener, listenerName, {from:account, gas: 9000000},
     function(error, result){ 
-        if(!error) sF_showHashResult(hashID, result);
+        if(!error) this.showHashResult(hashID, result);
         else console.log("error: " + error);
     });
 }  
+
+ZscSetup.prototype.setLogRecorderToListener(listener,listenerName, hashID) {
+    var myContract = web3.eth.contract(cC_getContractAbi(listenerName));
+    var myListener = myContract.at(listener);
+    var account = web3.eth.accounts[0];
+
+    myListener.setLogRecorder(logRecorderAdr, {from:account, gas: 9000000},
+    function(error, result){ 
+        if(!error) this.showHashResult(hashID, result);
+        else console.log("error: " + error);
+    });
+}  
+
+function sF_initSystemModule(module, extra, hashID) {
+    if (module == "AdmAdv") {
+    } else if (module == "PosAdv") {
+        sF_initPosAdv(hashID);
+    } else if (module == "DBDatabase") {
+        sF_initWalletManager(hashID);
+    } else if (module == "DBDatabase") {
+        sF_initDatabase(hashID);
+    } else if (module == "ControlApisAdv") {
+        if (extra == "DBDatabase") {
+            sF_setDatabaseAdr(hashID);
+        } else if (extra == "WalletManager") {
+            sF_setWalletManager(hashID);
+        } else if (extra == "AdmAdv") {
+            sF_setAdm(hashID);
+        } else if (extra == "PosAdv") {
+            sF_setPos(hashID);
+        } else {
+            var factoryAdr;
+            if (extra == "FactoryPro") factoryAdr = FactoryProAdr;
+            else if (extra == "FactoryRec") factoryAdr = FactoryRecAdr;
+            else if (extra == "FactoryTmp") factoryAdr = FactoryTmpAdr;
+            else if (extra == "FactoryAgr") factoryAdr = FactoryAgrAdr;
+            sF_addFactory(extra, factoryAdr, hashID + extra);
+        }
+    } else {
+        var factoryAdr;
+        if (module == "FactoryPro") factoryAdr = FactoryProAdr;
+        else if (module == "FactoryRec") factoryAdr = FactoryRecAdr;
+        else if (module == "FactoryTmp") factoryAdr = FactoryTmpAdr;
+        else if (module == "FactoryAgr") factoryAdr = FactoryAgrAdr;
+
+        sF_initFactory(module, factoryAdr, hashID + module);
+    }
+}
+
 
 
