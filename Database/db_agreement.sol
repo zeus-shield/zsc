ae1f994294cf776c57c8d455960cf4f905e72fed
Eric-Yao <norhustar@hotmail.com>
2018-04-16 02:20:15 +0800

    updated contract DBAgreement

--- Database/db_agreement.sol
+++ Database/db_agreement.sol
@@ -10,8 +10,9 @@ contract DBAgreement is DBNode {
     uint private status_; // 0: CREATED; 1: READY; 2: PUBLISHED; 3: PAID; 4: NOTPAID
     uint private startTime_;
     uint private duration_;
+    uint private price_;
+    uint private refundPercentage_; // 1 : 100
     bytes32 private walletSymbol_;
-    uint private amount_;
 
     // Constructor
     function DBAgreement(bytes32 _name) public DBUser(_name) {
@@ -20,12 +21,9 @@ contract DBAgreement is DBNode {
     }
 
     function initParameters() internal {
-        addParameter("duration");
         addParameter("status");
         addParameter("startTime");
         addParameter("endTime");
-        addParameter("insuredAmount");
-        addParameter("paymentAmount");
     }
 
     function setParameter(bytes32 _parameter, string _value) public only_delegate(1) returns (bool) {
@@ -33,13 +31,19 @@ contract DBAgreement is DBNode {
             return false;  
 
         if (_parameter == "duration") {
-            duration_ = PlatString.stringToUint(_value);
+            duration = PlatString.stringToUint(_value);
+        } else if (_parameter == "walletSymbol") {
+            walletSymbol_ = PlatString.tobytes32(_value);
+        } else if (_parameter == "price") {
+            price_ = PlatString.stringToUint(_value);
+        } else if (_parameter == "fefund (%)") {
+            refundPercentage_ = PlatString.stringToUint(_value);
         }
         return super.setParameter(_parameter, _value);
     }
 
     function addParameter(bytes32 _parameter, string _value) public only_delegate(1) returns (bool) {
-        if (status_ > 0 )
+        if (status_ > 0)  
             return false;  
         return super.addParameter(_parameter, _value);
     }
@@ -53,24 +57,22 @@ contract DBAgreement is DBNode {
     function setAgreementStatus(bytes32 _tag) public only_delegate(1) returns (bool) {
         if (status_ > 2) return false;
 
-        if(status_ == 0 && _tag == "READY") status_ = 1;
-        else if (status_ == 1 && _tag == "PUBLISHED") status_ = 2;
-        else if (status_ == 2 && _tag == "READY") status_ = 1;
-        else return false;
-       
-        return super.setParameter("status",  _tag);
-    }
+        if(status_ == 0 && _tag == "READY") {
+            status_ = 1;
+        } else if (status_ == 1 && _tag == "PUBLISHED") {
+            status_ = 2;
+        } else if (status_ == 2 && _tag == "READY") {
+            status_ = 3;
+            startTime_ = now;
+            endTime_ = startTime_ + duration_;
     
-    function startInsurance(bytes32 _walletSymbol, uint _price) public only_delegate(1) {
-        walletSymbol_ = _walletSymbol;
-        price_ = _price;
-        status_ = 3;
-        startTime_ = now;
-        endTime_ = startTime_ + duration_;
-
-        super.setParameter("status", "PAID");
-        super.setParameter("startTime", PlatString.uintToString(startTime_));
-        super.setParameter("endTime", PlatString.uintToString(endTime_));
+            super.setParameter("status", "PAID");
+            super.setParameter("startTime", PlatString.uintToString(startTime_));
+            super.setParameter("endTime", PlatString.uintToString(endTime_));
+        } else {
+            return false;
+        }
+        return super.setParameter("status",  _tag);
     }
 
     function checkSenderType(bytes32 _type, bytes32 _sender) internal {
