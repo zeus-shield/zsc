586212808e7e8c7bbd14d37c17bad71945a878f8
Eric-Yao <norhustar@hotmail.com>
2018-04-15 22:33:35 +0800

    added functions refreshGetLog(), initSystemLog()

--- WebAdm/js/zsc_printlog.js
+++ WebAdm/js/zsc_printlog.js
@@ -13,3 +13,37 @@ function ZSCPrintLog(logRecorderAdr) {
 ZSCPrintLog.prototype.changeLogerModule = function(adr) {
     this.logerModuleAdr = adr;
 }
+
+ZSCPrintLog.prototype.refreshGetLog = function(logRecorderAdr, adr, elementID) {
+    var myContract = web3.eth.contract(cC_getContractAbi("LogRecorder"));
+    var myLogRecorder = myContract.at(this.logRecorderAdr);
+    var account = this.account;
+
+    var text = this.logMap.get(adr);
+    var index = this.logIndexMap.get(adr);
+
+    myLogRecorder.printLog(adr, index,
+    function(error, result){ 
+        if(!error) {
+            if (result != "null") {
+                //console.log(result);
+                this.logMap.set(adr, text + result + "\n");
+                index++;
+                sthis.logIndexMap.set(adr, index);
+            }
+            document.getElementById(elementID).innerText = sF_logMap.get(this.logerModuleAdr);
+        }
+        else console.log("error: " + error);
+    });
+}
+
+ZSCPrintLog.prototype.initSystemLog = function(adrs, elementID, initialModuleIndex) {
+    this.logerModuleAdr = adrs[initialModuleIndex];
+    for (var i = 0; i < adrs.length; ++i) {
+        this.logMap.set(adrs[i], "");
+        this.logIndexMap.set(adrs[i], 0);
+        window.setInterval("sF_refreshGetLog('" + this.logRecorderAdr + "', '" + adrs[i] + "', '" + elementID + "')", 2000);
+    }
+}  
+
+
