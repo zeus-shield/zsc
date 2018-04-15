e6c9997190ffb9644ed36041a8ad74695c1a91cf
Eric-Yao <norhustar@hotmail.com>
2018-04-15 22:31:52 +0800

    updated contract DBTemplate
    added variable advertised_; updated functions initParameters(), setParameter(), addParameter(), removeParameter(); removed function setAdvertised();

--- Database/db_template.sol
+++ Database/db_template.sol
@@ -6,33 +6,47 @@ pragma solidity ^0.4.18;
 import "./db_entity.sol";
 
 contract DBTemplate is DBEntity {
-    mapping(address => bool) private userExist_;
+    mapping(address => bool) private fundamentalParass_;
     address[] private users_;
+    bool private advertised_;
     
     function DBTemplate(bytes32 _name) public DBEntity(_name) {
         setNodeType("template");
+        advertised_ = false;
     }
 
     function initParameters() internal {
-        addParameter("Automatic");
-        addParameter("Duration");
-        addParameter("Price (TestETH)");
-        addParameter("Price (TestZSC)");
-        addParameter("RefundPercentage");
+        addParameter("automatic");
+        addParameter("duration");
+        addParameter("token-symbol");
+        addParameter("price");
+        addParameter("fefund (%)");
+
+        fundamentalParass_["automatic"] = true;
+        fundamentalParass_["duration"] = true;
+        fundamentalParass_["token-symbol"] = true;
+        fundamentalParass_["price"] = true;
+        fundamentalParass_["fefund (%)"] = true;
+    }
+
+    function setAdvertised() public only_delegate(1) {
+        advertised_ = true;
     }
 
     function setParameter(bytes32 _parameter, string _value) public only_delegate(1) returns (bool) {
-        if (numChildren() > 0) return false; 
+        if (advertised_0) return false; 
         return super.setParameter(_parameter, _value);
     }
 
     function addParameter(bytes32 _parameter, string _value) public only_delegate(1) returns (bool) {
-        if (numChildren() > 0) return false; 
+        if (advertised_) return false; 
         return super.addParameter(_parameter, _value);
     }
 
     function removeParameter(bytes32 _parameter) public only_delegate(1) returns (bool) {
-        if (numChildren() > 0) return false; 
+        if (advertised_) return false; 
+        if (fundamentalParass_[_parameter]) return false;
+        
         return super.removeParameter(_parameter);
     }
 }
