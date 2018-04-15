869258efecdd9ab5c9d3ee262b6ae90f75b38910
Eric-Yao <norhustar@hotmail.com>
2018-04-16 03:34:19 +0800

    updated contract SimulatorBase

--- Database/simulator_base.sol
+++ Database/simulator_base.sol
@@ -6,57 +6,55 @@ pragma solidity ^0.4.18;
 
 import "./db_node.sol";
 
-contract SimulatBase is DBNode {
-    struct SimultionRun {
-        bool started_;
-        bool running_ ;
-        bool autoReward_;  
-        uint startTime_;
-        uint endTime_; // in secons
-        uint probability_;   //from 0 to 1000
-        bytes32 provider_ ;
-        bytes32 receiver_;
-        bytes32 agreement_;
-    }
-    mapping(bytes32 => SimultionRun) private simulationRuns_;
-
+contract SimulatorBase is DBNode {
+    bool started_;
+    bool running_ ;
+    bool autoReward_;  
+    uint startTime_;
+    uint endTime_; // in secons
+    uint probability_;   //from 0 to 1000
+    bytes32 agreement_ ;
+    bytes32 provider_ ;
+    bytes32 receiver_;
+    
     uint private randSeed = 0;
 
-    function SimulatBase(bytes32 _name) public DBNode(_name) {
+    function SimulatorBase(bytes32 _name) public DBNode(_name) {
+        running_ = false;
     }
 
     function initParameters() internal {
     }
 
-    // Generates a random number
-    // Original file at 
-    // https://gist.github.com/alexvandesande/259b4ffb581493ec0a1c
-    function randGen(uint _min, uint _max) internal constant returns (uint256){
-        require(_max > _min);
-        randSeed++;
-        uint randValue = uint(sha3(block.blockhash(block.number-1), randSeed ))%(_max - _min);
-
-        return (randValue + _min);
+    function startSimulation(bool _autoReward, bytes32 _agreement, bytes32 _provider, bytes32 _receiver, uint _duration) public only_delegate(1) {
+        running_ = true;
+        autoReward_ = _autoReward;
+ 
+        startTime_   = now; 
+        endTime_     = startTime_ + _duration;
+        probability_ = ranGen(70, 100);
+        agreement_   = _agreement;
+        provider_    = _provider;
+        receiver_    = _receiver
     }
 
-    function addSimulationRun(bytes32 _name, bytes32 _provider, bytes32 _receiver, bytes32 _agreement, uint _duration) public only_delegate(1) {
-        require(!simulationRuns_[_name].started_);
-        uint current = now; 
-        simulationRuns_[_name] = SimultionRun(true, true, true, current, current + _duration, ranGen(70, 100), _provider, _receiver, _agreement);
+    function doesStarted() public only_delegate(1) constant returns (bool) { 
+        return started_;
+    } 
+
+    function doesFinished() public only_delegate(1) constant returns (bool) { 
+        return (!running_);
+    } 
+
+    function getSimulationProvider(bytes32 _agreement) public only_delegate(1) constant returns (bytes32) {
+        return provider_;
     }
 
-    function checkSimulationRun(bytes32 _name) public only_delegate(1) constant returns (bool) {
-        require(simulationRuns_[_name].started_);
+    function getSimulationReceiver(bytes32 _agreement) public only_delegate(1) constant returns (bytes32) {
+        return receiver_;
+    }
 
-        if (simulationRuns_[_name].running_ == true) {
-            if (now > simulationRuns_[_name].endTime_) {
-                simulationRuns_[_name].running_ = true;
-                uint rand = randGen(0, 100);
-                if (rand > simulationRuns_[_name].probability_) {
-                    return true;
-                }
-            }
-        }
-        return false;
+    function getSimulationReceiver(bytes32 _agreement) public only_delegate(1) constant returns (bytes32) {
+        return agreement_;
     }
 }
