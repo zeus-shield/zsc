bc2f69a8579dfa6023181922c820c973ac173869
Eric-Yao <norhustar@hotmail.com>
2018-04-15 21:35:21 +0800

    updated class ZSCWallet
    update properties; updated method loadEthBalance(); added method loadTokenBalance(), numTokenWallets()

--- WebClient/zsc_wallet.js
+++ WebClient/zsc_wallet.js
@@ -5,11 +5,14 @@ Copyright (c) 2018 ZSC Dev Team
 //class zscWallet
 function ZSCWallet(nm, abi, adr) {
     this.userName = nm;
+    this.ethAddress;
+    this.ethTotalBalance;
+    this.ethLockedBalance;
     this.tokenNos = 0;
-    this.nodeAddress = 0;
     this.tokenSymbol = [];
-    this.tokenBalance = [];
     this.tokenAddress = [];
+    this.tokenTotalBalance = [];
+    this.tokenLockedBalance = [];
     this.myControlApi = web3.eth.contract(abi).at(adr);
 }
 ZSCWallet.prototype.getUserName = function() {return this.userName;}
@@ -22,11 +25,16 @@ ZSCWallet.prototype.getTokenAddress = function(index) { return this.tokenAddress
 
 ZSCWallet.prototype.getTokenNos = function() { return this.tokenNos; }
 
-ZSCWallet.prototype.loadEthBalance = function(func) {
-    this.myControlApi.getElementEthBalance(this.name, function(error, balance){ 
+ZSCWallet.prototype.loadEthBalance = function(func, locked) {
+    this.myControlApi.getElementBalance(this.name, "ETH", locked,
+    	function(error, balance){ 
         if(!error) {
-            this.ethBalance = balance;  
-            myControlApi.getElementAddress(this.name, function(error, address){ 
+        	if (!locked) {
+        		this.ethTotalBalance = balance;  
+            } else {
+        		this.ethLockedBalance = balance;  
+            }
+            myControlApi.getUserWalletAddress(this.name, "ETH", function(error, address){ 
                 if(!error) { 
                     this.nodeAddress = address; 
                     func(); 
@@ -36,4 +44,39 @@ ZSCWallet.prototype.loadEthBalance = function(func) {
             });
         }
     });
+}
+
+ZSCWallet.prototype.loadTokenBalance = function(func, index, locked) {
+	var symbol = this.tokenSymbol[index];
+    this.myControlApi.getElementBalance(this.name, symbol, locked,
+    	function(error, balance){ 
+        if(!error) {
+        	if (!locked) {
+        		this.tokenTotalBalance[index] = balance;  
+            } else {
+        		this.tokenLockedBalance[index] = balance;  
+            }
+            myControlApi.getUserWalletAddress(this.name, symbol, function(error, address){ 
+                if(!error) { 
+                    this.tokenAddress[index] = address; 
+                    func(); 
+                } else {
+                    console.log("error: " + error);
+                }
+            });
+        }
+    });
+}
+
+ZSCWallet.prototype.numTokenWallets = function(func) {
+    this.myControlApi.numRegisteredErc20Tokens(this.name, 
+        {from: bF_getEthAccount()},
+        function(error, num){ 
+            if(!error) { 
+                this.parameNos = num.toString(10); 
+                func();
+            } else {
+                console.log("error: " + error);
+            }
+         });
 }
