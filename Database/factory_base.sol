4d11e72c80eddf7b600a3d85923f9040ba1a802f
Eric-Yao <norhustar@hotmail.com>
2018-04-16 02:50:02 +0800

    updated contract FactoryBase

--- Database/factory_base.sol
+++ Database/factory_base.sol
@@ -14,7 +14,12 @@ contract FactoryBase is Object {
     address private bindedDB_;
     address private apiController_;
 
-    function FactoryBase(bytes32 _name) public Object(_name) {}
+    uint private nodeNos_;
+    mapping(uint => bytes32) private nodeNames_;
+
+    function FactoryBase(bytes32 _name) public Object(_name) {
+        nodeNos_ = 0;
+    }
 
     function setupFactoryRoot() internal;
 
@@ -36,5 +41,30 @@ contract FactoryBase is Object {
             setDelegate(_controller, 1);
         }
         setupFactoryRoot();
+    }
+
+    function addNodeName(bytes32 _name) internal {
+        nodeNames_[nodeNos_] = _name;
+        nodeNos_++;
+    }
+
+    function removeNodeName(bytes32 _name) internal {
+        uint index = 0;
+        for (uint i = 0; i < nodeNos_; ++i) {
+            if (_name == nodeNames_[i]) {
+                index = 0;
+                break;
+            }
+        }
+        delete nodeNames_[index];
+        nodeNos_--;
+    }
+
+    function numNodeNames() public only_delegate(1) constant returns (uint) {
+        return nodeNos_;
+    }
+
+    function getNodeNameByIndex(uint _index) public only_delegate(1) constant returns (bytes32) {
+        return nodeNames_[_index];
     }
 }
