6077b460d90a1a0cb6d15f1b2d662fab456e0659
Eric-Yao <norhustar@hotmail.com>
2018-04-17 00:36:24 +0800

    updated contract SimulatorManagement
    updated variables; removed funtions randGen(); updated functions addSimulationRun(), checkSimulationRun(), conductReward();

--- Database/simulator_manager.sol
+++ Database/simulator_manager.sol
@@ -4,27 +4,12 @@ Copyright (c) 2018 ZSC Dev Team
 
 pragma solidity ^0.4.18;
 
-import "./object.sol";
-
-contract SimulatorBase is Object {
-    function doesStarted() public only_delegate(1) constant returns (bool);
-    function doesFinished() public only_delegate(1) constant returns (bool);
-    function getSimulationProvider(bytes32 _name) public only_delegate(1) constant returns (bytes32);
-    function getSimulationReceiver(bytes32 _name) public only_delegate(1) constant returns (bytes32);
-    function getSimulationAgreement(bytes32 _name) public only_delegate(1) constant returns (bytes32);
-    function getAddressInfo() public only_delegate(1) constant returns (address, address, address);
-}
+import "./simulator_base.sol";
 
 contract SimulatorManager is Object {
-    struct SimultionRun {
-        bool rewarded_;
-        address adr_;
-        uint probability_;
-    }
-    mapping(bytes32 => SimultionRun) private simulationRuns_;
-    mapping(bytes32 => bool) private simulationExists_;
+    mapping(bytes32 => address) private simulationRuns_;
+    mapping(bytes32 => bool) private rewarded_;
 
-    uint private randSeed_= 0;
     address private bindedDB_;
     address private apiController_;
 
@@ -44,38 +29,26 @@ contract SimulatorManager is Object {
             setDelegate(_controller, 1);
         }
     }
+    
+    /*_proLev: 1 : 100*/
+    function addSimulationRun(bytes32 _name, uint _proLevel, address _agreement, address _provider, address _receiver) public only_delegate(1) {
+        require(simulationRuns_[_name] == address(0));
 
-
-    // Generates a random number
-    // Original file at 
-    // https://gist.github.com/alexvandesande/259b4ffb581493ec0a1c
-    function randGen(uint _min, uint _max, uint _seed) internal constant returns (uint){
-        require(_max > _min);
-        uint randValue = uint(keccak256(block.blockhash(block.number-1), _seed ))%(_max - _min);
-
-        return (randValue + _min);
-    }
-
-    function addSimulationRun(bytes32 _name, address _adr,  uint _proLevel /*1 : 100*/) public only_delegate(1) {
-        require(!simulationExists_[_name]);
-        uint prob = randGen(_proLevel, 100, randSeed_++);
-
-        simulationExists_[_name] = true;
-        simulationRuns_[_name] = SimultionRun(false, _adr, prob);
+        address adr = new SimulatorBase(_name);
+        SimulatorBase(adr).startSimulation(_proLevel, _agreement, _provider, _receiver);
+        simulationRuns_[_name] = adr;
+        rewarded_[_name] = false;
     }
 
     function checkSimulationRun(bytes32 _name) public only_delegate(1) constant returns (bool) {
-        require(simulationExists_[_name]);
-        address adr = simulationRuns_[_name].adr_;
-        uint seed = 0;
+        require(simulationRuns_[_name] != address(0));
 
-        if (simulationRuns_[_name].rewarded_ == false) {
+        address adr = simulationRuns_[_name];
+
+        if (rewarded_[_name] == false) {
             if (SimulatorBase(adr).doesStarted()) {
                 if (SimulatorBase(adr).doesFinished()) {
-                    uint rand = randGen(0, 100, seed++);
-                    if (rand < simulationRuns_[_name].probability_) {
-                        return true;
-                    }
+                    return SimulatorBase(adr).needReward();   
                 }
             }
         }
@@ -83,8 +56,9 @@ contract SimulatorManager is Object {
     }
  
     function conductReward(bytes32 _name) public only_delegate(1) constant {
-        require(simulationExists_[_name]);
-        address sim = simulationRuns_[_name].adr_;
+        require(simulationRuns_[_name] != address(0));
+
+        address sim = simulationRuns_[_name];
         address agreement;
         address provider;
         address receiver;
