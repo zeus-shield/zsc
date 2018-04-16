944c8de3bf5c2ba44daa7f9edaa3efc02641b206
Eric-Yao <norhustar@hotmail.com>
2018-04-16 19:45:20 +0800

    updated api functions

--- Database/control_apis.sol
+++ Database/control_apis.sol
@@ -27,32 +27,32 @@ contract ControlApis is ControlBase {
     }
 
     /// @dev Check the element wheather or not existing
-    /// @param _node The name of the element to be checked
-    function doesElementExist(bytes32 _node) public only_registered(_node) constant returns (bool) {
-        return (getDBNode(_node) != DBNode(0));
+    /// @param _enName The name of the element to be checked
+    function doesElementExist(bytes32 _enName) public only_registered(_enName) constant returns (bool) {
+        return (getDBNode(_enName) != DBNode(0));
     }
 
     /// @dev Creat an element
     /// @param _typeInUint The type of the factory for creating the element
-    /// @param _node The name of the element belonging to the user
+    /// @param _enName The name of the element belonging to the user
     /// @param _extraInfo The extra information
     /// @param _extraAdr The extra address
-    function createElement(uint _typeInUint, bytes32 _node, bytes32 _extraInfo, address _extraAdr) public returns (address) {
-        require(onlyRegisteredOrDelegated(_node, msg.sender));
+    function createElement(uint _typeInUint, bytes32 _enName, bytes32 _extraInfo, address _extraAdr) public returns (address) {
+        require(onlyRegisteredOrDelegated(_enName, msg.sender));
         address creatorAdr = _extraAdr;
         if (creatorAdr == 0) {
             creatorAdr = msg.sender;
         }
-        return createFactoryNode(mapType(_typeInUint), _node, _extraInfo, creatorAdr);
+        return createFactoryNode(mapType(_typeInUint), _enName, _extraInfo, creatorAdr);
     }
 
-    function enableElementWallet(uint _typeInUint/*5: eth; 6: erc20*/, bytes32 _user, bytes32 _tokeSymbol, address _extraAdr) public returns (address) {
-        require(onlyRegisteredOrDelegated(_user, msg.sender));
+    function enableElementWallet(uint _typeInUint/*5: eth; 6: erc20*/, bytes32 _enName, bytes32 _tokeSymbol, address _extraAdr) public returns (address) {
+        require(onlyRegisteredOrDelegated(_enName, msg.sender));
         address creatorAdr = _extraAdr;
         if (creatorAdr == 0) {
             creatorAdr = msg.sender;
         }
-        return enableWallet(mapType(_typeInUint), _user, _tokeSymbol, creatorAdr);
+        return enableWallet(mapType(_typeInUint), _enName, _tokeSymbol, creatorAdr);
     }
 
     /// @dev Get the element by its address
@@ -63,9 +63,9 @@ contract ControlApis is ControlBase {
     }
 
     /// @dev Get the type of an element
-    /// @param _node The name of the element belonging to the user
-    function getElementType(bytes32 _node) public only_delegate(1) constant returns (bytes32) {
-        DBNode nd = getDBNode( _node);
+    /// @param _enName The name of the element belonging to the user
+    function getElementType(bytes32 _enName) public only_delegate(1) constant returns (bytes32) {
+        DBNode nd = getDBNode( _enName);
         require(nd != DBNode(0));
         return nd.getNodeType();
     }
@@ -84,37 +84,37 @@ contract ControlApis is ControlBase {
     }
 
     /// @dev Add a paramter to an element
-    /// @param _node The name of the existing element
+    /// @param _enName The name of the existing element
     /// @param _parameter The name of the added parameter
-    function addElementParameter(bytes32 _node, bytes32 _parameter) public only_registered(_node) returns (bool) {
-        return operateNodeParameter("add", _node, _parameter, "");
+    function addElementParameter(bytes32 _enName, bytes32 _parameter) public only_registered(_enName) returns (bool) {
+        return operateNodeParameter("add", _enName, _parameter, "");
     }
 
     /// @dev Set the value to a paramter of an element 
-    /// @param _node The name of the element
+    /// @param _enName The name of the element
     /// @param _parameter The name of the existing parameter
     /// @param _value The parameter value
-    function setElementParameter(bytes32 _node, bytes32 _parameter, string _value) public only_registered(_node) returns (bool) {
-        return operateNodeParameter("set", _node, _parameter, _value);
+    function setElementParameter(bytes32 _enName, bytes32 _parameter, string _value) public only_registered(_enName) returns (bool) {
+        return operateNodeParameter("set", _enName, _parameter, _value);
     }
 
     /// @dev Get the value of a paramter of an element
-    /// @param _node The name of the element
+    /// @param _enName The name of the element
     /// @param _parameter The name of the existing parameter
-    function getElementParameter(bytes32 _node, bytes32 _parameter) public only_registered(_node) constant returns (string) {
-        return getControlInfoParameterValue(_node, _parameter);
+    function getElementParameter(bytes32 _enName, bytes32 _parameter) public only_registered(_enName) constant returns (string) {
+        return getControlInfoParameterValue(_enName, _parameter);
     }
 
     /// @dev Get the address of the element 
-    /// @param _node The name of the element
-    function getElementAddress(bytes32 _node) public only_registered(_node) constant returns (address) {
-        return address(getDBNode(_node));
+    /// @param _enName The name of the element
+    function getElementAddress(bytes32 _enName) public only_registered(_enName) constant returns (address) {
+        return address(getDBNode(_enName));
     }
 
     /// @dev Get the eth balance of the element
-    /// @param _node The name of the element
-    function getElementBalance(bytes32 _node, bytes32 _symbol, bool _locked) public only_registered(_node) constant returns (uint256) {
-        string memory str = PlatString.append(_node, "-", _symbol);
+    /// @param _enName The name of the element
+    function getElementBalance(bytes32 _enName, bytes32 _symbol, bool _locked) public only_registered(_enName) constant returns (uint256) {
+        string memory str = PlatString.append(_enName, "-", _symbol);
         bytes32 walletName = PlatString.tobytes32(str);
         require(getDBNode(walletName) != DBNode(0));
 
@@ -122,13 +122,13 @@ contract ControlApis is ControlBase {
     }
 
     /// @dev Get the number of paramters of an element
-    /// @param _node The name of the existing element
-    function numElementParameters(bytes32 _node) public only_registered(_node) constant returns (uint) {
-        return  getDBNode(_node).numParameters();
+    /// @param _enName The name of the existing element
+    function numElementParameters(bytes32 _enName) public only_registered(_enName) constant returns (uint) {
+        return  getDBNode(_enName).numParameters();
     }
 
     /// @dev Get the number of paramters of an element
-    /// @param _node The name of the existing element
+    /// @param _enName The name of the existing element
     /// @param _index The index of the parameter
     /* Example:
         var num = numNodeParameters("test");
@@ -136,16 +136,16 @@ contract ControlApis is ControlBase {
             var para = getNodeParameterNameByIndex("test", 0);
         }
     */
-    function getElementParameterNameByIndex(bytes32 _node, uint _index) public only_registered(_node) constant returns (bytes32) {
-        return  getDBNode(_node).getParameterNameByIndex(_index);
+    function getElementParameterNameByIndex(bytes32 _enName, uint _index) public only_registered(_enName) constant returns (bytes32) {
+        return  getDBNode(_enName).getParameterNameByIndex(_index);
     }
 
     /// @dev Transfer ETH from a user element to the destination address
-    /// @param _node The name of the existing element
+    /// @param _enName The name of the existing element
     /// @param _dest The destination address
     /// @param _amount The amount of ETH to be transferred
-    function elementTransferValue(bytes32 _node, bytes32 _symbol, address _dest, uint256 _amount) public only_registered(_node) returns (bool) {
-        string memory str = PlatString.append(_node, "-", _symbol);
+    function elementTransferValue(bytes32 _enName, bytes32 _symbol, address _dest, uint256 _amount) public only_registered(_enName) returns (bool) {
+        string memory str = PlatString.append(_enName, "-", _symbol);
         bytes32 walletName = PlatString.tobytes32(str);
         require(getDBNode(walletName) != DBNode(0));
 
@@ -153,18 +153,18 @@ contract ControlApis is ControlBase {
     }
 
     /// @dev Get the number of element binded to the node
-    /// @param _node The name of the existing element
-    /// @param _elementType The type of the element
-    function numBindedElements(bytes32 _node, uint _elementType) public only_registered(_node) constant returns (uint) {
-        return getDBNode(_node).numBindedEntities(mapType(_elementType));
+    /// @param _enName The name of the existing element
+    /// @param _enType The type of the element
+    function numBindedElements(bytes32 _enName, uint _enType) public only_registered(_enName) constant returns (uint) {
+        return getDBNode(_enName).numBindedEntities(mapType(_enType));
     }
 
     /// @dev Get the name of the element binded to the element by index
-    /// @param _node The name of the existing element
-    /// @param _elementType The type of the element
+    /// @param _enName The name of the existing element
+    /// @param _enType The type of the element
     /// @param _index The index of the template
-    function getBindedElementNameByIndex(bytes32 _node, uint _elementType, uint _index) public only_registered(_node) constant returns (bytes32) {
-        return getDBNode(_node).getBindedEntityNameByIndex(mapType(_elementType), _index);
+    function getBindedElementNameByIndex(bytes32 _enName, uint _enType, uint _index) public only_registered(_enName) constant returns (bytes32) {
+        return getDBNode(_enName).getBindedEntityNameByIndex(mapType(_enType), _index);
     }
 
     /// @dev Announce an insurance agreement by a provider
@@ -176,10 +176,10 @@ contract ControlApis is ControlBase {
     }
 
     /// @dev Buy an insurance agreement with Eth from a provider
-    /// @param _user The receiver name
+    /// @param _enName The receiver name
     /// @param _agreement The agreement name
-    function purchaseInsurance(bytes32 _user, bytes32 _agreement) public only_registered(_user) returns (bool) {
-        return getDBNode(_agreement).setAgreementStatus("PUBLISHED", _user);
+    function purchaseInsurance(bytes32 _enName, bytes32 _agreement) public only_registered(_enName) returns (bool) {
+        return getDBNode(_agreement).setAgreementStatus("PUBLISHED", _enName);
     }
 
     function registerErc20Token(bytes32 _name, bytes32 _symbol, uint _decimals, address _tokenAdr) public only_delegate(1) returns (bool) {
@@ -190,19 +190,19 @@ contract ControlApis is ControlBase {
         return manageErc20TokenContract(false, 0, _symbol, 0, 0);
     }
 
-    function numRegisteredErc20Tokens() public only_delegate(1) constant returns (uint) {
+    function numRegisteredErc20Tokens(bytes32 _enName) public only_registered(_enName) constant returns (uint) {
         getWalletManager().numTokenSymbols();
     }
 
-    function getErc20TokenSymbolByIndex(uint _index) public only_delegate(1) constant returns (uint) {
+    function getErc20TokenSymbolByIndex(bytes32 _enName, uint _index) public only_registered(_enName) constant returns (uint) {
         getWalletManager().getTokenSymbolByIndex(_index);
     }
 
-    function getUserWalletAddress(bytes32 _user, bytes32 _tokenSymbol) public only_delegate(1) constant returns (address) {
-        DBNode nd = getDBNode( _user);
+    function getUserWalletAddress(bytes32 _enName, bytes32 _tokenSymbol) public only_registered(_enName) constant returns (address) {
+        DBNode nd = getDBNode( _enName);
         require(nd != DBNode(0));
 
-        string memory temp = PlatString.append(_user, "-", _tokenSymbol);
+        string memory temp = PlatString.append(_enName, "-", _tokenSymbol);
         address(getDBNode(PlatString.tobytes32(temp)));
     }
 
