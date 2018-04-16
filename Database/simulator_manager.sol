afcb005be198a6ca861750aea7bf7d273469ac76
Eric-Yao <norhustar@hotmail.com>
2018-04-16 21:16:38 +0800

    updated contract SimulatorManager
    updated struct SimultionRun; updated functions constructor, initSimulatorManager(), addSimulationRun(), checkSimulationRun(), conductReward(); removed function initParameters();

--- Database/simulator_manager.sol
+++ Database/simulator_manager.sol
@@ -6,33 +6,30 @@ pragma solidity ^0.4.18;
 
 import "./object.sol";
 
-contract contract SimulatorBase is Object {
+contract SimulatorBase is Object {
     function doesStarted() public only_delegate(1) constant returns (bool);
     function doesFinished() public only_delegate(1) constant returns (bool);
     function getSimulationProvider(bytes32 _name) public only_delegate(1) constant returns (bytes32);
     function getSimulationReceiver(bytes32 _name) public only_delegate(1) constant returns (bytes32);
-    function getSimulationReceiver(bytes32 _name) public only_delegate(1) constant returns (bytes32);
+    function getSimulationAgreement(bytes32 _name) public only_delegate(1) constant returns (bytes32);
     function getAddressInfo() public only_delegate(1) constant returns (address, address, address);
 }
 
 contract SimulatorManager is Object {
     struct SimultionRun {
-        bool exists_;
         bool rewarded_;
-        bool adr_;
-        bool probablity_;
+        address adr_;
+        uint probability_;
     }
     mapping(bytes32 => SimultionRun) private simulationRuns_;
+    mapping(bytes32 => bool) private simulationExists_;
 
-    uint private randSeed = 0;
+    uint private randSeed_= 0;
     address private bindedDB_;
     address private apiController_;
 
 
-    function SimulatorBase(bytes32 _name) public DBNode(_name) {
-    }
-
-    function initParameters() internal {
+    function SimulatorManager(bytes32 _name) public Object(_name) {
     }
 
     function initSimulatorManager(address _controller, address _database) public only_delegate(1)  {
@@ -52,28 +49,30 @@ contract SimulatorManager is Object {
     // Generates a random number
     // Original file at 
     // https://gist.github.com/alexvandesande/259b4ffb581493ec0a1c
-    function randGen(uint _min, uint _max) internal constant returns (uint256){
+    function randGen(uint _min, uint _max, uint _seed) internal constant returns (uint){
         require(_max > _min);
-        randSeed++;
-        uint randValue = uint(sha3(block.blockhash(block.number-1), randSeed ))%(_max - _min);
+        uint randValue = uint(keccak256(block.blockhash(block.number-1), _seed ))%(_max - _min);
 
         return (randValue + _min);
     }
 
-    function addSimulationRun(bytes32 _name, bytes32 _adr,  uint _proLevel /*1 : 100*/) public only_delegate(1) {
-        require(!simulationRuns_[_agreement].exists_);
+    function addSimulationRun(bytes32 _name, address _adr,  uint _proLevel /*1 : 100*/) public only_delegate(1) {
+        require(!simulationExists_[_name]);
+        uint prob = randGen(_proLevel, 100, randSeed_++);
 
-        simulationRuns_[_name] = SimultionRun(true, false, _adr, ranGen(_proLevel, 100));
+        simulationExists_[_name] = true;
+        simulationRuns_[_name] = SimultionRun(false, _adr, prob);
     }
 
     function checkSimulationRun(bytes32 _name) public only_delegate(1) constant returns (bool) {
-        require(simulationRuns_[_name].exists_);
+        require(simulationExists_[_name]);
         address adr = simulationRuns_[_name].adr_;
+        uint seed = 0;
 
         if (simulationRuns_[_name].rewarded_ == false) {
             if (SimulatorBase(adr).doesStarted()) {
                 if (SimulatorBase(adr).doesFinished()) {
-                    uint rand = randGen(0, 100);
+                    uint rand = randGen(0, 100, seed++);
                     if (rand < simulationRuns_[_name].probability_) {
                         return true;
                     }
@@ -83,12 +82,12 @@ contract SimulatorManager is Object {
         return false;
     }
  
-    function conductReward(bytes32 _name) {
-        require(simulationRuns_[_name].exists_);
-        address simulator = simulationRuns_[_name].adr_;
+    function conductReward(bytes32 _name) public only_delegate(1) constant {
+        require(simulationExists_[_name]);
+        address sim = simulationRuns_[_name].adr_;
         address agreement;
-        address provider
-        address receiver 
-        (agreement, provider, receiver) = SimulatorBase(adr).getAddressInfo();
+        address provider;
+        address receiver;
+        (agreement, provider, receiver) = SimulatorBase(sim).getAddressInfo();
     }
 }
