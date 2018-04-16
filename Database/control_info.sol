8f190c81519a43589a1285d6608665a2984da605
Eric-Yao <norhustar@hotmail.com>
2018-04-16 19:45:11 +0800

    updated contract ControlInfo

--- Database/control_info.sol
+++ Database/control_info.sol
@@ -7,58 +7,46 @@ pragma solidity ^0.4.18;
 import "./plat_string.sol";
 import "./object.sol";
 
-contract ControlInfo is Object {
-    enum ManagementType {REGISTERD, SUSPEND, ACTIVE}
-    struct UserInfo {
-        bool tag_;
-        bytes32 name_; 
-        bytes32 type_; 
-    }
-    
+contract ControlInfo is Object {   
     struct ParameterInfo {
-        mapping (bytes32 => string) value_;
         address nodeAdr_;
         address creator_; // ETH wallet address
+        mapping (bytes32 => string) value_;
     }
     
-    mapping(address => UserInfo) private users_;
-    mapping(bytes32 => ParameterInfo) private parameters_;
-
+    mapping(bytes32 => ParameterInfo) private nodeParameters_;
+    mapping(bytes32 => bool) private nodeExists_;
     
-    modifier only_registered(bytes32 _node) {
-        require(onlyRegisteredOrDelegated(_node, msg.sender)); 
+    modifier only_registered(bytes32 _nodeName) {
+        require(onlyRegisteredOrDelegated(_nodeName, msg.sender)); 
         _;
     }
 
     function ControlInfo() public {}
     
-    function checkAllowedUser(bytes32 _node) internal constant returns (bool);
+    function checkAllowedUser(bytes32 _nodeName) internal constant returns (bool);
 
-    function onlyRegisteredOrDelegated(bytes32 _node, address _sender) internal constant returns (bool) {
-        return (_sender == parameters_[_node].creator_ || isDelegate(_sender, 1)); 
+    function onlyRegisteredOrDelegated(bytes32 _nodeName, address _sender) internal constant returns (bool) {
+        return (_sender == nodeParameters_[_nodeName].creator_ || isDelegate(_sender, 1)); 
     }
 
     function _recordString(bytes32 _nodeName, bytes32 _parameter, string _value) public {
-        require(msg.sender == parameters_[_nodeName].nodeAdr_);
-        parameters_[_nodeName].value_[_parameter] = _value;
-    }
-
-    function checkNodeExist(bytes32 _name) internal constant returns (bool) { 
-        return (parameters_[_name].nodeAdr_ != 0); 
+        require(msg.sender == nodeParameters_[_nodeName].nodeAdr_);
+        nodeParameters_[_nodeName].value_[_parameter] = _value;
     }
 
     function registerNode(bytes32 _nodeName, address _nodeAdr, address _creator) internal {
-        require(!checkNodeExist(_nodeName));        
-        parameters_[_nodeName].nodeAdr_ = _nodeAdr;
-        parameters_[_nodeName].creator_ = _creator;
+        require(!nodeExists_[_nodeName]);        
+        nodeParameters_[_nodeName].nodeAdr_ = _nodeAdr;
+        nodeParameters_[_nodeName].creator_ = _creator;
     }
 
     function getControlInfoNodeAddress(bytes32 _nodeName)internal constant returns (address)  {
-        return parameters_[_nodeName].nodeAdr_;
+        return nodeParameters_[_nodeName].nodeAdr_;
     }
     
-    function getControlInfoParameterValue(bytes32 _node, bytes32 _parameter) internal constant returns (string) {
-        return parameters_[_node].value_[_parameter];
+    function getControlInfoParameterValue(bytes32 _nodeName, bytes32 _parameter) internal constant returns (string) {
+        return nodeParameters_[_nodeName].value_[_parameter];
     }
 }
 
