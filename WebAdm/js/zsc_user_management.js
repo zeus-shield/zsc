52614178dd4fd9f6a9feaa961e5b86ef575f7a77
Eric-Yao <norhustar@hotmail.com>
2018-04-17 00:32:36 +0800

    updated class ZSCUserManagement

--- WebAdm/js/zsc_user_management.js
+++ WebAdm/js/zsc_user_management.js
@@ -14,28 +14,7 @@ function ZSCUserMangement(adr, abi) {
     this.myControlApi = web3.eth.contract(abi).at(adr);
 }
 
-ZSCUserMangement.prototype.showHashResult = function(elementID, hash, func){
-    web3.eth.getTransactionReceipt(hash, 
-    function(error, result){ 
-        if(!error) {
-            var show;
-            if (result == null) {
-                show =  "(pending)" + hash ;
-                this.howHashResult(elementID, hash, func);
-            } else {
-                if (result.status == 0) {
-                    show = "(failure)" + hash;
-                } else {
-                    show = "(succeeded)" + hash ;
-                    func();
-                }
-            }
-            document.getElementById(elementID).innerText = show;
-        } else {
-            console.log("error: " + error);
-        }
-    });
-} 
+ZSCUserMangement.prototype = new ZSCJsBase();
 
 ZSCUserMangement.prototype.addUser = function(userNameId, hashId, func){
     var userName = document.getElementById(userNameId).value; 
