d5aed371acdb00a672ae59ae1be54c971c718090
Eric-Yao <norhustar@hotmail.com>
2018-04-17 18:17:15 +0800

    updated contract SimulatorManager

--- Database/simulator_manager.sol
+++ Database/simulator_manager.sol
@@ -6,8 +6,15 @@ pragma solidity ^0.4.18;
 
 import "./simulator_base.sol";
 
+contract WalletBase is Object {
+
+}
+
 contract SimulatorManager is Object {
-    mapping(bytes32 => address) private simulationRuns_;
+    uint private simulationNos_;
+    mapping(uint => address) private simulationRuns_;
+    mapping(bytes32 => uint) private simulationIndices_;
+    mapping(bytes32 => bool) private simulationExist_;
     mapping(bytes32 => bool) private rewarded_;
 
     address private bindedDB_;
@@ -15,6 +22,7 @@ contract SimulatorManager is Object {
 
 
     function SimulatorManager(bytes32 _name) public Object(_name) {
+        simulationNos_ = 0;
     }
 
     function initSimulatorManager(address _controller, address _database) public only_delegate(1)  {
@@ -32,18 +40,26 @@ contract SimulatorManager is Object {
     
     /*_proLev: 1 : 100*/
     function addSimulationRun(bytes32 _name, uint _proLevel, address _agreement, address _provider, address _receiver) public only_delegate(1) {
-        require(simulationRuns_[_name] == address(0));
+        require(simulationExist_[_name] == address(0));
 
         address adr = new SimulatorBase(_name);
         SimulatorBase(adr).startSimulation(_proLevel, _agreement, _provider, _receiver);
-        simulationRuns_[_name] = adr;
+        simulationExist_[_name] = bool;
+        simulationIndices_[_name] = simulationNos_;
+        simulationRuns_[simulationNos_] = adr;
+        simulationNos_++;
         rewarded_[_name] = false;
     }
 
     function checkSimulationRun(bytes32 _name) public only_delegate(1) constant returns (bool) {
-        require(simulationRuns_[_name] != address(0));
+        require(simulationExist_[_name] != address(0));
+        return checkSimulationRunByIndex(simulationIndices_[_name]);
+    }
 
-        address adr = simulationRuns_[_name];
+    function checkSimulationRunByIndex(uint _index) internal constant returns (bool) {
+        require(_index < simulationNos_);
+
+        address adr = simulationRuns_[_index];
 
         if (rewarded_[_name] == false) {
             if (SimulatorBase(adr).doesStarted()) {
@@ -55,13 +71,28 @@ contract SimulatorManager is Object {
         return false;
     }
  
-    function conductReward(bytes32 _name) public only_delegate(1) constant {
-        require(simulationRuns_[_name] != address(0));
+    function conductReward(bytes32 _name) public only_delegate(1) constant returns (bool) {
+        require(simulationExist_[_name] != address(0));
+        return conductRewardByIndex(simulationIndices_[_name]);
+    }
 
-        address sim = simulationRuns_[_name];
+    function conductRewardByIndex(uint _index) internal constant returns (bool) {
+        require(_index < simulationNos_);
+
+        address sim = simulationRuns_[_index];
         address agreement;
         address provider;
         address receiver;
         (agreement, provider, receiver) = SimulatorBase(sim).getAddressInfo();
+        return true;
     }
+
+    function runSimulation() returns(bool res) internal {
+        for (uint i = 0; i < simulationNos_; ++i) {
+            if (checkSimulationRunByIndex(i)) {
+                conductReward(i);
+            }
+        }
+    }
+    
 }
