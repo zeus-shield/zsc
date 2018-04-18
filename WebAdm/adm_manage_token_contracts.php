6cb0fa5d1c78933b458b8dcb990a154b777caa26
Eric-Yao <norhustar@hotmail.com>
2018-04-18 19:22:32 +0800

    updated html content

--- WebAdm/adm_manage_token_contracts.php
+++ WebAdm/adm_manage_token_contracts.php
@@ -14,7 +14,14 @@ echo $htmlModules->loadScriptFiles();
 ?>
 <script type="text/javascript">
     var web3 = setupWeb3js(false);
- 
+    var ControlApisAdvAdr = "<?php echo $htmlModules->readModuleAddress('ControlApisAdv')?>";
+    var walletManager = new ZSCWalletMangement(ControlApisAdvAdr, cC_getContractAbi('ControlApisAdv'));
+
+    function addTokenContract(nameId, symbolId, decimalsId, adrId, hashId) {
+        walletManager.addTokenContractInfo(nameId, symbolId, decimalsId, adrId, hashId, function() {
+        	window.location.reload(true);
+        });
+    }
 
 </script>
 </head>
@@ -31,11 +38,17 @@ echo $htmlModules->loadScriptFiles();
         <text>Symbol</text>  <input type="text" id="TokenSymbol" value="test"></input> <br>
         <text>Decimals</text>  <input type="text" id="TokenDecimals" value="test"></input> <br>
         <text>Address</text>  <input type="text" id="TokenAddress" value="test"></input> <br>
-
+        <button type="button" onClick="addTokenContract('TokenName', 'TokenSymbol', 'TokenDecimals', 'TokenAddress')">Add a token contract</button> <br>
+        <text id="AddUserHash"></text>
     </div>
 
     <div class="well" id="WalletManagement"> </div>
 
+<script type="text/javascript">
+	walletManager.loadErcTokenContracts(function(){
+		walletManager.loadWalletManagementHtml("WalletManagement");
+	});
+</script>
 
 </body>
 </html>
