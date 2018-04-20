cd5dea1706e308ccbdc2647a9ee6634853a21462
Eric-Yao <norhustar@hotmail.com>
2018-04-20 14:44:38 +0800

    updated contract DBTemplate

--- Database/db_template.sol
+++ Database/db_template.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.4.18;
 import "./db_entity.sol";
 
 contract DBTemplate is DBEntity {
-    mapping(address => bool) private fundamentalParass_;
+    mapping(address => bool) private fundamentalParas_;
     bool private addedProvider_;
     
     function DBTemplate(bytes32 _name) public DBEntity(_name) {
@@ -23,24 +23,25 @@ contract DBTemplate is DBEntity {
         addParameter("duration");
         addParameter("provider");
 
-        fundamentalParass_["automatic"]    = true;
-        fundamentalParass_["duration"]     = true;
-        fundamentalParass_["walletSymbol"] = true;
-        fundamentalParass_["price"]        = true;
-        fundamentalParass_["fefund (%)"]   = true;
-        fundamentalParass_["duration"]     = true;
-        fundamentalParass_["provider"]     = true;
+        fundamentalParas_["automatic"]    = true;
+        fundamentalParas_["duration"]     = true;
+        fundamentalParas_["walletSymbol"] = true;
+        fundamentalParas_["price"]        = true;
+        fundamentalParas_["fefund (%)"]   = true;
+        fundamentalParas_["duration"]     = true;
+        fundamentalParas_["provider"]     = true;
     }
 
     function setParameter(bytes32 _parameter, string _value) public only_delegate(1) returns (bool) {
         if (numChildren() > 0) return false; 
 
-        if (_parameter = "provider") return false;
-        
         if (addedProvider_ == false) {
             addedProvider_ = true;
-            super.setParameter("provider", name());
+            return super.setParameter("provider", name());
         }
+
+        if (_parameter == "provider") return false;
+
         return super.setParameter(_parameter, _value);
     }
 
