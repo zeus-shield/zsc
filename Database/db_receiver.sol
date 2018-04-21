5f52b9b0641089bc8d1af753236cfed27095d13c
Eric-Yao <norhustar@hotmail.com>
2018-04-21 17:24:52 +0800

    updated contract DBReceiver
    updated variables; added functions addChild(), numChildren(); getChildByIndex();

--- Database/db_receiver.sol
+++ Database/db_receiver.sol
@@ -6,6 +6,10 @@ pragma solidity ^0.4.18;
 import "./db_user.sol";
 
 contract DBReceiver is DBUser {
+    uint agrNos_;
+    mapping(uint => address) private agreements_;
+    mapping(address => bool) private agreementExist_;
+
     // Constructor
     function DBReceiver(bytes32 _name) public DBUser(_name) {
         setNodeType("receiver"); 
@@ -20,5 +24,20 @@ contract DBReceiver is DBUser {
         addParameter("userBirthday");
         addParameter("userIdentification");
         addParameter("userResidentialAddress");
+    }
+
+    function addChild(address _adr) public only_delegate(1) returns (bool) {
+        require(!agreementExist_[_adr]);
+        agreementExist_[_adr] = true;
+        agreements_[agrNos_] = _adr;
+    } 
+
+    function numChildren() public only_delegate(1) returns (uint) {
+        return agrNos_;
+    }
+
+    function getChildByIndex(uint _index) public only_delegate(1) returns (address) {
+        require(_index < agrNos_);
+        return agreements_[_index];
     }
 }
