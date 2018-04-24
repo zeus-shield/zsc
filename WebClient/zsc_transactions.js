7e9b1b9ec9e614b9ffb84cbc98e858395d07f295
Eric-Yao <norhustar@hotmail.com>
2018-04-24 12:24:26 +0800

    updated class ZSCTransactions
    added methods loadTransactions(), numTransactions(), loadTransactionInfoByIndex(); updated other methods

--- WebClient/zsc_transactions.js
+++ WebClient/zsc_transactions.js
@@ -9,27 +9,64 @@ function ZSCTransactions(nm, abi, adr) {
     this.tokenSymbol;
     this.timeMoments = [];
     this.inputTags = [];
+    this.txHash = [];
     this.amounts = [];
     this.senders = [];
     this.receivers = [];
     this.myControlApi = web3.eth.contract(abi).at(adr);
 }
-ZSCWallet.prototype.getUserName = function() {return this.userName;}
+ZSCTransactions.prototype.getUserName = function() {return this.userName;}
 
-ZSCWallet.prototype.setTokenSymbol = function(symbol) {return this.tokenSymbol = symbol;}
+ZSCTransactions.prototype.setTokenSymbol = function(symbol) {return this.tokenSymbol = symbol;}
 
-ZSCWallet.prototype.getTokenSymbol = function() { return this.tokenSymbol;}
+ZSCTransactions.prototype.getTokenSymbol = function() { return this.tokenSymbol;}
 
-ZSCWallet.prototype.getTimeMoment = function(index) { return this.timeMoments[index];}
+ZSCTransactions.prototype.getTimeMoment = function(index) { return this.timeMoments[index];}
 
-ZSCWallet.prototype.getAmount = function(index) { return this.amounts[index];}
+ZSCTransactions.prototype.getAmount = function(index) { return this.amounts[index];}
 
-ZSCWallet.prototype.getSender = function(index) { return this.senders[index];}
+ZSCTransactions.prototype.getSender = function(index) { return this.senders[index];}
 
-ZSCWallet.prototype.getReceiver = function(index) { return this.receivers[index];}
+ZSCTransactions.prototype.getReceiver = function(index) { return this.receivers[index];}
 
-ZSCWallet.prototype.getInputTag = function(index) { return this.inputTags[index];}
+ZSCTransactions.prototype.getInputTag = function(index) { return this.inputTags[index];}
 
-ZSCWallet.prototype.getTransactionNos = function() { return this.nos; }
+ZSCTransactions.prototype.getTransactionNos = function() { return this.nos; }
 
+ZSCTransactions.prototype.loadTransactions = function(func) {
+    this.numTransactions(function() {
+        for (var i = 0; i < this.walletNos; ++i) {
+            this.loadTransactionInfoByIndex(i, function(index){
+                if (indx == this.walletNos - 1) {
+                    func();
+                }
+            });
+        }
+    });
+}
 
+ZSCTransactions.prototype.numTransactions = function(func) {
+    this.myControlApi.numUserTransactions(this.userName, this.tokenSymbol, 
+        {from: this.account, gas: 9000000},
+        function(error, result){ 
+            if(!error) {
+                this.walletNos = result;
+                func();
+            } else {
+                console.log("error: " + error);
+            }
+        });
+}
+
+ZSCTransactions.prototype.loadTransactionInfoByIndex = function(index, func) {
+    this.myControlApi.getUserTransactionByIndex(this.userName, this.tokenSymbol, index,
+        {from: this.account, gas: 9000000},
+        function(error, result){ 
+            if(!error) {
+                //parserTransactionInfoByIndex(result, index)
+                func(index);
+            } else {
+                console.log("error: " + error);
+            }
+        });
+}
