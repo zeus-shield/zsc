e461ecbe7a633e0aa1278d8c8b70a31af1d7768e
Eric-Yao <norhustar@hotmail.com>
2018-04-24 10:22:33 +0800

    updated contract FactoryBase

--- Database/factory_base.sol
+++ Database/factory_base.sol
@@ -6,29 +6,19 @@ pragma solidity ^0.4.18;
 
 import "./db_entity.sol";
 
-contract DBDatabase is Object { 
-    function getNode(bytes32 _name) public only_delegate(1) constant returns (address);
-}
-
 contract FactoryBase is Object {
     address private bindedDB_;
     address private apiController_;
 
-    uint private nodeNos_;
-    mapping(uint => bytes32) private nodeNames_;
-
     function FactoryBase(bytes32 _name) public Object(_name) {
-        nodeNos_ = 0;
     }
 
     function setupFactoryRoot() internal;
 
     function createNode(bytes32 _nodeName, address _parent, address _creator) public returns (address);
 
-    function getBindedApiController() public only_delegate(1) constant returns (address) { return apiController_;}
+    function getBindedDB() internal only_delegate(1) constant returns (address) { return bindedDB_;}
 
-    function getBindedDB() public only_delegate(1) constant returns (address) { return bindedDB_;}
-    
     function initFactory(address _controller, address _database) public only_delegate(1)  {
         require(_database != 0);
         bindedDB_ = _database;
@@ -43,28 +33,4 @@ contract FactoryBase is Object {
         setupFactoryRoot();
     }
 
-    function addNodeName(bytes32 _name) internal {
-        nodeNames_[nodeNos_] = _name;
-        nodeNos_++;
-    }
-
-    function removeNodeName(bytes32 _name) internal {
-        uint index = 0;
-        for (uint i = 0; i < nodeNos_; ++i) {
-            if (_name == nodeNames_[i]) {
-                index = 0;
-                break;
-            }
-        }
-        delete nodeNames_[index];
-        nodeNos_--;
-    }
-
-    function numNodeNames() public only_delegate(1) constant returns (uint) {
-        return nodeNos_;
-    }
-
-    function getNodeNameByIndex(uint _index) public only_delegate(1) constant returns (bytes32) {
-        return nodeNames_[_index];
-    }
 }
