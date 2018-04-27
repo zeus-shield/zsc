bfc787dec1674aeeddcf0934d058ac428c33deff
Eric-Yao <norhustar@hotmail.com>
2018-04-27 15:59:25 +0800

    updated contract DBEntity

--- Database/db_item.sol
+++ Database/db_item.sol
@@ -1,14 +1,14 @@
 /*
 Copyright (c) 2018, ZSC Dev Team
-2018-02-09: v0.01
 */
 
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.21;
+
 import "./db_entity.sol";
 
 contract DBItem is DBEntity {   
     // Constructor
-    function DBItem(bytes32 _name) public DBEntity(_name) {
+    constructor(bytes32 _name) public DBEntity(_name) {
     	setEntityType("item");
     }
     
