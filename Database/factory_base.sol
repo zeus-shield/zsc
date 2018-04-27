eb270a7c1ce1028c6fb9b1c65e850fd9c39cd0ff
Eric-Yao <norhustar@hotmail.com>
2018-04-27 15:56:11 +0800

    updated contract FactoryBase

--- Database/factory_base.sol
+++ Database/factory_base.sol
@@ -1,8 +1,8 @@
 /*
-Copyright (c) 2018 ZSC Dev Team
+Copyright (c) 2018, ZSC Dev Team
 */
 
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.21;
 
 import "./db_entity.sol";
 
@@ -10,16 +10,18 @@ contract FactoryBase is Object {
     address private bindedDB_;
     address private apiController_;
 
-    function FactoryBase(bytes32 _name) public Object(_name) {
+    constructor(bytes32 _name) public Object(_name) {
     }
 
     function setupFactoryRoot() internal;
 
     function createNode(bytes32 _nodeName, address _parent, address _creator) public returns (address);
 
-    function getBindedDB() internal only_delegate(1) constant returns (address) { return bindedDB_;}
+    function getBindedDB() internal constant returns (address) { return bindedDB_;}
 
-    function initFactory(address _controller, address _database) public only_delegate(1)  {
+    function initFactory(address _controller, address _database) public {
+        checkDelegate(msg.sender, 1);
+        
         require(_database != 0);
         bindedDB_ = _database;
 
