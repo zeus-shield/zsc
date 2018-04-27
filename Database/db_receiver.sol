53db7ca463e3643a80908956d013d76099a4b835
Eric-Yao <norhustar@hotmail.com>
2018-04-27 15:55:27 +0800

    updated contract DBReceiver

--- Database/db_receiver.sol
+++ Database/db_receiver.sol
@@ -1,8 +1,9 @@
 /*
-Copyright (c) 2018 ZSC Dev, Zeusshield Blockchain Technology Development Co., Ltd
+Copyright (c) 2018, ZSC Dev Team
 */
 
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.21;
+
 import "./db_user.sol";
 
 contract DBReceiver is DBUser {
@@ -11,7 +12,7 @@ contract DBReceiver is DBUser {
     mapping(address => bool) private agreementExist_;
 
     // Constructor
-    function DBReceiver(bytes32 _name) public DBUser(_name) {
+    constructor(bytes32 _name) public DBUser(_name) {
         setNodeType("receiver"); 
     }
 
@@ -26,17 +27,23 @@ contract DBReceiver is DBUser {
         addParameter("userResidentialAddress");
     }
 
-    function addChild(address _adr) public only_delegate(1) returns (bool) {
+    function addChild(address _adr) public returns (bool) {
+        checkDelegate(msg.sender, 1);
+
         require(!agreementExist_[_adr]);
         agreementExist_[_adr] = true;
         agreements_[agrNos_] = _adr;
     } 
 
-    function numChildren() public only_delegate(1) returns (uint) {
+    function numChildren() public returns (uint) {
+        checkDelegate(msg.sender, 1);
+
         return agrNos_;
     }
 
-    function getChildByIndex(uint _index) public only_delegate(1) returns (address) {
+    function getChildByIndex(uint _index) public returns (address) {
+        checkDelegate(msg.sender, 1);
+
         require(_index < agrNos_);
         return agreements_[_index];
     }
