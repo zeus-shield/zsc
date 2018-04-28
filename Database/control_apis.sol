bd59cf1d4f08f62a3ff2ff0adfb2217cb770fd0b
Eric-Yao <norhustar@hotmail.com>
2018-04-28 19:36:58 +0800

    updated api functions

--- Database/control_apis.sol
+++ Database/control_apis.sol
@@ -17,41 +17,50 @@ contract ControlApis is ControlBase {
     /// @dev Set the zsc adm address
     /// @param _adm The address of the zsc adm 
     /// @param _db The address of the database 
-    function setSystemModules(address _adm, address _db, address _walletGM, address _simulatorGM, address _pos, address _zscToken) public only_delegate(1) {
+    function setSystemModules(address _adm, address _db, address _walletGM, address _simulatorGM, address _pos, address _zscToken) public {
+        checkDelegate(msg.sender, 1);
         setSystemModuleAdrs(_adm, _db, _walletGM, _simulatorGM, _pos, _zscToken);
     }
 
     /// @dev Add the database factory of managing the elements
     /// @param _typeInUint The type of the database factory
     /// @param _adr The address of the database factory
-    function addElementFactory(uint _typeInUint, address _adr) public only_delegate(1) {
+    function addElementFactory(uint _typeInUint, address _adr) public {
+        checkDelegate(msg.sender, 1);
         addFactoryAdr(mapType(_typeInUint), _adr);
     }
 
-    function registerErc20Token(bytes32 _symbol, bytes32 _name, uint _decimals, address _tokenAdr) public only_delegate(1) returns (bool) {
+    function registerErc20Token(bytes32 _symbol, bytes32 _name, uint _decimals, address _tokenAdr) public returns (bool) {
+        checkDelegate(msg.sender, 1);
         return manageErc20TokenContract(true, _name, _symbol, _decimals, _tokenAdr);
     }
 
-    function removeErc20Token(bytes32 _symbol) public only_delegate(1) returns (bool) {
+    function removeErc20Token(bytes32 _symbol) public returns (bool) {
+        checkDelegate(msg.sender, 1);
         return manageErc20TokenContract(false, 0, _symbol, 0, 0);
     }
 
-    function runSimulationTest(uint _steps) public only_delegate(1) {
+    function runSimulationTest(uint _steps) public {
+        checkDelegate(msg.sender, 1);
         getSimulatorManager().runSimulation(_steps);
     }
 
-    function getTokenContractInfoByIndex(uint _index) public only_delegate(1) constant returns (string) {
+    function getTokenContractInfoByIndex(uint _index) public constant returns (string) {
+        checkDelegate(msg.sender, 1);
         return prepareTokenContractInfoByIndex(_index);
     }
 
     /// @dev Get the number of elements of the database
-    function numElements() public only_delegate(1) constant returns (uint) { 
+    function numElements() public constant returns (uint) { 
+        checkDelegate(msg.sender, 1);
         return getDBDatabase().numNodes(); 
     }
     
     /// @dev Get the element name by the index
     /// @param _index The index of the element in the database
-    function getElementNameByIndex(uint _index) public only_delegate(1) constant returns (bytes32) { 
+    function getElementNameByIndex(uint _index) public constant returns (bytes32) { 
+        checkDelegate(msg.sender, 1);
+
         address nd = getDBDatabase().getNodeByIndex(_index);
         require(nd != address(0));
         return Object(nd).name();
@@ -60,7 +69,11 @@ contract ControlApis is ControlBase {
 
     /// @dev Check the element wheather or not existing
     /// @param _enName The name of the element to be checked
-    function doesElementExist(bytes32 _userName, bytes32 _enName) public only_registered(_userName) only_matched(_enName) constant returns (bool) {
+    function doesElementExist(bytes32 _userName, bytes32 _enName) public constant returns (bool) {
+        checkRegistered(_userName, msg.sender);
+        checkMatched(_userName, msg.sender);
+        checkDelegate(msg.sender, 1);
+
         return (getDBNode(_enName) != DBNode(0));
     }
 
@@ -69,7 +82,9 @@ contract ControlApis is ControlBase {
     /// @param _enName The name of the element belonging to the user
     /// @param _extraInfo The extra information
     /// @param _extraAdr The extra address
-    function createElement(bytes32 _userName, uint _typeInUint, bytes32 _enName, bytes32 _extraInfo, address _extraAdr) public only_registered(_userName) returns (address) {
+    function createElement(bytes32 _userName, uint _typeInUint, bytes32 _enName, bytes32 _extraInfo, address _extraAdr) public returns (address) {
+        checkRegistered(_userName, msg.sender);
+
         address creatorAdr;
         if (isDelegate(msg.sender, 1)) {
             creatorAdr = _extraAdr;
@@ -80,7 +95,9 @@ contract ControlApis is ControlBase {
         return createFactoryNode(mapType(_typeInUint), _enName, _extraInfo, creatorAdr);
     }
 
-    function enableElementWallet(bytes32 _userName, bytes32 _tokeSymbol, address _extraAdr) public only_registered(_userName) returns (address) {
+    function enableElementWallet(bytes32 _userName, bytes32 _tokeSymbol, address _extraAdr) public returns (address) {
+        checkRegistered(_userName, msg.sender);
+
         address creatorAdr;
         if (isDelegate(msg.sender, 1)) {
             creatorAdr = _extraAdr;
@@ -99,14 +116,19 @@ contract ControlApis is ControlBase {
 
     /// @dev Get the element by its address
     /// @param _adr The address of the existing element
-    function getElementNameByAddress(bytes32 _userName, address _adr) public only_registered(_userName) constant returns (bytes32) {
+    function getElementNameByAddress(bytes32 _userName, address _adr) public constant returns (bytes32) {
+        checkRegistered(_userName, msg.sender);
+
         require (getDBDatabase().checkeNodeByAddress(_adr));
         return Object(_adr).name();
     }
 
     /// @dev Get the type of an element
     /// @param _enName The name of the element belonging to the user
-    function getElementType(bytes32 _userName, bytes32 _enName) public only_registered(_userName) only_matched(_enName) constant returns (bytes32) {
+    function getElementType(bytes32 _userName, bytes32 _enName) public constant returns (bytes32) {
+        checkRegistered(_userName, msg.sender);
+        checkMatched(_enName, msg.sender);
+
         DBNode nd = getDBNode( _enName);
         require(nd != DBNode(0));
         return nd.getNodeType();
@@ -116,7 +138,10 @@ contract ControlApis is ControlBase {
     /// @dev Add a paramter to an element
     /// @param _enName The name of the existing element
     /// @param _parameter The name of the added parameter
-    function addElementParameter(bytes32 _userName, bytes32 _enName, bytes32 _parameter) public only_registered(_userName) only_matched(_enName) returns (bool) {
+    function addElementParameter(bytes32 _userName, bytes32 _enName, bytes32 _parameter) public returns (bool) {
+        checkRegistered(_userName, msg.sender);
+        checkMatched(_enName, msg.sender);
+
         return operateNodeParameter("add", _enName, _parameter, "");
     }
 
@@ -124,26 +149,38 @@ contract ControlApis is ControlBase {
     /// @param _enName The name of the element
     /// @param _parameter The name of the existing parameter
     /// @param _value The parameter value
-    function setElementParameter(bytes32 _userName, bytes32 _enName, bytes32 _parameter, string _value) public only_registered(_userName) only_matched(_enName) returns (bool) {
+    function setElementParameter(bytes32 _userName, bytes32 _enName, bytes32 _parameter, string _value) public returns (bool) {
+        checkRegistered(_userName, msg.sender);
+        checkMatched(_enName, msg.sender);
+
         return operateNodeParameter("set", _enName, _parameter, _value);
     }
 
     /// @dev Get the value of a paramter of an element
     /// @param _enName The name of the element
     /// @param _parameter The name of the existing parameter
-    function getElementParameter(bytes32 _userName, bytes32 _enName, bytes32 _parameter) public only_registered(_userName) only_matched(_enName) constant returns (string) {
+    function getElementParameter(bytes32 _userName, bytes32 _enName, bytes32 _parameter) public constant returns (string) {
+        checkRegistered(_userName, msg.sender);
+        checkMatched(_enName, msg.sender);
+
         return getControlInfoParameterValue(_enName, _parameter);
     }
 
     /// @dev Get the address of the element 
     /// @param _enName The name of the element
-    function getElementAddress(bytes32 _userName, bytes32 _enName) public only_registered(_userName) only_matched(_enName) constant returns (address) {
+    function getElementAddress(bytes32 _userName, bytes32 _enName) public constant returns (address) {
+        checkRegistered(_userName, msg.sender);
+        checkMatched(_enName, msg.sender);
+
         return address(getDBNode(_enName));
     }
 
     /// @dev Get the eth balance of the element
     /// @param _enName The name of the element
-    function getElementBalance(bytes32 _userName, bytes32 _enName, bytes32 _symbol, bool _locked) public only_registered(_userName) only_matched(_enName) constant returns (uint256) {
+    function getElementBalance(bytes32 _userName, bytes32 _enName, bytes32 _symbol, bool _locked) public constant returns (uint256) {
+        checkRegistered(_userName, msg.sender);
+        checkMatched(_enName, msg.sender);
+
         string memory str = PlatString.append(_enName, "-", _symbol);
         bytes32 walletName = PlatString.tobytes32(str);
         require(getDBNode(walletName) != DBNode(0));
@@ -153,7 +190,10 @@ contract ControlApis is ControlBase {
 
     /// @dev Get the number of paramters of an element
     /// @param _enName The name of the existing element
-    function numElementParameters(bytes32 _userName, bytes32 _enName) public only_registered(_userName) only_matched(_enName) constant returns (uint) {
+    function numElementParameters(bytes32 _userName, bytes32 _enName) public constant returns (uint) {
+        checkRegistered(_userName, msg.sender);
+        checkMatched(_enName, msg.sender);
+
         return  getDBNode(_enName).numParameters();
     }
 
@@ -166,38 +206,55 @@ contract ControlApis is ControlBase {
             var para = getNodeParameterNameByIndex("test", 0);
         }
     */
-    function getElementParameterNameByIndex(bytes32 _userName, bytes32 _enName, uint _index) public only_registered(_userName) only_matched(_enName) constant returns (bytes32) {
+    function getElementParameterNameByIndex(bytes32 _userName, bytes32 _enName, uint _index) public constant returns (bytes32) {
+        checkRegistered(_userName, msg.sender);
+        checkMatched(_enName, msg.sender);
+
         return  getDBNode(_enName).getParameterNameByIndex(_index);
     }
 
     /// @dev Transfer ETH from a user element to the destination address
     /// @param _dest The destination address
     /// @param _amount The amount of ETH to be transferred
-    function elementTransferValue(bytes32 _userName, address _src, address _dest, uint256 _amount) public only_registered(_userName) returns (uint) {
+    function elementTransferValue(bytes32 _userName, address _src, address _dest, uint256 _amount) public returns (uint) {
+        checkRegistered(_userName, msg.sender);
+
         return  DBNode(_src).executeTransaction(_dest, _amount, "null");
     }
 
-    function elementInformTransfer(bytes32 _userName, bytes32 _enName, address _dest, uint256 _amount) public only_registered(_userName) only_matched(_enName) returns (bool) {
+    function elementInformTransfer(bytes32 _userName, bytes32 _enName, address _dest, uint256 _amount) public returns (bool) {
+        checkRegistered(_userName, msg.sender);
+        checkMatched(_enName, msg.sender);
+
         return  conductInformTransaction(_enName, _dest, _amount);
     }
 
     /// @dev Announce an insurance agreement by a provider
     /// @param _agrName The agreement name
-    function publishAgreement(bytes32 _userName, bytes32 _agrName) public only_registered(_userName) only_matched(_agrName) returns (uint) {
+    function publishAgreement(bytes32 _userName, bytes32 _agrName) public returns (uint) {
+        checkRegistered(_userName, msg.sender);
+        checkMatched(_agrName, msg.sender);
+
         return conductPublishAgreement(_userName, _agrName, msg.sender);
     }
 
-    function numTemplates(bytes32 _userName) public only_registered(_userName) constant returns (uint) {
+    function numTemplates(bytes32 _userName) public constant returns (uint) {
+        checkRegistered(_userName, msg.sender);
+
         address adr = getDBNode(_userName).getHandler("template");
         return DBNode(adr).numChildren();
     }
 
-    function getTemplateNameByIndex(bytes32 _userName, uint _index) public only_registered(_userName) constant returns (bytes32) {
+    function getTemplateNameByIndex(bytes32 _userName, uint _index) public constant returns (bytes32) {
+        checkRegistered(_userName, msg.sender);
+
         address adr = getDBNode(_userName).getChildByIndex(_index);
         return Object(adr).name();
     }
 
-    function numAgreements(bytes32 _userName) public only_registered(_userName) constant returns (uint) {
+    function numAgreements(bytes32 _userName) public constant returns (uint) {
+        checkRegistered(_userName, msg.sender);
+
         bytes32 userType = getDBNode(_userName).getNodeType();
         address userAdr = address(getDBNode(_userName));
         address agrAdr; 
@@ -212,7 +269,9 @@ contract ControlApis is ControlBase {
         }
     }
 
-    function getAgreementNameByIndex(bytes32 _userName, uint _index) public only_registered(_userName) constant returns (bytes32) {
+    function getAgreementNameByIndex(bytes32 _userName, uint _index) public constant returns (bytes32) {
+        checkRegistered(_userName, msg.sender);
+
         bytes32 userType = getDBNode(_userName).getNodeType();
         address userAdr = address(getDBNode(_userName));
         address agrAdr; 
@@ -228,7 +287,9 @@ contract ControlApis is ControlBase {
         }
     }
 
-    function deleteAgreementByIndex(bytes32 _userName, uint _index) public only_registered(_userName) returns (bool) {
+    function deleteAgreementByIndex(bytes32 _userName, uint _index) public returns (bool) {
+        checkRegistered(_userName, msg.sender);
+
         address adr = getDBNode(_userName).getChildByIndex(_index);
         return deleteAgreement(Object(adr).name());
     }
@@ -236,20 +297,30 @@ contract ControlApis is ControlBase {
     /// @dev Buy an insurance agreement from a provider
     /// @param _userName The receiver name
     /// @param _agrName The agreement name
-    function purchaseAgreement(bytes32 _userName, bytes32 _agrName) public only_registered(_userName) returns (uint) {
+    function purchaseAgreement(bytes32 _userName, bytes32 _agrName) public returns (uint) {
+        checkRegistered(_userName, msg.sender);
+
+        bool ret = preparePurchaseAgreement(_userName, _agrName);
+        require(ret);        
         return conductPurchaseAgreement(_userName, _agrName); 
     }
 
 
-    function numRegisteredErc20Tokens(bytes32 _userName) public only_registered(_userName) constant returns (uint) {
+    function numRegisteredErc20Tokens(bytes32 _userName) public constant returns (uint) {
+        checkRegistered(_userName, msg.sender);
+
         return getWalletManager().numTokenContracts() + 1;
     }
 
-    function getTokenBalanceInfoByIndex(bytes32 _userName, uint _index) public only_registered(_userName) constant returns (string) {
+    function getTokenBalanceInfoByIndex(bytes32 _userName, uint _index) public constant returns (string) {
+        checkRegistered(_userName, msg.sender);
+
         return prepareTokenBalanceInfoByIndex(_userName, _index);
     }
 
-    function getUserWalletAddress(bytes32 _userName, bytes32 _tokenSymbol) public only_registered(_userName) constant returns (address) {
+    function getUserWalletAddress(bytes32 _userName, bytes32 _tokenSymbol) public constant returns (address) {
+        checkRegistered(_userName, msg.sender);
+
         DBNode nd = getDBNode( _userName);
         require(nd != DBNode(0));
 
@@ -257,7 +328,9 @@ contract ControlApis is ControlBase {
         return address(getDBNode(PlatString.tobytes32(temp)));
     }
 
-    function numUserTransactions(bytes32 _userName, bytes32 _tokenSymbol) public only_registered(_userName) constant returns (uint) {
+    function numUserTransactions(bytes32 _userName, bytes32 _tokenSymbol) public constant returns (uint) {
+        checkRegistered(_userName, msg.sender);
+
         DBNode nd = getDBNode( _userName);
         require(nd != DBNode(0));
 
@@ -265,7 +338,9 @@ contract ControlApis is ControlBase {
         return getDBNode(PlatString.tobytes32(temp)).numTransactions();
     }
 
-    function getUserTransactionByIndex(bytes32 _userName, bytes32 _tokenSymbol, uint _index) public only_registered(_userName) constant returns (string) {
+    function getUserTransactionByIndex(bytes32 _userName, bytes32 _tokenSymbol, uint _index) public constant returns (string) {
+        checkRegistered(_userName, msg.sender);
+
         DBNode nd = getDBNode( _userName);
         require(nd != DBNode(0));
 
