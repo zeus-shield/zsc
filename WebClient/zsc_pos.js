9c9915a6fb60f4c5a241fda3b15f70f7e0476e90
Eric-Yao <norhustar@hotmail.com>
2018-05-01 21:24:44 +0800

    updated class ZSCPos

--- WebClient/zsc_pos.js
+++ WebClient/zsc_pos.js
@@ -5,18 +5,29 @@ Copyright (c) 2018 ZSC Dev Team
 function ZSCPos(nm, abi, adr) {
     this.userName = nm;
     this.rewardNos;
-    this.rewardTime = [];
-    this.rewardAmount = [];
+    this.rewardTimes = [];
+    this.rewardAmounts = [];
+    this.spUsedNos;
+    this.spUsedTimes = [];
+    this.spUsedAmounts = [];
     this.myControlApi = web3.eth.contract(abi).at(adr);
 }
+
+ZSCPos.prototype = new ZSCClient();
 
 ZSCPos.prototype.getUserName = function() {return this.userName;}
 
 ZSCPos.prototype.getRewardNos = function() {return this.rewardNos;}
 
-ZSCPos.prototype.getRewardTime = function(index) {return this.rewardTime[index];}
+ZSCPos.prototype.getSPUsedNos = function() {return this.spUsedNos;}
 
-ZSCPos.prototype.getRewardAmount = function(index) {return this.rewardAmount[index];}
+ZSCPos.prototype.getRewardTime = function(index) {return this.rewardTimes[index];}
+
+ZSCPos.prototype.getRewardAmount = function(index) {return this.rewardAmounts[index];}
+
+ZSCPos.prototype.getSPUsedTime = function(index) {return this.spUsedTimes[index];}
+
+ZSCPos.prototype.getSPUsedAmount = function(index) {return this.spUsedAmounts[index];}
 
 ZSCPos.prototype.loadRewardHistory = function(func) {
     this.numRewards(function() {
@@ -30,9 +41,26 @@ ZSCPos.prototype.loadRewardHistory = function(func) {
     });
 }
 
-ZSCTemplate.prototype.numRewards = function(func) {
+ZSCPos.prototype.numRewards = function(func) {
    
 }
 
-ZSCTemplate.prototype.getRewardInfo = function(index, func) {
+ZSCPos.prototype.getRewardInfo = function(index, func) {
 }
+
+ZSCPos.prototype.loadSPUsedHistory = function(func) {
+    this.numSPUsed(function() {
+        for (var i = 0; i < this.spUsedNos; ++i) {
+            this.getSPUsedInfo(i, function(index){
+                if (indx == this.spUsedNos - 1) {
+                    func();
+                }
+            });
+        }
+    });
+}
+
+ZSCPos.prototype.getSPUsedInfo = function(index, func) {
+}
+
+
