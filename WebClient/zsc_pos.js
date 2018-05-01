7e75e21c226542fc1d21a9f57aeb1813cdeb5fc0
Eric-Yao <norhustar@hotmail.com>
2018-05-01 22:04:07 +0800

    updated class ZSCPos

--- WebClient/zsc_pos.js
+++ WebClient/zsc_pos.js
@@ -30,9 +30,9 @@ ZSCPos.prototype.getSPUsedTime = function(index) {return this.spUsedTimes[index]
 ZSCPos.prototype.getSPUsedAmount = function(index) {return this.spUsedAmounts[index];}
 
 ZSCPos.prototype.loadRewardHistory = function(func) {
-    this.numRewards(function() {
+    this.numMiningInfo(true, function() {
         for (var i = 0; i < this.rewardNos; ++i) {
-            this.getRewardInfo(i, function(index){
+            this.getRewardInfo(true, i, function(index){
                 if (indx == this.rewardNos - 1) {
                     func();
                 }
@@ -41,17 +41,10 @@ ZSCPos.prototype.loadRewardHistory = function(func) {
     });
 }
 
-ZSCPos.prototype.numRewards = function(func) {
-   
-}
-
-ZSCPos.prototype.getRewardInfo = function(index, func) {
-}
-
-ZSCPos.prototype.loadSPUsedHistory = function(func) {
-    this.numSPUsed(function() {
+ZSCPos.prototype.loadSpUsedHistory = function(func) {
+    this.numMiningInfo(false, function() {
         for (var i = 0; i < this.spUsedNos; ++i) {
-            this.getSPUsedInfo(i, function(index){
+            this.getRewardInfo(false, i, function(index){
                 if (indx == this.spUsedNos - 1) {
                     func();
                 }
@@ -60,7 +53,58 @@ ZSCPos.prototype.loadSPUsedHistory = function(func) {
     });
 }
 
-ZSCPos.prototype.getSPUsedInfo = function(index, func) {
+ZSCPos.prototype.numMiningInfo = function(isReward, func) {
+    this.myControlApi.numStakerMining(this.userName, true, 
+        {from: this.getAccount()},
+        function(error, result){ 
+            if(!error) {
+                if (isReward) {
+                    this.rewardNos = result;
+                } else {
+                    this.spUsedNos = result;
+                }
+                func();
+            } else {
+                console.log("error: " + error);
+            }
+        });
+}
+
+ZSCPos.prototype.getMiningInfo = function(isReward, index, func) {
+    this.myControlApi.getStakerMiningInfoByIndex(this.userName, true, index,
+        {from: this.getAccount()},
+        function(error, result){ 
+            if(!error) {
+                this.parserMiningInfo(isReward, info, index);
+                func(index);
+            } else {
+                console.log("error: " + error);
+            }
+        });
 }
 
 
+ZSCPos.prototype.parserMiningInfo = function(isReward, info, index) {
+    var found1 = urlinfo.indexOf("?");
+    var found2 = urlinfo.indexOf("=");
+
+    if (found1 == -1 || found2 == -1) return false;
+
+    var len = urlinfo.length;
+    var offset = urlinfo.indexOf("?");
+    var newsidinfo = urlinfo.substr(offset,len)
+    var newsids = newsidinfo.split("&");
+
+    var timeInfo  = newsids[0];
+    var amountInfo = newsids[1];
+
+    if (isReward) {
+        this.rewardTimes[index] = timeInfo.split("=")[1];
+        this.rewardAmounts[index] = amountInfo.split("=")[1];
+    } else {
+        this.spUsedTimes[index] = timeInfo.split("=")[1];
+        this.spUsedAmounts[index] = amountInfo.split("=")[1];    
+    }
+
+    return true;
+}
