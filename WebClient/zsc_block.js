12503f44570d5212584dbb890288de123b48c662
Eric-Yao <norhustar@hotmail.com>
2018-05-01 21:21:26 +0800

    updated class ZSCBlock

--- WebClient/zsc_block.js
+++ WebClient/zsc_block.js
@@ -6,6 +6,7 @@ function ZSCBlock(nm, abi, adr) {
     this.userName = nm;
     this.totalBlockNos;
     this.minedBlockNos;
+    this.poolIndex;
     this.blockSizes = [];
     this.blockTxNos = [];
     this.myControlApi = web3.eth.contract(abi).at(adr);
@@ -19,7 +20,8 @@ ZSCBlock.prototype.getTotalBlockNos = function() {return this.totalBlockNos;}
 
 ZSCBlock.prototype.getMinedBlockNos = function() {return this.minedBlockNos;}
 
-ZSCBlock.prototype.loadBlocks = function(func) {
+ZSCBlock.prototype.loadBlocks = function(poolIndex, func) {
+    this.poolIndex = poolIndex;
     this.numBlocks(function() {
         for (var i = 0; i < this.agrNos; ++i) {
             this.getBlockInfoByIndex(i, function(index){
@@ -31,10 +33,36 @@ ZSCBlock.prototype.loadBlocks = function(func) {
     });
 }
 
-ZSCBlock.prototype.numBlocks= function(func) {
+ZSCBlock.prototype.numBlocks = function(func) {
+    this.myControlApi.numBlockInfo(this.userName, this.poolIndex, 
+        {from: this.getAccount()},
+        function(error, result){ 
+            if(!error) {
+                parserBlockNosInfo(result);
+                func();
+            } else {
+                console.log("error: " + error);
+            }
+        });
+}
+
+ZSCBlock.prototype.getBlockInfoByIndex = function(blockIndex, func) {
+    this.myControlApi.getBlockInfoByIndex(this.userName, this.poolIndex, blockIndex,
+        {from: this.getAccount()},
+        function(error, result){ 
+            if(!error) {
+                parserBlockInfo(result, blockIndex);
+                func(blockIndex);
+            } else {
+                console.log("error: " + error);
+            }
+        });
+}
+
+ZSCBlock.prototype.parserBlockNosInfo = function(info) {
 
 }
 
-ZSCBlock.prototype.getBlockInfoByIndex = function(index, func) {
+ZSCBlock.prototype.parserBlockInfo = function(info, index) {
 
 }
