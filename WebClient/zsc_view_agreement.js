e1585fad9429df00c4ab05ac4cb8b5b3935acc90
Eric-Yao <norhustar@hotmail.com>
2018-05-03 23:37:18 +0800

    added methods purchaseAgreement(), loadAllAgreementsHtml();

--- WebClient/zsc_view_agreement.js
+++ WebClient/zsc_view_agreement.js
@@ -51,3 +51,42 @@ ZSCViewAgreement.prototype.getFactoryAgrNameByIndex = function(index, func) {
             }
         });
 }
+
+ZSCViewAgreement.prototype.purchaseAgreement = function(index, func) {
+    this.myControlApi.purchaseAgreement(this.userName, this.agrNames[index] ,
+        {from: this.getAccount(), gasPrice: this.getGasPrice(1), gas : this.getGasLimit(20)}, 
+        function(error, result){ 
+            if(!error) {
+                func(result);
+            } else {
+                console.log("error: " + error);
+            }
+        });
+}
+
+ZSCViewAgreement.prototype.loadAllAgreementsHtml = function(elementId, func1, func2)  {
+    var showPrefix = func1 + "('"; 
+    var showSuffix = "')";
+
+    var purchasePrefix = func2 + "('"; 
+    var purchaseSuffix = "')";
+
+    var text ="";
+    text += '<div class="well">';
+    text += '<table align="center" style="width:800px;min-height:30px">'
+    text += '<tr>'
+    text += '   <td>Index</td> <td>Agreement name</td>'
+    text += '</tr>'
+
+    for (var i = 0; i < this.tmpNos; ++i) {
+        text += '<tr>'
+        text += '   <td><text>' + i + '</text></td>'
+        text += '   <td><text>' + this.agrNames[i] + '</text></td>'
+        text += '   <td><button type="button" onClick="' + showPrefix + this.agrNames[i] + showSuffix + '">Details</button></td>'
+        text += '   <td><button type="button" onClick="' + purchasePrefix + this.agrNames[i] + purchaseSuffix + '">Purchase</button></td>'
+        text += '</tr>'
+    }
+    text += '</table></div>'
+
+    document.getElementById(elementId).innerHTML = text;  
+}
