4a8672d27d8b1d6243119c5feeaf38a3252108b2
Eric-Yao <norhustar@hotmail.com>
2018-05-03 22:03:20 +0800

    updated class ZSCTemplate

--- WebClient/zsc_templates.js
+++ WebClient/zsc_templates.js
@@ -30,7 +30,7 @@ ZSCTemplate.prototype.loadTempates = function(func) {
 
 ZSCTemplate.prototype.numTemplates= function(func) {
     this.myControlApi.numTemplates(this.userName,
-        {from: this.getAccount(), gas: this.getGasLimit(20)},
+        {from: this.getAccount()},
         function(error, result){ 
             if(!error) {
                 this.agrNos = result.toString(10);
@@ -43,7 +43,7 @@ ZSCTemplate.prototype.numTemplates= function(func) {
 
 ZSCTemplate.prototype.getTmpNameByIndex = function(index, func) {
     this.myControlApi.getTemplateNameByIndex(this.userName, index,
-        {from: this.getAccount(), gas: this.getGasLimit(20)},
+        {from: this.getAccount()},
         function(error, result){ 
             if(!error) {
                 this.tmpNames[index] = web3.toUtf8(result);
@@ -54,24 +54,42 @@ ZSCTemplate.prototype.getTmpNameByIndex = function(index, func) {
         });
 }
 
-ZSCTemplate.prototype.loadTemplatesHtml = function(elementId)  {
-    var timeMoment;
-    var inputTag;
-    var amount;
-    var sender;
-    var receiver;
+/* zsc API:
+   function createElement(bytes32 _userName, uint _typeInUint, bytes32 _enName, bytes32 _extraInfo, address _extraAdr) public returns (address) 
+*/
+
+ZSCTemplate.prototype.enableAsAgreement = function(index, func) {
+    this.myControlApi.createElement(this.userName, 5, this.tmpNames[i] + "-agr", this.tmpNames[i], 0,
+        {from: this.getAccount(), gasPrice: this.getGasPrice(1), gas : this.getGasLimit(55000)},
+        function(error, result){ 
+            if(!error) {
+                func();
+            } else {
+                console.log("error: " + error);
+            }
+        });
+}
+
+ZSCTemplate.prototype.loadTemplatesHtml = function(elementId, funcSetPara, funcPublish)  {
+    var funcSetParaPrefix = funcSetPara + "('"; 
+    var funcSetParaSuffix = "')";
+
+    var funcPublishPrefix = funcPublish + "('";
+    var funcPublishSuffix = "')";
 
     var text ="";
     text += '<div class="well">';
     text += '<table align="center" style="width:800px;min-height:30px">'
     text += '<tr>'
-    text += '   <td>Index</td> <td>Template name</td>'
+    text += '   <td>Index</td> <td>Template name</td> <td>Template details</td> <td>Publish Template</td>'
     text += '</tr>'
 
     for (var i = 0; i < this.tmpNos; ++i) {
         text += '<tr>'
         text += '   <td><text>' + i + '</text></td>'
         text += '   <td><text>' + this.tmpNames[i]  + '</text></td>'
+        text += '   <td><button type="button" onClick="' + funcSetParaPrefix + this.tmpNames[i] + funcSetParaSuffix + '">Show</button></td>'
+        text += '   <td><button type="button" onClick="' + funcPublishPrefix + i + funcPublishSuffix + '">Publish</button></td>'
         text += '</tr>'
     }
     text += '</table></div>'
