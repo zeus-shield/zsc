6933faadaa6642b53ad7dbcbc691329726e808e7
Eric-Yao <norhustar@hotmail.com>
2018-05-04 23:42:47 +0800

    updated class ZSCShowUserTransactions
    updated method loadUserTransactionByIndex(); added method parserUserTransactionByIndex();

--- WebAdm/js/zsc_show_user_transactions.js
+++ WebAdm/js/zsc_show_user_transactions.js
@@ -48,5 +48,41 @@ ZSCShowUserTransactions.prototype.numUserTransactions = function(func) {
 }
 
 ZSCShowUserTransactions.prototype.loadUserTransactionByIndex = function(index, func) {
-
+    this.myControlApi.getUserTransactionByIndex(this.userName, this.tokenSymbol, index,
+        {from: this.account},
+        function(error, result){ 
+            if(!error) {
+                parserUserTransactionByIndex(result, index)
+                func(index);
+            } else {
+                console.log("error: " + error);
+            }
+        });
 }
+
+ZSCShowUserTransactions.prototype.parserUserTransactionByIndex = function(urlinfo, index) {
+    var found1 = urlinfo.indexOf("?");
+    var found2 = urlinfo.indexOf("=");
+
+    if (found1 == -1 || found2 == -1) return false;
+
+    var len = urlinfo.length;
+    var offset = urlinfo.indexOf("?");
+    var newsidinfo = urlinfo.substr(offset,len)
+    var newsids = newsidinfo.split("&");
+
+    var timeInfo     = newsids[0];
+    var inputInfo    = newsids[1];
+    var txInfo       = newsids[2];
+    var amountInfo   = newsids[3];
+    var senderInfo   = newsids[4];
+    var receiverInfo = newsids[5];
+
+    this.timeMoments[index] = timeInfo.split("=")[1];
+    this.inputTags[index]   = inputInfo.split("=")[1];
+    this.txHash[index]      = txInfo.split("=")[1];
+    this.amounts[index]     = amountInfo.split("=")[1];
+    this.senders[index]     = senderInfo.split("=")[1];
+    this.receivers[index]   = receiverInfo.split("=")[1];
+}
+
