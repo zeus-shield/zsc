2ae13be0775e39d97e3f64ca76708f030cc2838d
Eric-Yao <norhustar@hotmail.com>
2018-05-05 21:06:14 +0800

    updated contract FactoryManager
    added abstract contract DBDatabase; updated functions initFactoryManager(), addFactory(); added functions getBindedDB(), removeFactory();

--- Database/factory_manager.sol
+++ Database/factory_manager.sol
@@ -6,6 +6,10 @@ pragma solidity ^0.4.21;
 
 import "./object.sol";
 
+contract DBDatabase is Object {
+    function delegateFactory(address _factoryAdr, uint _priority) public;
+}
+
 contract FactoryManager is Object {
     address private bindedDB_;
     address private apiController_;
@@ -14,18 +18,26 @@ contract FactoryManager is Object {
     constructor(bytes32 _name) public Object(_name) {
     }
 
+    function getBindedDB() public constant returns (address) { 
+        checkDelegate(msg.sender, 1);
+        return bindedDB_;
+    }
+
     function initFactoryManager(address _controller, address _database) public {
         checkDelegate(msg.sender, 1);
         
         require(_database != 0);
-        bindedDB_ = _database;
+        if (bindedDB_ != _database) {
+            setDelegate(bindedDB_, 0);
+            setDelegate(_database, 1);
+            bindedDB_ = _database;
+        }
 
-        if (_controller != 0 && _controller != apiController_) {
-            if (apiController_ != 0) {
-                setDelegate(apiController_, 0);
-            }
-            apiController_ = _controller;
+        require(_database != 0);
+        if (_controller != apiController_) {
+            setDelegate(apiController_, 0);
             setDelegate(_controller, 1);
+            apiController_ = _controller;
         }
     }
 
@@ -33,16 +45,30 @@ contract FactoryManager is Object {
         checkDelegate(msg.sender, 1);
         if (factories_[_type] == address(0)) {
             factories_[_type] = _adr;
+            setDelegate(_adr, 1);
+            Object(bindedDB_).delegateFactory(_adr, 1);
             return true;
         } else {
             return false;
         }
     }
+
+    function removeFactory(bytes32 _type) public returns (bool) {
+        checkDelegate(msg.sender, 1);
+        if (factories_[_type] != address(0)) {
+            factories_[_type] = address(0);
+            setDelegate(_adr, 0);
+            Object(bindedDB_).delegateFactory(_adr, 0);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     function getFactory(bytes32 _type) public return (address) {
         checkDelegate(msg.sender, 1);
         require(actories_[_type] != address(0));
         return actories_[_type];
     }
 
-    function getBindedDB() internal constant returns (address) { return bindedDB_;}
 }
