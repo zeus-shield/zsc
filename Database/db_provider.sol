8157b7816c2ecab2160de3ff0214230b7418ffeb
Eric-Yao <norhustar@hotmail.com>
2018-05-06 21:54:26 +0800

    updated contract DBProvider

--- Database/db_provider.sol
+++ Database/db_provider.sol
@@ -35,6 +35,16 @@ contract DBProvider is DBUser {
         addFundamentalParameter("claimPhone");
     }
 
+    function numTemplateChildren(uint _tmpIndex) internal constant returns (uint) {
+        address tmpNode = DBNode(getHandler("template")).getChildByIndex(_tmpIndex);
+        return DBNode(tmpNode).numChildren();
+    }
+
+    function getTemplateChildByIndex(uint _tmpIndex, uint _childIndex) internal constant returns (address) {
+        address tmpNode = DBNode(getHandler("template")).getChildByIndex(_tmpIndex);
+        return DBNode(tmpNode).getChildByIndex(_childIndex);
+    }
+    
     function addParameter(bytes32 _parameter) public returns (bool) {
         checkDelegate(msg.sender, 1);
         return false;
@@ -46,5 +56,41 @@ contract DBProvider is DBUser {
         return false;
         return super.removeParameter(_parameter);
     }
+    
+    function numAgreements() public constant returns (uint) {
+        checkDelegate(msg.sender, 1);
 
+        uint agrNos = 0;
+        address agrParentNode = getHandler("provider");
+        uint tmpNos = DBNode(agrParentNode).numChildren();
+
+        for (uint i = 0; i < tmpNos; ++i) {
+            agrNos = agrNos.add(numTemplateChildren(i));
+        }
+        return agrNos;
+    }
+
+    function getAgreementByIndex(uint _index) public constant returns (address) {
+        checkDelegate(msg.sender, 1);
+
+        uint agrNos = 0;
+        address agrParentNode = getHandler("provider");
+        uint tmpNos = DBNode(agrParentNode).numChildren();
+        address child;
+        uint pos = 0;
+
+        for (uint i = 0; i < tmpNos; ++i) {
+            child = DBNode(agrParentNode).getChildByIndex(i);
+            agrNos = DBNode(child).numChildren();
+            for (uint j = 0; j < agrNos; ++j) {
+                if (_index == pos) {
+                    return getTemplateChildByIndex(i, j);
+                } else {
+                    pos++;
+                }
+            }
+        }
+
+        revert();
+    }
 }
