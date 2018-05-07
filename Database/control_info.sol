2e96c3137917228084a0ce2d431dc7207999c3f2
Eric-Yao <norhustar@hotmail.com>
2018-05-07 16:29:01 +0800

    updated contract ControlInfo

--- Database/control_info.sol
+++ Database/control_info.sol
@@ -10,8 +10,10 @@ import "./object.sol";
 contract ControlInfo is Object {   
     struct ParameterInfo {
         bytes32 userName_;
+        mapping (address => bool) signatures_; // ETH wallet address
+
         address nodeAdr_;
-        mapping (bytes32 => string) creator_; // ETH wallet address
+        address creator_;
         mapping (bytes32 => string) value_;
     }
     
@@ -32,47 +34,52 @@ contract ControlInfo is Object {
 
     constructor(bytes32 _name) public Object(_name){}
 
+    function registerNode(bytes32 _userName, bytes32 _nodeName, address _nodeAdr, address _creator) private {
+        nodeParameters_[_nodeName].userName_ = _userName;
+        nodeParameters_[_nodeName].nodeAdr_  = _nodeAdr;
+        nodeParameters_[_nodeName].creator_  = _creator;
+        nodeParameters_[_nodeName].signatures_[_creator] = true;
+    }
+
     function allowedUser(bytes32 _userName, address _sender) internal constant returns (bool);
+
     function addAllowedUser(bytes32 _userName, address _creator) internal returns (bool);
 
     function checkMatched(bytes32 _userName, bytes32 _enName, address _sender) internal constant {
         if (isDelegate(_sender, 1)) return;
-        if (nodeParameters_[_enName].creator_[_sender]) return;
+        if (nodeParameters_[_enName].signatures_[_sender]) return;
         if (nodeParameters_[_enName].userName_ == _userName) return;
         revert();
     }
 
     function checkRegistered(bytes32 _userName, address _sender) internal constant{
-        require(allowedUser(_userName, msg.sender) || isDelegate(_sender, 1)); 
-    }
-
-    function _recordString(bytes32 _nodeName, bytes32 _parameter, string _value) public {
-        require(msg.sender == nodeParameters_[_nodeName].nodeAdr_);
-        nodeParameters_[_nodeName].value_[_parameter] = _value;
+        require(allowedUser(_userName, _sender) || isDelegate(_sender, 1)); 
     }
 
     function registerSignature(bytes32 _userName, address _sigAdr) internal {
-        require(nodeExists_[_nodeName]);   
-        nodeParameters_[_nodeName].creator_[_sigAdr] = true;
+        require(nodeExists_[_userName]);   
+        addAllowedUser(_userName, _sigAdr);
+        nodeParameters_[_userName].signatures_[_sigAdr] = true;
     }
-    
 
-    function registerNode(bool _isUser, bytes32 _userName,  bytes32 _nodeName, address _nodeAdr, address _creator) internal {
+    function registerUserNode(bytes32 _userName,  bytes32 _nodeName, address _nodeAdr, address _creator) internal {
         require(!nodeExists_[_nodeName]);   
-        if (_isUser) {
-            addAllowedUser(_nodeName, _creator);
-        }
-        nodeParameters_[_nodeName].userName_ = _userName;
-        nodeParameters_[_nodeName].nodeAdr_ = _nodeAdr;
-        nodeParameters_[_nodeName].creator_[_creator] = true;
-    }
+        addAllowedUser(_nodeName, _creator);
+        registerNode(_userName, _nodeName, _nodeAdr, _creator);
+    }  
 
-    function getControlInfoNodeAddress(bytes32 _nodeName)internal constant returns (address)  {
-        return nodeParameters_[_nodeName].nodeAdr_;
+    function registerEntityNode(bytes32 _userName,  bytes32 _nodeName, address _nodeAdr, address _creator) internal {
+        require(!nodeExists_[_nodeName]);   
+        registerNode(_userName, _nodeName, _nodeAdr, _creator);
     }
     
-    function getControlInfoParameterValue(bytes32 _nodeName, bytes32 _parameter) internal constant returns (string) {
+    function getNodeParameterValue(bytes32 _nodeName, bytes32 _parameter) internal constant returns (string) {
         return nodeParameters_[_nodeName].value_[_parameter];
+    }
+
+    function setNodeParameterValue(bytes32 _nodeName, bytes32 _parameter, string _value) internal returns (bool) {
+        nodeParameters_[_nodeName].value_[_parameter] = _value;
+        return true;
     }
 }
 
