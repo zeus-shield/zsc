4e9d6480694479e08f26fc9b583a7b1a3e23d64c
Eric-Yao <norhustar@hotmail.com>
2018-05-08 22:31:54 +0800

    updated contract FactoryBase

--- Database/factory_base.sol
+++ Database/factory_base.sol
@@ -4,45 +4,15 @@ Copyright (c) 2018, ZSC Dev Team
 
 pragma solidity ^0.4.21;
 
-import "./object.sol";
+import "./module_base.sol";
 
-contract FactoryBase is Object {
-    address private database_;
-    address private factoryGM_;
-
+contract FactoryBase is ModuleBase {
     constructor(bytes32 _name) public Object(_name) {
     }
 
     function setupFactoryRoot() internal;
 
     function createNode(bytes32 _nodeName, address _parent, address _creator) public returns (address);
-
-    function numFactoryElements() public constant returns (uint);
-
-    function getFactoryElementByIndex(uint _index) public constant returns (address);
-
-    function setDatabase(address _adr) public {
-        require(database_ == address(0));
-        
-        checkDelegate(msg.sender, 1);
-        database_ = _adr;
-    } 
-
-    function getBindedDatabase() internal constant returns (address) { 
-        return database_;
-    }
-
-    function initFactory(address _factoryGM) public {
-        checkDelegate(msg.sender, 1);
-        
-        require(_factoryGM != 0);
-
-        if (factoryGM_ != _factoryGM) {
-            setDelegate(factoryGM_, 0);
-            setDelegate(_factoryGM, 1);
-            factoryGM_ = _factoryGM;
-        }
-        setupFactoryRoot();
-    }
-
 }
+
+
