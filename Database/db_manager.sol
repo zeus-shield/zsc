aef0f34a90322a986c5324caf46bec95afb8e58e
Eric-Yao <norhustar@hotmail.com>
2018-05-08 14:08:59 +0800

    added file db_manager.sol

---
+++ Database/db_manager.sol
@@ -0,0 +1,60 @@
+/*
+Copyright (c) 2018 ZSC Dev Team
+*/
+
+pragma solidity ^0.4.21;
+
+import "./object.sol";
+
+contract DBManager is Object {
+    uint private databaseNos_;
+    address[] private databases_;
+    mapping(bytes32 => uint) private databaseIndice_;
+    mapping(bytes32 => bool) private databaseExists_;
+
+    address private controlApiAdr_;
+
+    constructor(bytes32 _name) public Object(_name) {
+    }
+
+    function initDBManager(address _controlApiAdr) public {
+        checkDelegate(msg.sender, 1);
+
+        require(_controlApiAdr != 0);
+        if (_controlApiAdr != controlApiAdr_) {
+            setDelegate(controlApiAdr_, 0);
+            setDelegate(_controlApiAdr, 1);
+            controlApiAdr_ = _controlApiAdr;
+        }
+    }
+
+    function addDBDatabase(bytes32 _name, address _adr) public returns (bool) {
+        checkDelegate(msg.sender, 1); 
+        if (!databaseExists_[_name]) {
+            return false;
+        }
+
+        uint index = databaseNos_;
+        databaseNos_++;
+
+        databaseExists_[_name] = true;
+        databaseIndice_[_name] = index;
+        databases_[index] = _adr;
+
+        return true;
+    }
+    
+    function getDBDatabase(bytes32 _name) public returns (address) {
+        checkDelegate(msg.sender, 1); 
+        require(databaseExists_[_name]);
+        return databases_[databaseIndice_[name]];
+    }
+
+    function removeDBDatabase(bytes32 _name) public returns (address) {
+        checkDelegate(msg.sender, 1); 
+        require(databaseExists_[_name]);
+        address adr = databases_[databaseIndice_[name]];
+        return Object(adr).kill();
+    }
+}
+
