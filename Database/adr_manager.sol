6fa944281ae5a8e471e9430759c6585d6798d169
Eric-Yao <norhustar@hotmail.com>
2018-05-09 22:54:44 +0800

    added file adr_manager.sol

---
+++ Database/adr_manager.sol
@@ -0,0 +1,74 @@
+/*
+Copyright (c) 2018, ZSC Dev Team
+*/
+
+pragma solidity ^0.4.21;
+
+import "./object.sol";
+
+contract AdrManager is Object {
+    address private systemGM_;
+    uint private adrNos_;
+
+    mapping(uint => bytes32) private names_;
+    mapping(uint => address) private adrs_;
+
+    mapping(bytes32 => uint) private indice_;
+    mapping(bytes32 => bool) private exists_;
+
+    // Constructor
+    constructor(bytes32 _name) public Object(_name) {
+    }
+
+    function initAdrManager(address _systemGM) internal {
+        require(_systemGM != 0);
+        if (_systemGM != systemGM_) {
+            setDelegate(systemGM_, 0);
+            setDelegate(_systemGM, 1);
+            systemGM_ = _systemGM;
+        }
+    }
+
+    function numAdrs() public constant returns (uint) {
+        checkDelegate(msg.sender, 1);
+        return adrNos_;
+    }
+
+    function addAdr(bytes32 _name, address _id) public returns (bool) {
+        checkDelegate(msg.sender, 1);
+        require(!exists_[_name]);
+        
+        exists_[_name] = true;
+        indice_[_name] = idNos_;
+        names_[idNos_] = _name;
+        ids_.push(_id);
+        idNos_++;
+
+        return true;
+    }
+
+    function removeAdr(bytes32 _name) public returns (bool) {
+        checkDelegate(msg.sender, 1);
+        require(exists_[_name]);
+
+        uint index = indice_[_name];
+
+        names_[index] = names_[idNos_ - 1];
+        adrs_[index] = adrs_[adrNos_ - 1];
+        indice_[names_[index]] = index;
+
+        delete adrs_[adrNos_ - 1];
+        delete names_[adrNos_ - 1];
+        exists_[_name] = false;
+        adrNos_--;
+
+        return true;
+    }
+
+    function getAdr(bytes32 _name) public constant returns (address) {
+        checkDelegate(msg.sender, 1);
+        require(exists_[_name]);
+        return adrs_[indice_[_name]];
+    }
+
+}
