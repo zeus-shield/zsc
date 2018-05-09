6ccb2f5bf59754f1f1b20ddc1111f37290f86f82
Eric-Yao <norhustar@hotmail.com>
2018-05-09 22:53:45 +0800

    updated contract DBManager

--- Database/db_manager.sol
+++ Database/db_manager.sol
@@ -4,63 +4,39 @@ Copyright (c) 2018 ZSC Dev Team
 
 pragma solidity ^0.4.21;
 
-import "./object.sol";
+import "./adr_manager.sol";
 
-contract DBManager is Object {
-    uint private databaseNos_;
-    address[] private databases_;
-    mapping(bytes32 => uint) private databaseIndice_;
-    mapping(bytes32 => bool) private databaseExists_;
+contract DBDatabase {
+    function kill() public;
+}
 
-    constructor(bytes32 _name) public Object(_name) {
+contract DBManager is AdrManager {
+    constructor(bytes32 _name) public AdrManager(_name) {
     }
 
-    function initDBManager(address _controlApiAdr) public {
+    function initDBManager(address _systemGM) public {
         checkDelegate(msg.sender, 1);
-
-        require(_controlApiAdr != 0);
-        if (_controlApiAdr != controlApiAdr_) {
-            setDelegate(controlApiAdr_, 0);
-            setDelegate(_controlApiAdr, 1);
-            controlApiAdr_ = _controlApiAdr;
-        }
+        initAdrManager(_systemGM);
     }
 
-    function addDatabase(bytes32 _name, address _adr) public returns (bool) {
+    function addAdr(bytes32 _name, address _adr) public returns (bool) {
         checkDelegate(msg.sender, 1); 
-        require(_controlApiAdr != address(0));
+        require(systemGM_ != address(0));
         
-        if (!databaseExists_[_name]) {
-            return false;
-        }
-
-        uint index = databaseNos_;
-        databaseNos_++;
-
-        databaseExists_[_name] = true;
-        databaseIndice_[_name] = index;
-        databases_[index] = _adr;
-
-        return true;
-    }
-    
-    function getDatabase(bytes32 _name) public returns (address) {
-        checkDelegate(msg.sender, 1); 
-        require(databaseExists_[_name]);
-        return databases_[databaseIndice_[name]];
+        return super.addAdr(_name, _adr);
     }
 
-    function removeDatabase(bytes32 _name) public returns (address) {
+    function removeAdr(bytes32 _name) public returns (address) {
         checkDelegate(msg.sender, 1); 
-        require(databaseExists_[_name]);
-        address adr = databases_[databaseIndice_[name]];
-        return Object(adr).kill();
+        DBDatabase(adr).kill();
+
+        return super.removeAdr(_name);
     }
 
-    function delegateObject(address _adr, uint _priority) public {
+    function delegateObject(bytes32 _databaseName, address _objectAdr, uint _priority) public {
         checkDelegate(msg.sender, 1); 
-        for (uint i = 0; i < databaseNos_; ++i) {
-            Object(database_[i]).setDelegate(_adr, _priority); 
-        }
+
+        address dbAdr = getAdr(_databaseName);
+        object(dbAdr).setDelegate(_objectAdr, _priority);
     }
 }
