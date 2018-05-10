3a24bbee03d8c346a108a734e4bd84062bd729be
Eric-Yao <norhustar@hotmail.com>
2018-05-10 15:49:52 +0800

    updated contract SystemManager

--- Database/system_manager.sol
+++ Database/system_manager.sol
@@ -8,6 +8,7 @@ import "./object.sol";
 
 contract AdrManager is Object {
     function addAdr(bytes32 _name, address _database) public returns (bool);
+    function getAdr(bytes32 _name) public returns (address);
     function setFactoryDatabase(bytes32 _factoryName, address _databaseAdr) public;
     function delegateObject(bytes32 _databaseName, address _objectAdr, uint _priority) public;
 }
@@ -50,14 +51,47 @@ contract SystemManager is Object {
         AdrManager(databaseGM_).addAdr(_name, _adr);
     }
 
-    function addModuleManager(bytes32 _name, address _adr) public returns (bool) {
+    function getFactory(bytes32 _name) public returns (address) {
         checkDelegate(msg.sender, 1);
 
+        return AdrManager(_factoryGM).getAdr(_name);
+    }
+
+    function getDatabase(bytes32 _name) public returns (address) {
+        checkDelegate(msg.sender, 1);
+
+        return AdrManager(_databaseGM).getAdr(_name);
+    }
+
+    function addModuleManager(bytes32 _name, address _adr) public returns (bool) {
+        checkDelegate(msg.sender, 1);
         require(!moduleExists_(_name));
 
         modules_[_name] = _adr;
-        modules_[_name] = true;
+        moduleExists_[_name] = true;
+
         return true;
+    }
+
+    function getModuleManager(bytes32 _name) public returns (address) {
+        checkDelegate(msg.sender, 1);
+        require(moduleExists_(_name));
+        return modules_[_name];
+    }
+
+    function mapFactoryDatabase(bytes32 _factroyName, bytes32 _databaseName) public {
+        checkDelegate(msg.sender, 1);
+
+        address factoryAdr = getFactory(_factoryName);
+
+        AdrManager(databaseAdr).delegateObject(_databaseName, factoryAdr, 1);
+    }
+
+    function mapModuleDatabase(bytes32 _muduleName, bytes32 _databaseName) public {
+        checkDelegate(msg.sender, 1);
+        require(!moduleExists_(_name));
+
+        AdrManager(databaseAdr).delegateObject(_databaseName, moduleExists_(_name), 1);
     }
 }
 
