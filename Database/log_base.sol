2aa2b877b633d902647829a6e451baa8163f4d1e
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-05-10 23:27:01 +0800

    modify log_base.sol

--- Database/log_base.sol
+++ Database/log_base.sol
@@ -4,7 +4,9 @@ Copyright (c) 2018, ZSC Dev Team
 
 pragma solidity ^0.4.21;
 
-contract LogBase {
+import "./delegate.sol";
+
+contract LogBase is Delegated {
     function addLog(string _log, bool _newLine) public;
     function printLog(address _addr, uint _index) public constant returns (string);
 }
