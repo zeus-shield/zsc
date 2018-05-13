75840af65a98d2888d78dfc3935b760d8f152ca0
Eric-Yao <norhustar@hotmail.com>
2018-05-13 17:11:29 +0800

    updated contract DBManager
    updated function operateNodeParameter(); added functions addNodeParameter(), setNodeParameterValue(), getNodeParameterValue()

--- Database/db_manager.sol
+++ Database/db_manager.sol
@@ -9,7 +9,28 @@ import "./adr_manager.sol";
 contract DBManager is AdrManager {
     constructor(bytes32 _name) public AdrManager(_name) {
     }
-    
+
+   /*
+      internal functions
+   */
+    function operateNodeParameter(bytes32 _operation, bytes32 _nodeName, bytes32 _parameter, string _value) internal returns (bool) {
+        string memory str = ""; 
+        str = PlatString.append(str, PlatString.bytes32ToString(_nodeName), " : " );
+        str = PlatString.append(str, PlatString.bytes32ToString(_parameter), " : " , _value);
+        if (_operation == "add") {
+            str = PlatString.append("addNodeParameter - ", str);
+            ret = DBNode(ndAdr).addParameter(_parameter);
+        } else if (_operation == "set") {
+            str = PlatString.append("setNodeParameter - ", str);
+            ret = DBNode(ndAdr).setParameter(_parameter, _value);
+        }        
+        addLog(str, true);
+        return ret;
+    }
+
+   /*
+      public functions
+   */
     function addAdr(bytes32 _name, address _adr) public returns (bool) {
         checkDelegate(msg.sender, 1); 
         require(systemGM_ != address(0));
@@ -24,19 +45,35 @@ contract DBManager is AdrManager {
         return super.removeAdr(_name);
     }
 
-    function operateNodeParameter(bytes32 _operation, bytes32 _userName, bytes32 _node, bytes32 _parameter, string _value) internal returns (bool) {
-        bool ret;
-        string memory str = ""; 
-        str = PlatString.append(str, PlatString.bytes32ToString(_node), " : " );
-        str = PlatString.append(str, PlatString.bytes32ToString(_parameter), " : " , _value);
-        if (_operation == "add") {
-            str = PlatString.append("addNodeParameter - ", str);
-            ret = getDBNode(_node).addParameter(_parameter);
-        } else if (_operation == "set") {
-            str = PlatString.append("setNodeParameter - ", str);
-            ret = getDBNode(_node).setParameter(_parameter, _value);
-        }        
-        addLog(str, true);
-        return ret;
+    function addNodeParameter(bytes32 _dbName, bytes32 _nodeName, bytes32 _parameter) public returns (bool) {
+        checkDelegate(msg.sender, 1); 
+
+        setDatabase(_dbName);
+
+        address dbAdr = getDatabase();
+        address ndAdr = DBDatabase(dbAdr).getNode(_nodeName);
+
+        return operateNodeParameter("add", _nodeName, _parameter, "null"); 
+    }
+
+    function setNodeParameterValue(bytes32 _dbName, bytes32 _nodeName, bytes32 _parameter, bytes32 _value) public returns (bool) {
+        checkDelegate(msg.sender, 1); 
+        setDatabase(_dbName);
+
+        address dbAdr = getDatabase();
+        address ndAdr = DBDatabase(dbAdr).getNode(_nodeName);
+
+        return operateNodeParameter("set", _nodeName, _parameter, _value); 
+    }
+
+    function getNodeParameterValue(bytes32 _dbName, bytes32 _nodeName, bytes32 _parameter) public constant returns (bytes32) {
+        checkDelegate(msg.sender, 1); 
+        setDatabase(_dbName);
+
+        address dbAdr = getDatabase();
+        address ndAdr = DBDatabase(dbAdr).getNode(_nodeName);
+
+        return DBNode(ndAdr).getParameter(_parameter);
     }
 }
+
