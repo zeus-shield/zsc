3c6a26e137b7928e0186770eb2494cabad98a2dd
Eric-Yao <norhustar@hotmail.com>
2018-05-18 14:12:32 +0800

    updated functions

--- WebAdm/js/createContract.js
+++ WebAdm/js/createContract.js
@@ -10,34 +10,52 @@ function cC_getContractFullName(contractName) {
         return "./zsc/db/Database/ZSCTestToken.sol:ZSCTestToken";
     } else if (contractName == 'AdmAdv') {
         return "./zsc/adm_adv.sol:AdmAdv";
+
     } else if (contractName == 'PosAdv') {
         return "./zsc/pos_adv.sol:PosAdv";
+
     } else if (contractName == 'LogRecorder') {
         return "./zsc/db/Database/log_recorder.sol:LogRecorder";
-    } else if (contractName == 'WalletManager') {
-        return "./zsc/db/Database/wallet_manager.sol:WalletManager";
+
     } else if (contractName == 'DatabaseManager') {
-        return "./zsc/db/Database/database_manager.sol:DatabaseManager";
+        return "./zsc/db/Database/sys_gm_db.sol:SysGmDb";
+
+    } else if (contractName == 'WalletManager') {
+        return "./zsc/db/Database/sys_gm_wallet.sol:SysGmWallet";
+
+    } else if (contractName == 'SystemOverlayer') {
+        return "./zsc/db/Database/sys_overlayer.sol:SysOverlayer";
+
     } else if (contractName == 'SimulatorManager') {
-        return "./zsc/db/Database/simulator_manager.sol:SimulatorManager";
+        return "./zsc/db/Database/sys_gm_simulator.sol:SimulatorManager";
+
     } else if (contractName == 'FactoryManager') {
-        return "./zsc/db/Database/factory_manager.sol:FactoryManager";
+        return "./zsc/db/Database/sys_gm_factory.sol:FactoryManager";
+
     } else if (contractName == 'DBDatabase') {
         return "./zsc/db/Database/db_database.sol:DBDatabase";
+
     } else if (contractName == 'FactoryPro') {
         return "./zsc/factory_pro.sol:FactoryPro";
+
     } else if (contractName == 'FactoryRec') {
         return "./zsc/factory_pro.sol:FactoryRec";
+
     } else if (contractName == 'FactoryStaker') {
         return "./zsc/factory_staker.sol:FactoryStaker";
+
     } else if (contractName == 'FactoryTmp') {
         return "./zsc/factory_tmp.sol:FactoryTmp";
+
     } else if (contractName == 'FactoryAgr') {
         return "./zsc/factory_agr.sol:FactoryAgr";
+
     } else if (contractName == 'FactoryWalletEth') {
         return "./zsc/factory_wallet_eth.sol:FactoryWalletEth";
+
     } else if (contractName == 'FactoryWalletErc20') {
         return "./zsc/factory_wallet_erc20.sol:FactoryWalletErc20";
+
     } else if (contractName == 'ControlApisAdv') {
         return "./zsc/control_apis_adv.sol:ControlApisAdv";
     } 
@@ -48,34 +66,52 @@ function cC_getCompiledFile(contractName) {
         return compiledZSCTestToken;
     } else if (contractName == "AdmAdv") {
         return compiledAdmAdv;
+
     } else if (contractName == 'LogRecorder') {
         return compiledLogRecorder;
+
     } else if (contractName == 'PosAdv') {
         return compiledPosAdv;
+
+    } else if (contractName == 'SystemOverlayer') {
+        return compiledSystemOverlayer;
+
     } else if (contractName == 'WalletManager') {
         return compiledWalletManager;
+
     } else if (contractName == 'SimulatorManager') {
         return compiledSimulatorManager;
+
     } else if (contractName == 'FactoryManager') {
         return compiledSimulatorManager;
+
     } else if (contractName == 'DatabaseManager') {
         return compiledDatabaseManager;
+
     } else if (contractName == 'DBDatabase') {
         return compiledDatabase;
+
     } else if (contractName == 'FactoryPro') {
         return compiledFactoryPro;
+
     } else if (contractName == 'FactoryRec') {
         return compiledFactoryRec;
+
     } else if (contractName == 'FactoryStaker') {
         return compiledFactoryStaker;
+
     } else if (contractName == 'FactoryTmp') {
         return compiledFactoryTmp;
+
     } else if (contractName == 'FactoryAgr') {
         return compiledFactoryAgr;
+
     } else if (contractName == 'FactoryWalletEth') {
         return compiledFactoryWalletEth;
+
     } else if (contractName == 'FactoryWalletErc20') {
         return compiledFactoryWalletErc20;
+
     } else if (contractName == 'ControlApisAdv') {
         return compiledApisAdv;
     }
