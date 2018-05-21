c8bfc02707bdaa19043a5a9de3c2f0c2fb06b83b
Eric-Yao <norhustar@hotmail.com>
2018-05-21 17:03:39 +0800

    updated contract SysGmDb

--- Database/sys_gm_db.sol
+++ Database/sys_gm_db.sol
@@ -11,31 +11,15 @@ contract SysGmDb is SysComGroup {
     }
 
     /*
-      internal functions
-    */
-    function operateNodeParameter(bytes32 _operation, bytes32 _nodeName, bytes32 _parameter, string _value) internal returns (bool) {
-        string memory str = ""; 
-        str = PlatString.append(str, PlatString.bytes32ToString(_nodeName), " : " );
-        str = PlatString.append(str, PlatString.bytes32ToString(_parameter), " : " , _value);
-        if (_operation == "add") {
-            str = PlatString.append("addNodeParameter - ", str);
-            ret = DBNode(ndAdr).addParameter(_parameter);
-        } else if (_operation == "set") {
-            str = PlatString.append("setNodeParameter - ", str);
-            ret = DBNode(ndAdr).setParameter(_parameter, _value);
-        }        
-        addLog(str, true);
-        return ret;
-    }
-
-    /*
       public functions
     */
-    function removeAdr(bytes32 _name) public returns (address) {
+    function removeAdr(bytes32 _dbName) public returns (bool) {
         checkDelegate(msg.sender, 1); 
-        Object(adr).kill();
 
-        return super.removeAdr(_name);
+        address dbAdr = getAdr(_dbName);
+        Object(dbAdr).kill();
+
+        return super.removeAdr(_dbName);
     }
 }
 
