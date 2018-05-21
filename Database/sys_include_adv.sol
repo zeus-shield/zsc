c90b56a41e5feb3b7933e6f31e782060c37522e3
Eric-Yao <norhustar@hotmail.com>
2018-05-21 12:31:53 +0800

    updated abstract contracts

--- Database/sys_include_adv.sol
+++ Database/sys_include_adv.sol
@@ -4,7 +4,7 @@ Copyright (c) 2018, ZSC Dev Team
 
 pragma solidity ^0.4.21;
 
-contract PosManager is Object {
+contract PosManager {
     function registerStaker(address _nodeAddress) public;
     function removeStaker(address _nodeAddress) public;
 
@@ -12,7 +12,7 @@ contract PosManager is Object {
     function getBlockInfoByIndex(uint _poolIndex, uint _blockIndex) public constant returns (uint, uint, uint);
 }
 
-contract WalletManager is Object {
+contract WalletManager {
     function initWalletManager(address _controller, address _database) public;
     function doesTokenContractAdded() public constant returns (bool);
     function addTokenContract(bytes32 _name, bytes32 _symbol, uint _decimals, address _tokenAdr) public returns (bool);
@@ -24,15 +24,17 @@ contract WalletManager is Object {
     function getTokenInfoByIndex(uint _index) public constant returns (bytes32, bytes32, bytes32, uint, address);
     function enableWalletByUser(bytes32 _user, bytes32 _tokeSymbol, address _creator) public returns (address);
 
-    function conductPurchaseAgreement(bool _isFirstSubmit, bytes32 _userName, bytes32 _agrName, address _sigAdr) internal returns (uint);
+    function conductPurchaseAgreement(bool _isFirstSubmit, bytes32 _userName, bytes32 _agrName, address _sigAdr) public returns (uint);
+    function conductPublishAgreement(bytes32 _userName, bytes32 _agrName, address _creator) public returns (uint);
+    function conductInformTransaction(bytes32 _userName, bytes32 _enName, address _dest, uint256 _amount) public returns (bool);
     function formatWalletName(bytes32 _userName, bytes32 _tokenSymbol) public pure returns (bytes32);
 }
 
-contract SimulatorManager is Object {
+contract SimulatorManager {
     function addSimulationRun(uint _proLevel, uint _price, uint _lockedAmount, uint _end, address _agrWallet, address _proWallet, address _recWallet) public returns (bytes32);
     function runSimulation(uint _steps) public;
 }
 
 contract FactoryManager {
-    function createFactoryNode(bytes32 _type, bytes32 _userName, bytes32 _nodeName, bytes32 _extra, address _creator) public returns (address) {
+    function createFactoryNode(bytes32 _type, bytes32 _userName, bytes32 _nodeName, bytes32 _extra, address _creator) public returns (address);
 }
