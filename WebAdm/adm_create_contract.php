f4a63e994cc26756e74874b9c00966078311dfce
Eric-Yao <norhustar@hotmail.com>
2018-05-23 21:32:27 +0800

    updated php/html contents

--- WebAdm/adm_create_contract.php
+++ WebAdm/adm_create_contract.php
@@ -8,16 +8,16 @@ Copyright (c) 2018 ZSC Dev Team
 <html>
 <head>
 <?php 
-    include("adm_header.php"); 
-    $htmlModules= new ZscHtmlModules();
-    echo $htmlModules->loadScriptFiles();
+include("adm_header.php");
+
+    $htmlObjects = new ZSCHtmlObjects();
+    echo $htmlObjects->loadScriptFiles();
 ?>
 <script type="text/javascript">
     var web3 = setupWeb3js(false);
     
     function gotoConfigureLogRecorder() {
-        var href = "adm_configure_logrecorder.php";
-        href    += cC_getUrlSuffixForControlPage();
+        var href = "adm_configure_logrecorder.php" + cC_getUrlSuffixForControlPage();
         window.location.href = href;
     }
 
@@ -26,12 +26,12 @@ Copyright (c) 2018 ZSC Dev Team
 <body>
 
 <?php 
-    echo $htmlModules->loadHeader();
+    echo $htmlObjects->loadHeader();
 
     echo '<div class="page-header"> <font size="5" color="blue" >Setup ZSC system in the testing envrioment</font></div>';
-    echo $htmlModules->loadAllAdrs();
+    echo $htmlObjects->loadAllAdrs();
 
-    echo $htmlModules->loadCreateContract('cC_setupContract');
+    echo $htmlObjects->loadCreateContract('cC_setupContract');
 ?>
     <div class="well">
         <button type="button" onClick="gotoConfigureLogRecorder()">Next: configure log recorder</button>
