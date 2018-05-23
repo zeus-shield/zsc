b7ea498b7a08aa30de689a829e0a8f6f4bfd122a
Eric-Yao <norhustar@hotmail.com>
2018-05-23 21:33:07 +0800

    updated php/html contents

--- WebAdm/adm_configure_logrecorder.php
+++ WebAdm/adm_configure_logrecorder.php
@@ -10,9 +10,9 @@ include("adm_header.php");
 
 $urlSuffixTag = false;
 
-$htmlModules= new ZscHtmlModules();
+$htmlObjects= new ZSCHtmlObjects();
 
-function recordSystemModuleAdrs($modules) {
+function recordSystemObjectAdrs($modules) {
     $system_modules = $modules->getModuleArray();
     $num = count($system_modules);
     
@@ -22,72 +22,75 @@ function recordSystemModuleAdrs($modules) {
 
         if(!empty($adr)){
             $urlSuffixTag = true;
-            $modules->writeModuleAddress($module, $adr);
+            $modules->writeObjectAddress($module, $adr);
         }
     }
 }
-recordSystemModuleAdrs($htmlModules);
+recordSystemObjectAdrs($htmlObjects);
 ?>
 
 <html>
 <head>
-<?php echo $htmlModules->loadScriptFiles(); ?>
+<?php echo $htmlObjects->loadScriptFiles(); ?>
 <script type="text/javascript">
     var web3 = setupWeb3js(false);
     
+    var recorderAdr = "<?php echo $htmlObjects->readModuleAddress('LogRecorder')?>";
+    var zscTokenAddress = "<?php echo $htmlObjects->readModuleAddress('zscTokenAddress')?>";
+    var factoryModuleAdrs = <?php echo $htmlObjects->getFactoryModuleAddressArrayInString()?>;
+
+    var zscSetup = new ZSCSetup(recorderAdr, zscTokenAddress, logedModuleAdrs);
+
     function getObjectAdr(module) {
         var adr;
-        if (module == "TestToken") adr = "<?php echo $htmlModules->readObjectAddress('TestToken')?>";        
-        else if (module == "LogRecorder") adr = "<?php echo $htmlModules->readObjectAddress('LogRecorder')?>";        
+        if (module == "TestToken") adr = "<?php echo $htmlObjects->readObjectAddress('TestToken')?>";        
+        else if (module == "LogRecorder") adr = "<?php echo $htmlObjects->readObjectAddress('LogRecorder')?>";        
 
-        else if (module == "DBDatabase") adr = "<?php echo $htmlModules->readObjectAddress('DBDatabase')?>";
-        else if (module == "AdmAdv") adr = "<?php echo $htmlModules->readObjectAddress('AdmAdv')?>";        
-        else if (module == "PosAdv") adr = "<?php echo $htmlModules->readObjectAddress('PosAdv')?>";
-        else if (module == "WalletManager") adr = "<?php echo $htmlModules->readObjectAddress('WalletManager')?>";
-        else if (module == "SimulatorManager") adr = "<?php echo $htmlModules->readObjectAddress('SimulatorManager')?>";
-        else if (module == "DatabaseManager") adr = "<?php echo $htmlModules->readObjectAddress('DatabaseManager')?>";
-        else if (module == "FactoryManager") adr = "<?php echo $htmlModules->readObjectAddress('FactoryManager')?>";
-        else if (module == "SystemOverlayer") adr = "<?php echo $htmlModules->readObjectAddress('SystemOverlayer')?>";
-        else if (module == "ControlApisAdv") adr = "<?php echo $htmlModules->readObjectAddress('ControlApisAdv')?>";
+        else if (module == "DBDatabase") adr = "<?php echo $htmlObjects->readObjectAddress('DBDatabase')?>";
+        else if (module == "AdmAdv") adr = "<?php echo $htmlObjects->readObjectAddress('AdmAdv')?>";        
+        else if (module == "PosAdv") adr = "<?php echo $htmlObjects->readObjectAddress('PosAdv')?>";
+        else if (module == "WalletManager") adr = "<?php echo $htmlObjects->readObjectAddress('WalletManager')?>";
+        else if (module == "SimulatorManager") adr = "<?php echo $htmlObjects->readObjectAddress('SimulatorManager')?>";
+        else if (module == "DatabaseManager") adr = "<?php echo $htmlObjects->readObjectAddress('DatabaseManager')?>";
+        else if (module == "FactoryManager") adr = "<?php echo $htmlObjects->readObjectAddress('FactoryManager')?>";
+        else if (module == "SystemOverlayer") adr = "<?php echo $htmlObjects->readObjectAddress('SystemOverlayer')?>";
+        else if (module == "ControlApisAdv") adr = "<?php echo $htmlObjects->readObjectAddress('ControlApisAdv')?>";
 
 
-        else if (module == "FactoryPro") adr = "<?php echo $htmlModules->readObjectAddress('FactoryPro')?>";
-        else if (module == "FactoryRec") adr = "<?php echo $htmlModules->readObjectAddress('FactoryRec')?>";
-        else if (module == "FactoryTmp") adr = "<?php echo $htmlModules->readObjectAddress('FactoryTmp')?>";
-        else if (module == "FactoryAgr") adr = "<?php echo $htmlModules->readObjectAddress('FactoryAgr')?>";
+        else if (module == "FactoryPro") adr = "<?php echo $htmlObjects->readObjectAddress('FactoryPro')?>";
+        else if (module == "FactoryRec") adr = "<?php echo $htmlObjects->readObjectAddress('FactoryRec')?>";
+        else if (module == "FactoryTmp") adr = "<?php echo $htmlObjects->readObjectAddress('FactoryTmp')?>";
+        else if (module == "FactoryAgr") adr = "<?php echo $htmlObjects->readObjectAddress('FactoryAgr')?>";
 
-        else if (module == "FactoryWalletEth") adr = "<?php echo $htmlModules->readObjectAddress('FactoryWalletEth')?>";
-        else if (module == "FactoryWalletErc20") adr = "<?php echo $htmlModules->readObjectAddress('FactoryWalletErc20')?>";
+        else if (module == "FactoryWalletEth") adr = "<?php echo $htmlObjects->readObjectAddress('FactoryWalletEth')?>";
+        else if (module == "FactoryWalletErc20") adr = "<?php echo $htmlObjects->readObjectAddress('FactoryWalletErc20')?>";
 
         return adr;
     }
 
     function registerToLogRecorder(module, elementId) {
-        logAdr = "<?php echo $htmlModules->readObjectAddress('LogRecorder')?>";
-        sF_registerListenerToLogRecorder(logAdr,  getObjectAdr(module), module, elementId);
+        var logAdr = "<?php echo $htmlObjects->readObjectAddress('LogRecorder')?>";
+        zscSetup.registerListenerToLogRecorder(logAdr,  getObjectAdr(module), module, elementId);
     }
 
     function setLogRecorderToListener(module, elementId) {
-        logAdr = "<?php echo $htmlModules->readObjectAddress('LogRecorder')?>";
-        sF_setLogRecorderToListener(logAdr,  getObjectAdr(module), module, elementId);
+        var logAdr = "<?php echo $htmlObjects->readObjectAddress('LogRecorder')?>";
+        zscSetup.setLogRecorderToListener(logAdr,  getObjectAdr(module), module, elementId);
     }
 
 </script>
 </head>
 <body>
-<?php echo $htmlModules->loadHeader();?>
-<script type="text/javascript">
-    var web3 = setupWeb3js(false);
-    
-</script>
+<?php echo $htmlObjects->loadHeader();?>
+
 </head>
 <body>
-<?php echo $htmlModules->loadAllAdrs();
+<?php echo $htmlObjects->loadAllAdrs();
     echo '<div class="page-header"> <font size="5" color="blue" >Setup ZSC system in the testing envrioment</font></div>';
 
-    echo $htmlModules->loadRegisterLogRecorderHtml('registerToLogRecorder');
+    echo $htmlObjects->loadRegisterLogRecorderHtml('registerToLogRecorder');
     
-    echo $htmlModules->loadSetLogRecorderHtml('setLogRecorderToListener');
+    echo $htmlObjects->loadSetLogRecorderHtml('setLogRecorderToListener');
 
     echo '<div class="well">
         <button type="button" onClick="window.location.href=\'./adm_control_apis_adv.php\'">Next: go to control page</button>
