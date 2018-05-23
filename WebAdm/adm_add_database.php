6510c56a4a3046ed745eb1b5cd330d43104b4a8a
Eric-Yao <norhustar@hotmail.com>
2018-05-23 21:00:30 +0800

    updated php content

--- WebAdm/adm_add_database.php
+++ WebAdm/adm_add_database.php
@@ -6,16 +6,16 @@ Copyright (c) 2018 ZSC Dev Team
 
 <?php
 include("adm_header.php");
-$htmlModules = new ZscHtmlModules();
+$htmlObjects = new ZSCHtmlObjects();
 ?>
 
 <html>
 <head>
-<?php echo $htmlModules->loadScriptFiles(); ?>
+<?php echo $htmlObjects->loadScriptFiles(); ?>
 <script type="text/javascript">
-    var recorderAdr = "<?php echo $htmlModules->readModuleAddress('LogRecorder')?>";
-    var zscTokenAddress = "<?php echo $htmlModules->readModuleAddress('zscTokenAddress')?>";
-    var factoryModuleAdrs = <?php echo $htmlModules->getFactoryModuleAddressArrayInString()?>;
+    var recorderAdr = "<?php echo $htmlObjects->readModuleAddress('LogRecorder')?>";
+    var zscTokenAddress = "<?php echo $htmlObjects->readModuleAddress('zscTokenAddress')?>";
+    var factoryModuleAdrs = <?php echo $htmlObjects->getFactoryModuleAddressArrayInString()?>;
 
     var web3 = setupWeb3js(false);
     var zscSetup = new ZSCSetup(recorderAdr, zscTokenAddress, logedModuleAdrs);
@@ -27,9 +27,9 @@ $htmlModules = new ZscHtmlModules();
 </head>
 <body>
 <?php 
-    echo $htmlModules->loadHeader(); 
+    echo $htmlObjects->loadHeader(); 
     echo '<div class="page-header"> <font size="5" color="blue" >Setup ZSC system in the testing envrioment</font></div>';
-    echo $htmlModules->loadAllAdrs();
+    echo $htmlObjects->loadAllAdrs();
 ?>
 
     <div class="well">
