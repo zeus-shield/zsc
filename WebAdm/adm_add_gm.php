ba8e1bf8d1cdaec6b4d84d5ffd13803988f22570
Eric-Yao <norhustar@hotmail.com>
2018-05-23 22:19:17 +0800

    added file adm_add_gm.php

---
+++ WebAdm/adm_add_gm.php
@@ -0,0 +1,41 @@
+<?php 
+/*
+Copyright (c) 2018 ZSC Dev Team
+*/
+?>
+
+<?php
+include("adm_header.php");
+$htmlObjects = new ZSCHtmlObjects();
+?>
+
+<html>
+<head>
+<?php echo $htmlObjects->loadScriptFiles(); ?>
+<script type="text/javascript">
+    var recorderAdr = "<?php echo $htmlObjects->readModuleAddress('LogRecorder')?>";
+    var zscTokenAddress = "<?php echo $htmlObjects->readModuleAddress('zscTokenAddress')?>";
+    var factoryModuleAdrs = <?php echo $htmlObjects->getFactoryModuleAddressArrayInString()?>;
+
+    var web3 = setupWeb3js(false);
+    var zscSetup = new ZSCSetup(recorderAdr, zscTokenAddress, logedModuleAdrs);
+
+    function addGM(module, elementId) {
+        zscSetup.addGMModule(module, elementId);
+    }
+</script>
+</head>
+<body>
+<?php 
+    echo $htmlObjects->loadHeader(); 
+    echo '<div class="page-header"> <font size="5" color="blue" >Setup ZSC system in the testing envrioment</font></div>';
+    echo $htmlObjects->loadAllAdrs();
+?>
+
+    <div class="well">
+        <?php echo $htmlObjects->loadAddGMs('addGM');?>
+    </div>
+
+</body>
+</html>
+
