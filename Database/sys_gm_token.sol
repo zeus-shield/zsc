0214e8db2be243043b6ebff50db56269607c6989
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-05-28 09:42:06 +0800

    implement function 'addToken' for sys_gm_token.sol

--- Database/sys_gm_token.sol
+++ Database/sys_gm_token.sol
@@ -46,8 +46,18 @@ contract SysGmToken is SysComModule {
         return exists_[_symbol];
     }
 
-    function addToken(bytes32 _name, bytes32 _symbol, uint _decimals, address _tokenAdr) public returns (bool) {
-        /* TODO */
+    function addToken(bytes32 _name, bytes32 _symbol, uint _decimals, address _address) public returns (bool) {
+        /* check delegate */
+        checkDelegate(msg.sender, 1);
+
+        /* check exist */
+        require(false == exists_[_symbol]);
+
+        exists_[_symbol] = true;
+        indexs_[_symbol] = number_;
+        tokens_[number_] = TokenInfo("true", _name, _symbol, _decimals, _address);
+        number_ ++;
+
         return true;
     }
 
