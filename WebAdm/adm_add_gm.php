959396a23a97e20e59aa12ede4e561d2cf402652
Eric-Yao <norhustar@hotmail.com>
2018-05-29 23:59:24 +0800

    update php content

--- WebAdm/adm_add_gm.php
+++ WebAdm/adm_add_gm.php
@@ -13,9 +13,9 @@ $htmlObjects = new ZSCHtmlObjects();
 <head>
 <?php echo $htmlObjects->loadScriptFiles(); ?>
 <script type="text/javascript">
-    var recorderAdr = "<?php echo $htmlObjects->readModuleAddress('LogRecorder')?>";
-    var zscTokenAddress = "<?php echo $htmlObjects->readModuleAddress('zscTokenAddress')?>";
-    var factoryModuleAdrs = <?php echo $htmlObjects->getFactoryModuleAddressArrayInString()?>;
+    var recorderAdr = "<?php echo $htmlObjects->readObjectAddress('LogRecorder')?>";
+    var zscTokenAddress = "<?php echo $htmlObjects->readObjectAddress('TestToken')?>";
+    var logedModuleAdrs = <?php echo $htmlObjects->getLogedObjectAddressArrayInString()?>;
 
     var web3 = setupWeb3js(false);
     var zscSetup = new ZSCSetup(recorderAdr, zscTokenAddress, logedModuleAdrs);
@@ -28,7 +28,7 @@ $htmlObjects = new ZSCHtmlObjects();
 <body>
 <?php 
     echo $htmlObjects->loadHeader(); 
-    echo '<div class="page-header"> <font size="5" color="blue" >Setup ZSC system in the testing envrioment</font></div>';
+    echo '<div class="page-header"> <font size="5" color="blue" >Add Component - Manager </font></div>';
     echo $htmlObjects->loadAllAdrs();
 ?>
 
