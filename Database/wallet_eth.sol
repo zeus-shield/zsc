12833a4750ce7c2ee9f0fb1dccdac471420da3ff
Eric-Yao <norhustar@hotmail.com>
2018-05-30 00:52:31 +0800

    updated contract WalletEth

--- Database/wallet_eth.sol
+++ Database/wallet_eth.sol
@@ -10,42 +10,31 @@ contract WalletEth is WalletBase {
 
     // Constructor
     function WalletEth(bytes32 _name) public WalletBase(_name) {
-        setNodeType("wallet-eth"); 
+        nodeType_ = "wallet-eth"; 
     }
 
     function() public payable {        
+        // must not less than 0.01 ether
         if (msg.value < (1 ether) / 100) {
             revert();
         } else {
-            bytes32 strData = PlatString.tobytes32(string(msg.data));
-            recordInput(msg.sender, msg.value, strData);
-            changeValue(true, false, msg.value);
+            recordInput(msg.sender, msg.value);
+            changeValue(true, msg.value);
         }
     }
 
-    function getBlance(bool _locked) public constant returns (uint256) {
+    function getBlance() public constant returns (uint) {
         checkDelegate(msg.sender, 1);
-        uint total = address(this).balance;
-        uint amount;
-
-        if (_locked) { 
-            amount = super.getBlance(true);
-            require(amount <= total);
-        } else {
-            amount = super.getBlance(false);
-            require(amount == total);
-        }
-        return amount;
+        return address(this).balance;
     }
 
     function executeTransaction(address _dest, uint256 _amount, bytes _data) public returns (uint) {
         checkDelegate(msg.sender, 1);
         checkBeforeSent(_dest, _amount);        
-        bytes32 strData = PlatString.tobytes32(string(_data));
 
         if (_dest.call.value(_amount)(_data)) {
-            recordOut(_dest, _amount, strData);
-            changeValue(false, strData == "locked", _amount);
+            recordOut(_dest, _amount);
+            changeValue(false,  _amount);
             return _amount;
         } else {
             return 0;
