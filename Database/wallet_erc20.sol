e2ef94b6981193047b6aac0573b588a94fb480fb
Eric-Yao <norhustar@hotmail.com>
2018-05-30 00:52:22 +0800

    updated contract WalletErc20

--- Database/wallet_erc20.sol
+++ Database/wallet_erc20.sol
@@ -11,7 +11,7 @@ contract WalletErc20 is WalletBase {
 
     // Constructor
     function WalletErc20(bytes32 _name) public WalletBase(_name) {
-        setNodeType("wallet-erc20"); 
+        nodeType_ = "wallet-erc20";
     }
 
     function setERC20TokenAddress(address _tokenAdr) public {
@@ -19,29 +19,19 @@ contract WalletErc20 is WalletBase {
         _erc20TokenAdr = _tokenAdr;
     }
 
-    function getBlance(bool _locked) public constant returns (uint256) {
+    function getBlance() public constant returns (uint) {
         checkDelegate(msg.sender, 1);
         uint total = ERC20Interface(_erc20TokenAdr).balanceOf(address(this));
-        uint amount;
-
-        if (_locked) { 
-            amount = super.getBlance(true);
-            require(amount <= total);
-        } else {
-            amount = super.getBlance(false);
-            require(amount == total);
-        }
-        return amount;
+        return total;
     }
 
-    function executeTransaction(address _dest, uint256 _amount, bytes _data) public returns (uint) {
+    function executeTransaction(address _dest, uint256 _amount) public returns (uint) {
         checkDelegate(msg.sender, 1);
         checkBeforeSent(_dest, _amount);
-        bytes32 strData = PlatString.tobytes32(string(_data));
 
         if (ERC20Interface(_erc20TokenAdr).transfer(_dest, _amount)) {
-            recordOut(_dest, _amount, strData);
-            changeValue(false, strData == "locked", _amount);
+            recordOut(_dest, _amount);
+            changeValue(false, _amount);
             return _amount;
         } else {
             return 0;
@@ -51,7 +41,7 @@ contract WalletErc20 is WalletBase {
     function informTransaction(address _src, address _dest, uint256 _amount) public {
         require(_dest == address(this));
         checkDelegate(msg.sender, 1);
-        recordInput(_src, _amount, "");
-        changeValue(true, false, _amount);
+        recordInput(_src, _amount);
+        changeValue(true, _amount);
     }
 }
