eb97586131042045a703f41bf0907e5eef956635
Eric-Yao <norhustar@hotmail.com>
2018-05-31 17:20:01 +0800

    updated class ZSCUserManagement

--- WebAdm/js/zsc_user_management.js
+++ WebAdm/js/zsc_user_management.js
@@ -11,32 +11,32 @@ function ZSCUserManagement(admAdr, abi) {
     this.userNodeAdr = [];
     this.userEthAdr = [];
     this.userZscAdr = [];
+    this.userActived = [];
     this.userNos = 0;
     this.account = web3.eth.accounts[0];
     this.myAdmAdv = web3.eth.contract(abi).at(admAdr);
+    this.phpCallback;
 }
 
 ZSCUserManagement.prototype = new ZSCJsBase();
 
-ZSCUserManagement.prototype.addUser = function(userNameId, hashId, func){
+ZSCUserManagement.prototype.addUser = function(userNameId, hashId){
     var userName = document.getElementById(userNameId).value; 
-    this.myAdmAdv.addUser(userName, {from: this.account, gas: 9000000},
-    function(error, result){ 
-        if(!error) cC_showHashResultTest(hashId, result, function(){console.log("ok");});
-        else console.log("error: " + error);
+    this.myAdmAdv.addUser(userName, 
+        {from: this.account, gas: 9000000},
+        function(error, result){ 
+            if(!error) cC_showHashResultTest(hashId, result, function() {window.location.reload(true);});
+            else console.log("error: " + error);
     });
 }  
 
 //ZSCUserMangement.prototype.setUserActiveStatus = (username, status, elementId, function() {}
 
 
-ZSCUserManagement.prototype.loadUsers = function(func) {
+ZSCUserManagement.prototype.loadUsers = function(phpFunc) {
+    this.phpCallback = phpFunc;
     this.numUsers(this, function(gm) {
-        gm.loadUserInfos(function(index){
-            if (index == gm.parameNos - 1) {
-                func();
-            }
-        });
+        gm.loadUserInfos();
     });
 }
 
@@ -53,12 +53,12 @@ ZSCUserManagement.prototype.numUsers = function(gm, func) {
          });
 }
 
-ZSCUserManagement.prototype.loadUserInfos = function(func) {
+ZSCUserManagement.prototype.loadUserInfos = function() {
     for (var i = 0; i < this.userNos; ++i) {
         this.loadUserInfoByIndex(this, i, function(gm, index, userInfo) {
-                gm.parserUserInfo(userInfo);
+                gm.parserUserInfo(index, userInfo);
                 if (index == gm.userNos - 1) {
-                    func(index);
+                    gm.phpCallback();
                  }
             });
     } 
@@ -77,75 +77,99 @@ ZSCUserManagement.prototype.loadUserInfoByIndex = function(gm, index, func) {
         });
 }
 
-ZSCUserManagement.prototype.parserUserInfo = function(info) {
+ZSCUserManagement.prototype.parserUserInfo = function(index, info) {
     var len        = info.length;
     var offset     = info.indexOf("?");
     var newsidinfo = info.substr(offset,len)
     var newsids    = newsidinfo.split("&");
-    var index      = this.userNos;
 
     var userName    = newsids[0];
-    var userStatus  = newsids[1];
-    var userType    = newsids[2];
-    var userId      = newsids[3];
-    var userNodeAdr = newsids[4];
-    var userEthAdr  = newsids[5];
-    var userZscAdr  = newsids[6];
+    var userActived = newsids[1];
+    var userStatus  = newsids[2];
+    var userType    = newsids[3];
+    var userId      = newsids[4];
+    var userNodeAdr = newsids[5];
+    var userEthAdr  = newsids[6];
+    var userZscAdr  = newsids[7];
 
     this.userName[index]    = userName.split("=")[1];
+    this.userActived[index] = userActived.split("=")[1];
     this.userStatus[index]  = userStatus.split("=")[1];
     this.userType[index]    = userType.split("=")[1];
-    this.userId[index]      = userId.split("=")[1];
-    this.userNodeAdr[index] = userNodeAdr.split("=")[1];
-    this.userEthAdr[index]  = userEthAdr.split("=")[1];
-    this.userZscAdr[index]  = userZscAdr.split("=")[1];
-    this.userNos++;
+    this.userId[index]      = "0x" + userId.split("=")[1];
+    this.userNodeAdr[index] = "0x" + userNodeAdr.split("=")[1];
+    this.userEthAdr[index]  = "0x" + userEthAdr.split("=")[1];
+    this.userZscAdr[index]  = "0x" + userZscAdr.split("=")[1];
 }
 
-ZSCUserManagement.prototype.loadUserManagementHtml = function(showDetails, approve, setStatus, elementId) {
+ZSCUserManagement.prototype.loadUserManagementHtml = function(showDetails, setStatus, elementId) {
     var showPrefix = showDetails + "('"; 
     var showSuffix = "')";
-
-    var approvePrefix = approve + "('"; 
-    var approveSuffix = "')";
 
     var setStatusPrefix = setStatus + "('"; 
     var setStatusSuffix = "')";
 
+    /*
     var text = '<table align="center" style="width:800px;min-height:30px">'
     text += '<tr>'
-    text += '   <td>user name</td>  <td>user type</td>  <td>user status</td>  <td>user id</td> <td>node adr</td> <td>eth adr</td> <td>zsc adr</td> <td>operations</td> <td>user info</td>  <td>log</td>  '
-    text += '</tr>'
+    text += '   <td>user info</td> <td>adrs</td> <td>operations</td> <td>details</td>  '
+    text += '</tr> '
+    text += '<tr> <td>---</td> <td>---</td> <td>---</td> <td>---</td> </tr>'
 
     for (var i = 0; i < this.userNos; ++i) {
-        var name = this.userName[i];
         var hashId = this.userName[i] + "Hash"
         text += '</tr>';
-        text += '   <td><button type="button" onClick="' + showPrefix + "'" + name + "', '" + hashId + "'" + showSuffix + '">Approve</button>'
-        text += '   <td><text>' + names    + '</text></td>'
-        text += '   <td><text>' + this.userType[i]    + '</text></td>'
-        text += '   <td><text>' + this.userStatus[i]  + '</text></td>'
-        text += '   <td><text>' + this.userId[i]      + '</text></td>'
-        text += '   <td><text>' + this.userNodeAdr[i] + '</text></td>'
-        text += '   <td><text>' + this.userEthAdr[i]  + '</text></td>'
-        text += '   <td><text>' + this.userZscAdr[i]  + '</text></td>'
-        text += '   <td><button type="button" onClick="' + setStatusPrefix + "'" + name + "', '" + ",'true'," + hashId + "'"  + setStatusSuffix + '">Active</button>'
+        text += '   <td><text>   name: ' + this.userName[i]    + '</text><br>'
+        text += '       <text>actived: ' + this.userActived[i] + ' </text><br>'
+        text += '       <text>   type: ' + this.userType[i]    + ' </text><br>'
+        text += '       <text> status: ' + this.userStatus[i]  + ' </text>'
+        text += '   </td>'
+        text += '   <td><text>   id: ' + this.userId[i]      + ' </text><br>'
+        text += '       <text> node: ' + this.userNodeAdr[i] + ' </text><br>'
+        text += '       <text id="'+ hashId + '"> log: </text>'
+        text += '   </td>'
+        text += '   <td><button type="button" onClick="' + setStatusPrefix + "'" + name + "', '" + ",'true'," + hashId + "'"  + setStatusSuffix + '">Active</button><br>'
         text += '       <button type="button" onClick="' + setStatusPrefix + "'" + name + "', '" + ",'false',"+ hashId + "'" + setStatusSuffix + '">Deactive</button>'
         text += '   </td>'
-        text += '   <td><button type="button" onClick="' + showPrefix + "'wallets', '" + name + "', '" + hashId + "'" + showSuffix + '">Wallets</button>'
-        text += '       <button type="button" onClick="' + showPrefix + "'templates', '" + name + "', '" + hashId + "'" + showSuffix + '">Templates</button>'
+        text += '   <td><button type="button" onClick="' + showPrefix + "'wallets', '" + name + "', '" + hashId + "'" + showSuffix + '">Wallets</button><br>'
+        text += '       <button type="button" onClick="' + showPrefix + "'templates', '" + name + "', '" + hashId + "'" + showSuffix + '">Templates</button><br>'
         text += '       <button type="button" onClick="' + showPrefix + "'agreements', '" + name + "', '" + hashId + "'" + showSuffix + '">Agreements</button>'
         text += '   </td>'
-        text += '   <td><text id="'+ hashId + '"></text></td>'
         text += '</tr>'
+        text += '<tr> <td>---</td> <td>---</td> <td>---</td> <td>---</td> </tr>'
+    }
+    */
+
+    var text = ' <text id="ButtonHashId"> </text>'
+    text += '<table align="center" style="width:800px;min-height:30px">'
+    text += '<tr>'
+    text += '   <td>name</td> <td>actived</td> <td>status</td> <td>type</td>  <td>active</td>  <td>deactive</td>  <td>detail</td> '
+    text += '</tr> '
+    text += '<tr> <td>---</td> <td>---</td> <td>---</td> <td>---</td> <td>---</td> <td>---</td> <td>---</td>  </tr>'
+
+    var hashId = "ButtonHashId";
+    for (var i = 0; i < this.userNos; ++i) {
+        text += '</tr>';
+        text += '   <td><text> ' + this.userName[i]    + '</text></td>'
+        text += '   <td><text> ' + this.userActived[i] + ' </text></td>'
+        text += '   <td><text> ' + this.userType[i]    + ' </text></td>'
+        text += '   <td><text> ' + this.userStatus[i]  + ' </text></td>'
+        text += '   <td><button type="button" onClick="' + setStatusPrefix + this.userName[i] + "', 'true', '"  + hashId + setStatusSuffix + '">Active</button></td>'
+        text += '   <td><button type="button" onClick="' + setStatusPrefix + this.userName[i] + "', 'false', '" + hashId + setStatusSuffix + '">Deactive</button></td>'
+        text += '   <td><button type="button" onClick="' + showPrefix + showSuffix + '">Details</button></td>'
+        text += '</tr>'
+        text += '<tr> <td>---</td> <td>---</td> <td>---</td> <td>---</td> <td>---</td> <td>---</td> <td>---</td> </tr>'
     }
     document.getElementById(elementId).innerHTML = text;  
 }
 
-ZSCUserManagement.prototype.setUserStatus = function(userName, status, func) {
-    this.myAdmAdv.setUserStatus(userName, status, {from: this.account, gas: 9000000},
+ZSCUserManagement.prototype.setUserActiveState = function(userName, status, hashId) {
+    var tag;
+    if (status == "false") tag = false;
+    else tag = true;
+    this.myAdmAdv.setUserActiveState(userName, tag, {from: this.account, gas: 9000000},
         function(error, result) { 
-            if(!error) cC_showHashResult(hashId, result, func)
+            if(!error) cC_showHashResultTest(hashId, result, function() {window.location.reload(true);})
              else console.log("error: " + error);
         });
 }
