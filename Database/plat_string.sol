09d2dcf56fa4809d8855a7549a1d0e40523b83c9
Eric-Yao <norhustar@hotmail.com>
2018-06-01 23:54:16 +0800

    updated function uintToString()

--- Database/plat_string.sol
+++ Database/plat_string.sol
@@ -197,6 +197,7 @@ library PlatString {
         return result;
     }
 
+    /*
     function uintToString(uint v) internal pure returns (string) {
         uint maxlength = 100;
         bytes memory reversed = new bytes(maxlength);
@@ -212,5 +213,26 @@ library PlatString {
         }
         return string(s);
     }
+    */
 
+    // Original file at 
+    // https://github.com/oraclize/ethereum-api/blob/master/oraclizeAPI_0.5.sol
+    // function uint2str(uint i) internal pure returns (string);
+    function uintToString(uint i) internal pure returns (string) {
+        if (i == 0) return "0";
+        
+        uint j = i;
+        uint len;
+        while (j != 0){
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len - 1;
+        while (i != 0){
+            bstr[k--] = byte(48 + i % 10);
+            i /= 10;
+        }
+        return string(bstr);
+    }
 }
