41d895df79c78af87c7f196a702c81396c4966bc
Eric-Yao <norhustar@hotmail.com>
2018-06-02 21:32:50 +0800

    updated class ZSCEelement

--- WebClient/zsc_element.js
+++ WebClient/zsc_element.js
@@ -34,10 +34,11 @@ ZSCElement.prototype.doesElementExisit = function(func) {
 
 ZSCElement.prototype.loadParameterNamesAndvalues = function(func) {
     var gm = this;
+    var callBack = func;
 
     gm.numParameters(gm, function() {
-        gm.loadParameterNames(gm, function() {
-            gm.loadParameterValues(gm, function(index){
+        gm.loadParameterNames(gm, function(gm) {
+            gm.loadParameterValues(gm, function(gm, index){
                 if (index == gm.parameNos - 1) {
                     callBack();
                 }
@@ -51,11 +52,11 @@ ZSCElement.prototype.numParameters = function(gm, func) {
     var myControlApi = web3.eth.contract(gm.contractAbi).at(gm.contractAdr);
 
     myControlApi.numElementParameters(gm.userName, gm.enName, 
-        {from: this.getAccount()},
+        {from: gm.account},
         function(error, num){ 
             if(!error) { 
                 gm.parameNos = num.toString(10); 
-                func();
+                func(gm);
             } else {
                 console.log("error: " + error);
             }
@@ -67,10 +68,10 @@ ZSCElement.prototype.loadParameterNames = function(gm, func) {
     var myControlApi = web3.eth.contract(gm.contractAbi).at(gm.contractAdr);
 
     for (var i = 0; i < gm.parameNos; ++i) {
-        loadParameterNameByIndex(gm, i, function(index, para) {
+        gm.loadParameterNameByIndex(gm, i, function(index, para) {
             gm.parameterNames[index] = para;
             if (index == gm.parameNos - 1) {
-                func();
+                func(gm, index);
             }
         });
     } 
@@ -80,8 +81,8 @@ ZSCElement.prototype.loadParameterNameByIndex = function(gm, index, func) {
     var callBack = func;
     var myControlApi = web3.eth.contract(gm.contractAbi).at(gm.contractAdr);
 
-    this.myControlApi.getElementParameterNameByIndex(this.userName, this.enName, index, 
-        {from: this.getAccount()},
+    myControlApi.getElementParameterNameByIndex(gm.userName, gm.enName, index, 
+        {from: gm.account},
         function(error, para){ 
             if(!error) {
                 var ret = web3.toUtf8(para);
@@ -92,23 +93,29 @@ ZSCElement.prototype.loadParameterNameByIndex = function(gm, index, func) {
         });
 }
 
-ZSCElement.prototype.loadParameterValues = function(func) {
-    for (var i = 0; i < this.parameNos; ++i) {
-        loadParameterValueByIndex(i, function(index, value) {
-            if (index == this.parameNos - 1) {
-                func(index);
+ZSCElement.prototype.loadParameterValues = function(gm, func) {
+    var callBack = func;
+    for (var i = 0; i < gm.parameNos; ++i) {
+        gm.loadParameterValueByIndex(gm, i, function(gm, index) {
+            if (index == gm.parameNos - 1) {
+                callBack(gm, index);
             }
         });
     } 
 } 
 
-ZSCElement.prototype.loadParameterValueByIndex = function(index, func){ 
-    this.myControlApi.getElementParameter(this.userName, this.enName, this.getParameterName(index), 
-        {from: this.getAccount()},
+ZSCElement.prototype.loadParameterValueByIndex = function(gm, index, func){ 
+    var callBack = func;
+    var myControlApi = web3.eth.contract(gm.contractAbi).at(gm.contractAdr);
+
+    myControlApi.getElementParameter(gm.userName, gm.enName, gm.parameterNames[index], 
+        {from: gm.account},
         function(error, value){ 
             if(!error) {
-                this.parameterValues[index] = value;
-                func(index, value);
+                gm.parameterValues[index] = web3.toUtf8(value);
+                if (index == gm.parameNos - 1) {
+                     callBack(gm, index);
+                }
             } else { 
                 console.log("error: " + error);
             }
@@ -116,25 +123,26 @@ ZSCElement.prototype.loadParameterValueByIndex = function(index, func){
 }
 
 
-ZSCElement.prototype.setElementParameter = function(logID, hr) {
-    var myControlApi =  this.myControlApi;
+ZSCElement.prototype.setElementParameter = function(logID) {
+    var gm = this;
+    var myControlApi = web3.eth.contract(gm.contractAbi).at(gm.contractAdr);
 
     var info = "";
     var count = 0;
 
-    for (var i = 0; i < uF_parameters.length; ++i) {
-        var value = document.getElementById(this.parameterNames[i]).value;
-        if (value != this.parameterValues[i]) {
+    for (var i = 0; i < gm.parameterNames.length; ++i) {
+        var value = document.getElementById(gm.parameterNames[i]).value;
+        if (value != gm.parameterValues[i]) {
             count ++;
-            this.parameterValues[i] = value;
+            gm.parameterValues[i] = value;
 
-            info += "{<" + this.parameterNames[i] + ">" + "<" + value + ">}";
+            info += "{<" + gm.parameterNames[i] + ">" + "<" + value + ">}";
         }
     }
 
     if (count > 0) {
-        myControlApi.setElementMultipleParameters(hr, info,  
-            {from: this.getAccount(), gasPrice: this.getGasPrice(1), gas : this.getGasLimit(55000)}, 
+        myControlApi.setElementMultipleParameters(gm.userName, gm.enName, info,  
+            {from:gm.account, gas: 9000000},
             function(error, result){ 
                 if(!error) bF_showHashResult(logID, result, function(){});
                 else console.log("error: " + error);
@@ -148,16 +156,21 @@ ZSCElement.prototype.loadParametersHtml = function(elementId, funcName) {
    
     var text ="";
     text += '<div class="well">';
-   
+    text += '<table align="center" style="width:400px;min-height:30px">'
+
     for (var i = 0; i < this.parameNos; ++i) {
-        text += '   <text>' + this.parameterNames[i] + ': </text>'
-        text += '   <input type="text" id="' + this.parameterValues[i] + '"></input>'
+        text += '<tr>'
+        text += '  <td> <text>' + this.parameterNames[i] + ': </text> </td>'
+        text += '  <td> <input type="text" id="' + this.parameterNames[i] + '" value="' + this.parameterValues[i] + '"></input> </td>'
+        text += '</tr>'
     }
-    text += '</div>'
+    text += '</table></div>'
+    text += '<div>'
     text += '   <button type="button" onClick="' + functionInput + '">Submit Changes</button>'
     text += '   <text id="SubmitChangesHash"></text>'
     text += '</div>'
 
     document.getElementById(elementId).innerHTML = text;  
 }
+
 
