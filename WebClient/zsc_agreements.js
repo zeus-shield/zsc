8035d6327ffe19d82b305184c65354f7c060c65e
Eric-Yao <norhustar@hotmail.com>
2018-06-03 01:42:23 +0800

    updated class ZSCAgreement

--- WebClient/zsc_agreements.js
+++ WebClient/zsc_agreements.js
@@ -5,59 +5,87 @@ Copyright (c) 2018 ZSC Dev Team
 //class zscWallet
 function ZSCAgreement(nm, abi, adr) {
     this.userName = nm;
+    this.tmpName;
     this.agrNos = 0;
-    this.agrName = [];
-    this.myControlApi = web3.eth.contract(abi).at(adr);
+    this.agrNames = [];
+    this.balance = [];
+    this.account = web3.eth.accounts[0];
+    this.contractAdr = adr;
+    this.contractAbi = JSON.parse(abi);
 }
-
-ZSCViewAgreement.prototype = new ZSCClient();
 
 ZSCAgreement.prototype.getUserName = function() {return this.userName;}
 
+ZSCAgreement.prototype.setTemplateName = function(name) {this.tmpName = name;}
+
 ZSCAgreement.prototype.loadAgreements = function(func) {
-    this.numAgreements(function() {
-        for (var i = 0; i < this.agrNos; ++i) {
-            this.getAgrNameByIndex(i, function(index){
-                if (indx == this.agrNos - 1) {
-                    func();
-                }
-            });
+    var gm = this;
+    var callBack = func;
+    var myControlApi = web3.eth.contract(gm.contractAbi).at(gm.contractAdr);
+
+    gm.numAgreements(gm, function(gm) {
+        if (gm.agrNos == 0) {
+            callBack();
+        } else {
+            for (var i = 0; i < gm.agrNos; ++i) {
+                gm.getAgrNameByIndex(gm, i, function(gm, j){
+                    callBack();
+                    gm.getAgrBalance(gm, j, function(gm, index) {
+                        if (index == gm.agrNos - 1) {
+                            callBack();
+                        }
+                    });
+                });
+            }
         }
     });
 }
 
-ZSCAgreement.prototype.numAgreements= function(func) {
-    this.myControlApi.numAgreements(this.userName,
-        {from: this.getAccount(), gas: 9000000},
+
+ZSCAgreement.prototype.numAgreements= function(gm, func) {
+    var callBack = func;
+    var myControlApi = web3.eth.contract(gm.contractAbi).at(gm.contractAdr);
+
+    myControlApi.numElementChildren(gm.userName, gm.tmpName,
+        {from: gm.account},
         function(error, result){ 
             if(!error) {
-                this.agrNos = result.toString(10); 
-                func();
+                gm.agrNos = result.toString(10);
+                callBack(gm);
             } else {
                 console.log("error: " + error);
             }
         });
 }
 
-ZSCAgreement.prototype.getAgrNameByIndex = function(index, func) {
-    this.myControlApi.getAgreementNameByIndex(this.userName, index,
-        {from: this.getAccount(), gas: 9000000},
+ZSCAgreement.prototype.getAgrNameByIndex = function(gm, index, func) {
+    var callBack = func;
+    var myControlApi = web3.eth.contract(gm.contractAbi).at(gm.contractAdr);
+    
+    myControlApi.getElementChildNameByIndex(gm.tmpName, index,
+        {from: gm.account},
         function(error, result){ 
             if(!error) {
-                this.agrName[index] = web3.toUtf8(result);
-                func(index);
+                gm.agrNames[index] = web3.toUtf8(result);
+                if (index == gm.agrNos - 1) {
+                    callBack(gm, index);
+                }
             } else {
                 console.log("error: " + error);
             }
         });
 }
 
-ZSCAgreement.prototype.submitPublishAgreement = function(index, func) {
-    this.myControlApi.submitPublishAgreement(this.userName, this.agrNames[index] ,
-        {from: this.getAccount(), gasPrice: this.getGasPrice(1), gas : this.getGasLimit(20)}, 
+ZSCAgreement.prototype.getAgrBalance = function(gm, index, func) {
+    var callBack = func;
+    var myControlApi = web3.eth.contract(gm.contractAbi).at(gm.contractAdr);
+    
+    myControlApi.getElementBalance(gm.agrNames[index], "ZSC",
+        {from: gm.account},
         function(error, result){ 
             if(!error) {
-                func(result);
+                gm.balance[index] = web3.toUtf8(10);
+                callBack(gm, index);
             } else {
                 console.log("error: " + error);
             }
@@ -76,31 +104,25 @@ ZSCAgreement.prototype.confirmPublishAgreement = function(index, func) {
         });
 }
 
-ZSCAgreement.prototype.loadAgreementsHtml = function(elementId, func1, func2, func3)  {
-    var showPrefix = func1 + "('"; 
-    var showSuffix = "')";
-
-    var publishPrefix = func2 + "('"; 
-    var publishSuffix = "')";
-
-    var confirmPrefix = func3 + "('"; 
-    var confirmSuffix = "')";
+ZSCAgreement.prototype.loadAgreementsHtml = function(elementId, tag, func1)  {
+    var funcSetParaPrefix = funcSetPara + "('"; 
+    var funcSetParaSuffix = "')";
 
     var text ="";
     text += '<div class="well">';
-    text += '<table align="center" style="width:800px;min-height:30px">'
+    text += '<table align="center" style="width:700px;min-height:30px">'
     text += '<tr>'
-    text += '   <td>Index</td> <td>Agreement name</td> <td> </td> <td> </td> <td> </td>'
+    text += '   <td>Name</td> <td>Details</td> <td>Balance</td>'
     text += '</tr>'
+    text += '<tr> <td>---</td> <td>---</td> <td>---</td> </tr>'
 
-    for (var i = 0; i < this.tmpNos; ++i) {
+    for (var i = 0; i < this.agrNos; ++i) {
         text += '<tr>'
-        text += '   <td><text>' + i + '</text></td>'
-        text += '   <td><text>' + this.agrName[i] + '</text></td>'
-        text += '   <td><button type="button" onClick="' + showPrefix + this.agrNames[i] + showSuffix + '">Details</button></td>'
-        text += '   <td><button type="button" onClick="' + publishPrefix + i + publishSuffix + '">Publish</button></td>'
-        text += '   <td><button type="button" onClick="' + confirmPrefix + i + confirmSuffix + '">Confirm Publish</button></td>'
+        text += '   <td><text>' + this.agrNames[i]  + '</text></td>'
+        text += '   <td><text>' + this.balance[i]  + '</text></td>'
+        text += '   <td><button type="button" onClick="' + funcSetParaPrefix + this.agrNames[i] + funcSetParaSuffix + '">Edit</button></td>'
         text += '</tr>'
+        text += '<tr> <td>---</td> <td>---</td> <td>---</td>  </tr>'
     }
     text += '</table></div>'
 
