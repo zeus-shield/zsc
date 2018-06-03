bf4434d840b201fe2fa46cd14ce6365c0d2f1b8b
Eric-Yao <norhustar@hotmail.com>
2018-06-03 11:38:26 +0800

    updated contract FactoryBase

--- Database/factory_base.sol
+++ Database/factory_base.sol
@@ -10,10 +10,29 @@ contract FactoryBase is Object {
 	address bindedDB_;
 	address controlApi_;
 
+    uint private factoryNodeNos_ = 0;
+    mapping(uint => bytes32) private factoryNodes_;
+
     function FactoryBase(bytes32 _name) public Object(_name) {
     }
 
+    function addFactoryNode(bytes32 _nodeName) internal {
+        factoryNodes_[factoryNodeNos_] = _nodeName;
+        factoryNodeNos_++;
+    }
+
     function createNode(bytes32 _nodeName, address _parent, address _creator) public returns (address);
+
+    function numFactoryNodes() public constant returns (uint) {
+        checkDelegate(msg.sender, 1);
+        return factoryNodeNos_;
+    }
+
+    function getFactoryNodeNameByIndex(uint _index) public constant returns (bytes32) {
+        checkDelegate(msg.sender, 1);
+        require(_index < factoryNodeNos_);
+        return factoryNodes_[_index];
+    }
 
     function setDatabase(address _adr) public {        
         checkDelegate(msg.sender, 1);
@@ -33,7 +52,7 @@ contract FactoryBase is Object {
         checkDelegate(msg.sender, 1);
         require(_controlApi != 0);
 
-        addLog("-0-", true);
+        addLog("initFactory", true);
 
         if (controlApi_ != _controlApi) {
             if (controlApi_ == 0) {
