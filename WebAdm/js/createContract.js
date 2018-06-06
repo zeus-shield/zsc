0621dc355f2431cfb7fdf702ecb81332b7163c6e
Eric-Yao <norhustar@hotmail.com>
2018-06-06 17:59:43 +0800

    updated function cC_getContractFullName()

--- WebAdm/js/createContract.js
+++ WebAdm/js/createContract.js
@@ -7,33 +7,33 @@ var cC_ModuleAdr = [];
 
 function cC_getContractFullName(contractName) {
     if (contractName == 'TestToken') {
-        return "./zsc/db/Database/ZSCTestToken.sol:ZSCTestToken";
+        return "./zsc/db/EthCore/ZSCTestToken.sol:ZSCTestToken";
     } else if (contractName == 'AdmAdv') {
         return "./zsc/adm_adv.sol:AdmAdv";
 
     } else if (contractName == 'PosAdv') {
-        return "./zsc/db/Database/pos_base.sol:PosBase";
+        return "./zsc/db/EthCore/pos_base.sol:PosBase";
 
     } else if (contractName == 'LogRecorder') {
-        return "./zsc/db/Database/log_recorder.sol:LogRecorder";
+        return "./zsc/db/EthCore/log_recorder.sol:LogRecorder";
 
     } else if (contractName == 'DatabaseManager') {
-        return "./zsc/db/Database/sys_gm_db.sol:SysGmDb";
+        return "./zsc/db/EthCore/sys_gm_db.sol:SysGmDb";
 
     } else if (contractName == 'WalletManager') {
-        return "./zsc/db/Database/sys_gm_wallet.sol:SysGmWallet";
+        return "./zsc/db/EthCore/sys_gm_wallet.sol:SysGmWallet";
 
     } else if (contractName == 'SystemOverlayer') {
-        return "./zsc/db/Database/sys_overlayer.sol:SysOverlayer";
+        return "./zsc/db/EthCore/sys_overlayer.sol:SysOverlayer";
 
     } else if (contractName == 'SimulatorManager') {
-        return "./zsc/db/Database/sys_gm_simulator.sol:SysGmSimulator";
+        return "./zsc/db/EthCore/sys_gm_simulator.sol:SysGmSimulator";
 
     } else if (contractName == 'FactoryManager') {
-        return "./zsc/db/Database/sys_gm_factory.sol:SysGmFactory";
+        return "./zsc/db/EthCore/sys_gm_factory.sol:SysGmFactory";
 
     } else if (contractName == 'DBDatabase') {
-        return "./zsc/db/Database/db_database.sol:DBDatabase";
+        return "./zsc/db/EthCore/db_database.sol:DBDatabase";
 
     } else if (contractName == 'FactoryPro') {
         return "./zsc/factory_pro.sol:FactoryPro";
@@ -176,6 +176,46 @@ function cC_setupContract(contractName, paramId) {
             cC_showCreatingResult("text", contractName + "Log", logResult);
         }
     })
+}
+
+function cC_killContract(contractName, adr) {
+    var logResult = "";
+    var abi = web3.eth.contract(cC_getContractAbi(contractName)); 
+    var account = web3.eth.accounts[0];
+    
+    var myControlApi = web3.eth.contract(abi).at(adr);
+
+    myControlApi.kill(
+        {from: account, gas: 9000000},
+        function(error, result){ 
+            if(!error) {
+                cC_showHashResultTest(contractName + "Log", result, function() {});
+            } else {
+                console.log("error: " + error);
+            }
+        });
+
+
+    var greeter = greeterContract.new(parameter, {account, data: databin, gas: 7500000}, function(e, contract){
+        if(!e) {
+            if(!contract.address) {
+                logResult += "Send transactionHash: " + contract.transactionHash + "\nwaiting to be mined... ";
+                cC_showCreatingResult("text", contractName + "Log", logResult);
+            } else {
+                logResult += "Mined! Address: " + contract.address + "\n";
+                cC_showCreatingResult("text", contractName + "Adr", contractName + ": " + contract.address);
+                cC_showCreatingResult("text", contractName + "Log", logResult);
+                cC_showCreatingResult("input", contractName + "AdrPara", contract.address);
+
+                var index = cC_ModuleName.length;
+                cC_ModuleName[index] = contractName;
+                cC_ModuleAdr[index] = contract.address;
+            }
+        } else {
+            logResult += ": Error!! - " + e;
+            cC_showCreatingResult("text", contractName + "Log", logResult);
+        }
+    }) 
 }
 
 function cC_getUrlSuffixForControlPage() {
