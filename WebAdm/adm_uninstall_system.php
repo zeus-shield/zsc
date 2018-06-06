625587bc9e9466fcf4e650f77ec57bb2d1d51465
Eric-Yao <norhustar@hotmail.com>
2018-06-06 17:59:59 +0800

    added file adm_uninstall_system.php

---
+++ WebAdm/adm_uninstall_system.php
@@ -0,0 +1,40 @@
+
+<?php 
+/*
+Copyright (c) 2018 ZSC Dev Team
+*/
+?>
+
+<html>
+<head>
+<?php 
+include("adm_header.php");
+
+    $htmlObjects = new ZSCHtmlObjects();
+    echo $htmlObjects->loadScriptFiles();
+?>
+<script type="text/javascript">
+    var web3 = setupWeb3js(false);
+    //var web3 = new Web3(web3.currentProvider);
+
+</script>
+</head>
+<body>
+
+<?php 
+    echo $htmlObjects->loadHeader();
+
+    echo '<div class="page-header"> <font size="5" color="blue" >Setup ZSC system in the testing envrioment</font></div>';
+    echo $htmlObjects->loadAllAdrs();
+
+    echo $htmlObjects->loadCreateContract('cC_killContract');
+?>
+    <div class="well">
+        <button type="button" onClick="gotoConfigureLogRecorder()">Next: configure log recorder</button>
+    </div>
+
+</body>
+</html>
+
+
+
