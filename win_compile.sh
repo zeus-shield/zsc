ec4ed596bf366d8081a24e4d1d0fe3268fc1a458
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-06-12 11:57:25 +0800

    add log_recorder/pos_block/pos_base/sys_overlayer in win_compile.sh

--- win_compile.sh
+++ win_compile.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 SFLAGS="--optimize --overwrite --pretty-json --combined-json bin,abi"
+CORE="EthCore"
 
-solc ${SFLAGS} ./EthCore/db_database.sol -o build/db_database
+echo "########## compiling log_recorder.sol ##########"
+solc ${SFLAGS} ./${CORE}/log_recorder.sol -o build/log_recorder
+
+echo "########## compiling pos_block.sol ##########"
+solc ${SFLAGS} ./${CORE}/pos_block.sol -o build/pos_block
+
+echo "########## compiling pos_base.sol ##########"
+solc ${SFLAGS} ./${CORE}/pos_base.sol -o build/pos_base
+
+echo "########## compiling sys_overlayer.sol ##########"
+solc ${SFLAGS} ./${CORE}/sys_overlayer.sol -o build/sys_overlayer
+
+
 
 read -p "Press any key to continue."
