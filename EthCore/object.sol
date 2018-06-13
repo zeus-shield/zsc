6b220d59c0c90a5723743d0799c7225cea80986b
Eric-Yao <norhustar@hotmail.com>
2018-06-14 01:29:22 +0800

    updated contract Object

--- EthCore/object.sol
+++ EthCore/object.sol
@@ -20,9 +20,14 @@ contract Recorder {
     function addLog(string _log, bool _newLine) public;
 }
 
+contract TimeStamp {
+    function getTime(uint _time) public constant returns (bytes32);
+}
+
 contract Object is Delegated {
     bytes32 private name_ = "null";
     address public logRecorder_ = 0;
+    address public timer_ = 0;
 
     // Constructor
     function Object(bytes32 _name) public { 
@@ -37,14 +42,18 @@ contract Object is Delegated {
         return name_;
     }
 
-    function getName() public constant returns (bytes32) { 
+    function setLogRecorderAndTimer(address _rocorderAdr, address _timerAdr) public {
         checkDelegate(msg.sender, 1);
-        return name_;
+        logRecorder_ = _rocorderAdr;
+        timer_ = _timerAdr;
     }
 
-    function setLogRecorder(address _adr) public {
-        checkDelegate(msg.sender, 1);
-        logRecorder_ = _adr;
+    function getTimeStr(uint _time) internal constant returns (bytes32) {
+        if (timer_ != address(0)) {
+            return TimeStamp(timer_).getTime(_time);
+        } else {
+            return 0;
+        }
     }
 
     function addLog(string _log, bool _newLine) public {
