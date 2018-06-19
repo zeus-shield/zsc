eee39ffe484323101e68d93e7bc544e6473af32e
Eric-Yao <norhustar@hotmail.com>
2018-06-19 19:41:21 +0200

    updated contract PosBlock

--- EthCore/pos_block.sol
+++ EthCore/pos_block.sol
@@ -8,10 +8,8 @@ import "./object.sol";
 import "./plat_math.sol";
 
 contract PosBlock is Object {
-    struct TxHash {
-        bool sentIn_;
+    struct TxInfo {
         address sender_;
-        address receiver_;
         uint gasUsage_;
         uint reigsterTime_;
     }
@@ -20,9 +18,7 @@ contract PosBlock is Object {
     uint private sizeLimit_;
     uint private currentSize_;
     uint private txNos_;
-    mapping(uint => TxHash) private txHashs_;
-    mapping(bytes32 => uint) private txIndice_;
-    mapping(bytes32 => bool) private txExists_;
+    mapping(uint => TxInfo) private txInfos_;
 
     address private previousBlock_ = address(0);
     address private nextBlock_ = address(0);
@@ -41,15 +37,13 @@ contract PosBlock is Object {
 
     function setPreviousBlock(address _previous) public {
         checkDelegate(msg.sender, 1);
-
-        require(previousBlock_ != address(0));
+        require(_previous != address(0));
         previousBlock_ = _previous;
     }
 
     function setNextBlock(address _next) public {
         checkDelegate(msg.sender, 1);
-
-        require(nextBlock_ != address(0));
+        require(_next != address(0));
         nextBlock_ = _next;
     }
 
@@ -86,37 +80,22 @@ contract PosBlock is Object {
         }
     } 
     
-    function registerTx(bool _sentIn, bytes32 _tx, address _sender, address _receiver, uint _gasUsage) public returns (bool) {
+    function registerTx(address _sender, uint _gasUsage) public returns (bool) {
         checkDelegate(msg.sender, 1);
 
         if (minedStatus_) return false;
 
-        txExists_[_tx] = true;
-        txIndice_[_tx] = txNos_;
-        txHashs_[txNos_] = TxHash(_sentIn, _sender, _receiver, _gasUsage, now);
+        txHashs_[txNos_] = TxInfo(_sender, _gasUsage, now);
         txNos_++;
-        minedStatus_ = true;
         return true;
     }
 
-    function getTxByIndex(uint _index) public constant returns (address, address, uint, uint) {
+    function getTxInfoByIndex(uint _index) public constant returns (address, uint, uint) {
         checkDelegate(msg.sender, 1);
 
         require(_index < txNos_);
         
-        return (txHashs_[_index].sender_, txHashs_[_index].receiver_, txHashs_[_index].gasUsage_, txHashs_[_index].reigsterTime_);
-    }
-
-    function getTxByHash(bytes32 _tx) public constant returns (address, address, uint, uint) {
-        checkDelegate(msg.sender, 1);
-
-        require(txExists_[_tx]);
-        return getTxByIndex(txIndice_[_tx]);
-    }
-
-    function doesBlockMined() public constant returns (bool) {
-        checkDelegate(msg.sender, 1);
-        return minedStatus_;
+        return (txHashs_[_index].sender_, txHashs_[_index].gasUsage_, txHashs_[_index].reigsterTime_);
     }
     
     function getBlockLimit() public constant returns (uint) {
@@ -124,7 +103,7 @@ contract PosBlock is Object {
         return sizeLimit_;
     }
 
-    function numTx() public constant returns (uint) {
+    function numTxInfos() public constant returns (uint) {
         checkDelegate(msg.sender, 1);
         return txNos_;
     }
