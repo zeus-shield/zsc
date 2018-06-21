b1f19dd86a9f93a93c4dc7365496bca831b89c11
Eric-Yao <norhustar@hotmail.com>
2018-06-21 17:50:15 +0200

    updated class ZSCModuleAdrs

--- WebClient/zsc_module_adrs.js
+++ WebClient/zsc_module_adrs.js
@@ -19,12 +19,13 @@ ZSCModuleAdrs.prototype.setUserType = function(type) {this.userType = type;}
 
 ZSCModuleAdrs.prototype.getModuleAdrs = function(func) {  
     var gm = this;
+    var callBack = func;
     var myControlApi = web3.eth.contract(gm.contractAbi).at(gm.contractAdr);
     myControlApi.getModuleAddresses({from: gm.account},
         function(error, result){ 
         if(!error) {
-            parserAdrInfo(result);
-            func();
+            gm.parserAdrInfo(gm, result);
+            callBack();
         } else {
             console.log("error: " + error);
         }
@@ -39,8 +40,33 @@ ZSCModuleAdrs.prototype.parserAdrInfo = function(gm, info) {
 
     for (var i = 0; i < 10; ++i) {
         gm.adrName[i]  = newsids[i].split("=")[0];
-        gm.adrValue[i] = newsids[i].split("=")[1];
+        gm.adrValue[i] = "0x" + newsids[i].split("=")[1];
     }
+    gm.adrName[0] = "TestZSC-Token";
+}
+
+ZSCModuleAdrs.prototype.loadModuleAdrsHtml = function(elementId)  {
+    var titlle = this.userType + " [" + this.userName + "] - wallet info"
+
+    text = '<div class="well"> <text> ' + titlle + ' </text></div>';
+
+    text += '<div class="well">';
+    text += '<table align="center" style="width:600px;min-height:30px">'
+    text += '<tr>'
+    text += '   <td>ZSC Module Name</td> <td> Address </td> '
+    text += '</tr>'
+    text += '<tr> <td>---</td> <td>---</td></tr>'
+
+    for (var i = 0; i < 10; ++i) {
+        text += '<tr>'
+        text += '   <td><text>' + this.adrName[i]  + '</text></td>'
+        text += '   <td><text> <a href="https://rinkeby.etherscan.io/address/' + this.adrValue[i] + '" target="_blank" >' + this.adrValue[i] + '</a></text></td>'
+        text += '</tr>'
+    }
+    text += '</table></div>'
+    text += '</div>'
+
+    document.getElementById(elementId).innerHTML = text;  
 }
 
 
