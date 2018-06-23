eac8d3c588437fa1f4f2741d82485426c0cc1e59
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-06-23 21:04:37 +0800

    add 'ai_search.sol' for search feature

---
+++ EthCore/ai_search.sol
@@ -0,0 +1,11 @@
+        /*
+Copyright (c) 2018, ZSC Dev Team
+*/
+
+pragma solidity ^0.4.21;
+
+import "./object.sol";
+
+contract AISearch is Object {
+    function AISearch(bytes32 _name) public Object(_name) {}
+}
