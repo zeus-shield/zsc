e5840056f585256966f8c5cd51cd4c9bbfcfb62b
Eric-Yao <norhustar@hotmail.com>
2018-06-26 19:45:02 +0200

    updated file sys_include.sol

--- EthCore/sys_include.sol
+++ EthCore/sys_include.sol
@@ -4,6 +4,7 @@ Copyright (c) 2018, ZSC Dev Team
 
 pragma solidity ^0.4.21;
 
+/*
 contract DBDatabase {
     function getNode(bytes32 _name) public constant returns (address);
     function destroyNode(address _node) public returns (bool);
@@ -65,8 +66,63 @@ contract WalletBase {
     function setLockValue(bool _tag, uint _amount, uint _duration, address _agreementAdr) public returns (bool);
     function executeTransaction(address _dest, uint256 _amount) public returns (bool);
 }
+*/
 
-contract SysOverlayer {
-    function getComponent(bytes32 _type, bytes32 _name) public constant returns (address);
-    function addComponent(bytes32 _type, bytes32 _name, address _adr) public returns (bool);
+contract DBNode {
+    function getNodeType() public constant returns (bytes32);
+    function getBlance() public constant returns (uint256);
+
+    function addParameter(bytes32 _parameter) public returns (bool);
+    //function removeParameter(bytes32 _parameter) public returns (bool);
+    function setParameter(bytes32 _parameter, bytes32 _value) public returns (bool);
+    function getParameter(bytes32 _parameter) public constant returns (bytes32);
+    function numParameters() public constant returns (uint);
+    function getParameterNameByIndex(uint _index) public constant returns (bytes32);
+
+    function setERC20TokenAddress(address _tokenAdr) public;
+    //function submitTransaction(address _dest, uint256 _amount, bytes _data, address _user) public returns (uint);
+    //function confirmTransaction(address _sigAdr) public returns (uint);    
+    function executeTransaction(address _dest, uint256 _amount) public returns (uint);
+    function informTransaction(address _src, uint256 _amount) public;
+    function numTransactions() public constant returns (uint);
+    function getTransactionInfoByIndex(uint _index) public constant returns (uint, bool,  bytes32, uint, address, address);
+
+    function setAgreementStatus(bytes32 _tag, bytes32 receiver) public returns (bool);
+    //function configureHandlers() public returns (bool);
+    //function getHandler(bytes32 _type) public constant returns (address);
+
+    function bindAgreement(address _adr) public;
+    function numAgreements() public constant returns (uint);
+    function numTemplates() public constant returns (uint);
+    function getAgreementByIndex(uint _index) public constant returns (address);
+    function getTemplateByIndex(uint _index) public constant returns (address);
+
+    function numChildren() public constant returns(uint);
+    function getChildByIndex(uint _index) public  constant returns(address);
+    function addChild(address _node) public returns (address);
+
+    //function getMiningInfoByIndex(bool _isReward, uint _index) public constant returns (uint, uint);
+    //function numMiningInfo(bool _isReward) public constant returns (uint);
+
+    //function addSignature(address _sigAdr) public returns (bool);
+    //function getAgreementInfo() public constant returns (bytes32, bytes32, uint, uint, bytes32, uint);
+
+    function simulatePayforInsurance() public returns (uint);
 }
+
+contract DBFactory {
+    function setDatabase(address _adr) public;
+    function createNode(bytes32 _nodeName, address _parent) public returns (address);
+    function numFactoryNodes() public constant returns (uint);
+    function getFactoryNodeNameByIndex(uint _index) public constant returns (bytes32);
+}
+
+contract DBDatabase {
+    function delegateFactory(address _adr, uint _priority) public;
+    function getNode(bytes32 _name) public constant returns (address);
+    function getRootNode() public constant returns (address);
+    function destroyNode(address _node) public returns (bool);
+    function checkeNodeByAddress(address _adr) public constant returns (bool);
+    function _addNode(address _node) public ;
+}
+
