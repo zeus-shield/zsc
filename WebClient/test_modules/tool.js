657d7c74c346d6e4fc779f700b29c5e6f178fff9
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-06-29 11:14:44 +0800

    up tool.js for deploy contract

--- WebClient/test_modules/tool.js
+++ WebClient/test_modules/tool.js
@@ -1,1 +1,19 @@
+const Web3 = require('web3');
+
+let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
 function Tool() {};
+
+Tool.prototype.DeployContract = function() {
+  console.log("DeployContract");
+}
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = new Tool();
+} else {
+  if (typeof define === 'functon' && define.amd) {
+    define([], function() {return Tool;});
+  } else {
+    window.Tool = new Tool;
+  }
+}
