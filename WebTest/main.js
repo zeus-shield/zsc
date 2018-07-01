482a47ad0077481bd5bfd2311d8d7980e0b19637
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-01 12:06:58 +0800

    up 'main.js' for 'WebTest'

--- WebTest/main.js
+++ WebTest/main.js
@@ -1,20 +1,35 @@
-alert("main loading");
 
 require.config({
     paths: {
         "test_search": "test_search",
+        "test_analysis": "test_analysis"
     }
 });
 
 require(['test_search'], function (){});
 require(['test_analysis'], function (){});
 
-function addIframe(operation) {
+function main(module, operation) {
     let test = 0;
-    //let string = '[Module]:' + 'Search' + ', ' + '[Operation]:' + operation;
-    //alert(string);
-    console.log('[Module]:%s, [Operation]:%s', 'Search', operation);
-    test = new TestSearch();
+    let string = 'Module:' + '[' + module + ']' + ', ' + 'Operation:' + '['+ operation + ']';
+    alert(string);
+    //console.log('[Module]:%s, [Operation]:%s', module, operation);
+
+    switch(module) {
+        case 'Search': {
+            test = new TestSearch();
+            break;
+        }
+        case 'Analysis': {
+            test = new TestAnalysis();
+            break;
+        }
+        default:
+            let error = 'Module:' + '[' + module + ']' + ' Error';
+            alert(error);
+            break;
+    }
+
     test.setOutput('output');
     test.do(operation);
 }
