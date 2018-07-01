f10c2c0b0a8d711b7ed92b565b1a9f6b9b2c42f1
Eric-Yao <norhustar@hotmail.com>
2018-07-01 19:42:00 +0200

    updated file erc721_adv.sol

--- EthCore/erc721_adv.sol
+++ EthCore/erc721_adv.sol
@@ -4,8 +4,25 @@ Copyright (c) 2018, ZSC Dev Team
 
 pragma solidity ^0.4.21;
 
-import "./erc721_base.sol";
 import "./delegate.sol";
+
+
+/// @title ERC-721 Non-Fungible Token Standard
+/// @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
+contract ERC721 {
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+    event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
+    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
+    function balanceOf(address _owner) external view returns (uint256);
+    function ownerOf(uint256 _tokenId) external view returns (address);
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes data) external payable;
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId) external payable;
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable;
+    function approve(address _approved, uint256 _tokenId) external payable;
+    function setApprovalForAll(address _operator, bool _approved) external;
+    function getApproved(uint256 _tokenId) public view returns (address);
+    function isApprovedForAll(address _owner, address _operator) external view returns (bool);
+}
 
 /**
  * @title ERC721Adv
