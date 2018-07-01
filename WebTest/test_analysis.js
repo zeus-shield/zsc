03a8d4c4d789e634beab07a3232a6c9255f997f0
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-01 12:09:41 +0800

    up 'test_analysis.js' for 'WebTest'

--- WebTest/test_analysis.js
+++ WebTest/test_analysis.js
@@ -1,10 +1,17 @@
 
-function TestAnalysis() {};
+function TestAnalysis() {
+    this.outputId;
+};
+
+TestAnalysis.prototype.setOutput = function(id) {
+    console.log("TestAnalysis.setOutput()");
+    this.outputId = id;
+}
 
 TestAnalysis.prototype.deploy = function() {
     console.log("TestAnalysis.deploy()");
-    let tool = new Tool();
-    tool.DeployContract();
+    //let tool = new Tool();
+    //tool.DeployContract();
 }
 
 TestAnalysis.prototype.create = function() {
@@ -13,6 +20,20 @@ TestAnalysis.prototype.create = function() {
 
 TestAnalysis.prototype.do = function(operation) {
     console.log("TestAnalysis.do(%s)", operation);
+    switch(operation) {
+        case 'Deploy':
+            this.deploy();
+            break;
+        case 'Create':
+            this.create();
+            break;
+        default:
+            //console.assert("Assertion of score length failed");
+            let element = document.getElementById(this.outputId);
+            element.style.fontSize = "small";//10 +'pt';
+            element.innerHTML = "Operation Error!";
+            break;
+    }
 }
 
 // Node.js and other environments that support module.exports.
