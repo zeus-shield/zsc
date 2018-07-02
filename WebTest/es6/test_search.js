b3973644301cc51d402a229a1074dc9752b07197
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-02 19:40:18 +0800

    add es6/test_search.js for WebTest

---
+++ WebTest/es6/test_search.js
@@ -0,0 +1,36 @@
+
+export default class TestSearch {
+    constructor() {
+        this.outputId = ``;
+    }
+
+    setOutput(id) {
+        console.log(`TestSearch.setOutput(%s)`, id);
+        this.outputId = id;
+    }
+
+    deploy() {
+        console.log(`TestSearch.deploy()`);
+    }
+
+    create() {
+        console.log(`TestSearch.create()`);
+    }
+
+    do(operation) {
+        console.log(`TestSearch.do(%s)`, operation);
+        switch(operation) {
+            case `Deploy`:
+                this.deploy();
+                break;
+            case `Create`:
+                this.create();
+                break;
+            default:
+                let element = document.getElementById(this.outputId);
+                element.style.fontSize = `small`;//10 +`pt`;
+                element.innerHTML = `Operation Error!`;
+                break;
+        }
+    }
+}
