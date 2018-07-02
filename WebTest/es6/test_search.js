0753a3e28dd43ffb13f497df2d3a89c22dd56af5
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-02 22:05:58 +0800

    up string format

--- WebTest/es6/test_search.js
+++ WebTest/es6/test_search.js
@@ -1,39 +1,39 @@
 
-//private value
-const outputId = Symbol(`outputId`);
+//private member
+const outputId = Symbol('outputId');
 
 export default class TestSearch {
 
     constructor() {
-        this[outputId] = ``;
+        this[outputId] = '';
     }
 
     setOutput(id) {
-        console.log(`TestSearch.setOutput(%s)`, id);
+        console.log('TestSearch.setOutput(%s)', id);
         this[outputId] = id;
     }
 
     deploy() {
-        console.log(`TestSearch.deploy()`);
+        console.log('TestSearch.deploy()');
     }
 
     create() {
-        console.log(`TestSearch.create()`);
+        console.log('TestSearch.create()');
     }
 
     do(operation) {
-        console.log(`TestSearch.do(%s)`, operation);
+        console.log('TestSearch.do(%s)', operation);
         switch(operation) {
-            case `Deploy`:
+            case 'Deploy':
                 this.deploy();
                 break;
-            case `Create`:
+            case 'Create':
                 this.create();
                 break;
             default:
                 let element = document.getElementById(this[outputId]);
-                element.style.fontSize = `small`;//10 +`pt`;
-                element.innerHTML = `Operation Error!`;
+                element.style.fontSize = 'small';//10 +'pt';
+                element.innerHTML = 'Operation Error!';
                 break;
         }
     }
--- WebTest/main.js
+++ WebTest/main.js
@@ -14,24 +14,24 @@ import TestAnalysis from './test_analysis.js';
 
 export default function main(module, operation) {
     let test = 0;
-    let string = `Module:` + `[` + module + `]` + `, ` + `Operation:` + `[`+ operation + `]`;
+    let string = `Module:[${module}], Operation:[${operation}]`;
     alert(string);
 
     switch(module) {
-        case `Search`: {
+        case 'Search': {
             test = new TestSearch();
             break;
         }
-        case `Analysis`: {
+        case 'Analysis': {
             test = new TestAnalysis();
             break;
         }
         default:
-            let error = `Module:` + `[` + module + `]` + ` Error!`;
+            let error = `Module:[${module}] Error!`;
             alert(error);
             break;
     }
 
-    test.setOutput(`output`);
+    test.setOutput('output');
     test.do(operation);
 }
