8d9cb3c4c745fb5003a00c7e5533788ebf033059
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-03 11:28:58 +0800

    add file.js for WebTest

---
+++ WebTest/file.js
@@ -0,0 +1,56 @@
+
+import TestSearch from './es6/test_search.js';
+import TestAnalysis from './test_analysis.js';
+
+export default function file(module, input) {
+    //support chrome IE10
+    if (window.FileReader) {
+        var file = input.files[0];
+        var filename = file.name.split(".")[0];
+        var reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = function() {
+            let test;
+            //console.log(this.result);
+            //alert(this.result);
+
+            switch(module) {
+                case 'Search': {
+                    test = new TestSearch();
+                    break;
+                }
+                case 'Analysis': {
+                    test = new TestAnalysis();
+                    break;
+                }
+                default:
+                    let error = `Module:[${module}] Error!`;
+                    alert(error);
+                    break;
+            }
+
+            if('undefined' != typeof test) {
+                test.setCompiledJson(this.result);
+            }
+            
+        }
+    } 
+    //support IE 7 8 9 10
+    else if (typeof window.ActiveXObject != 'undefined'){
+        var xmlDoc; 
+        xmlDoc = new ActiveXObject("Microsoft.XMLDOM"); 
+        xmlDoc.async = false; 
+        xmlDoc.load(input.value); 
+        alert(xmlDoc.xml); 
+    } 
+    //support FF
+    else if (document.implementation && document.implementation.createDocument) { 
+        var xmlDoc; 
+        xmlDoc = document.implementation.createDocument("", "", null); 
+        xmlDoc.async = false; 
+        xmlDoc.load(input.value); 
+        alert(xmlDoc.xml);
+    } else { 
+        alert('error'); 
+    } 
+}
