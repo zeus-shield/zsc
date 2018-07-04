a0d27ffafe7e9406604f60d502d999d9acf63ffb
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-04 10:02:11 +0800

    add contract.js

---
+++ WebTest/contract.js
@@ -0,0 +1,15 @@
+
+//private member
+
+export default class Contract {
+    constructor() {
+    }
+
+    add(module, contractAddress) {
+        console.log('Contract.add()');
+    }
+
+    find(module) {
+        console.log('Contract.find()');
+    }
+}
