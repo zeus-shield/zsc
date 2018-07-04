814026f61e4ef8a04a1e43bc253f2f849c30e382
Eric-Yao <norhustar@hotmail.com>
2018-07-04 19:33:24 +0200

    updated contract PosGasPool

--- EthCore/pos_gas_pool.sol
+++ EthCore/pos_gas_pool.sol
@@ -7,14 +7,26 @@ pragma solidity ^0.4.21;
 import "./delegate.sol";
 
 contract PosGasPool is Delegated {
+    struct BlockInfo {
+        bool mined_;
+        uint gasUsage_;
+        uint reigsterTime_;
+    }
+
+    uint totalBlockNos_;
+    uint minedBlockNos_;
     uint minedGasUsage_;
     uint remaingGasUsage_;
+    mapping(uint => BlockInfo) blocks_;
     
     ////////////////
+    uint blockSizeLimit_;
+
     address private posGm_;
 
     // Constructor
     function PosGasPool() public {
+        blockSizeLimit_ = 1024 * 1024 * 2;
     } 
 
     function initPosGasPool(address _posGmAdr) public {
@@ -26,6 +38,35 @@ contract PosGasPool is Delegated {
 
     function registerGasUsage(uint _gasUsage) public {
         checkDelegate(msg.sender, 1);
+        require(posGm_ != address(0));
+
         remaingGasUsage_ = remaingGasUsage_.add(_gasUsage);
-    }   
+        blocks_[totalBlockNos_] = BlockInfo(false, _gasUsage, now);
+        totalBlockNos_++;
+    }
+
+    function getBlockByIndex(uint _blockIndex) public view returns (uint, uint) {
+        checkDelegate(msg.sender, 1);
+        require(_blockIndex < totalBlockNos_);
+        return (blocks_[_blockIndex].gasUsage_, blocks_[_blockIndex].reigsterTime_);
+    }
+
+    function numBlocks() public view returns (uint, uint) { 
+        checkDelegate(msg.sender, 1);
+        return (totalBlockNos_, minedBlockNos_); 
+    }
+    
+    function minePendingBlockByIndex(uint _index) public returns (uint) {
+        checkDelegate(msg.sender, 1);
+        require(_index < totalBlockNos_);
+        
+        if (blocks_[_index].mined_ == true) {
+            return 0;
+        } else {
+            blocks_[_index].mined_ = true;
+            minedBlockNos_++;
+            return blocks_[_index].gasUsage_;
+        }
+    }
 }
+
