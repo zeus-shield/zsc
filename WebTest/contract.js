fff7943a8606d254a0c98edee98bc4110d7bee69
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-04 13:05:26 +0800

    add map for 'contract.js'

--- WebTest/contract.js
+++ WebTest/contract.js
@@ -1,15 +1,19 @@
 
 //private member
+const map = Symbol('map');
 
 export default class Contract {
     constructor() {
+        this[map] = new Map();
     }
 
     add(module, contractAddress) {
         console.log('Contract.add()');
+        this[map].set(module, contractAddress);
     }
 
     find(module) {
         console.log('Contract.find()');
+        return this[map].get(module);
     }
 }
