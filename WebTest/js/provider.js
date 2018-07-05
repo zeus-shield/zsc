af87112f568a63e98b3922f7a80e7d43deb63495
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-05 09:17:17 +0800

    add provider.js

---
+++ WebTest/js/provider.js
@@ -0,0 +1,58 @@
+/*
+ Copyright (c) 2018 ZSC Dev Team
+*/
+
+import Output from './output.js';
+
+export default function provider(injected) {
+    let Web3 = require('web3');
+    let provider;
+    let type = '';
+
+    if(web3.currentProvider && !window.injectedWeb3) {
+        window.injectedWeb3 = web3.currentProvider;
+    }
+
+    if(true == injected) {
+        if(window.injectedWeb3) {
+            provider = window.injectedWeb3;
+        } else {
+            Output(window.outputElement, 'small', 'red', `Injected Web3 do not exist!`);
+            return;
+        }
+    } else {
+        if(window.localhostWeb3) {
+            provider = window.localhostWeb3;
+        } else {
+            provider = new Web3.providers.HttpProvider("http://localhost:7545");
+            window.localhostWeb3 = provider;
+        }
+    }
+
+    web3 = new Web3(provider);
+    //web3.setProvider(provider);
+    
+    type = (injected)?'injected':'localhost';
+    if(!web3.isConnected()) {
+        Output(window.outputElement, 'small', 'red', `Web3(${type}) do not connected!`);
+    } else {
+        Output(window.outputElement, 'small', 'red', `Web3(${type}) connected!`);
+    }
+
+    web3.eth.defaultAccount = web3.eth.coinbase;
+
+    return;
+}
+
+// Node.js and other environments that support module.exports.
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = provider();
+} else {
+    // AMD.
+    if (typeof define === 'functon' && define.amd) {
+        define([], function() {return provider;});
+    // Browser.
+    } else {
+        window.provider = provider;
+    }
+}
