7ce1089316a485809d0c7068683fcf4f538c239d
Eric-Yao <norhustar@hotmail.com>
2018-07-06 21:02:54 +0200

    Revert "updated contract WalletBase"
    This reverts commit 520f1ac62d6351b6ebf656eaa5b80d742d3596ba.

--- EthCore/wallet_base.sol
+++ EthCore/wallet_base.sol
@@ -4,9 +4,9 @@ Copyright (c) 2018, ZSC Dev Team
 
 pragma solidity ^0.4.21;
 
-import "./delegate.sol";
+import "./db_entity.sol";
 
-contract WalletBase is Delegated {
+contract WalletBase is DBNode {
     struct Payment {
         uint time_;
         bool isInput_;
@@ -19,7 +19,7 @@ contract WalletBase is Delegated {
     mapping(uint => Payment) private payments_;
 
     // Constructor
-    function WalletBase() public Delegated() {
+    function WalletBase(bytes32 _name) public DBNode(_name) {
     }
 
     function checkBeforeSent(address _dst, uint _amount) internal constant {
