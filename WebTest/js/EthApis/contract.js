bee3e07a697658476bf0b7c80ef436b403dbaad9
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-09 20:23:46 +0800

    add contract.js for ethapis

--- WebTest/js/EthApis/contract.js
+++ WebTest/js/EthApis/contract.js
@@ -7,13 +7,13 @@ import Http from './http.js';
 //private member
 const http = Symbol('http');
 
-export default class Account {
+export default class Contract {
     constructor() {
         this[http] = new Http();
     }
 
-    tockentx(address) {
-        let url = `http://api.etherscan.io/api?module=account&action=tokentx&address=${address}&startblock=0&endblock=999999999&sort=asc`;
+    getabi(address) {
+        let url = `https://api.etherscan.io/api?module=contract&action=getabi&address=${address}`;
 
         this[http].get(url, 10000, function(data) {
             console.log(data);
