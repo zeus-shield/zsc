50a9381059287f667439e8ab30774ea8b2ed31ac
Eric-Yao <norhustar@hotmail.com>
2018-07-11 22:30:27 +0200

    updated contract PosMinerRobot

--- EthCore/pos_miner_robot.sol
+++ EthCore/pos_miner_robot.sol
@@ -6,8 +6,8 @@ pragma solidity ^0.4.21;
 
 import "./erc721_adv.sol";
 
-contract PosProofPower is Erc721Adv {
-   struct VirtualPowerUnit {
+contract PosMinerRobot is Erc721Adv {
+   struct RobotUnit {
         address user_;
         bool buyable_;
         bool activated_;
@@ -21,47 +21,36 @@ contract PosProofPower is Erc721Adv {
     }
 
     address private posGm_;
-    uint private vpuNos_;
-    uint private totalPower_;
-    mapping(uint => VirtualPowerUnit) internal vpus_;
+    uint private robotNos_;
+    mapping(uint => RobotUnit) internal robots_;
 
-    function PosProofPower() public Erc721Adv {
-        totalPower_ = 0;
-        vpuNos_ = 0;
+    function PosMinerRobot() public Erc721Adv {
+        robotNos_ = 0;
     }
 
-    function checkVpuUser(address _user, uint _vpuId) internal view {
-        require(_user == vpus_[_vpuId]._user);
+    function checkRobotUser(address _user, uint _robotId) internal view {
+        require(_user == robots_[_robotId]._user);
     }
 
-    function initPosProofPower(address _posGmAdr) public {
+    function initPosVirtualRobot(address _posGmAdr) public {
         checkDelegate(msg.sender, 1);
         require(_posGmAdr != address(0));
         posGm_ = _posGmAdr;
         setDelegate(posGm_, 1);
     }
 
-
-    function getVpuInfo(uint _vpuId) public view returns (address, bool, bool, uint, uint, uint, uint, uint, uint, uint) {
+    function getRobotInfo(uint _robotId) public view returns (address, bool, bool, uint, uint, uint, uint, uint, uint, uint) {
         checkDelegate(msg.sender, 1);
-        require(_vpuId < vpuNos_);
-        return (vpus_[_vpuId].user_,
-                vpus_[_vpuId].buyable_,
-                vpus_[_vpuId].activated_,
-                vpus_[_vpuId].level_,
-                vpus_[_vpuId].price_,
-                vpus_[_vpuId].start_, 
-                vpus_[_vpuId].end_, 
-                vpus_[_vpuId].curStakerPoint_, 
-                vpus_[_vpuId].maxStakerPoint_,
-                vpus_[_vpuId].rewardRatio_);
+        require(_robotId < robotNos_);
+        return (robots_[_robotId].user_,
+                robots_[_robotId].buyable_,
+                robots_[_robotId].activated_,
+                robots_[_robotId].level_,
+                robots_[_robotId].price_,
+                robots_[_robotId].start_, 
+                robots_[_robotId].end_, 
+                robots_[_robotId].curStakerPoint_, 
+                robots_[_robotId].maxStakerPoint_,
+                robots_[_robotId].rewardRatio_);
     }
-    function setRewardRatio(uint _level, uint _ratio, uint _maxStakePoint) public;
-    function createVpu(address _user, uint _level, uint _price) public returns (uint);
-    function destroyVpu(address _owner, uint _vpuId) public;
-    function setRewardRatio(uint _level, uint _ratio, uint _maxStakePoint) public;
-    function publishVpu(address _user, uint _vpuId, uint _price) public;
-    function activeVpu(address _user, uint _vpuId, uint _stakePoint, uint _durationInDays) public;
-    function claimable(address _staker, uint _vpuId) public view returns (bool);
-    function claimRwardFromVpu(address _staker, uint _vpuId) public returns (uint);
 }
