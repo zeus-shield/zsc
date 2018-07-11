f1b99433679e324f1a86842c0b41bb93aa5d12af
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-11 21:45:47 +0800

    add 'eth_getBlockByNumber()' for ethapis

--- WebTest/js/EthApis/proxy.js
+++ WebTest/js/EthApis/proxy.js
@@ -19,4 +19,12 @@ export default class Proxy {
             console.log(data);
         });
     }
+
+    eth_getBlockByNumber(tag) {
+        let url = `https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=${tag}&boolean=true`;
+
+        this[http].get(url, 10000, function(data) {
+            console.log(data);
+        });
+    }
 }
