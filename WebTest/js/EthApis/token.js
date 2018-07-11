7d1681d0ba6862894e43a4d622a8bfe75c8a8969
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-11 21:49:17 +0800

    add token.js for ethapis

---
+++ WebTest/js/EthApis/token.js
@@ -0,0 +1,22 @@
+/*
+ Copyright (c) 2018 ZSC Dev Team
+*/
+
+import Http from './http.js';
+
+//private member
+const http = Symbol('http');
+
+export default class Token {
+    constructor() {
+        this[http] = new Http();
+    }
+
+    tokensupply(address) {
+        let url = `https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress=${address}`;
+
+        this[http].get(url, 10000, function(data) {
+            console.log(data);
+        });
+    }
+}
