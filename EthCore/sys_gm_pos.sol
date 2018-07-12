d9504b702cf42ec8107893ddd682abfc8926e61b
Eric-Yao <norhustar@hotmail.com>
2018-07-12 20:46:13 +0200

    updated contract SysGmPos

--- EthCore/sys_gm_pos.sol
+++ EthCore/sys_gm_pos.sol
@@ -4,34 +4,57 @@ Copyright (c) 2018, ZSC Dev Team
 
 pragma solidity ^0.4.21;
 
-import "./sys_include.sol";
 import "./sys_gm_base.sol";
+import "./erc721_adv.sol";
 
-contract PosProofPower {
-    function createVPUs(uint[] _ratio, uint[] _power, uint[] _priceMin) public;
-    function createVPUs(uint _number, uint _ratio, uint _power, uint _priceMin) public;
-    function getVPUInfo(uint _vpuId) public view returns (uint, uint, uint, uint);
-    function purchaseVPU(address _buyerAdr, uint _vpuId) public;
-}
+contract SysGmPos is Erc721Adv, SysGmBase {
+    struct RobotUnit {
+        address user_;
+        bool buyable_;
+        bool activated_;
+        uint level_;
+        uint price_;
+        uint start_;
+        uint end_;
+        uint curStakerPoint_;
+        uint maxStakerPoint_;
+        uint rewardRatio_;
+    }
 
-contract SysGmPos is SysGmBase {
-    address proofPower_;
-
+    uint internal robotNos_;
+    mapping(uint => RobotUnit) internal robots_;
+    
     // Constructor
     function SysGmPos(bytes32 _name) public SysGmBase(_name) {
     } 
 
-    function configurePos(address _proofPower) public {
+    function checkRobotUser(address _user, uint _robotId) internal view {
+        require(_user == robots_[_robotId].user_);
+    }
+    
+    function getRobotPrice(uint _robotId) public view returns (uint) {
         checkDelegate(msg.sender, 1);
-        require(_proofPower != address(0));
+        require(_robotId < robotNos_);
+        return robots_[_robotId].price_;
+    }
 
-        if (proofPower_ == address(0)) {
-            proofPower_  = _proofPower;
-        } else {
-            if (proofPower_ != _proofPower) {
-                Object(proofPower_).setDelegate(address(this), 0);
-                proofPower_  = _proofPower;
-            }
-        }
+    function getRobotStatus(uint _robotId) public view returns (address, bool, bool) {
+        checkDelegate(msg.sender, 1);
+        require(_robotId < robotNos_);
+        return (robots_[_robotId].user_,
+                robots_[_robotId].buyable_,
+                robots_[_robotId].activated_);
+    }
+    
+    function getRobotInfo(uint _robotId) public view returns (uint, uint, uint, uint, uint, uint, uint) {
+        checkDelegate(msg.sender, 1);
+        require(_robotId < robotNos_);
+        return (robots_[_robotId].level_,
+                robots_[_robotId].price_,
+                robots_[_robotId].start_, 
+                robots_[_robotId].end_, 
+                robots_[_robotId].curStakerPoint_, 
+                robots_[_robotId].maxStakerPoint_,
+                robots_[_robotId].rewardRatio_);
     }
 }
