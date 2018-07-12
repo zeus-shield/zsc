87b2179729fdc893efcd28f5e7c9ced6a1f810f2
Eric-Yao <norhustar@hotmail.com>
2018-07-12 21:03:50 +0200

    updated class ZSCRobotsInit

--- WebClient/staker/zsc_robots_init.js
+++ WebClient/staker/zsc_robots_init.js
@@ -5,8 +5,6 @@ Copyright (c) 2018 ZSC Dev Team
 //class 
 function ZSCRobotInit(nm, abi, adr) {
     this.userName = nm;
-    this.robotNos = 0;
-    this.robotIds = [];
     this.account = web3.eth.accounts[0];
     this.contractAdr = adr;
     this.contractAbi = JSON.parse(abi);
@@ -18,3 +16,18 @@ ZSCRobotInit.prototype.getUserName = function() {return this.userName;}
 
 ZSCRobotInit.prototype.getRobotId = function(index) { return this.robotIds[index];}
 
+ZSCRobotInit.prototype.numAllInitialRobots = function(func) {
+    var gm = this;
+    var callBack = func;
+    var myControlApi = web3.eth.contract(gm.contractAbi).at(gm.contractAdr);
+    
+    myControlApi.numInitialRobots({from: gm.account},
+        function(error, result){ 
+            if(!error) {
+                gm.robotNos = result.toString(10); 
+                callBack(gm);
+            } else {
+                console.log("error: " + error);
+            }
+        });
+}
