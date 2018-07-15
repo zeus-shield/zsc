8ad68425fbec83ca59bbf7c6f392c45d44daf0eb
Eric-Yao <norhustar@hotmail.com>
2018-07-15 20:39:48 +0200

    updated html contents

--- WebClient/index.php
+++ WebClient/index.php
@@ -4,53 +4,42 @@ Copyright (c) 2018 ZSC Dev Team
 */
 ?>
 
-<?php
-include("include.php");
-$htmlObjects = new ZSCInclude();
-?>
-
 <html>
 <head>
-<?php echo $htmlObjects->loadScriptFiles(); ?>
+<?php 
+include("include.php");
+$htmlObjects = new ZSCInclude();
+echo $htmlObjects->loadScriptFiles(); 
+?>
 </head>
 <body>
     <div class="col-lg-12">
         <br><i>Web-client for testing ZSC system on the Rinkeby</i><br><br>
-        <div class="well" id="PageAlert"></div>
-        <div class="well" id="PageHeader"></div>
-        <div class="well" id="PageBody">
-            <table align="center">
-                <tr><td>
-                    <button type="button" onClick="applyForZSCUser('provider', 'ApplyForProviderHash')">Apply for provider</button> <br><br>
-                    <button type="button" onClick="applyForZSCUser('receiver', 'ApplyForProviderHash')">Apply for receiver</button> <br><br>
-                    <button type="button" onClick="applyForZSCUser('staker',   'ApplyForProviderHash')">Apply for staker</button> <br>
-                    <text id="ApplyForUserHash"></text>
-                </td></tr>
-            </table>
-        </div>
+        <div class="well" id="PageAlert"><?php echo $htmlObjects->loadAlert(); ?></div>
+        <div class="well" id="PageHeader"><?php echo $htmlObjects->loadPosHeader(); ?></div>
+        <div class="well" id="PageBody"></div>
     </div>
+
 <script type="text/javascript">
     var web3;
-    if (doesLocalWeb3js()) { web3 = setupWeb3js(); }
-    else { web3 = new Web3(web3.currentProvider);} //Metamask
-
     var zscUser = new ZSCUser();
-    var zscElement;
-    var zscWalletGM;
-    var zscTransGM;
-    var zscPosGM;
-    var zscModuleAdrGM;
-
-    function htmlLoadLogin() {
-        document.getElementById("PageAlert").innerHTML = <?php echo $htmlObjects->loadAlert(); ?>;  
-        document.getElementById("PageHeader").innerHTML = <?php echo $htmlObjects->loadPosHeader(); ?>;  
-    }
+    if (doesLocalWeb3js()) { web3 = setupWeb3js();} 
+    else { web3 = new Web3(web3.currentProvider);} //Metamask
 
     function checkUser(adrId) { 
         zscUser.tryLogin(function(ret) {
+            var textBody;
             if(ret) {
-                document.getElementById("PageBody").innerHTML = "<br><br><i>Welcome!</i><br><br>";  
-            } 
+                textBody = '<table align="center"> <tr><td><i>Welcome!</i> </td></tr></table>'; 
+            } else {
+                document.getElementById("PageHeader").innerHTML = '<table align="center"> <tr><td><i>Apply one of the following user types!</i> </td></tr></table>'; 
+                textBody = ''
+                + '<table align="center"><tr><td>'
+                + '   <button type="button" onClick="applyForZSCUser('+ "'staker', 'ApplyForUserHash')" + ' ">Apply for staker</button> <br><br>'
+                + '   <text id="ApplyForUserHash"></text>'
+                + '</td></tr></table>'
+            }
+            document.getElementById("PageBody").innerHTML = textBody;
         });
     }
 
@@ -58,7 +47,6 @@ $htmlObjects = new ZSCInclude();
         zscUser.activeByUser(userType, hashId);
     }
 
-    htmlLoadLogin();
     checkUser();
 </script>
 
