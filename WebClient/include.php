b9f729f03c2d707648cc92b85b2a2ddaf791f401
Eric-Yao <norhustar@hotmail.com>
2018-07-15 17:30:45 +0200

    updated function loadScriptFiles(); added functions loadAlert(), loadPosHeader()

--- WebClient/include.php
+++ WebClient/include.php
@@ -6,9 +6,8 @@ Copyright (c) 2018, ZSC Dev Team
 
 <?php
 
-class PosInc {
+class ZSCInclude {
     public function __construct(){
-        parent::__construct();
     }
 
     public  function __destruct() {
@@ -20,24 +19,45 @@ class PosInc {
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src=".././web3.js/dist/web3.js"></script>
-        <script type="text/javascript" src="../web3js.js"></script>
+        <script type="text/javascript" src="./web3.js/dist/web3.js"></script>
+        <script type="text/javascript" src="./web3js.js"></script>
         
-        <script type="text/javascript" src="../userFunctions.js"></script>
-        <script type="text/javascript" src="../basicFunctions.js"></script>
+        <script type="text/javascript" src="./common/userFunctions.js"></script>
+        <script type="text/javascript" src="./common/basicFunctions.js"></script>
         
-        <script type="text/javascript" src="../zsc_element.js"></script>
-        <script type="text/javascript" src="../zsc_transactions.js"></script>
-        <script type="text/javascript" src="../zsc_user.js"></script>
-        <script type="text/javascript" src="../zsc_wallet.js"></script>
-        <script type="text/javascript" src="../zsc_module_adrs.js"></script>
+        <script type="text/javascript" src="./common/zsc_element.js"></script>
+        <script type="text/javascript" src="./common/zsc_transactions.js"></script>
+        <script type="text/javascript" src="./common/zsc_user.js"></script>
+        <script type="text/javascript" src="./common/zsc_wallet.js"></script>
+        <script type="text/javascript" src="./common/zsc_module_adrs.js"></script>
         
-        <script type="text/javascript" src="./js/zsc_element.js"></script>
-        <script type="text/javascript" src="./js/zsc_transactions.js"></script>
-        <script type="text/javascript" src="./js/zsc_user.js"></script>
-        <script type="text/javascript" src="./js/zsc_wallet.js"></script>
-        <script type="text/javascript" src="./js/zsc_module_adrs.js"></script>
+        <script type="text/javascript" src="./pos/pos_robot_enhance.js"></script>
+        <script type="text/javascript" src="./pos/pos_robot_gen0.js"></script>
+        <script type="text/javascript" src="./pos/pos_robot_owned.js"></script>
+        <script type="text/javascript" src="./pos/pos_staker_info.js"></script>
         ';
+        return $text;
+    }
+
+    public function loadAlert() {
+        $text="'".
+        "<i>Needs to install MetaMask extension</i><br>". 
+        "<i>(需要安装MetaMask插件才能显示登录框以其他相关页面)</i><br><br>".
+        "<i>Both the FireFox and Chrome browsers are recommended</i><br>".
+        "<i>(推荐使用火狐或者Chrome浏览器)</i>";  
+        return $text;
+    }
+    
+    public function loadPosHeader() {
+        $text="'".
+        '<table align="center" style="width:600px">'.
+        ' <tr>'.
+        '  <td align="center"><a href="pos_staker_info.php">User Profile</a></td>'.
+        '  <td align="center"><a href="pos_robot_gen0.php">Create Gen0 Robot</a></td>'.
+        '  <td align="center"><a href="pos_robot_owned.php">Owned Market</a></td>'.
+        '  <td align="center"><a href="pos_robot_market.php">Robot Market</a></td>'.
+        ' </tr>'.
+        '</table>'."'";
         return $text;
     }
 }
