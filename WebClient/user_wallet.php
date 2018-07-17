6db528da107fec9c52a6779f2ba9bc25165525da
Eric-Yao <norhustar@hotmail.com>
2018-07-17 21:12:17 +0200

    updated php content

--- WebClient/user_wallet.php
+++ WebClient/user_wallet.php
@@ -38,7 +38,55 @@ session_start();
     /////////////////////////////
 
     function loadHtml(elementId, func1, func2, func3) {
-       
+        var transPrefix = func1 + "('"; 
+        var transSuffix = "')";
+    
+        var showTransPrefix = func2 + "('";
+        var showTransSuffix = "')";
+    
+        var enableWalletPrefix = func3 + "('";
+        var enableWalletSuffix = "')";
+    
+        var symbol;
+        var adr;
+        var balance;
+        var hashId;
+    
+        var titlle = userLogin.getUserType() + " [" + userLogin.getUserName() + "] - profile: " 
+    
+        text = '<div class="well"> <text> ' + titlle + ' </text></div>';
+        text += '<div class="well">';
+
+        if (userWallet.nomTokens)
+    
+        for (var i = 0; i < userWallet.tokenNos; ++i) {
+            symbol = userWallet.tokenSymbol[i];
+            adr = userWallet.tokenAddress[i];
+            balance = userWallet.tokenBalance[i];
+            hashId = symbol + "Hash";
+            sentoId = symbol + "Dest";
+            amountId = symbol + "Amount";
+    
+            text += '---------------</text><br>'
+            if (userWallet.tokenStatus[i] == "false") {
+                text += '<button type="button" onClick="' + enableWalletPrefix + 'EnableZSCWalletHash' + "', '" + hashId + enableWalletSuffix + '">Enable TestZSC Wallet</button><br>'
+                text += '<text id="EnableZSCWalletHash" value = "log:"> </text> <br>';
+                text += '<text>---------------</text><br>'
+            } else {
+                text += 'Symbol: <text>Test' + symbol + '</text><br>'
+                text += 'Address: <text> <a href="https://rinkeby.etherscan.io/address/0x' + adr + '#tokentxns" target="_blank" >0x' + adr + '</a></text><br>'
+                text += 'Balance: <text>' + balance + '</text><br><br>'
+                text += '---------------</text><br>'
+                text += 'Dest-adr<input id="' + sentoId + '"></input> <br> Amount:<input id="' + amountId + '"></input> <br>'
+                text += '  <button type="button" onClick="' + transPrefix + symbol + "', '" + sentoId + "', '" + amountId + "', '" + hashId + transSuffix + '">  Transfer  </button> <br>'
+                text += '<text id="'+ hashId + '" value = "log:"> </text> <br>';
+                text += '<text>---------------</text><br>'
+            }
+    
+        }
+        text += '</div>'
+    
+        document.getElementById(elementId).innerHTML = text;  
     }
 
     function submitParameterProfileChanges(logID) {
