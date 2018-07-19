b0746946774dea55bf3d4054008a10fac5ca507a
Eric-Yao <norhustar@hotmail.com>
2018-07-19 15:55:56 +0200

    updated class PosRobotOwned

--- WebClient/pos/pos_robot_owned.js
+++ WebClient/pos/pos_robot_owned.js
@@ -7,6 +7,15 @@ function ZSCRobotOwned(acount, adr, abi) {
     this.userType;
     this.robotNos = 0;
     this.robotIds = [];
+    this.robotLevs = [];
+    this.robotMaxSP = [];
+    this.robotCurSP = [];
+    this.robotEnhanceProb = [];
+    this.robotMineStart = [];
+    this.robotMineEnd = [];
+    this.robotPrceToEnhance = [];
+    this.robotPrceToCreate = [];
+    this.robotPrceForSale = [];
     this.itemTags = [];
     this.account = acount;
     this.contractAdr = adr;
@@ -15,9 +24,17 @@ function ZSCRobotOwned(acount, adr, abi) {
     this.gasLimit = bF_getGasLimit(700);
 }
 
-ZSCRobotOwned.prototype.getUserName = function() {return this.userName;}
-ZSCRobotOwned.prototype.getRobotId = function(index) { return this.robotIds[index];}
-
+ZSCRobotOwned.prototype.getRobotNos = function() { return this.robotNos;}
+ZSCRobotOwned.prototype.getRobotId  = function(index) { return this.robotIds[index];}
+ZSCRobotOwned.prototype.getRobotLev = function(index) { return this.robotLevs[index];}
+ZSCRobotOwned.prototype.getMaxSP = function(index) { return this.robotMaxSP[index];}
+ZSCRobotOwned.prototype.getCurSP = function(index) { return this.robotCurSP[index];}
+ZSCRobotOwned.prototype.getEnhanceProb = function(index) { return this.robotEnhanceProb[index];}
+ZSCRobotOwned.prototype.getMineStart = function(index) { return this.robotMineStart[index];}
+ZSCRobotOwned.prototype.getMineEnd = function(index) { return this.robotMineEnd[index];}
+ZSCRobotOwned.prototype.getPrceToEnhance = function(index) { return this.robotPrceToEnhance[index];}
+ZSCRobotOwned.prototype.getPrceToCreate = function(index) { return this.robotPrceToCreate[index];}
+ZSCRobotOwned.prototype.getPrceForSale = function(index) { return this.robotPrceForSale[index];}
 
 ZSCRobotOwned.prototype.resetAllItemTags = function(gm) {
     for (var i = 0; i < gm.userNos; ++i) {
@@ -76,11 +93,16 @@ ZSCRobotOwned.prototype.activeMinerRobot = function(robotId, rewardType, func) {
         });
 }
 
-
-
-
-
-
-
+ZSCRobotOwned.prototype.loadUsers = function(phpFunc) {
+    this.phpCallback = phpFunc;
+    this.numUsers(this, function(gm) {
+        gm.resetAllItemTags(gm);
+       if (gm.userNos == 0) {
+            gm.phpCallback();
+        } else {
+            gm.loadUserInfos();
+        }
+    });
+}
 
   
