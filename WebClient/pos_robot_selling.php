9d15fa654809d3b1121a85ca138b4aad933806f8
Eric-Yao <norhustar@hotmail.com>
2018-07-20 22:12:33 +0200

    updated function loadUserRobots(); added function sellRobot(), cancelSelling(), cancelSelling()

--- WebClient/pos_robot_selling.php
+++ WebClient/pos_robot_selling.php
@@ -42,9 +42,86 @@ session_start();
 
     /////////////////////////////
     function loadUserRobots() {
+        userRobotGM.loadUserRobots(function() {
+            loadHtml("PageBody", "sellRobot", "cancelSelling");
+        });
     }
 
-    function sellRobot(hashId) {
+    function sellRobot(hashId, robotId, priceId) {
+        var price = document.getElementById(priceId).value;
+        userRobotGM.publishMinerRobot(hashId, robotId, price, function() {                
+        });
+    }
+
+    function cancelSelling(hashId, robotId) {
+        userRobotGM.cancalSellingMinerRobot(hashId, robotId, function() {                
+        });
+    }
+
+    function loadHtml(elementId, sellRobot, cancleSelling) {
+        var sellPrefix = sellRobot + "('OperationHash', '"; 
+        var sellSuffix = "')";
+
+        var cancelPrefix = cancleSelling + "('OperationHash', '"; 
+        var cancelSuffix = "')";
+
+        var symbol;
+        var adr;
+        var balance;
+        var hashId;
+        var robotNos = userRobotGM.getRobotNos();
+    
+        var titlle = "user owned robots: " 
+
+        text  = '<div class="well">' + titlle + '<br>';
+        text  = '<text id="OperationHash" value = "log:"> </text> </div>';
+
+        text += '<div class="well">';
+        text += '<table align="center" style="width:600px;min-height:30px">'
+        text += '   <tr> <td>Robot ID</td> <td>Status</td> <td>Level</td> <td>Max SP</td> <td> Price (ETH) </td> <td> Sell </td> <td> Cancel </td></tr> '
+        text += '   <tr> <td>------</td> <td>------</td> <td>------</td> <td>------</td> <td>------</td> <td>------</td> <td>------</td> </tr>'
+
+        for (var i = 0; i < robotNos; ++i) {
+            var robotId = userRobotGM.getRobotId(i);
+            var status = "Idle"; 
+            var priceId = "Price-" + robotId;
+
+            if (!userRobotGM.miningable(i)) {
+                status = "Mining";
+            } else {
+                if (userRobotGM.getPriceForSale(i) > 0) {
+                    status = "Selling";
+                } 
+            }
+
+            if (status != "Mining") {
+                text += '<tr>'
+                text += '   <td><text>' + robotId + '</text></td>'
+                text += '   <td><text>' + status + '</text></td>'
+                text += '   <td><text>' + userRobotGM.getRobotLev(i) + '</text></td>'
+                text += '   <td><text>' + userRobotGM.getMaxSP(i) + '</text></td>'
+
+                if (status == "Idle") {
+                    text += '   <td><input style="width:100px" id="' + priceId + '"></input><br>'
+                    text += '   <td><button type="button" onClick="' + sellPrefix + robotId + "', '" + priceId + "', '" + sellSuffix + '"> Sell </button></td>'
+                } else {
+                    text += '   <td><text>--</text></td>'
+                    text += '   <td><text>--</text></td>'
+                }
+
+                if (status == "Selling") {
+                    text += '   <td><button type="button" onClick="' + cancelPrefix + robotId + cancelSuffix + '"> Cancel </button></td>'
+                } else {
+                    text += '   <td><text>--</text></td>'
+                }
+                text += '</tr>'
+                text += '   <tr> <td>------</td> <td>------</td> <td>------</td> <td>------</td> <td>------</td> <td>------</td> <td>------</td> </tr>'
+            }
+        }
+        text += '</table>'
+        text += '</div>'
+    
+        document.getElementById(elementId).innerHTML = text;  
     }
 
 </script>
