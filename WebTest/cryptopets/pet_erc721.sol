f559ef4666afded2e347250f8ad961dbcf02df6c
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-22 19:07:42 +0800

    add constructor and relationship for pet_erc721.sol

--- WebTest/cryptopets/pet_erc721.sol
+++ WebTest/cryptopets/pet_erc721.sol
@@ -2,7 +2,12 @@
  Copyright (c) 2018, ZSC Dev Team
 */
 
+import "./pet_base.sol";
+import "./erc721.sol";
+
 pragma solidity ^0.4.21;
 
-contract PetERC721 {
+contract PetERC721 is PetBase, ERC721 {
+
+    function PetERC721() public PetBase() {}
 }
