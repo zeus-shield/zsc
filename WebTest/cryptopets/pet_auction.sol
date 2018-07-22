ac3b8ca1e90d14d7e1bb8e56da32f03e3a25e56c
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-22 19:13:56 +0800

    add constructor and relationship for pet_auction.sol

--- WebTest/cryptopets/pet_auction.sol
+++ WebTest/cryptopets/pet_auction.sol
@@ -2,7 +2,11 @@
  Copyright (c) 2018, ZSC Dev Team
 */
 
+import "./pet_breeding.sol";
+
 pragma solidity ^0.4.21;
 
-contract PetAuction {
+contract PetAuction is PetBreeding {
+
+    function PetAuction() public PetBreeding() {}
 }
