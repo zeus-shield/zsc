8f79755d615e3ac77f8a3fbdc8e0c87bc9162fbf
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-22 19:04:02 +0800

    add interface for erc721.sol

--- WebTest/cryptopets/erc721.sol
+++ WebTest/cryptopets/erc721.sol
@@ -5,4 +5,24 @@
 pragma solidity ^0.4.21;
 
 contract ERC721 {
+    // Required methods
+    function totalSupply() public view returns (uint256 total);
+    function balanceOf(address _owner) public view returns (uint256 balance);
+    function ownerOf(uint256 _tokenId) external view returns (address owner);
+    function approve(address _to, uint256 _tokenId) external;
+    function transfer(address _to, uint256 _tokenId) external;
+    function transferFrom(address _from, address _to, uint256 _tokenId) external;
+
+    // Events
+    event Transfer(address _from, address _to, uint256 _tokenId);
+    event Approval(address _owner, address _approved, uint256 _tokenId);
+
+    // Optional
+    // function name() public view returns (string name);
+    // function symbol() public view returns (string symbol);
+    // function tokensOfOwner(address _owner) external view returns (uint256[] tokenIds);
+    // function tokenMetadata(uint256 _tokenId, string _preferredTransport) public view returns (string infoUrl);
+
+    // ERC-165 Compatibility (https://github.com/ethereum/EIPs/issues/165)
+    function supportsInterface(bytes4 _interfaceID) external view returns (bool);
 }
