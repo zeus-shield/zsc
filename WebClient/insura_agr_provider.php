8f414bcf2a2f3bae99b5704953a48b80bfc67269
Eric-Yao <norhustar@hotmail.com>
2018-07-24 21:02:15 +0200

    added file insura_agr_provider.php

---
+++ WebClient/insura_agr_provider.php
@@ -0,0 +1,51 @@
+<?php 
+/*
+Copyright (c) 2018 ZSC Dev Team
+*/
+session_start();
+?>
+
+<html>
+<head>
+<?php 
+    include("include.php");
+    $htmlObjects = new ZSCInclude($_SESSION["userType"]);
+    echo $htmlObjects->loadScriptFiles(); 
+?>
+</head>
+<body>
+    <div class="col-lg-12">
+        <br><i>Web-client for testing ZSC system on the Rinkeby</i><br><br>
+        <div class="well" id="PageAlert"><?php echo $htmlObjects->loadAlert(); ?></div>
+        <div class="well" id="PageHeader"><?php echo $htmlObjects->loadHeader(); ?></div>
+        <div class="well" id="PageBody"></div>
+    </div>
+
+<script type="text/javascript">
+    /////////////////////////////
+    <?php echo $htmlObjects->loadWeb3();?>
+    var checkeWeb3Account = <?php echo $htmlObjects->checkWeb3Account();?>;
+    var userType = <?php echo "'".$_SESSION["userType"]."'";?>;
+    var userLogin;
+    var providerAgreementGM;
+
+    checkeWeb3Account(function(account) {
+        userLogin = new ZSCLogin(account);
+        userLogin.tryLogin(userType, function(ret) {
+            if(!ret) {  
+                window.location.href = "index.php";
+            } else {
+                providerAgreementGM = new ZSCAgreementProvider(account, userLogin.getControlApisAdr(), userLogin.getControlApisFullAbi());
+                loadPublishedAgreements();
+            }
+        });
+    });
+
+    /////////////////////////////
+    function loadPublishedAgreements() {
+    }
+
+</script>
+
+</body>
+</html>
