592a2175d68f456d31b2743ea9594682900ea323
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-26 20:45:56 +0800

    add function _owns and _approve for pet_erc721.sol

--- WebTest/cryptopets/pet_erc721.sol
+++ WebTest/cryptopets/pet_erc721.sol
@@ -11,6 +11,14 @@ contract PetERC721 is PetBase, ERC721 {
 
     function PetERC721() public PetBase() {}
 
+    function _owns(address _owner, uint256 _tokenId) internal view returns (bool) {
+        return kittyIndexToOwner[_tokenId] == _owner;
+    }
+
+    function _approve(uint256 _tokenId, address _approved) internal {
+        indexToApproved_[_tokenId] = _approved;
+    }
+
     function totalSupply() public view returns (uint256) {
         return pets_.length - 1;
     }
