0201d200ec3d2e16d0d755ed7273ef883fa3e7c2
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-27 19:57:10 +0800

    add funtion '_approvedFor' for pet_erc721.sol

--- WebTest/cryptopets/pet_erc721.sol
+++ WebTest/cryptopets/pet_erc721.sol
@@ -12,7 +12,11 @@ contract PetERC721 is PetBase, ERC721 {
     function PetERC721() public PetBase() {}
 
     function _owns(address _owner, uint256 _tokenId) internal view returns (bool) {
-        return kittyIndexToOwner[_tokenId] == _owner;
+        return indexToOwner[_tokenId] == _owner;
+    }
+
+    function _approvedFor(address _claimant, uint256 _tokenId) internal view returns (bool) {
+        return indexToApproved_[_tokenId] == _claimant;
     }
 
     function _approve(uint256 _tokenId, address _approved) internal {
