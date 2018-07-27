59c9e0500e900aa64b6467346bbbca21289b76ad
Eric-Yao <norhustar@hotmail.com>
2018-07-27 14:53:11 +0200

    added file z_market.php

--- WebClient/z_market.php
+++ WebClient/z_market.php
@@ -3,11 +3,18 @@
 Copyright (c) 2018 ZSC Dev Team
 */
 $tokenId = $_GET["tokenId"];
+$picPrefix = "robot";
+$picSuffix = ".jpg";
+if ($tokenId < 3) {
+	$picPrefix = "art".$tokenId;
+}
+
+
 echo '{'.
-  '"name": "Robot token #'.$tokenId.','.
-  '"image_url": "http://yongyao.se/WebClient/pics/robot.jpeg",'.
+  '"name": "Robot token #'.$tokenId.'",'.
+  '"image_url": "http://yongyao.se/WebClient/pics/'.$picPrefix.$picSuffix.'",'.
   '"home_url": "http://yongyao.se/WebClient/",'.
-  '"description": "This is the amazing Robot ##'.$tokenId.', please buy me!",'.
-  '}'.
+  '"description": "This is the amazing Robot #'.$tokenId.', please buy me!"'.
+  '}';
 ?>
 
