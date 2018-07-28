9c977031901c65d7e29a6c2dc84a69e08ea51756
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-28 21:07:29 +0800

    add interface signature of erc721 and erc165 for pet_erc721.sol

--- WebTest/cryptopets/pet_erc721.sol
+++ WebTest/cryptopets/pet_erc721.sol
@@ -12,6 +12,21 @@ contract PetERC721 is PetBase, ERC721 {
     string constant name = "CryptoPets";
     string constant symbol = "CP";
 
+    bytes4 constant InterfaceSignature_ERC165 =
+        bytes4(keccak256('supportsInterface(bytes4)'));
+
+    bytes4 constant InterfaceSignature_ERC721 =
+        bytes4(keccak256('name()')) ^
+        bytes4(keccak256('symbol()')) ^
+        bytes4(keccak256('totalSupply()')) ^
+        bytes4(keccak256('balanceOf(address)')) ^
+        bytes4(keccak256('ownerOf(uint256)')) ^
+        bytes4(keccak256('approve(address,uint256)')) ^
+        bytes4(keccak256('transfer(address,uint256)')) ^
+        bytes4(keccak256('transferFrom(address,address,uint256)')) ^
+        bytes4(keccak256('tokensOfOwner(address)')) ^
+        bytes4(keccak256('tokenMetadata(uint256,string)'));
+
     function PetERC721() public PetBase() {}
 
     function _owns(address _owner, uint256 _tokenId) internal view returns (bool) {
