2bcb352d3a7ba6ea06a186434c858bde14477281
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-07-28 12:20:03 +0800

    add function [_getXmlHttpObject] for http.js

--- WebTest/js/EthApis/http.js
+++ WebTest/js/EthApis/http.js
@@ -2,14 +2,38 @@
  Copyright (c) 2018 ZSC Dev Team
 */
 
+//private member
+const _getXmlHttpObject = Symbol('_getXmlHttpObject');
+
 export default class Http {
     constructor() {}
+
+    [_getXmlHttpObject]() {
+
+        let xmlHttp = null;
+
+        try {
+            // Firefox, Opera 8.0+, Safari
+            xmlHttp = new XMLHttpRequest();
+        }
+        catch (e) {
+            // Internet Explorer
+            try {
+                xmlHttp=new ActiveXObject("Msxml2.XMLHTTP");
+            }
+            catch (e) {
+                xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+            }
+        }
+        return xmlHttp;
+    }
 
     get(url, time, func) {
         let request = 0;
         let timer = 0;
         let timeout = false;
 
+        /*
         if (window.XMLHttpRequest) {
             // code for all new browsers
             request = new XMLHttpRequest();
@@ -18,6 +42,8 @@ export default class Http {
             // code for IE5 and IE6
             request = new ActiveXObject("Microsoft.XMLHTTP");
         }
+        */
+        request = this.[_getXmlHttpObject];
 
         timer = setTimeout(function() {
             timeout = true;
