aabb48294c0691cef5a8fd2b8a5d59e3b14820da
Eric-Yao <norhustar@hotmail.com>
2018-07-29 17:17:37 +0200

    added function loadHtml()

--- WebClient/pos_robot_takeout.php
+++ WebClient/pos_robot_takeout.php
@@ -52,6 +52,53 @@ session_start();
         userRobotGM.takeOutToOwner(hashId, robotId);
     }
 
+    function loadHtml(elementId, takeout) {
+        var transferPrifix, transferSuffix;
+        transferPrifix = takeout + "('OperationHash', '"; 
+        transferSuffix = "')"; 
+
+        var robotNos = userRobotGM.getRobotNos();
+    
+        var titlle = "Transfer robots: Inner wallet -> Outside wallet" 
+
+        text  = '<div class="well" align="center" >' + titlle + '<br>';
+        text  = '<text id="OperationHash" value = "log:"> </text> </div>';
+
+        text += '<div class="well">';
+        text += '<table align="center" style="width:600px;min-height:30px">'
+
+        text += '   <tr> <td>Robot ID</td> <td>Status</td> <td>Level</td> <td>Max SP</td> <td>Take out to MetaMask</td> </tr> '
+        text += '   <tr> <td>------</td> <td>------</td> <td>------</td> <td>------</td> </tr>'
+
+        for (var i = 0; i < robotNos; ++i) {
+            var robotId = userRobotGM.getRobotId(i);
+            var status = "Idle"; 
+            if (!userRobotGM.miningable(i)) {
+                status = "Mining";
+            } else {
+                if (userRobotGM.getPriceForSale(i) > 0) {
+                    status = "Selling";
+                } 
+            }
+    
+            text += '<tr>'
+            text += '   <td><text>' + robotId + '</text></td>'
+            text += '   <td><text>' + status + '</text></td>'
+            text += '   <td><text>' + userRobotGM.getRobotLev(i) + '</text></td>'
+            text += '   <td><text>' + userRobotGM.getMaxSP(i) + '</text></td>'
+            if (status == "Idle") {
+                text += '   <td><button type="button" onClick="' + transferPrifix + robotId + transferSuffix + '"> Take out </button></td>'
+            } else {
+                text += '   <td><text>Not Available</text></td>'
+            }
+            text += '</tr>'
+            text += '   <tr> <td>------</td> <td>------</td> <td>------</td> <td>------</td>  </tr>'
+        }
+        text += '</table>'
+        text += '</div>'
+    
+        document.getElementById(elementId).innerHTML = text;  
+    }
 
 </script>
 
