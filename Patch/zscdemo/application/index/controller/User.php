e18d07ac4c33a42f25cad061982916fb0802dea7
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-08-01 21:50:07 +0800

    add 'User.php' for zscdemo

---
+++ Patch/zscdemo/application/index/controller/User.php
@@ -0,0 +1,18 @@
+<?php
+// +----------------------------------------------------------------------
+// | SentCMS [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.tensent.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: molong <molong@tensent.cn> <http://www.tensent.cn>
+// +----------------------------------------------------------------------
+
+namespace app\index\controller;
+use app\common\controller\Fornt;
+use think\Cookie;
+use think\Cache;
+use think\Db;
+
+class User extends Fornt{
+
+}
