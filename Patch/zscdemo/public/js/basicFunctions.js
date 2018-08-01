e31ad7c08cababf86bce17e77b85ba4bb0c363d4
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-08-01 21:58:59 +0800

    add 'basicFunctions.js' for zscdemo

---
+++ Patch/zscdemo/public/js/basicFunctions.js
@@ -0,0 +1,42 @@
+/*
+Copyright (c) 2018 ZSC Dev Team
+*/
+
+
+function bF_getEthAccount() {
+    //console.log(web3.eth.accounts[0])
+    var account = web3.eth.accounts[0];
+    //if (account == undefined) alert("Need to login in MetaMask!!");
+    return account;
+}
+
+function bF_getGasPrice() {
+    return 30 * 1000000000; //30 * gwei
+}
+
+function bF_getGasLimit(limit) {
+    return limit * 10**4; //limits * 1 million
+}
+
+function bF_showHashResult(elementID, hash, func) {
+    web3.eth.getTransactionReceipt(hash, 
+    function(error, result){ 
+        if(!error) {
+            var show;
+            if (result == null) {
+                show =  "(pending)" + hash ;
+                bF_showHashResult(elementID, hash, func);
+            } else {
+                if (result.status == 0) {
+                    show = "(failure)" + hash;
+                } else {
+                    show = "(succeeded)" + hash ;
+                    func();
+                }
+            }
+            document.getElementById(elementID).innerText = show;
+        } else {
+            console.log("error: " + error);
+        }
+    });
+}
