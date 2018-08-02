5b217f363bc833489d4bea76c2967dfd885ae038
scy-aojiao <1652336252@qq.com>
2018-08-02 18:43:03 +0800

    add function userLogin

--- Patch/zscdemo/application/index/model/User.php
+++ Patch/zscdemo/application/index/model/User.php
@@ -33,4 +33,53 @@ class User extends Base{
 		$sex = [0=>'男',1=>'女',2=>"未设置"];
        	return $sex[$value];
 	}
+/**
+	 * 用户登录
+	 * @param  [type] $data [登录参数]
+	 * @return [type]       [boole]
+	 */
+	public function userLogin($data,$reg='')
+	{
+		$userInfo = $this->where('account',$data['account'])->find();
+
+		if(empty($userInfo)){
+			return -1;//没有该用户
+		}elseif($userInfo['status']==0){
+			return -2;//用户审核没有通过
+		}
+
+		$password = $this->pwdEncrypt($data['password'],$userInfo['salt']);
+
+		if($userInfo['password'] == $password && $data['remePwd']==1){
+			//保存Cookie
+     		Cookie::set("uid",$userInfo['uid'],60*60*30);//默认保存时间为一个月
+			Cookie::set("account",$userInfo['account'],60*60*30);
+			Cookie::set("zscAccount",$userInfo['account'],60*60*30);//默认保存时间为一个月
+			Cookie::set("zscPassword",$data['password'],60*60*30);
+	
+
+     		if(preg_match("/^1[34578]\d{9}$/", $data['account'])){
+				Cookie::set('type',0,60*60*30);//个人用户登录
+ 			}else{
+				Cookie::set('type',1,60*60*30);//企业用户登录
+ 			}
+     		return true;
+		}elseif($userInfo['password'] == $password){
+			//不保存cookie, 关闭浏览器自动清楚
+     		Cookie::set("uid",$userInfo['uid']);
+			Cookie::set("account",$userInfo['account']);
+			Cookie::set("zscAccount",$userInfo['account']);//默认保存时间为一个月
+			Cookie::set("zscPassword",$data['password']);
+		
+
+     		if(preg_match("/^1[34578]\d{9}$/", $data['account'])){
+				Cookie::set('type',0);//个人用户登录
+ 			}else{
+				Cookie::set('type',1);//企业用户登录
+ 			}
+     		return true;
+		}else{
+			return false;//密码错误
+		}
+	}
 }
