87b01fff69028a5acb729d89f167b2d47ca26f42
swedenwolf <xmuguoyang@gmail.com>
2018-08-09 17:23:18 +0800

    added functons for Recognizee.php

--- Patch/zscdemo/application/index/model/Recognizee.php
+++ Patch/zscdemo/application/index/model/Recognizee.php
@@ -21,6 +21,35 @@ class Recognizee extends Base{
        	return $sex[$value];
 	}
 
+	protected function setPapersTimeAttr($value)
+    {
+        return strtotime($value);
+    }
+
+    protected function getPapersTimeAttr($value)
+    {
+        return date('Y-m-d',$value);
+    }
+
+    protected function setUidAttr($value)
+    {
+    	return Cookie::get('uid');
+    }
+
+
+
+
+ //    protected function getPapersImgxAttr($value, $data){
+	// 	return get_cover($value,'path');
+	// }
+
+	// protected function getPapersImgYAttr($value, $data){
+	// 	return get_cover($value,'path');
+	// }
+
+	// protected function getHandIdentityImgAttr($value, $data){
+	// 	return get_cover($value,'path');
+	// }
 
 
 }
