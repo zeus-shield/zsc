deee45efc286b0fdcf0faa93e798a65a785a490f
swedenwolf <xmuguoyang@gmail.com>
2018-08-09 17:20:18 +0800

    added functions for Order.php

--- Patch/zscdemo/application/index/model/Order.php
+++ Patch/zscdemo/application/index/model/Order.php
@@ -30,7 +30,47 @@ class Order extends Base{
     {
        return date('YmdHi') . str_pad(mt_rand(1, 99999), 5, '0', STR_PAD_LEFT);;
     }
-   
+    //起保时间
+    protected function getStartProtectAttr($value)
+    {
+        return date('Y-m-d',$value);
+    }
+    //到期时间
+    protected function getEndTimeAttr($value)
+    {
+        return date('Y-m-d',$value);
+    }
+    //交费方式
+    protected function getPayTypeAttr($value)
+    {
+        if($value){
+            return "年交";
+        }else{
+            return "季交";
+        }
+    }
+    //附加长期医疗
+    protected function getMedicalAttr($value)
+    {
+        if($value){
+            return $value;
+        }else{
+            return "不投保";
+        }
+    }
+    //pid
+    protected function getPidAttr($value)
+    {
+        $name = Db::name('product')->where('pid',$value)->column('name');
+
+        return $name;
+    }
+
+    protected function setUidAttr($value)
+    {
+    	return Cookie::get('uid');
+    }
+
  //    protected function getPapersImgxAttr($value, $data){
 	// 	return get_cover($value,'path');
 	// }
