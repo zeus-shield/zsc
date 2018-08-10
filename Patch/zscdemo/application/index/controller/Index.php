3d1216df74b1237e4b5e357622851523b43d1ec9
swedenwolf <xmuguoyang@gmail.com>
2018-08-10 17:22:23 +0800

    added file Patch/zscdemo/application/index/controller/Index.php

---
+++ Patch/zscdemo/application/index/controller/Index.php
@@ -0,0 +1,28 @@
+<?php
+// +----------------------------------------------------------------------
+// | SentCMS [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.tensent.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: molong <molong@tensent.cn> <http://www.tensent.cn>
+// +----------------------------------------------------------------------
+
+namespace app\index\controller;
+use app\common\controller\Fornt;
+use think\Cookie;
+class Index extends Fornt {
+
+	//网站首页
+	public function index()
+	{
+		
+	}
+
+	//保险理赔
+	public function claims()
+	{
+		
+
+	}
+
+}
