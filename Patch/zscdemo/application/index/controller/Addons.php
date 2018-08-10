6a03c1c640db4a58663f66329da6e16a6592c022
swedenwolf <xmuguoyang@gmail.com>
2018-08-10 17:01:41 +0800

    added file Patch/zscdemo/application/index/controller/Addons.php

---
+++ Patch/zscdemo/application/index/controller/Addons.php
@@ -0,0 +1,15 @@
+<?php
+// +----------------------------------------------------------------------
+// | SentCMS [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.tensent.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: molong <molong@tensent.cn> <http://www.tensent.cn>
+// +----------------------------------------------------------------------
+
+namespace app\index\controller;
+use app\common\controller\Fornt;
+
+class Addons extends Fornt {
+	
+}
