8be45bffbd830ada4b685e42332abc23dba7cd61
swedenwolf <xmuguoyang@gmail.com>
2018-08-10 17:27:34 +0800

    Patch/zscdemo/application/index/controller/Product.php

---
+++ Patch/zscdemo/application/index/controller/Product.php
@@ -0,0 +1,21 @@
+<?php
+// +----------------------------------------------------------------------
+// | SentCMS [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.tensent.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: molong <molong@tensent.cn> <http://www.tensent.cn>
+// +----------------------------------------------------------------------
+
+namespace app\index\controller;
+use app\common\controller\Fornt;
+use think\Db;
+class Product extends Fornt{
+
+	/**
+	 * 产品列表页面
+	 * @return [type] [description]
+	 */
+	
+
+}
