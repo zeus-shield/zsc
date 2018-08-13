35a901a12cf88fab3ec8d71b481247021bfc513b
swedenwolf <xmuguoyang@gmail.com>
2018-08-13 18:06:15 +0800

    added file Patch/zscdemo/application/index/controller/Zscprovider.php

---
+++ Patch/zscdemo/application/index/controller/Zscprovider.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace app\index\controller;
+use app\common\controller\Fornt;
+use think\Cookie;
+use think\Cache;
+use think\Db;
+class Zscprovider extends Fornt{
+    public function _initialize()
+	{
+		parent::_initialize();
+		
+	}
+
+}
