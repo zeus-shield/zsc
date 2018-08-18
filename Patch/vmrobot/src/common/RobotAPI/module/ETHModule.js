6e9d49b9298dea13320bf9e7a8a5e497c5a43c1a
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-08-18 17:02:46 +0800

    add function 'LoadModule' for ETHModule.js

--- Patch/vmrobot/src/common/RobotAPI/module/ETHModule.js
+++ Patch/vmrobot/src/common/RobotAPI/module/ETHModule.js
@@ -1,6 +1,13 @@
 /*
 Copyright (c) 2018 ZSC Dev Team
 */
+import ZSCWallet from '../common/zsc_wallet';
+import ZSCLogin from '../common/zsc_login';
+import ZSCRobotOwned from '../pos/pos_robot_owned';
+import ZSCRobotMarket from '../pos/pos_robot_market';
+
 const ETHModule = {
+    LoadModule(acount, module) {
+    },
 }
 export default ETHModule;
