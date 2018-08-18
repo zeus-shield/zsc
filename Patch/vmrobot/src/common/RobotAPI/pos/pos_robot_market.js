9a7d528015b69779425ce9b7e88915dd044374ba
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-08-18 17:05:07 +0800

    add function 'constructor' for pos_robot_market.js

--- Patch/vmrobot/src/common/RobotAPI/pos/pos_robot_market.js
+++ Patch/vmrobot/src/common/RobotAPI/pos/pos_robot_market.js
@@ -1,6 +1,11 @@
 /*
 Copyright (c) 2018 ZSC Dev Team
 */
+import {
+    bF
+} from '../common/basicFunctions'
 //class 
 export default class ZSCRobotMarket {
+    constructor(account, adr, abi) {
+    }
 }
