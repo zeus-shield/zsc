43c2394655f690628c2eddb61ea6799ca8b3950b
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-08-18 17:05:55 +0800

    add function 'constructor' for pos_robot_owned.js

--- Patch/vmrobot/src/common/RobotAPI/pos/pos_robot_owned.js
+++ Patch/vmrobot/src/common/RobotAPI/pos/pos_robot_owned.js
@@ -1,6 +1,11 @@
 /*
 Copyright (c) 2018 ZSC Dev Team
 */
+import {
+    bF
+} from '../common/basicFunctions';
 //class 
 export default class ZSCRobotOwned {
+    constructor(acount, adr, abi) {
+    }
 }
