8851df9cf6997b0c40e08841f8fd2c9295716b9c
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-08-19 19:18:44 +0800

    add function 'constructor' for zsc_wallet.js

--- Patch/vmrobot/src/common/RobotAPI/common/zsc_wallet.js
+++ Patch/vmrobot/src/common/RobotAPI/common/zsc_wallet.js
@@ -1,6 +1,11 @@
 /*
 Copyright (c) 2018 ZSC Dev Team
 */
+import {
+    bF
+} from './basicFunctions'
 //class zscWallet
 export default class ZSCWallet {
+    constructor(acount, adr, abi) {
+    }
 }
