8f220ac01a22c19f911e117932e66d830e2ec63d
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-08-20 19:17:28 +0800

    implement function 'constructor' for pos_robot_market.js

--- Patch/vmrobot/src/common/RobotAPI/pos/pos_robot_market.js
+++ Patch/vmrobot/src/common/RobotAPI/pos/pos_robot_market.js
@@ -7,6 +7,18 @@ import {
 //class 
 export default class ZSCRobotMarket {
     constructor(account, adr, abi) {
+        this.robotNos = 0;
+        this.robotIds = [];
+        this.robotLevs = [];
+        this.robotMaxSP = [];
+        this.robotSeller = [];
+        this.robotPrceForSale = [];
+        this.itemTags = [];
+        this.account = account;
+        this.contractAdr = adr;
+        this.contractAbi = JSON.parse(abi);
+        this.gasPrice = bF.bF_getGasPrice();
+        this.gasLimit = bF.bF_getGasLimit();
     }
     getRobotNos() {
     }
