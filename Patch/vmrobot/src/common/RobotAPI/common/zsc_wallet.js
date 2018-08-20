672e7d18b416670a524e0362467d3f6729c93767
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-08-20 19:16:46 +0800

    implement function 'constructor' for zsc_wallet.js

--- Patch/vmrobot/src/common/RobotAPI/common/zsc_wallet.js
+++ Patch/vmrobot/src/common/RobotAPI/common/zsc_wallet.js
@@ -7,5 +7,18 @@ import {
 //class zscWallet
 export default class ZSCWallet {
     constructor(acount, adr, abi) {
+        this.userType;
+        this.tokenNos = 0;
+        this.tokenSymbol = [];
+        this.tokenBalance = [];
+        this.tokenStatus = [];
+        this.tokenLocked = [];
+        this.tokenAddress;
+        this.itemTags = [];
+        this.account = acount;
+        this.contractAdr = adr;
+        this.contractAbi = JSON.parse(abi);
+        this.gasPrice = bF.bF_getGasPrice();
+        this.gasLimit = bF.bF_getGasLimit();
     }
 }
