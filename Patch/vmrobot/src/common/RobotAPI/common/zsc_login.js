2d6e29e2cd3696793c389f11577931f46fcf3448
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-08-20 19:16:17 +0800

    implement function 'constructor' for zsc_login.js

--- Patch/vmrobot/src/common/RobotAPI/common/zsc_login.js
+++ Patch/vmrobot/src/common/RobotAPI/common/zsc_login.js
@@ -6,5 +6,14 @@ import {
 } from './basicFunctions'
 export default class ZSCLogin {
     constructor(userAccount) {
+        this.admAdr = "0x162b40e67f72a8ffc13b24b4f15ac7b98d92e454";
+        this.userStatus;
+        this.userType;
+        this.controlApisAdr;
+        this.controlApisFullAbi;
+        this.account = userAccount;
+        this.myAdmAdv = web3.eth.contract(this.getLoginAbi()).at(this.admAdr);
+        this.gasPrice = bF.bF_getGasPrice();
+        this.gasLimit = bF.bF_getGasLimit();
     }
 }
