af6b1d1f24893d592eff37e4ceac1d446b7b36ca
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-08-20 19:15:22 +0800

    implement function 'bF_getEthAccount' and 'bF_getGasPrice' for basicFunctions.js

--- Patch/vmrobot/src/common/RobotAPI/common/basicFunctions.js
+++ Patch/vmrobot/src/common/RobotAPI/common/basicFunctions.js
@@ -4,8 +4,13 @@ Copyright (c) 2018 ZSC Dev Team
 
 export const bF = {
     bF_getEthAccount() {
+        //console.log(web3.eth.accounts[0])
+        var account = web3.eth.accounts[0];
+        //if (account == undefined) alert("Need to login in MetaMask!!");
+        return account;
     },
 
     bF_getGasPrice() {
+        return 20 * 1000000000; //30 * gwei
     },
 }
