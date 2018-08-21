054e3475cf24d0dc2a4d7c4a9b15dbcf7c42ead2
Eric-Yao <norhustar@hotmail.com>
2018-08-21 15:36:06 +0800

    updated contract SysGmEquip

--- EthCore/sys_gm_equip.sol
+++ EthCore/sys_gm_equip.sol
@@ -23,4 +23,30 @@ contract SysGmEquip is Erc721Adv, SysGmBase {
     function SysGmEquip(bytes32 _name) public SysGmBase(_name) {
     } 
 
+    function checkEquipUser(address _user, uint _equipId) internal view {
+        require(_user == ownerOf(_equipId));
+    }
+
+    function setEquipStatus(uint _equipId, bytes32 _status) internal {
+        robots_[_equipId].status_ = _status;
+    }
+
+    function getEquipStatus(uint _equipId) internal view returns (bytes32) {
+        return robots_[_equipId].status_;
+    }
+
+    function numEquips() public view returns (uint) {
+        return robotNos_;  
+    }
+
+    function getEquipInfo(uint _equipId) public view returns (bytes32, uint, uint, uint, uint) {
+        checkDelegate(msg.sender, 1);
+        require(_equipId < equipNos_);
+        
+        return (robots_[_equipId].status_,
+                robots_[_equipId].level_,
+                robots_[_equipId].price_,
+                robots_[_equipId].spEffect_,
+                robots_[_equipId].rewardEffect_);
+    }
 }
