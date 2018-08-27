95b1142ac8da24a7ffffc260b581823b7f73068e
swedenwolf <xmuguoyang@gmail.com>
2018-08-27 14:35:06 +0800

    added file 'zsc_agreements_all.js'

---
+++ Patch/zscdemo/public/js/zsc_agreements_all.js
@@ -0,0 +1,19 @@
+/*
+Copyright (c) 2018 ZSC Dev Team
+*/
+
+//class zscWallet
+function ZSCAgreementAll(nm, abi, adr) {
+    this.userName = nm;
+    this.userType;
+    this.allAgrNos = 0;
+    this.allAgrNames = [];
+    this.allAgrStatus = [];
+    this.itemTags = [];
+    this.account = web3.eth.accounts[0];
+    this.contractAdr = adr;
+    this.contractAbi = JSON.parse(abi);
+    this.gasPrice = bF_getGasPrice();
+    this.gasLimit = bF_getGasLimit(700);
+}
+
