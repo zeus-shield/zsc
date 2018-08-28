91ed367d137123c1b355fdba8c96004b1954476f
scy-aojiao <1652336252@qq.com>
2018-08-28 18:06:59 +0800

    add class 'ZSCAgreement'

---
+++ Patch/zscdemo/public/js/zsc_agreements.js
@@ -0,0 +1,17 @@
+/*
+Copyright (c) 2018 ZSC Dev Team
+*/
+
+//class zscWallet
+function ZSCAgreement(nm, abi, adr) {
+    this.userName = nm;
+    this.tmpName;
+    this.agrNos = 0;
+    this.agrNames = [];
+    this.balance = [];
+    this.account = web3.eth.accounts[0];
+    this.contractAdr = adr;
+    this.contractAbi = JSON.parse(abi);
+    this.gasPrice = bF_getGasPrice();
+    this.gasLimit = bF_getGasLimit(700);
+}
