cc0ae3327e7c8521ae67d3f8785bee448aaa05b9
swedenwolf <xmuguoyang@gmail.com>
2018-08-28 11:51:20 +0800

    added file 'zsc_transactions.js'

---
+++ Patch/zscdemo/public/js/zsc_transactions.js
@@ -0,0 +1,21 @@
+/*
+Copyright (c) 2018 ZSC Dev Team
+*/
+
+//class zscWallet
+function ZSCTransactions(nm, abi, adr) {
+    this.userName = nm;
+    this.transNos;
+    this.tokenSymbol;
+    this.timeMoments = [];
+    this.inputTags = [];
+    this.txHash = [];
+    this.amounts = [];
+    this.senders = [];
+    this.receivers = [];
+    this.contractAdr = adr;
+    this.contractAbi = JSON.parse(abi);
+    this.account = web3.eth.accounts[0];
+    this.gasPrice = bF_getGasPrice();
+    this.gasLimit = bF_getGasLimit(700);
+}
