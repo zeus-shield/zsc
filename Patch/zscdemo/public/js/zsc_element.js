91d239760f77b91cc66e8dfa33843cf61a672860
swedenwolf <xmuguoyang@gmail.com>
2018-08-28 11:56:21 +0800

    added file 'zsc_element.js'

---
+++ Patch/zscdemo/public/js/zsc_element.js
@@ -0,0 +1,22 @@
+/*
+Copyright (c) 2018 ZSC Dev Team
+*/
+
+//class zscElement
+function ZSCElement(userName, controlApisAdvAbi, controlApisAdvAdr) {
+    this.userName = userName;
+    this.userType;
+    this.enName;
+    this.parameNos = 0;
+    this.ethBalance = 0;
+    this.nodeAddress = 0;
+    this.parameterNames = [];
+    this.parameterValues = [];
+    this.nameTags = [];
+    this.valueTags = [];
+    this.account = web3.eth.accounts[0];
+    this.contractAdr = controlApisAdvAdr;
+    this.contractAbi = JSON.parse(controlApisAdvAbi);
+    this.gasPrice = bF_getGasPrice();
+    this.gasLimit = bF_getGasLimit(700);
+}
