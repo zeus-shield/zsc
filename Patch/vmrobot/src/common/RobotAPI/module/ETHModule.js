5fdf06c6142f8d2cdb8da1ed85b7b6c2b3d68947
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-08-29 18:16:54 +0800

    add interfaces for ETHModule.js in Patch.

--- Patch/vmrobot/src/common/RobotAPI/module/ETHModule.js
+++ Patch/vmrobot/src/common/RobotAPI/module/ETHModule.js
@@ -5,9 +5,18 @@ import ZSCWallet from '../common/zsc_wallet';
 import ZSCLogin from '../common/zsc_login';
 import ZSCRobotOwned from '../pos/pos_robot_owned';
 import ZSCRobotMarket from '../pos/pos_robot_market';
+import Robot from '../../Robot';
 
 const ETHModule = {
     LoadModule(acount, module) {
+    },
+
+    /**
+     * 获取交易市场机器人
+     * @param {*} acount 用户钱包地址
+     * @param {*} RobotId 机器人ID
+     */
+    getMarketRobot(acount, RobotId) {
     },
     GetDealRobot(acount, RobotId, wallet) {
     },
