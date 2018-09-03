c891d5500741166fbc97f41e709095cbc92aef75
swedenwolf <xmuguoyang@gmail.com>
2018-09-03 15:04:26 +0200

    added function submitTransferValue() for 'zsc_wallet.js'

--- Patch/zscdemo/public/js/zsc_wallet.js
+++ Patch/zscdemo/public/js/zsc_wallet.js
@@ -27,3 +27,21 @@ ZSCWallet.prototype.setUserName = function(nm) {this.userName = nm; }
 ZSCWallet.prototype.setUserType = function(type) {this.userType = type;}
 
 ZSCWallet.prototype.getUserType = function() {return this.userType;}
+
+ZSCWallet.prototype.submitTransferValue = function(tokenSymbol, destAddress, amount, logId, func) {  
+    var gm = this;
+    var callBack = func;
+    var myControlApi = web3.eth.contract(gm.contractAbi).at(gm.contractAdr);
+
+    if (destAddress != 0 && amount > 0) {
+        myControlApi.submitTransfer(gm.userName, tokenSymbol, destAddress, web3.toWei(amount, 'ether') , 
+            {from: gm.account, gasPrice: gm.gasPrice, gas: gm.gasLimit},
+            function(error, result){ 
+            if(!error) {
+                bF_showHashResult(logId, result, callBack);
+            } else {
+                console.log("error: " + error);
+            }
+        });
+    }
+}
