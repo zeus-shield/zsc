4db02a192d3c46075b1bd82cde36f8a9ddcd67d5
swedenwolf <xmuguoyang@gmail.com>
2018-09-03 15:01:56 +0200

    added function getLoginAbi() for 'zsc_user.js'

--- Patch/zscdemo/public/js/zsc_user.js
+++ Patch/zscdemo/public/js/zsc_user.js
@@ -20,3 +20,30 @@ ZSCUser.prototype.getUserStatus = function () { return this.userStatus; }
 ZSCUser.prototype.getUserType = function () { return this.userType; }
 ZSCUser.prototype.getControlApisAdr = function () { return this.controlApisAdr; }
 ZSCUser.prototype.getControlApisFullAbi = function () { return this.controlApisFullAbi; }
+ZSCUser.prototype.getLoginAbi = function () {
+    return [{ "constant": true, "inputs": [], "name": "getControlApisFullAbi", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "getControlApisAdr", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "_user", "type": "bytes32" }, { "name": "_pass", "type": "bytes32" }], "name": "tryLogin", "outputs": [{ "name": "", "type": "bytes32" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_hexx", "type": "bytes32" }, { "name": "_type", "type": "bytes32" }], "name": "activeByUser", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "_hexx", "type": "bytes32" }], "name": "getUserType", "outputs": [{ "name": "", "type": "bytes32" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "_user", "type": "bytes32" }, { "name": "_hexx", "type": "bytes32" }], "name": "keepOnline", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "_hexx", "type": "bytes32" }], "name": "getUserStatus", "outputs": [{ "name": "", "type": "bytes32" }], "payable": false, "stateMutability": "view", "type": "function" },{
+        "constant": true,
+        "inputs": [],
+        "name": "numUsers",
+        "outputs": [{
+            "name": "",
+            "type": "uint256"
+        }],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    }, {
+        "constant": true,
+        "inputs": [{ "name": "_index", "type": "uint256" }],
+        "name": "getUserInfoByIndex",
+        "outputs": [{
+            "name": "",
+            "type": "string"
+        }],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+
+    } 
+    ];
+}
