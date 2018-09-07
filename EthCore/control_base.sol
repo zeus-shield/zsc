92fd853a5e9358f20d96abfa2dbd0735189caa2f
Eric-Yao <norhustar@hotmail.com>
2018-09-07 08:42:17 +0800

    updated abstract contract DBModule

--- EthCore/control_base.sol
+++ EthCore/control_base.sol
@@ -73,23 +73,22 @@ contract DBModule {
     function getTokenInfoBySymbol(bytes32 _symbol) public view returns (bytes32, bytes32, bytes32, uint, address);
 
     /*ERC721 for miner robot begin*/
-    function balanceOf(address _owner) public view returns (uint);
-    function tokenOfOwnerByIndex(address _owner, uint _index) public view returns (uint);
-    function getRobotInfo(uint _robotId) public view returns (bytes32, uint, uint, uint, uint, uint, uint);
-    function getExtraEffect(uint _robotId) public view returns (uint _extraSp, uint _extraUpgradProb);
-    function getLevelInfo(uint _index) public view returns (uint, uint, uint, uint);
+    //function balanceOf(address _owner) public view returns (uint);
+    //function tokenOfOwnerByIndex(address _owner, uint _index) public view returns (uint);
+    //function getRobotInfo(uint _robotId) public view returns (bytes32, uint, uint, uint, uint, uint, uint);
+    //function getExtraEffect(uint _robotId) public view returns (uint _extraSp, uint _extraUpgradProb);
+    //function getLevelInfo(uint _index) public view returns (uint, uint, uint, uint);
 
-    function createRobot(address _user, uint _level) public returns (uint);
-    function activeRobot(address _user, uint _robotId, uint _durationInDays, uint _totalZSC) public returns (uint);
-    function enhanceRobot(address _user, uint _robotId) public returns (uint);
-    function publishRobot(address _seller, uint _robotId, uint _price) public;
-    function cancelAuction(address _seller, uint _robotId) public;
-    function purchaseRobot(address _buyer, uint _robotId) public returns (address, uint);
-    function getReward(address _user, uint _robotId) public view returns (uint);
-    function claimReward(address _user, uint _robotId) public returns (uint, uint);
-    function numSellingRobots() public view returns (uint);
-    function getSellingRobotByIndex(uint _index) public view returns (uint, uint, uint, uint, address);
-    function safeTransferFrom(address _from, address _to, uint _tokenId) public;
+    //function createRobot(address _user, uint _level) public returns (uint);
+    //function activeRobot(address _user, uint _robotId, uint _durationInDays, uint _totalZSC) public returns (uint);
+    //function publishRobot(address _seller, uint _robotId, uint _price) public;
+    //function cancelAuction(address _seller, uint _robotId) public;
+    //function purchaseRobot(address _buyer, uint _robotId) public returns (address, uint);
+    //function getReward(address _user, uint _robotId) public view returns (uint);
+    //function claimReward(address _user, uint _robotId) public returns (uint, uint);
+    //function numSellingRobots() public view returns (uint);
+    //function getSellingRobotByIndex(uint _index) public view returns (uint, uint, uint, uint, address);
+    //function safeTransferFrom(address _from, address _to, uint _tokenId) public;
     /*ERC721 for miner robot end*/
 }
 
