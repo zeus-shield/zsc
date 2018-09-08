7a43b8a6f161253adc12ad11f8754617ca34df9e
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-09-08 09:23:37 +0800

    add compile.sh for WebTest

---
+++ WebTest/compile.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SFLAGS="--optimize --overwrite --pretty-json --combined-json bin,abi"
+CORE="sol"
+
+SOURCES=(
+sample
+)
+
+for((i=0;i<${#SOURCES[@]};i++));
+do
+SOURCE=${SOURCES[i]}".sol";
+echo "########## compiling "${SOURCE}" ##########"
+
+# Compiling to JSON file(build/xxx/combined.json)
+solc ${SFLAGS} ./${CORE}/${SOURCE} -o build/${SOURCES[i]}
+
+# Compiling to JS file(WebAdm/js/compiled_xxx.js)
+# COMPILED="`solc ${SFLAGS} ./${CORE}/${SOURCE}`"
+# echo "########## compiled "${COMPILED}" ##########"
+# echo "var compiled_"${SOURCES[i]}"=${COMPILED}" > ./compiled/"${SOURCES[i]}".js
+done
+
+read -p "Press any key to continue."
