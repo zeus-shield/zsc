f878f0c0541d5cd00113645cbb31cb268b110fab
Eric-Yao <norhustar@hotmail.com>
2018-09-10 13:32:02 +0800

    added new struct CtgUnit; updated contract SysGmPos

--- EthCore/sys_gm_pos.sol
+++ EthCore/sys_gm_pos.sol
@@ -17,6 +17,7 @@ contract SysGmPos is Erc721Adv, SysGmBase {
     struct RobotUnit {
         bytes32 status_;
         bytes32 name_;
+        uint rare_;
         uint spLev_;
         uint spBase_;
         uint rrLev_;
@@ -24,9 +25,21 @@ contract SysGmPos is Erc721Adv, SysGmBase {
         uint mineStart_;
         uint mineEnd_;
     }
-
     uint private robotNos_;
     mapping(uint => RobotUnit) private robots_;
+
+    struct CtgUnit {
+        bytes32 name_;
+        uint rare_;
+        uint spBase_;
+        uint rrBase_;
+    }
+    uint private ctgNos_;
+    mapping(uint => RobotUnit) private ctgs_;
+    mapping(bytes32 => bool) private ctgExits_;
+    mapping(bytes32 => uint) private ctgIndice_;
+
+    address public extraEffectObj_;
 
     function SysGmPosAdv(bytes32 _name) public SysGmPos(_name) {
         dayInSeconds_ = DAY_IN_SECONDS;
@@ -44,29 +57,50 @@ contract SysGmPos is Erc721Adv, SysGmBase {
         return index;
     }
 
-    function checkUnittUser(address _user, uint _unitId) internal view {
+    function checkUnitUser(address _user, uint _unitId) internal view {
         require(_user == ownerOf(_unitId));
     }
 
+    function setUnitName(uint _unitId, bytes32 _name) internal {
+        robots_[_unitId].name_ = _name;
+    }
+    
     function setUnitStatus(uint _unitId, bytes32 _status) internal {
         robots_[_unitId].status_ = _status;
     }
 
+    function setUnitRare(uint _unitId, bytes32 _rare) internal {
+        robots_[_unitId].rare_ = _rare;
+    }
+
+    function setUnitSPLev(uint _unitId, uint _lev) internal {
+        robots_[_unitId].spLev_ = _lev;
+    }
+
+    function setUnitRRLev(uint _unitId, uint _lev) internal {
+        robots_[_unitId].rrLev_ = _lev;
+    }
+
     //////////////////////
+    function setExtraEffectObj(address _adr) public {
+        checkDelegate(msg.sender, 1);
+        extraEffectObj_ = _adr;
+    }
+
     function numUnits() public view returns (uint) {
         return robotNos_;  
+    }
+
+    function getUnitName(uint _unitId) public view returns (bytes32) {
+        return robots_[_unitId].name_;
     }
 
     function getUnitStatus(uint _unitId) public view returns (bytes32) {
         return robots_[_unitId].status_;
     }
 
-    function getUnitName(uint _unitId) public view returns (bytes32) {
-        return robots_[_unitId].name_;
-    }
-
-    function getUnitName(uint _unitId) public view returns (bytes32) {
-        return robots_[_unitId].name_;
+    function getUnitRare(uint _unitId) public view returns (bytes32) {
+        return robots_[_unitId].rare_;
     }
 
     function getUnitSPBase(uint _unitId) public view returns (uint) {
