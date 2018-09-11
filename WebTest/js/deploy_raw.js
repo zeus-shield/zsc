91808fcc14fa72fae79f5ad97fa8652c771089c7
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-09-11 15:07:10 +0800

    up 'etherSpentInPendingTransactions' in deploy_raw.js for WebTest

--- WebTest/js/deploy_raw.js
+++ WebTest/js/deploy_raw.js
@@ -13,39 +13,27 @@ export default class DeployRaw {
         this[privateKeyRaw] = "0x748443675b8cc68e225d4d7f266d2e57a7157e28b55b7cf66409f76a02bd49ca";
     }
 
-    [etherSpentInPendingTransactions](address, callback) {
+    [etherSpentInPendingTransactions](address, func) {
         web3.currentProvider.sendAsync({
             method: "txpool_content",
             params: [],
             jsonrpc: "2.0",
             id: new Date().getTime()
         }, function (error, result) {
-            console.log("result:", result);
-            console.log("result.result:", result.result);
-            if(result.result.pending)
-            {
-                console.log("pending[address]:", result.result.pending[address]);
-                if(result.result.pending[address])
-                {
-                    var txns = result.result.pending[address];
-                    var cost = new BigNumber(0);
+            if(result.result.pending) {
+                if(result.result.pending[address]) {
+                    let txns = result.result.pending[address];
+                    let cost = new BigNumber(0);
                     
-                    for(var txn in txns)
-                    {
+                    for(let txn in txns) {
                         cost = cost.add((new BigNumber(parseInt(txns[txn].value))).add((new BigNumber(parseInt(txns[txn].gas))).mul(new BigNumber(parseInt(txns[txn].gasPrice)))));
                     }
-                    console.log(1);
-                    callback(null, web3.fromWei(cost, "ether"));
+                    func(null, web3.fromWei(cost, "ether"));
+                } else {
+                    func(null, "0");
                 }
-                else
-                {
-                    console.log(2);
-                    callback(null, "0");
-                }
-            }
-            else
-            {
-                callback(null, "0");
+            } else {
+                func(null, "0");
             }
         })
     }
