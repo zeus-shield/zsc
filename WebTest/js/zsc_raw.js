d51b0e3643dfd8e3167c342d627edcab54b4d61e
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-09-12 10:53:10 +0800

    up zsc_raw.js for WebTest

--- WebTest/js/zsc_raw.js
+++ WebTest/js/zsc_raw.js
@@ -7,49 +7,52 @@ import Output from './output.js';
 //private member
 const constractAbi = Symbol('abi');
 const constractAddress = Symbol('address');
-const account = Symbol('account');
-const rawAddress = Symbol('rawAddress');
-const privateKey = Symbol('privateKey');
+const addressRaw = Symbol('addressRaw');
+const privateKeyRaw = Symbol('privateKeyRaw');
 
 export default class ZSCRaw {
     constructor(abi, address) {
+        let isMetaMask = web3.currentProvider.isMetaMask;
+
         this[constractAbi] = abi;
         this[constractAddress] = address;
-        this[account] = web3.eth.accounts[0];
-        this[rawAddress] = "0x15ca13630ce52cd4e209012635f10b396e098296";
-        this[privateKey] = "0x748443675b8cc68e225d4d7f266d2e57a7157e28b55b7cf66409f76a02bd49ca";
+        this[addressRaw] = web3.eth.coinbase;
+        if (isMetaMask) {
+            //this[addressRaw] = "0xbaa43825f1bda3839c5f3038c65c504cb6d962c8";
+            this[privateKeyRaw] = "0x5367874f5f72d3e7554e7df202a4f79e1f4ed591c3bc5a78993390f3becf313f";
+        } else {
+            //this[addressRaw] = "0x15ca13630ce52cd4e209012635f10b396e098296";
+            this[privateKeyRaw] = "0x748443675b8cc68e225d4d7f266d2e57a7157e28b55b7cf66409f76a02bd49ca";
+        }
     }
 
     set(_index, _data) {
         let contractInstance = web3.eth.contract(this[constractAbi]).at(this[constractAddress]);
 
         // estimate gas
-        contractInstance.set.estimateGas(_index, _data,
-            function(error, result) {
-                if(!error) {
-                    //alert(result);
-                    // set
-                    contractInstance.set(_index, _data, {gas: result},
-                        function(error, result) { 
+        contractInstance.set.estimateGas(_index, _data, function(error, result) {
+            if(!error) {
+                // set
+                contractInstance.set(_index, _data, {gas: result}, function(error, result) { 
+                    if(!error) {
+                        Output(window.outputElement, 'small', 'red', `[TransactionHash]:${result}`);
+                        // get receipt
+                        let receipt = web3.eth.getTransactionReceipt(result, function(error, result) {
                             if(!error) {
-                                Output(window.outputElement, 'small', 'red', `[TransactionHash]:${result}`);
-                                // get receipt
-                                let receipt = web3.eth.getTransactionReceipt(result, function(error, result) {
-                                    if(!error) {
-                                        // console.log(result);
-                                        // Output(window.outputElement, 'small', 'red', result);
-                                    } else {
-                                        Output(window.outputElement, 'small', 'red', error);
-                                    }
-                                });
+                                // console.log(result);
+                                // Output(window.outputElement, 'small', 'red', result);
                             } else {
                                 Output(window.outputElement, 'small', 'red', error);
                             }
                         });
-                } else {
-                    Output(window.outputElement, 'small', 'red', error);
-                }
-            });
+                    } else {
+                        Output(window.outputElement, 'small', 'red', error);
+                    }
+                });
+            } else {
+                Output(window.outputElement, 'small', 'red', error);
+            }
+        });
     }
 
     setRaw(_index, _data) {
