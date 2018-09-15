ca54d7a7e4595b9b45bdd0f18b92ccac8c4016dc
Eric-Yao <norhustar@hotmail.com>
2018-09-15 15:38:08 +0800

    added abstract contract ControlApis; updated contract AdmBase

--- EthCore/adm_base.sol
+++ EthCore/adm_base.sol
@@ -6,6 +6,12 @@ pragma solidity ^0.4.21;
 
 import "./object.sol";
 
+contract ControlApis {
+    function createUserNode(bytes32 _factoryType, bytes32 _userName, address _extraAdr) public returns (address);
+    function setUserStatus(address _user, bool _tagg) public returns (bool);
+    function getModuleAdress(bytes32 _name) public view returns (address);
+}
+
 contract AdmBase is Object {
     address public controlApisAdr_;
     string public controlApisFullAib_;
@@ -25,11 +31,13 @@ contract AdmBase is Object {
         controlApisFullAib_ = _fullAbi; 
     }
 
-    function getControlApisFullAbi() public view returns (string) { 
-        return controlApisFullAib_; 
-    }
-
-    function getControlApisAdr() public view returns (address) { 
-        return controlApisAdr_; 
+    function getControlApisInfo() public view returns (string) {
+        address Erc721Adr = ControlApis(controlApisAdr_).getModuleAdress("gm-pos");
+        string memory str ="info?";
+        str = PlatString.append(str, "abi=",       controlApisFullAib_,    "&");
+        str = PlatString.append(str, "sysAdr=",    PlatString.addressToString(controlApisAdr_),  "&");
+        str = PlatString.append(str, "erc721Adr=", PlatString.addressToString(Erc721Adr),        "&");
+        str = PlatString.append(str, "end");
+        return str;
     }
 }
