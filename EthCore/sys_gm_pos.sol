172a30c16672cef3515e9ccc3baeec7477dc9aaf
Eric-Yao <norhustar@hotmail.com>
2018-09-19 17:46:51 +0800

    updated contract SysGmPos

--- EthCore/sys_gm_pos.sol
+++ EthCore/sys_gm_pos.sol
@@ -61,41 +61,50 @@ contract SysGmPos is Erc721Adv, SysGmBase {
         mapping(uint => bytes32) ctgs_;
     }
     mapping(uint => RareGroup) private rares_;
-    mapping(uint => uint) private rareProb_;
+    mapping(bytes32 => uint) private rareProb_;
 
     address public extraEffectObj_;
 
+    uint public priceToCreate_;
+    bool public publicTradeable_ = true;
+    string public tokenUri_;
+
     uint public minePerDay_;
     uint public rewardPerDay_;
+
+    uint public rootPri_ = 1;
+    uint public admPri_ = 5;
+    uint public subPri_ = 10;
 
     // Constructor
     function SysGmPos(bytes32 _name) public SysGmBase(_name) {
         dayInSeconds_ = DAY_IN_SECONDS;
     } 
 
-    function downscaledDay(uint ratio) public {
-        checkDelegate(msg.sender, 1);
-
-        require(ratio > 0 && ratio <= DAY_IN_SECONDS);
-        dayInSeconds_ = DAY_IN_SECONDS;
-        dayInSeconds_ = dayInSeconds_.div(ratio);   
+    //////////////////////////
+    function checkTradeAble(uint256 _unitId) internal view returns (bool) {
+        require(robots_[_unitId].status_ == "idle");
+        return publicTradeable_;
     }
 
-    function setPosRatio(uint _mineRaioPerDay, uint _rewardRatioPerDay) public {
-        checkDelegate(msg.sender, 1);
+    function tokenURI(uint _tokenId) public view returns (string) {
+        string memory url = PlatString.append(tokenUri_, PlatString.uintToString(_tokenId));
+        return url;
+    }
 
-        minePerDay_ = _mineRaioPerDay;
-        rewardPerDay_ = _rewardRatioPerDay;
+    //////////////////////////
+    function checkUnitUser(address _user, uint _unitId) internal view {
+        require(_user == ownerOf(_unitId));
     }
 
     function getRandomUnitCategory() internal view returns (bytes32) {
-        uint ran = random(0, 100);
+        uint ran = random(0, 10000);
         uint rareLev;
-        if (ran <= rareProb_[0]) {
+        if (ran <= rareProb_["R"]) {
             rareLev = 0;
-        } else if (ran >rareProb_[0] && ran <= rareProb_[1]) {
+        } else if (ran >rareProb_["R"] && ran <= rareProb_["SR"]) {
             rareLev = 1;
-        } else if (ran >rareProb_[1] && ran <= rareProb_[2]) {
+        } else if (ran >rareProb_["SR"] && ran <= rareProb_["SSR"]) {
             rareLev = 2;
         } else {
             rareLev = 3;
@@ -146,39 +155,54 @@ contract SysGmPos is Erc721Adv, SysGmBase {
         return index;
     }
 
-/*
-    function mintUnitSpec(address _user, bytes32 _ctgName, uint _spMax, uint _durationInDays) internal returns (uint) {
-        checkDelegate(msg.sender, 1);
-
-        uint cur = now;
-        uint index = robotNos_;
-        uint secs = _durationInDays.mul(dayInSeconds_);
-        robotNos_++;
-        _mint(_user, index);
-
-        uint ctgIndex = ctgIndice_[_ctgName];
-
-        robots_[index].specific_  = true;
-        robots_[index].status_ = "mining";
-        robots_[index].name_   = _ctgName;
-        robots_[index].rare_   = ctgs_[ctgIndex].rare_;
-        robots_[index].spLev_  = 0;
-        robots_[index].spBase_    = 0;
-        robots_[index].spCur_     = _spMax;
-        robots_[index].spMax_     = _spMax;
-        robots_[index].mineStart_ = cur;
-        robots_[index].mineEnd_   = cur.add(secs);
-        robots_[index].upProbBase_   = 0;
-
-        robots_[index].spEft_     = ctgs_[ctgIndex].spEftMin_;
-        robots_[index].rrEft_     = ctgs_[ctgIndex].rrEftMin_;
-        robots_[index].upProbEft_ = ctgs_[ctgIndex].upProbEftMin_;
-
-        return index;
+    //////////////////////////
+    function setPriorityValue(uint _adm, uint _sub) public {
+        checkDelegate(msg.sender, rootPri_);
+        admPri_ = _adm;
+        subPri_ = _sub;
     }
-*/   
+
+    function setCommenTokenURI(string _uri) public {
+        checkDelegate(msg.sender, admPri_);
+        tokenUri_ = _uri;
+    }
+
+    function setCreatePrice(uint _price) public {
+        checkDelegate(msg.sender, admPri_);
+        require(_price > 0);
+        priceToCreate_ = _price;
+    }
+
+    function setExtraEffectObj(address _adr) public {
+        checkDelegate(msg.sender, admPri_);
+        extraEffectObj_ = _adr;
+    }
+
+    function downscaledDay(uint ratio) public {
+        checkDelegate(msg.sender, admPri_);
+
+        require(ratio > 0 && ratio <= DAY_IN_SECONDS);
+        dayInSeconds_ = DAY_IN_SECONDS;
+        dayInSeconds_ = dayInSeconds_.div(ratio);   
+    }
+
+    function setPosRatio(uint _mineRaioPerDay, uint _rewardRatioPerDay) public {
+        checkDelegate(msg.sender, admPri_);
+
+        minePerDay_ = _mineRaioPerDay;
+        rewardPerDay_ = _rewardRatioPerDay;
+    }
+
+    function setRareThreshold(uint _N, uint _R, uint _SR, uint _SSR) public {
+        checkDelegate(msg.sender, admPri_);
+        rareProb_["N"] = _N;
+        rareProb_["R"] = _R;
+        rareProb_["SR"] = _SR;
+        rareProb_["SSR"] = _SSR;
+    }
+   
     function setUnitCategory(bytes32 _name, uint _rare, uint _spEftMin, uint _spEftMax, uint _rrEftMin, uint _rrEftMax, uint _upProbEftMin, uint _upProbEftMax) public {
-        checkDelegate(msg.sender, 1);
+        checkDelegate(msg.sender, subPri_);
         require(_rare < 4);
         uint index;
 
@@ -205,19 +229,7 @@ contract SysGmPos is Erc721Adv, SysGmBase {
         ctgs_[index].upProbEftMax_ = _upProbEftMax;
     }
 
-    function checkUnitUser(address _user, uint _unitId) internal view {
-        require(_user == ownerOf(_unitId));
-    }
-
-    function checkSpecificUnit(uint _unitId) internal {
-        if (robots_[_unitId].specific_ == false) {
-            return;
-        }
-
-        require(now < robots_[_unitId].mineEnd_);
-        robots_[_unitId].specific_ = false;
-    }
-
+    ///////////////
     function setUnitSpec(uint _unitId, bool _tag) public {
         checkDelegate(msg.sender, 1);
         robots_[_unitId].specific_ = _tag;
@@ -287,12 +299,24 @@ contract SysGmPos is Erc721Adv, SysGmBase {
         return robotNos_;  
     }
 
-    function isUnitSpecial(uint _unitId) public view returns (uint) {
-        if (robots_[_unitId].specific_) {
+    function getCreatePrice() public view returns (uint) {
+        return priceToCreate_;
+    }
+
+    function getPublicTradeableTag() public view returns (uint) {
+        if (publicTradeable_) {
             return 1;
         } else {
             return 0;
         }
+    }    
+
+    function getDayInSeconds() public view returns (uint) {
+        return dayInSeconds_;
+    }
+    
+    function getUnitSpec(uint _unitId) public view returns (bool) {
+        return (robots_[_unitId].specific_);
     }
 
     function getUnitSPMinedPerday() public view returns (uint) {
