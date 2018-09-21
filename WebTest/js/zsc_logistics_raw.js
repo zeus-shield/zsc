850bb2fbb1c762196aafff624daf3ab1948a2c81
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-09-21 21:27:10 +0800

    up zsc_logistics_raw.js for WebTest

--- WebTest/js/zsc_logistics_raw.js
+++ WebTest/js/zsc_logistics_raw.js
@@ -6,11 +6,10 @@ import Receipt from './receipt.js';
 import Output from './output.js';
 import Deploy from './deploy_raw.js';
 
-//private member
+// private member
 const constractAbi = Symbol('abi');
 const constractAddress = Symbol('constractAddress');
-const addressRaw = Symbol('addressRaw');
-const privateKeyRaw = Symbol('privateKeyRaw');
+const account = Symbol('account');
 
 export default class ZSCLogistics {
     constructor(abi, address) {
@@ -18,13 +17,11 @@ export default class ZSCLogistics {
 
         this[constractAbi] = abi;
         this[constractAddress] = address;
-        this[addressRaw] = web3.eth.coinbase;
+        this[account] = web3.eth.coinbase;
         if (isMetaMask) {
-            //this[addressRaw] = "0xbaa43825f1bda3839c5f3038c65c504cb6d962c8";
-            this[privateKeyRaw] = "0x5367874f5f72d3e7554e7df202a4f79e1f4ed591c3bc5a78993390f3becf313f";
+            //this[account] = "0xbaa43825f1bda3839c5f3038c65c504cb6d962c8";
         } else {
-            //this[addressRaw] = "0x15ca13630ce52cd4e209012635f10b396e098296";
-            this[privateKeyRaw] = "0x748443675b8cc68e225d4d7f266d2e57a7157e28b55b7cf66409f76a02bd49ca";
+            //this[account] = "0x15ca13630ce52cd4e209012635f10b396e098296";
         }
     }
 
