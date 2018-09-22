a9001c6daec927e42b484504db45582bcb565c6d
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-09-22 17:20:52 +0800

    up test_logistics.js for WebTest

--- WebTest/js/test_logistics_raw.js
+++ WebTest/js/test_logistics_raw.js
@@ -104,22 +104,21 @@ export default class TestLogisticsRaw {
 
     update() {
         console.log('TestLogisticsRaw.update()');
-        // let brief9 = "{\"error\":null,\"num\":\"JNTCU0600046689YQ\",\"transNum\":\"MSK0000027699\",\"model\":\"INFO9\",\"destinationCountry\":\"Russian\",\"lastStatus\":\"GTMS_SIGNED\"}";
-        // let newTracks5 = "{\"trackElementList\":[{\"type\":\"DC\",\"time\":\"2017-07-13 11:54:00\",\"country\":\"Russian\",\"city\":\"HangZhou\",\"facilityName\":\"NewTrack5-1\",\"timeZone\":\"+3\",\"desc\":\"NewTrack5-1\",\"actionCode\":\"GTMS_SIGNED\"}&{\"type\":\"DC\",\"time\":\"2017-07-07 17:39:09\",\"country\":\"Russian\",\"city\":\"ShangHai\",\"facilityName\":\"NewTrack5-2\",\"timeZone\":\"+3\",\"desc\":\"NewTrack5-2\",\"actionCode\":\"GWMS_ACCEPT\"}]}";
-        // let zsc = new ZSCLogistics(this[abi], this[contractAddress]);
+        let brief9 = "{\"error\":null,\"num\":\"JNTCU0600046689YQ\",\"transNum\":\"MSK0000027699\",\"model\":\"INFO9\",\"destinationCountry\":\"Russian\",\"lastStatus\":\"GTMS_SIGNED\"}";
+        let newTracks5 = "{\"trackElementList\":[{\"type\":\"DC\",\"time\":\"2017-07-13 11:54:00\",\"country\":\"Russian\",\"city\":\"HangZhou\",\"facilityName\":\"NewTrack5-1\",\"timeZone\":\"+3\",\"desc\":\"NewTrack5-1\",\"actionCode\":\"GTMS_SIGNED\"}&{\"type\":\"DC\",\"time\":\"2017-07-07 17:39:09\",\"country\":\"Russian\",\"city\":\"ShangHai\",\"facilityName\":\"NewTrack5-2\",\"timeZone\":\"+3\",\"desc\":\"NewTrack5-2\",\"actionCode\":\"GWMS_ACCEPT\"}]}";
+        let logistics = new Logistics(this[abi], this[contractAddress]);
 
-        // zsc.updateBrief("JNTCU0600046688YQ", "MSK0000027698", "INFO8", "Russian", "GTMS_SIGNED", function(error, result){
-        //     if (!error && "0x1" == result) {
-        //         zsc.updateBriefEx(brief9, function(error, result) {
-        //             //zsc.updateTracks("JNTCU0600046685YQ", tracks5, 0, function(error, result) {
-        //             zsc.updateTracks("JNTCU0600046685YQ", newTracks5, 1, function(error, result) {
-        //                 if (!error && "0x1" == result) {
-        //                     console.log("update testing finished.");
-        //                 }
-        //             });
-        //         })
-        //     }
-        // });
+        logistics.updateBrief("JNTCU0600046688YQ", "MSK0000027698", "INFO8", "Russian", "GTMS_SIGNED", function(error, result){
+            if (!error && "0x1" == result) {
+                logistics.updateBriefEx(brief9, function(error, result) {
+                    logistics.updateTracks("JNTCU0600046685YQ", newTracks5, 1, function(error, result) {
+                        if (!error && "0x1" == result) {
+                            console.log("update testing finished.");
+                        }
+                    });
+                })
+            }
+        });
     }
 
     get() {
