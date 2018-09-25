3e2e9f5fe2a3f20f37839e2f5f4e971e3fc23af8
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-09-25 14:23:31 +0800

    up 'create()' function for test_logistics_raw.js

--- WebTest/js/test_logistics_raw.js
+++ WebTest/js/test_logistics_raw.js
@@ -71,6 +71,9 @@ export default class TestLogisticsRaw {
     create() {
         console.log('TestLogisticsRaw.create()');
 
+        let status = "";
+        let string = "";
+
         // update testing data
         let info3 = "{\"error\":null,\"num\":\"JNTCU0600046683YQ\",\"transNum\":\"MSK0000027693\",\"model\":\"INFO3\",\"destinationCountry\":\"Russian\",\"lastStatus\":\"GTMS_SIGNED\",\"trackElementList\":[{\"type\":\"DC\",\"time\":\"2017-07-13 11:54:00\",\"country\":\"Russian\",\"city\":\"HangZhou\",\"facilityName\":\"Армавир\",\"timeZone\":\"+3\",\"desc\":\"Товар был успешно доставлен получателю. Спасибо что воспользовались нашими услугами\",\"actionCode\":\"GTMS_SIGNED\"}&{\"type\":\"DC\",\"time\":\"2017-07-07 17:39:09\",\"country\":\"Russian\",\"city\":\"ShangHai\",\"facilityName\":\"Sorting center of J-NET\",\"timeZone\":\"+3\",\"desc\":\"Order received successfully\",\"actionCode\":\"GWMS_ACCEPT\"}&{\"type\":\"DC\",\"time\":\"2017-07-07 17:39:00\",\"country\":\"Russian\",\"city\":\"BeiJing\",\"facilityName\":\"Sorting center of J-NET\",\"timeZone\":\"+3\",\"desc\":\"The parcel is ready to transfer to the courier\",\"actionCode\":\"VISIBLE_UNKOWN\"}]}";
         let info6 = "{\"error\":null,\"num\":\"JNTCU0600046686YQ\",\"transNum\":\"MSK0000027696\",\"model\":\"INFO6\",\"destinationCountry\":\"China\",\"lastStatus\":\"GTMS_SIGNED\",\"trackElementList\":[{\"type\":\"DC\",\"time\":\"2017-07-13 11:54:00\",\"country\":\"China\",\"city\":\"HangZhou\",\"facilityName\":\"SF\",\"timeZone\":\"+3\",\"desc\":\"SF is good.\",\"actionCode\":\"GTMS_SIGNED\"}&{\"type\":\"DC\",\"time\":\"2017-07-07 17:39:09\",\"country\":\"China\",\"city\":\"ShangHai\",\"facilityName\":\"Sorting center of J-NET\",\"timeZone\":\"+3\",\"desc\":\"Order received successfully\",\"actionCode\":\"GWMS_ACCEPT\"}&{\"type\":\"DC\",\"time\":\"2017-07-07 17:39:00\",\"country\":\"China\",\"city\":\"BeiJing\",\"facilityName\":\"Sorting center of J-NET\",\"timeZone\":\"+3\",\"desc\":\"The parcel is ready to transfer to the courier\",\"actionCode\":\"VISIBLE_UNKOWN\"}]}";
@@ -79,25 +82,114 @@ export default class TestLogisticsRaw {
         let tracks7 = "{\"trackElementList\":[{\"type\":\"DC\",\"time\":\"2017-07-13 11:54:00\",\"country\":\"Russian\",\"city\":\"HangZhou\",\"facilityName\":\"Track6-1\",\"timeZone\":\"+3\",\"desc\":\"Track6-1\",\"actionCode\":\"GTMS_SIGNED\"}&{\"type\":\"DC\",\"time\":\"2017-07-07 17:39:09\",\"country\":\"Russian\",\"city\":\"ShangHai\",\"facilityName\":\"Track6-2\",\"timeZone\":\"+3\",\"desc\":\"Track6-2\",\"actionCode\":\"GWMS_ACCEPT\"}&{\"type\":\"DC\",\"time\":\"2017-07-07 17:39:09\",\"country\":\"Russian\",\"city\":\"ShangHai\",\"facilityName\":\"Track6-3\",\"timeZone\":\"+3\",\"desc\":\"Track6-3\",\"actionCode\":\"GWMS_ACCEPT\"}]}";
 
         let logistics = new Logistics(this[abi], this[contractAddress]);
+        
+        // updateEx
         logistics.updateEx(info3, function(error, result) {
-            if (!error && "0x1" == result) {
-                logistics.update("JNTCU0600046684YQ", "MSK0000027694", "INFO4", "Russian", "GTMS_SIGNED", tracks4, function(error, result) {
-                    if (!error && "0x1" == result) {
-                        logistics.update("JNTCU0600046685YQ", "MSK0000027695", "INFO5", "Russian", "GTMS_SIGNED", tracks5, function(error, result) {
-                            if (!error && "0x1" == result) {
-                                logistics.updateEx(info6, function(error, result) {
-                                    if (!error && "0x1" == result) {
-                                        logistics.update("JNTCU0600046687YQ", "MSK0000027697", "INFO7", "Russian", "GTMS_SIGNED", tracks7, function(error, result) {
-                                            if (!error && "0x1" == result) {
-                                                console.log("create testing finished.");
+            if (!error) {
+                if ("" != result.status) {
+                    if ("0x1" == result.status) {
+                        status = "succeeded";
+                    } else {
+                        status = "failure";
+                    }
+
+                    string = `[TransactionHash]:${result.transactionHash}</br>[Status]:${status}</br>[Try]:${result.tryTimes}(times)`;
+                    Output(window.outputElement, 'small', 'red', string);
+
+                    // update
+                    logistics.update("JNTCU0600046684YQ", "MSK0000027694", "INFO4", "Russian", "GTMS_SIGNED", tracks4, function(error, result) {
+                        if (!error) {
+                            if ("" != result.status) {
+                                if ("0x1" == result.status) {
+                                    status = "succeeded";
+                                } else {
+                                    status = "failure";
+                                }
+
+                                string = `[TransactionHash]:${result.transactionHash}</br>[Status]:${status}</br>[Try]:${result.tryTimes}(times)`;
+                                Output(window.outputElement, 'small', 'red', string);
+
+                                // update
+                                logistics.update("JNTCU0600046685YQ", "MSK0000027695", "INFO5", "Russian", "GTMS_SIGNED", tracks5, function(error, result) {
+                                    if (!error) {
+                                        if ("" != result.status) {
+                                            if ("0x1" == result.status) {
+                                                status = "succeeded";
+                                            } else {
+                                                status = "failure";
                                             }
-                                        });
+
+                                            string = `[TransactionHash]:${result.transactionHash}</br>[Status]:${status}</br>[Try]:${result.tryTimes}(times)`;
+                                            Output(window.outputElement, 'small', 'red', string);
+
+                                            // updateEx
+                                            logistics.updateEx(info6, function(error, result) {
+                                                if (!error) {
+                                                    if ("" != result.status) {
+                                                        if ("0x1" == result.status) {
+                                                            status = "succeeded";
+                                                        } else {
+                                                            status = "failure";
+                                                        }
+
+                                                        string = `[TransactionHash]:${result.transactionHash}</br>[Status]:${status}</br>[Try]:${result.tryTimes}(times)`;
+                                                        Output(window.outputElement, 'small', 'red', string);
+
+                                                        // update
+                                                        logistics.update("JNTCU0600046687YQ", "MSK0000027697", "INFO7", "Russian", "GTMS_SIGNED", tracks7, function(error, result) {
+                                                            if (!error) {
+                                                                if ("" != result.status) {
+                                                                    if ("0x1" == result.status) {
+                                                                        status = "succeeded";
+                                                                    } else {
+                                                                        status = "failure";
+                                                                    }
+
+                                                                    string = `[TransactionHash]:${result.transactionHash}</br>[Status]:${status}</br>[Try]:${result.tryTimes}(times)`;
+                                                                    Output(window.outputElement, 'small', 'red', string);
+                                                                } else {
+                                                                    let status = "Try to get status again!";
+                                                                    let string = `[TransactionHash]:${result.transactionHash}</br>[Status]:${status}</br>[Try]:${result.tryTimes}(times)`;
+                                                                    Output(window.outputElement, 'small', 'red', string);
+                                                                }
+                                                            } else {
+                                                                Output(window.outputElement, 'small', 'red', error);
+                                                            }
+                                                        });
+                                                    } else {
+                                                        let status = "Try to get status again!";
+                                                        let string = `[TransactionHash]:${result.transactionHash}</br>[Status]:${status}</br>[Try]:${result.tryTimes}(times)`;
+                                                        Output(window.outputElement, 'small', 'red', string);
+                                                    }
+                                                } else {
+                                                    Output(window.outputElement, 'small', 'red', error);
+                                                }
+                                            });
+                                        } else {
+                                            let status = "Try to get status again!";
+                                            let string = `[TransactionHash]:${result.transactionHash}</br>[Status]:${status}</br>[Try]:${result.tryTimes}(times)`;
+                                            Output(window.outputElement, 'small', 'red', string);
+                                        }
+                                    } else {
+                                        Output(window.outputElement, 'small', 'red', error);
                                     }
                                 });
+                            } else {
+                                let status = "Try to get status again!";
+                                let string = `[TransactionHash]:${result.transactionHash}</br>[Status]:${status}</br>[Try]:${result.tryTimes}(times)`;
+                                Output(window.outputElement, 'small', 'red', string);
                             }
-                        });
-                    }
-                });
+                        } else {
+                            Output(window.outputElement, 'small', 'red', error);
+                        }
+                    });
+                } else {
+                    let status = "Try to get status again!";
+                    let string = `[TransactionHash]:${result.transactionHash}</br>[Status]:${status}</br>[Try]:${result.tryTimes}(times)`;
+                    Output(window.outputElement, 'small', 'red', string);
+                }
+            } else {
+                Output(window.outputElement, 'small', 'red', error);
             }
         });
     }
