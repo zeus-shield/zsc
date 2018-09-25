8c478b44dd76b5d428673626393ab789d551b01d
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-09-25 11:10:58 +0800

    reconstruct receipt.js

--- WebTest/js/receipt.js
+++ WebTest/js/receipt.js
@@ -2,74 +2,73 @@
  Copyright (c) 2018 ZSC Dev Team
 */
 
-import Output from './output.js';
+// import Output from './output.js';
 
-//private member
-//private function
+class Result {
+    constructor() {}
+}
+
+// private member
+// private function
 
 export default class Receipt {
     constructor() {}
 
-    getReceipt(hash, tryTimes, timeout, func) {
+    getReceipt(cmd, transactionHash, tryTimes, timeout, func) {
         let handler = this;
-        let status  = "Try to get status again!";
-        let string = "";
+        let result = new Result();
 
-        // if (undefined == hash) {
-        //     string = `[TransactionHash]:${hash}</br>[Status]:${status}`;
-        //     Output(window.outputElement, 'small', 'red', string);
-        //     return;
-        // }
-
-        web3.eth.getTransactionReceipt(hash, function(error, result) {
-            if (null != result) {
-                if ("0x1" == result.status) {
-                    status  = "succeeded";
-                } else {
-                    status  = "failure";
-                }
-                string = `[TransactionHash]:${hash}</br>[Status]:${status}`;
-                Output(window.outputElement, 'small', 'red', string);
-                if (null != func) {
-                    func(null, result.status);
-                }
-            } else {
-                tryTimes ++;
-                string = `[TransactionHash]:${hash}</br>[Status]:${status}</br>[Try]:${tryTimes}(times)`;
-                Output(window.outputElement, 'small', 'red', string);
-                setTimeout(function() {
-                    handler.getReceipt(hash, tryTimes, timeout, func);
-                }, timeout);
-            }
-        });
-    }
-
-    getReceiptForContractAddress(hash, tryTimes, timeout, caller, func) {
-        let handler = this;
-        let contractAddress = "Try to get contract address again!";
-        let string = "";
-
-        if (undefined == hash) {
-            contractAddress = "";
-            string = `[TransactionHash]:${hash}</br>[ContractAddress]:${contractAddress}`;
-            Output(window.outputElement, 'small', 'red', string);
+        // check param
+        if (("contractAddress" != cmd) && ("status" != cmd)) {
+            let error = new Error();
+            error.message = "Command Error!";
+            console.log(error);
+            func(error);
             return;
         }
 
-        web3.eth.getTransactionReceipt(hash, function(error, receipt) {
+        if (undefined == transactionHash) {
+            let error = new Error();
+            error.message = "TransactionHash Undefined!";
+            console.log(error);
+            func(error);
+            return;
+        }
+
+        tryTimes ++;
+        web3.eth.getTransactionReceipt(transactionHash, function(error, receipt) {
             if (null != receipt) {
-                contractAddress = receipt.contractAddress;
-                string = `[TransactionHash]:${hash}</br>[ContractAddress]:${contractAddress}`;
-                Output(window.outputElement, 'small', 'red', string);
                 if (null != func) {
-                    func(caller, contractAddress);
+                    result.transactionHash = transactionHash;
+
+                    if ("contractAddress" == cmd) {
+                        result.contractAddress = receipt.contractAddress;
+                    } 
+
+                    if ("status" == cmd) {
+                        result.status = receipt.status;
+                    }
+                    
+                    result.tryTimes = tryTimes;
+                    func(null, result);
                 }
             } else {
-                tryTimes ++;
-                string = `[TransactionHash]:${hash}</br>[ContractAddress]:${contractAddress}</br>[Try]:${tryTimes}(times)`;
-                Output(window.outputElement, 'small', 'red', string);
+                result.transactionHash = transactionHash;
+
+                if ("contractAddress" == cmd) {
+                    // result.contractAddress = "Try to get contract address again!";
+                    result.contractAddress = "";
+                } 
+
+                if ("status" == cmd) {
+                    // result.status = "Try to get status again!";
+                    result.status = "";
+                }
+
+                result.tryTimes = tryTimes;
+                func(null, result);
                 setTimeout(function() {
-                    handler.getReceiptForContractAddress(hash, tryTimes, timeout, caller, func);
+                    handler.getReceipt(cmd, transactionHash, tryTimes, timeout, func);
                 }, 1000);
             }
         });
