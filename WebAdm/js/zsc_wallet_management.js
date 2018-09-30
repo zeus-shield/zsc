87a5c709f4641cea460edfbe2546709d97917d15
Eric-Yao <norhustar@hotmail.com>
2018-09-30 20:46:51 +0800

    updated class SysGmToken

--- WebAdm/js/zsc_wallet_management.js
+++ WebAdm/js/zsc_wallet_management.js
@@ -9,6 +9,8 @@ function ZSCWalletMangement(controlApiAdr, controlApiAbi, tokenManagerAdr, token
     this.tokenSymbols = [];
     this.tokenDecimals = [];
     this.tokenAdrs = [];
+    this.posableTags = [];
+    this.tradeableTags = [];
     this.account = web3.eth.accounts[0];
     this.myControlApi = web3.eth.contract(controlApiAbi).at(controlApiAdr);
     this.myTokenManager= web3.eth.contract(tokenManagerAbi).at(tokenManagerAdr);
@@ -100,12 +102,16 @@ ZSCWalletMangement.prototype.parserTokenContractInfoByIndex = function(gm, urlin
     var symbolInfo   = newsids[2];
     var decimalsInfo = newsids[3];
     var addressInfo  = newsids[4];
+    var posableInfo  = newsids[5];
+    var tradeableInfo  = newsids[6];
 
     gm.tokenNames[index]  = nameInfo.split("=")[1];
     gm.tokenSymbols[index]  = symbolInfo.split("=")[1];
     gm.tokenStatus[index]   = statusInfo.split("=")[1];
     gm.tokenDecimals[index] = decimalsInfo.split("=")[1];
     gm.tokenAdrs[index]     = addressInfo.split("=")[1];
+    gm.posableTags[index]     = posableInfo.split("=")[1];
+    gm.tradeableTags[index]     = tradeableInfo.split("=")[1];
 
     return true;
 }
@@ -113,9 +119,9 @@ ZSCWalletMangement.prototype.parserTokenContractInfoByIndex = function(gm, urlin
 ZSCWalletMangement.prototype.loadWalletManagementHtml = function(elementId) {
     var text = '<table align="center" style="width:800px;min-height:30px">'
     text += '<tr>'
-    text += '   <td><text>Name</text></td> <td><text>Actived</text></td>  <td><text>Sysmbol</text></td>  <td><text>Decimals</text></td>  <td><text>Address</text></td> '
+    text += '   <td><text>Name</text></td> <td><text>Actived</text></td>  <td><text>Sysmbol</text></td>  <td><text>Decimals</text></td>  <td><text>Posable</text></td>    <td><text>tradeable</text></td> <td><text>Address</text></td> '
     text += '</tr>'
-    text += '<tr> <td>---</td> <td>---</td> <td>---</td>  <td>---</td> <td>---</td> </tr>'
+    text += '<tr> <td>---</td> <td>---</td> <td>---</td>  <td>---</td> <td>---</td>  <td>---</td> <td>---</td> </tr>'
 
     for (var i = 0; i < this.tokenNos; ++i) {
         text += '<tr>'
@@ -123,6 +129,8 @@ ZSCWalletMangement.prototype.loadWalletManagementHtml = function(elementId) {
         text += '   <td><text>' + this.tokenStatus[i]    + '</text></td>'
         text += '   <td><text>' + this.tokenSymbols[i]  + '</text></td>'
         text += '   <td><text>' + this.tokenDecimals[i]    + '</text></td>'
+        text += '   <td><text>' + this.posableTags[i]      + '</text></td>'
+        text += '   <td><text>' + this.tradeableTags[i]      + '</text></td>'
         text += '   <td><text>0x' + this.tokenAdrs[i]      + '</text></td>'
         text += '</tr>'
     }
