7d8051f8eddca9c1fbe1f2f2a78fa0ec03db8e88
Eric-Yao <norhustar@hotmail.com>
2018-10-01 21:22:30 +0800

    updated php/js contents

--- WebAdm/adm_init_sys_component.php
+++ WebAdm/adm_init_sys_component.php
@@ -55,13 +55,6 @@ $htmlObjects = new ZSChtmlObjects();
         zscSetup.setTokenAmountToUser(ethAmount, tokenSymbol, tokenAmount, elementId);
     }
 
-    function setTokenPosable(tokenSymbolId, tagId, elementId) {
-        var adr = "<?php echo $htmlObjects->readObjectAddress('ControlApisAdv')?>";
-        var tokenSymbol = document.getElementById(tokenSymbolId).value;
-        var tag = document.getElementById(tagId).value;
-        zscSetup.setTokenPosable(tokenSymbol, tag, elementId);
-    }
-
 </script>
 </head>
 <body>
@@ -88,15 +81,9 @@ $htmlObjects = new ZSChtmlObjects();
         <text>Step - 0 - 4</text>
         <button type="button" onClick="setTestTokenToUser('AllocatedEthAmount', 'AllocatedTokenSymbol', 'AllocatedTokenAmount', 'setTestTokenToUserHash')">Pre-allocate ETH & Token to tester</button> <br>
         ETH Amount: <input type="text" id="AllocatedEthAmount" value="0"></input> <br> 
-        Token Symbol: <input type="text" id="AllocatedTokenSymbol" value="TestZSC"></input> <br> 
+        Token Symbol: <input type="text" id="AllocatedTokenSymbol" value="TestMYT"></input> <br> 
         Token Amount: <input type="text" id="AllocatedTokenAmount" value="50000"></input> <br> 
         <text id="setTestTokenToUserHash"></text> <br><br>
-
-        <text>Step - 0 - 5</text>
-        <button type="button" onClick="setTokenPosable('PosableTokenSymbol', 'PosableTag', 'setTokenPosableHash')">Set token posable</button> <br>
-        Token Symbol: <input type="text" id="PosableTokenSymbol" value="TestZSC"></input> <br> 
-        Tag: <input type="text" id="PosableTag" value="true"></input> <br> 
-        <text id="setTokenPosableHash"></text> <br><br>
 
         <?php echo $htmlObjects->loadInitObjects('initSystemComponent');?>
 
