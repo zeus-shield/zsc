e80a59713e61c930c397ca2d214d3b65a307afaf
scy-aojiao <1652336252@qq.com>
2018-10-12 11:41:56 +0800

    add body css for App.vue

--- Patch/jnet/user_admin_front/App.vue
+++ Patch/jnet/user_admin_front/App.vue
@@ -15,3 +15,15 @@ export default {
 }
 
 </script>
+
+<style lang="scss">
+body {
+	margin: 0px;
+	padding: 0px;
+	/*background: url(assets/bg1.jpg) center !important;
+		background-size: cover;*/
+	// background: #1F2D3D;
+	font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
+	font-size: 14px;
+	-webkit-font-smoothing: antialiased;
+}
