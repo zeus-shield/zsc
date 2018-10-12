e6dc140049dc2087e4ff9f57a3375270b8de0359
scy-aojiao <1652336252@qq.com>
2018-10-12 11:40:39 +0800

    add express module for app.js

--- Patch/jnet/user_admin_backend/app.js
+++ Patch/jnet/user_admin_backend/app.js
@@ -1,1 +1,7 @@
 var express = require('express');
+var app = express();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'public')));
