73ae0c37175c67fd7d31bf223e553ac989032ea4
swedenwolf <xmuguoyang@gmail.com>
2018-10-12 15:22:48 +0800

    added function hF_loadButtionForEnablingElement() for 'htmlFormat.js'

--- Patch/zscdemo/public/js/htmlFormat.js
+++ Patch/zscdemo/public/js/htmlFormat.js
@@ -18,7 +18,15 @@ function hF_loadLogin(elementId) {
     document.getElementById(elementId).innerHTML = text;  
 }
 
-
+function hF_loadButtonForEnablingElement(elementId) {
+    var functionInput = "uF_creatProvider('CreateProvider', 'CreateProviderHash')";
+    var text = '<text>Enable the user in ZSC blockchain system</text>'
+    text += '<div class="well">'
+    text += '    <button type="button" onClick="' + functionInput + '">Enable User</button>'
+    text += '    <text id="CreateProviderHash"></text>'
+    text += '</div>'
+    document.getElementById(elementId).innerHTML = text;  
+}
 
 
 
