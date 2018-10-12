e958871dfa61dfb0f651687ec8497ab1e95b68ba
scy-aojiao <1652336252@qq.com>
2018-10-12 11:44:12 +0800

    add toolbar css for App.vue

--- Patch/jnet/user_admin_front/App.vue
+++ Patch/jnet/user_admin_front/App.vue
@@ -44,3 +44,13 @@ body {
 	vertical-align: baseline;
 	margin-right: 10px;
 }
+
+.toolbar {
+	background: #f2f2f2;
+	padding: 10px;
+	//border:1px solid #dfe6ec;
+	margin: 10px 0px;
+	.el-form-item {
+		margin-bottom: 10px;
+	}
+}
