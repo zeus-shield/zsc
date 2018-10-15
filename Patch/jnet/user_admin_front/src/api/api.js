ec6607aa9a7e6fbada789c6bd6cfd19f97849257
scy-aojiao <1652336252@qq.com>
2018-10-15 18:22:02 +0800

    add function 'getNums' for api.js

--- Patch/jnet/user_admin_front/src/api/api.js
+++ Patch/jnet/user_admin_front/src/api/api.js
@@ -7,3 +7,5 @@ let instance = axios.create({
 //jnet
 
 export const getTrack   = params => { return instance.post(`${base}/user/getTrack`,params).then(res => res.data); };
+
+export const getNums    = params => { return instance.post(`${base}/user/getNums`,params).then(res => res.data); };
