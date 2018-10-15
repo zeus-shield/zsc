1fad834af7eeb167cdc0fcb196d264c31c6e1426
scy-aojiao <1652336252@qq.com>
2018-10-15 18:22:24 +0800

    add function 'addOrder' for api.js

--- Patch/jnet/user_admin_front/src/api/api.js
+++ Patch/jnet/user_admin_front/src/api/api.js
@@ -9,3 +9,5 @@ let instance = axios.create({
 export const getTrack   = params => { return instance.post(`${base}/user/getTrack`,params).then(res => res.data); };
 
 export const getNums    = params => { return instance.post(`${base}/user/getNums`,params).then(res => res.data); };
+
+export const addOrder   = params => { return instance.post(`${base}/user/add`,  params ).then(res => res.data ); };
