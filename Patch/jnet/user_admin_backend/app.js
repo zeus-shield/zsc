2c35c4577339ac7af4b4d443f87ee82f7ca3b47c
scy-aojiao <1652336252@qq.com>
2018-10-19 18:12:03 +0800

    add use for app.js

--- Patch/jnet/user_admin_backend/app.js
+++ Patch/jnet/user_admin_backend/app.js
@@ -10,8 +10,15 @@ var user = require('./routes/user');
 var index = require('./routes/index');
 var config = require('./config/config');
 var app = express();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
