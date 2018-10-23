1db5ed0f2a21c2cd7d7e429f62fc5e9c1db06859
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-10-23 14:50:56 +0800

    up compile.sh for WebTest

--- WebTest/compile.sh
+++ WebTest/compile.sh
@@ -2,11 +2,14 @@
 
 SFLAGS="--optimize --overwrite --pretty-json --combined-json bin,abi"
 CORE="sol"
+MODULE="logistics"
+MODE="single"
+# MODE="multiple"
 IMPORTPATH="/=/"
 SOURCES=(
-logistics_string_min
-logistics_string
+logistics
 )
+BUILDPATH="./build"
 
 for((i=0;i<${#SOURCES[@]};i++));
 do
@@ -14,7 +17,10 @@ SOURCE=${SOURCES[i]}".sol";
 echo "########## compiling "${SOURCE}" ##########"
 
 # Compiling to JSON file(build/xxx/combined.json)
-solc ${SFLAGS} ${IMPORTPATH} ./${CORE}/${SOURCE} -o build/${SOURCES[i]}
+# solc ${SFLAGS} ${IMPORTPATH} ./${CORE}/${MODULE}/${MODE}/${SOURCE} -o ${BUILDPATH}/${MODULE}/${MODE}/${SOURCES[i]}
+solc ${SFLAGS} ${IMPORTPATH} ./${CORE}/${MODULE}/${MODE}/${SOURCE} -o ${BUILDPATH}/${MODULE}/${MODE}
+mv ${BUILDPATH}/${MODULE}/${MODE}/combined.json ${BUILDPATH}/${MODULE}/${MODE}/${SOURCES[i]}.json
+
 
 # Compiling to JS file(WebAdm/js/compiled_xxx.js)
 # COMPILED="`solc ${SFLAGS} ./${CORE}/${SOURCE}`"
