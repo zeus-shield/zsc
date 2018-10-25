d599322dbd11855afdc8fb5f916186833989037a
scy-aojiao <1652336252@qq.com>
2018-10-25 12:03:30 +0800

    add require for user.js

---
+++ Patch/jnet/user_admin_backend/routes/user.js
@@ -0,0 +1,13 @@
+/*
+*！！！！！！！！此文件内写有密码，不得上传至git！！！！！！！！！
+*！！！！！！！！此文件内写有密码，不得上传至git！！！！！！！！！
+*！！！！！！！！此文件内写有密码，不得上传至git！！！！！！！！！
+*！！！！！！！！此文件内写有密码，不得上传至git！！！！！！！！！
+*！！！！！！！！此文件内写有密码，不得上传至git！！！！！！！！！
+*！！！！！！！！此文件内写有密码，不得上传至git！！！！！！！！！
+*/
+
+var express = require('express');
+var router = express.Router();
+const axios = require('axios');
+const Order = require('./../model/Order');
