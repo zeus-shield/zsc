0de770edc22d3b67cd97005f2410598bbdcb398f
scy-aojiao <1652336252@qq.com>
2018-10-26 09:52:41 +0800

    add post'getTrack' for user.js

--- Patch/jnet/user_admin_backend/routes/user.js
+++ Patch/jnet/user_admin_backend/routes/user.js
@@ -11,3 +11,18 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 const Order = require('./../model/Order');
+
+router.post('/getTrack', function (req, res) {
+    let para = { num: req.body.num };
+    let url = `http://api2.j-net.cn/track/${para.num}`;
+
+    axios.get(url)
+        .then(response => {
+            res.json({
+                date: response.data
+            })
+        })
+        .catch(error => {
+            console.log(error);
+        });
+})
