ca54119d3b3cc84ff2cf5b60bcd77e8e7b1f3f98
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-10-29 11:37:11 +0800

    add functions for WebTest/sol/logistics/multiple/logistics.sol

--- WebTest/sol/logistics/multiple/logistics.sol
+++ WebTest/sol/logistics/multiple/logistics.sol
@@ -25,4 +25,50 @@ contract Logistics {
 
     /** @desc string(original num name) => bool(num exist flag) */
     mapping(string => bool) private numExist_;
+
+    // Constructor
+    function Logistics() public {
+        coreAddr_ = 0;
+    }
+
+    function setup(address _coreAddr) public {
+        coreAddr_ = _coreAddr;
+    }
+ 
+    // _updateType: 0 means overwrite, 1 means append
+    function updateTracks(string _num, string _tracks, uint _updateType) public {
+    }
+
+    function updateBrief(string _num, string _transNum, string _model,
+                         string _destinationCountry, string _lastStatus) public {
+    }
+
+    function updateBriefEx(string _num, string _brief) public {
+    }
+
+    function update(string _num, string _transNum, 
+                    string _model, string _destinationCountry,
+                    string _lastStatus, string _tracks) public {
+    }
+
+    function updateEx(string _num, string _info) public {
+    }
+
+    function remove(string _num) public {
+    }
+
+    function invalid(string _num) public {
+    }
+
+    function exist(string _num) public view returns (bool) {
+    }
+
+    function number() public view returns (uint) {
+    }
+
+    function numberOfTracks(string _num) public view returns (uint) {
+    }
+
+    function numberOfInvalid(string _num) public view returns (uint) {
+    }
 }
