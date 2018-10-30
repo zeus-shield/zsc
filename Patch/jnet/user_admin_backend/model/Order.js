0cdc3a691fdec9045247187c64e6798ba957ce63
scy-aojiao <1652336252@qq.com>
2018-10-30 15:40:59 +0800

    add OrderSchema for Order.js

--- Patch/jnet/user_admin_backend/model/Order.js
+++ Patch/jnet/user_admin_backend/model/Order.js
@@ -1,3 +1,15 @@
 var mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+    num: { type: String, default: '' },
+    startTime: { type: String, default: '' },
+    // firstHash: { type: String, default: '' },
+    // secondHash: { type: String, default: '' },
+    // thirdHash: { type: String, default: '' },
+    // fourthHash:{ type: String, default: ''},
+    // fifthHash:{ type: String, default: ''},
+    hashList:{ "type": "array", "items": "string", default: ''},
+   
+});
