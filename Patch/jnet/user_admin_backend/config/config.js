ae16b467f99fad529945366857a1bd853f2e24ba
scy-aojiao <1652336252@qq.com>
2018-11-06 15:27:09 +0800

    update config.js

--- Patch/jnet/user_admin_backend/config/config.js
+++ Patch/jnet/user_admin_backend/config/config.js
@@ -2,3 +2,8 @@ const path = require('path');
 
 let dev = "http://localhost:3001";
 let produ = "";
+
+let serverPath = dev;
+
+let robotSavePath = path.resolve(__dirname,'../public/images/robot')
+let robotServerPath = `${serverPath}/images/robot`;
