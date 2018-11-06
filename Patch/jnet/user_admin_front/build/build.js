3546bd91ba8a30103c834e1857c78fdf99017ae9
scy-aojiao <1652336252@qq.com>
2018-11-06 15:32:27 +0800

    update build.js

---
+++ Patch/jnet/user_admin_front/build/build.js
@@ -0,0 +1,14 @@
+require('./check-versions')()
+
+process.env.NODE_ENV = 'production'
+
+var ora = require('ora')
+var rm = require('rimraf')
+var path = require('path')
+var chalk = require('chalk')
+var webpack = require('webpack')
+var config = require('../config')
+var webpackConfig = require('./webpack.prod.conf')
+
+var spinner = ora('building for production...')
+spinner.start()
