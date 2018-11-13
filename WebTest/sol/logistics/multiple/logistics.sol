01f841bd0f61969fbc9ddc4667c79f891974be4f
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-11-13 15:42:07 +0800

    up WebTest/sol/logistics/multiple/logistics.sol

--- WebTest/sol/logistics/multiple/logistics.sol
+++ WebTest/sol/logistics/multiple/logistics.sol
@@ -1,20 +1,14 @@
 /**
- Copyright (c) 2018, deduotech.com
- 2018-10-19: v0.00.01
+  Copyright (c) 2018, ZSC Dev Team
+  2018-10-19: v0.00.01
  */
 
 pragma solidity ^0.4.25;
 // pragma experimental ABIEncoderV2;
 
 contract LogisticsCore {
-    function exist(string _num) external view returns (bool);
-    function number() external view returns (uint);
-    function numberOfTracks(string _num) external view returns (uint);
-    function getTracks(string _num) external view returns (string);
-    function getBrief(string _num) external view returns (string, string, string, string, string);
-    function getBriefEx(string _num) external view returns (string);
-    function getBriefByIndex(uint _index) external view returns (string, string, string, string, string);
-    function getBriefExByIndex(uint _index) external view returns (string);
+    function getParcel(string _num) external view returns (string, string, string, string, string);
+    function getParcelEx(string _num) external view returns (string);
 }
 
 contract Logistics {
@@ -38,61 +32,22 @@ contract Logistics {
         
         coreAddr_ = _coreAddr;
     }
- 
-    function exist(string _num) external view _checkCoreAddr returns (bool) {
-        // check param
-        if (0 == bytes(_num).length) {
-            return false;
-        }
 
-        return LogisticsCore(coreAddr_).exist(_num);
-    }
-
-    function number() external view _checkCoreAddr returns (uint) {
-        return LogisticsCore(coreAddr_).number();
-    }
-
-    function numberOfTracks(string _num) external view _checkCoreAddr returns (uint) {
-        // check param
-        if (0 == bytes(_num).length) {
-            return 0;
-        }
-
-        return LogisticsCore(coreAddr_).numberOfTracks(_num);
-    }
-
-    function getTracks(string _num) external view _checkCoreAddr returns (string) {
-        // check param
-        if (0 == bytes(_num).length) {
-            return "";
-        }
-
-        return LogisticsCore(coreAddr_).getTracks(_num);
-    }
-
-    function getBrief(string _num) external view _checkCoreAddr returns (string, string, string, string, string) {
+    function getParcel(string _num) external view _checkCoreAddr returns (string, string, string, string, string) {
         // check param
         if (0 == bytes(_num).length) {
             return ("", "", "", "", "");
         }
 
-        return LogisticsCore(coreAddr_).getBrief(_num);
+        return LogisticsCore(coreAddr_).getParcel(_num);
     }
 
-    function getBriefEx(string _num) external view _checkCoreAddr returns (string) {
+    function getParcelEx(string _num) external view _checkCoreAddr returns (string) {
         // check param
         if (0 == bytes(_num).length) {
             return "";
         }
 
-        return LogisticsCore(coreAddr_).getBriefEx(_num);
-    }
-
-    function getBriefByIndex(uint _index) external view _checkCoreAddr returns (string, string, string, string, string) {
-        return LogisticsCore(coreAddr_).getBriefByIndex(_index);
-    }
-
-    function getBriefExByIndex(uint _index) external view _checkCoreAddr returns (string) {
-        return LogisticsCore(coreAddr_).getBriefExByIndex(_index);
+        return LogisticsCore(coreAddr_).getParcelEx(_num);
     }
 }
