7eb34490245fc3beefb68b14bfc04a3d524a4a6d
scy-aojiao <1652336252@qq.com>
2018-11-16 17:03:51 +0800

    add addUser for Order.js

--- Patch/jnet/user_admin_backend/routes/User.js
+++ Patch/jnet/user_admin_backend/routes/User.js
@@ -1,3 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const user = require('../model/User');
+
+router.post('/addUser', function (req, res) {
+    let para = {    username: req.body.username,
+                    password: req.body.password };
+    
+    new user(para).save()
+
+    res.json({
+        status:4,
+        msg:"×¢²á³É¹¦",
+        result:""
+    })
+})
