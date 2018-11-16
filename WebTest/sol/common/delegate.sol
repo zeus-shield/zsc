986ada4ba012e35abd18993f0d49c679fd261380
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-11-16 11:46:55 +0800

    up WebTest/sol/common/delegate.sol

--- WebTest/sol/common/delegate.sol
+++ WebTest/sol/common/delegate.sol
@@ -7,147 +7,151 @@ pragma solidity ^0.4.25;
 // pragma experimental ABIEncoderV2;
 
 contract Delegate {
-    uint delegateNos_;
-    mapping (uint => address) private adrs_;
+    uint sum_;
+    mapping (uint => address) private addrs_;
     /** @desc prioritie map.
       * == 0: invalid
       * == 1: ownable
       * >= 2: define by user
       */
-    mapping (uint => uint) private priorities_;
-    mapping (address => uint) private indice_;
+    mapping (uint => uint) private prios_;
+    mapping (address => uint) private ids_;
     mapping (address => bool) private exists_;
 
     constructor() public {
-        adrs_[0] = msg.sender;
-        priorities_[0] = 1;
-        indice_[msg.sender] = 0;
+        addrs_[0] = msg.sender;
+        prios_[0] = 1;
+        ids_[msg.sender] = 0;
         exists_[msg.sender] = true;
-        delegateNos_ = 1;
+        sum_ = 1;
     }
 
     modifier _onlyOwner() {
         require(exists_[msg.sender]);
-        require(0 == indice_[msg.sender]);
-        require(adrs_[0] == msg.sender);
-        require(1 == priorities_[0]);
+        require(0 == ids_[msg.sender]);
+        require(addrs_[0] == msg.sender);
+        require(1 == prios_[0]);
         _;
     }
 
-    function _update(address _adr, uint _priority) private {
-        uint index;
-        if (exists_[_adr]) {
-            index = indice_[_adr];
-            // adrs_[index] = _adr;
-            priorities_[index] = _priority;
+    function _update(address _addr, uint _prio) private {
+        uint id;
+        if (exists_[_addr]) {
+            id = ids_[_addr];
+            // addrs_[id] = _addr;
+            prios_[id] = _prio;
         } else {
-            index = delegateNos_;
-            exists_[_adr] = true;
-            indice_[_adr] = index;
-            adrs_[index] = _adr;
-            priorities_[index] = _priority;
-            delegateNos_++;
+            id = sum_;
+            exists_[_addr] = true;
+            ids_[_addr] = id;
+            addrs_[id] = _addr;
+            prios_[id] = _prio;
+            sum_ ++;
         }
     }
 
-    function _swap(address _lAdr, uint _rNos) private {
-        uint lPriority = 0;
-        uint lNos = 0;
-        address rAddr = 0;
-        uint rPriority = 0;
+    function _swap(address _addr1, uint _id2) private {
+        uint prio1 = 0;
+        uint id1 = 0;
+        address addr2 = 0;
+        uint prio2 = 0;
 
-        rAddr = adrs_[_rNos];
-        rPriority = priorities_[_rNos];
-        // _rNos;
+        addr2 = addrs_[_id2];
+        prio2 = prios_[_id2];
+        // _id2;
 
-        // _lAdr
-        lNos = indice_[_lAdr];
-        lPriority = priorities_[lNos];
+        // _addr1
+        id1 = ids_[_addr1];
+        prio1 = prios_[id1];
 
-        adrs_[lNos] = rAddr;
-        priorities_[lNos] = rPriority;
-        indice_[rAddr] = lNos;
+        addrs_[id1] = addr2;
+        prios_[id1] = prio2;
+        ids_[addr2] = id1;
 
-        adrs_[_rNos] = _lAdr;
-        priorities_[_rNos] = lPriority;
-        indice_[_lAdr] = _rNos;
+        addrs_[_id2] = _addr1;
+        prios_[_id2] = prio1;
+        ids_[_addr1] = _id2;
     }
 
-    function checkDelegate(address _adr, uint _priority) public view returns (bool)  {
-        if (_adr == address(this)) return true;
-        if (0 == _priority) return false;
-        if (!exists_[_adr]) return false;
+    function checkDelegate(address _addr, uint _prio) public view returns (bool)  {
+        if (_addr == address(this)) return true;
+        if (0 == _prio) return false;
+        if (!exists_[_addr]) return false;
 
-        uint index = indice_[_adr];
-        return (priorities_[index] != 0 && priorities_[index] <= _priority);
+        uint id = ids_[_addr];
+        return (prios_[id] != 0 && prios_[id] <= _prio);
     }
 
     // This unnamed function is called whenever someone tries to send ether to it
     function() external payable { revert(); }
 
     function kill() external _onlyOwner {
-        selfdestruct(adrs_[0]);   
+        selfdestruct(addrs_[0]);   
     }
 
-    function transferOwnersihp(address _newOwner) external _onlyOwner {
-        if (0 != _newOwner) {
-            address owner = adrs_[0];
+    function transferOwnership(address _newOwner, uint _degradePrio) external _onlyOwner {
+        require(0 != _newOwner);
+        require(addrs_[0] != _newOwner);
+        require(1 < _degradePrio);
 
-            // upgrade new owner
-            _update(_newOwner, 1);
-            uint newOwnerNos = indice_[_newOwner];
+        // upgrade new owner
+        _update(_newOwner, 1);
 
-            // _swap old owner and new owner
-            _swap(_newOwner, 0);
-            require(0 == indice_[_newOwner]);
-            require(newOwnerNos == indice_[owner]);
+        // _swap old owner and new owner
+        uint newOwnerId = ids_[_newOwner];
+        address owner = addrs_[0];
+        _swap(_newOwner, 0);
+        require(0 == ids_[_newOwner]);
+        require(newOwnerId == ids_[owner]);
 
-            // downgrade old owner
-            priorities_[indice_[owner]] = 2;
-        }
+        // degrade old owner
+        prios_[ids_[owner]] = _degradePrio;
     }
 
-    function updateDelegate(address _adr, uint _priority) external _onlyOwner {
-        if (address(this) == _adr) return;
+    function updateDelegate(address _addr, uint _prio) external _onlyOwner {
+        if (address(this) == _addr) return;
 
         // owner's priority can't be changed
-        require(adrs_[0] != _adr);
+        require(addrs_[0] != _addr);
 
         // invalid priority and owner's priority can't be set
-        require(1 < _priority);
+        require(1 < _prio);
 
-        _update(_adr, _priority);
+        _update(_addr, _prio);
     }
 
-    function removeDelegate(address _adr) external _onlyOwner {
-        if (address(this) == _adr) return;
+    function removeDelegate(address _addr) external _onlyOwner {
+        if (address(this) == _addr) return;
 
         // owner's priority can't be remove
-        require(adrs_[0] != _adr);
+        require(addrs_[0] != _addr);
 
-        require(0 < delegateNos_);
-        require(exists_[_adr]);
+        require(0 < sum_);
+        require(exists_[_addr]);
 
-        _swap(_adr, delegateNos_-1);
-        require((delegateNos_-1) == indice_[_adr]);
+        uint id1 = ids_[_addr];
+        address addr2 = addrs_[sum_-1];
+        _swap(_addr, sum_-1);
+        require((sum_-1) == ids_[_addr]);
+        require(id1 == ids_[addr2]);
 
-        delete adrs_[delegateNos_-1];
-        delete priorities_[delegateNos_-1];
-        delete indice_[_adr];
+        delete addrs_[sum_-1];
+        delete prios_[sum_-1];
+        delete ids_[_addr];
 
-        delegateNos_ --;
+        sum_ --;
 
-        exists_[_adr] = false;
+        exists_[_addr] = false;
     }
 
     function numberOfDelegates() external view returns (uint) {
-        return delegateNos_;
+        return sum_;
     }
 
-    function getDelegateByIndex(uint _index) external view returns (address, uint) {
-        require(_index < delegateNos_);
-        require(exists_[adrs_[_index]]);
-        require(indice_[adrs_[_index]] == _index);
-        return (adrs_[_index], priorities_[_index]);
+    function getDelegateById(uint _id) external view returns (address, uint) {
+        require(_id < sum_);
+        require(exists_[addrs_[_id]]);
+        require(ids_[addrs_[_id]] == _id);
+        return (addrs_[_id], prios_[_id]);
     }
 }
