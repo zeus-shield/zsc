27e37733f54e3738ec627f531051c67da589ac20
scy-aojiao <1652336252@qq.com>
2018-11-16 17:23:52 +0800

    add function 'exec' for check-versions.js

--- Patch/jnet/user_admin_front/build/check-versions.js
+++ Patch/jnet/user_admin_front/build/check-versions.js
@@ -2,3 +2,6 @@ var chalk = require('chalk')
 var semver = require('semver')
 var packageConfig = require('../package.json')
 var shell = require('shelljs')
+function exec (cmd) {
+  return require('child_process').execSync(cmd).toString().trim()
+}
