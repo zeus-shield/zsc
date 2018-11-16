553f6c65696cc4f37620d6ee978b337e977833ae
scy-aojiao <1652336252@qq.com>
2018-11-16 17:14:17 +0800

    update build.js

--- Patch/jnet/user_admin_front/build/build.js
+++ Patch/jnet/user_admin_front/build/build.js
@@ -12,3 +12,16 @@ var webpackConfig = require('./webpack.prod.conf')
 
 var spinner = ora('building for production...')
 spinner.start()
+
+rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
+  if (err) throw err
+  webpack(webpackConfig, function (err, stats) {
+    spinner.stop()
+    if (err) throw err
+    process.stdout.write(stats.toString({
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false
+    }) + '\n\n')
