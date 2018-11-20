9a8d6da74bf6e34c9c7167fdfccbbec4efa9b824
scy-aojiao <1652336252@qq.com>
2018-11-20 11:31:08 +0800

    update check-versions.js

--- Patch/jnet/user_admin_front/build/check-versions.js
+++ Patch/jnet/user_admin_front/build/check-versions.js
@@ -33,3 +33,16 @@ module.exports = function () {
       )
     }
   }
+
+  if (warnings.length) {
+    console.log('')
+    console.log(chalk.yellow('To use this template, you must update following to modules:'))
+    console.log()
+    for (var i = 0; i < warnings.length; i++) {
+      var warning = warnings[i]
+      console.log('  ' + warning)
+    }
+    console.log()
+    process.exit(1)
+  }
+}
