e4fdd541b280949267cf61294c9d650f9b3a048e
scy-aojiao <1652336252@qq.com>
2018-11-20 11:28:40 +0800

    add versionRequirements for check-versions.js

--- Patch/jnet/user_admin_front/build/check-versions.js
+++ Patch/jnet/user_admin_front/build/check-versions.js
@@ -5,3 +5,11 @@ var shell = require('shelljs')
 function exec (cmd) {
   return require('child_process').execSync(cmd).toString().trim()
 }
+
+var versionRequirements = [
+  {
+    name: 'node',
+    currentVersion: semver.clean(process.version),
+    versionRequirement: packageConfig.engines.node
+  },
+]
