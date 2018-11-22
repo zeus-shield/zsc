14f29c4ea3dab6bff22008d9c87df540b294fb42
swedenwolf <xmuguoyang@gmail.com>
2018-11-22 10:05:03 +0800

    added 'User.php'

---
+++ Patch/zscdemo/application/admin/controller/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\admin\controller;
+use app\common\controller\Admin;
+
+class User extends Admin {
+
+
+	public function index() {
+		$nickname      = input('nickname');
+		$map['status'] = array('egt', 0);
+		if (is_numeric($nickname)) {
+			$map['uid|nickname'] = array(intval($nickname), array('like', '%' . $nickname . '%'), '_multi' => true);
+		} else {
+			$map['nickname'] = array('like', '%' . (string) $nickname . '%');
+		}
+
+		$order = "uid desc";
+		$list  = model('User')->where($map)->order($order)->paginate(15);
+
+		$data = array(
+			'list' => $list,
+			'page' => $list->render(),
+		);
+		$this->assign($data);
+		$this->setMeta('用户信息');
+		return $this->fetch();
+	}
