6df411b6c8606caf104f65e413e4acfd0c0d8b07
swedenwolf <xmuguoyang@gmail.com>
2018-11-22 10:11:57 +0800

    added 'Seo.php'

---
+++ Patch/zscdemo/application/admin/controller/Seo.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace app\admin\controller;
+use app\common\controller\Admin;
+
+class Seo extends Admin {
+
+	protected $seo;
+	protected $rewrite;
+
+	public function _initialize() {
+		parent::_initialize();
+		$this->seo     = model('SeoRule');
+		$this->rewrite = model('Rewrite');
+	}
+
+}
