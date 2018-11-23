52d9a3eaaaf71b7882917fd2ffa1aead485792f1
scy-aojiao <1652336252@qq.com>
2018-11-23 17:09:27 +0800

    add readyPromise for dev-server.js

--- Patch/jnet/user_admin_front/build/dev-server.js
+++ Patch/jnet/user_admin_front/build/dev-server.js
@@ -61,3 +61,10 @@ app.use(hotMiddleware)
 // serve pure static assets
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
+
+var uri = 'http://localhost:' + port
+
+var _resolve
+var readyPromise = new Promise(resolve => {
+  _resolve = resolve
+})
