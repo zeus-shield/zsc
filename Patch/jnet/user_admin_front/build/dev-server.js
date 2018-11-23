688e14907c34f4dee2cc5e7c1d0a84af6e0f12b9
scy-aojiao <1652336252@qq.com>
2018-11-23 17:06:35 +0800

    add default for dev-server.js

--- Patch/jnet/user_admin_front/build/dev-server.js
+++ Patch/jnet/user_admin_front/build/dev-server.js
@@ -11,3 +11,11 @@ var express = require('express')
 var webpack = require('webpack')
 var proxyMiddleware = require('http-proxy-middleware')
 var webpackConfig = require('./webpack.dev.conf')
+
+// default port where dev server listens for incoming traffic
+var port = process.env.PORT || config.dev.port
+// automatically open browser, if not set will be false
+var autoOpenBrowser = !!config.dev.autoOpenBrowser
+// Define HTTP proxies to your custom API backend
+// https://github.com/chimurai/http-proxy-middleware
+var proxyTable = config.dev.proxyTable
