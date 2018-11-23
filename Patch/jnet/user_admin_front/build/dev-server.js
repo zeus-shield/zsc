ec48d8663d0c4fe09135205e05010ae834be6e85
scy-aojiao <1652336252@qq.com>
2018-11-23 17:05:32 +0800

    add require for dev-server.js

--- Patch/jnet/user_admin_front/build/dev-server.js
+++ Patch/jnet/user_admin_front/build/dev-server.js
@@ -4,3 +4,10 @@ var config = require('../config')
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
 }
+
+var opn = require('opn')
+var path = require('path')
+var express = require('express')
+var webpack = require('webpack')
+var proxyMiddleware = require('http-proxy-middleware')
+var webpackConfig = require('./webpack.dev.conf')
