30034d95ccdcc972b37c7f36ae80368b3c46e7b2
scy-aojiao <1652336252@qq.com>
2018-11-23 17:08:05 +0800

    add compiler for dev-server.js

--- Patch/jnet/user_admin_front/build/dev-server.js
+++ Patch/jnet/user_admin_front/build/dev-server.js
@@ -31,3 +31,10 @@ var devMiddleware = require('webpack-dev-middleware')(compiler, {
 var hotMiddleware = require('webpack-hot-middleware')(compiler, {
   log: () => {}
 })
+// force page reload when html-webpack-plugin template changes
+compiler.plugin('compilation', function (compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+    hotMiddleware.publish({ action: 'reload' })
+    cb()
+  })
+})
