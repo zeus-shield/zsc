9c50cb33556203932245f0153759ca6e74bc9cc4
swedenwolf <xmuguoyang@gmail.com>
2018-11-27 14:43:54 +0800

    added 'Form.php'

---
+++ Patch/zscdemo/application/admin/controller/Form.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace app\admin\controller;
+use app\common\controller\Admin;
+
+class Form extends Admin {
+
+	//自定义表单
+	public function index(){
+		$list = array();
+
+		$data = array(
+			'list'   => $list,
+			//'page'   => $list->render()
+		);
+		$this->setMeta('自定义表单');
+		$this->assign($data);
+		return $this->fetch();
+	}
+}
