3ef95c8a74a96d1abac0c035e53ccc2ab521cf70
scy-aojiao <1652336252@qq.com>
2018-11-28 17:10:02 +0800

    update utils.js

--- Patch/jnet/user_admin_front/build/utils.js
+++ Patch/jnet/user_admin_front/build/utils.js
@@ -31,3 +31,15 @@ exports.cssLoaders = function (options) {
         })
       })
     }
+
+    // Extract CSS when that option is specified
+    // (which is the case during production build)
+    if (options.extract) {
+      return ExtractTextPlugin.extract({
+        use: loaders,
+        fallback: 'vue-style-loader'
+      })
+    } else {
+      return ['vue-style-loader'].concat(loaders)
+    }
+  }
