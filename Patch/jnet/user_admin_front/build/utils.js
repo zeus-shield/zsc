96f9b9bf5a179abfc3faf67180a2a966dcf8e22f
scy-aojiao <1652336252@qq.com>
2018-11-28 17:10:18 +0800

    update utils.js

--- Patch/jnet/user_admin_front/build/utils.js
+++ Patch/jnet/user_admin_front/build/utils.js
@@ -43,3 +43,15 @@ exports.cssLoaders = function (options) {
       return ['vue-style-loader'].concat(loaders)
     }
   }
+
+  // https://vue-loader.vuejs.org/en/configurations/extract-css.html
+  return {
+    css: generateLoaders(),
+    postcss: generateLoaders(),
+    less: generateLoaders('less'),
+    sass: generateLoaders('sass', { indentedSyntax: true }),
+    scss: generateLoaders('sass'),
+    stylus: generateLoaders('stylus'),
+    styl: generateLoaders('stylus')
+  }
+}
