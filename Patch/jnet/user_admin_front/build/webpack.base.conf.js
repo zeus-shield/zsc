0548f4e78d1940355df30c4671cd96ee783f7967
scy-aojiao <1652336252@qq.com>
2018-11-29 14:44:51 +0800

    add function 'resolve' for webpack.base.conf.js

--- Patch/jnet/user_admin_front/build/webpack.base.conf.js
+++ Patch/jnet/user_admin_front/build/webpack.base.conf.js
@@ -3,3 +3,7 @@ var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
 var webpack = require('webpack')
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
