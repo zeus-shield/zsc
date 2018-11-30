f089ed42a034cd032a9f3e86ea9ff1d4d23bba1b
scy-aojiao <1652336252@qq.com>
2018-11-30 11:25:08 +0800

    add Object for webpack.dev.conf.js

--- Patch/jnet/user_admin_front/build/webpack.dev.conf.js
+++ Patch/jnet/user_admin_front/build/webpack.dev.conf.js
@@ -5,3 +5,8 @@ var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+
+// add hot-reload related code to entry chunks
+Object.keys(baseWebpackConfig.entry).forEach(function (name) {
+  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
+})
