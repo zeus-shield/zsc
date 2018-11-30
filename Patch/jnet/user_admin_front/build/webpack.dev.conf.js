79188cf9f23ac9de73af04bf01c2ae9563c55be1
scy-aojiao <1652336252@qq.com>
2018-11-30 11:26:05 +0800

    update module for webpack.dev.conf.js

--- Patch/jnet/user_admin_front/build/webpack.dev.conf.js
+++ Patch/jnet/user_admin_front/build/webpack.dev.conf.js
@@ -15,3 +15,9 @@ module.exports = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
   },
+  // cheap-module-eval-source-map is faster for development
+  devtool: '#cheap-module-eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': config.dev.env
+    }),
