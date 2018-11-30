89c0d8f27963451d4361430de7b076ceac772b73
scy-aojiao <1652336252@qq.com>
2018-11-30 14:16:25 +0800

    add require for webpack.prod.conf.js

--- Patch/jnet/user_admin_front/build/webpack.prod.conf.js
+++ Patch/jnet/user_admin_front/build/webpack.prod.conf.js
@@ -7,3 +7,9 @@ var baseWebpackConfig = require('./webpack.base.conf')
 var CopyWebpackPlugin = require('copy-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+
+
+var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+
+var env = config.build.env
