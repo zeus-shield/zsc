3fb840f8361875a83dddf6e5657cfd6c6243115f
scy-aojiao <1652336252@qq.com>
2018-11-30 11:22:37 +0800

    add require for webpack.dev.conf.js

--- Patch/jnet/user_admin_front/build/webpack.base.conf.js
+++ Patch/jnet/user_admin_front/build/webpack.base.conf.js
@@ -57,3 +57,14 @@ module.exports = {
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  }
+}
