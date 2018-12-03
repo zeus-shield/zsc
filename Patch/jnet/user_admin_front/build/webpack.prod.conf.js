69245eee4c9c59035e80bcfdba29d2204153f52f
scy-aojiao <1652336252@qq.com>
2018-12-03 18:14:51 +0800

    update webpackConfig for webpack.prod.conf.js

--- Patch/jnet/user_admin_front/build/webpack.prod.conf.js
+++ Patch/jnet/user_admin_front/build/webpack.prod.conf.js
@@ -42,3 +42,10 @@ var webpackConfig = merge(baseWebpackConfig, {
     new ExtractTextPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css')
     }),
+    // Compress extracted CSS. We are using this plugin so that possible
+    // duplicated CSS from different components can be deduped.
+    new OptimizeCSSPlugin({
+      cssProcessorOptions: {
+        safe: true
+      }
+    }),
