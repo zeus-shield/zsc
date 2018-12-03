99c82e580ea383e38c37b4932156b79fd3d1650b
swedenwolf <xmuguoyang@gmail.com>
2018-12-03 16:06:13 +0800

    added function index() for 'Addons.php'

--- Patch/zscdemo/application/admin/controller/Addons.php
+++ Patch/zscdemo/application/admin/controller/Addons.php
@@ -17,3 +17,22 @@ class Addons extends Admin {
 	}
 	
 }
+	/**
+	 * 插件列表
+	 */
+	public function index($refresh = 0) {
+		if ($refresh) {
+			$this->addons->refresh();
+		}
+		$list = $this->addons->order('id desc')->paginate(25);
+		// 记录当前列表页的cookie
+		Cookie('__forward__', $_SERVER['REQUEST_URI']);
+
+		$data = array(
+			'list' => $list,
+			'page' => $list->render(),
+		);
+		$this->setMeta("插件管理");
+		$this->assign($data);
+		return $this->fetch();
+	}
