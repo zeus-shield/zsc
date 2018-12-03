d6818bee93a27097cc516b57b59918b8cfa589ed
scy-aojiao <1652336252@qq.com>
2018-12-03 18:11:18 +0800

    update webpackConfig for webpack.prod.conf.js

--- Patch/jnet/user_admin_front/build/webpack.prod.conf.js
+++ Patch/jnet/user_admin_front/build/webpack.prod.conf.js
@@ -27,3 +27,14 @@ var webpackConfig = merge(baseWebpackConfig, {
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
+  plugins: [
+    // http://vuejs.github.io/vue-loader/en/workflow/production.html
+    new webpack.DefinePlugin({
+      'process.env': env
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      sourceMap: true
+    }),
