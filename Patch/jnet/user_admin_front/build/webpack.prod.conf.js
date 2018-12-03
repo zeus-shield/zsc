22e46ee53e378710f260360ec42bbb6aaaebf1f1
scy-aojiao <1652336252@qq.com>
2018-12-03 18:10:18 +0800

    add webpackConfig for webpack.prod.conf.js

--- Patch/jnet/user_admin_front/build/webpack.prod.conf.js
+++ Patch/jnet/user_admin_front/build/webpack.prod.conf.js
@@ -13,3 +13,11 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 
 var env = config.build.env
+
+var webpackConfig = merge(baseWebpackConfig, {
+  module: {
+    rules: utils.styleLoaders({
+      sourceMap: config.build.productionSourceMap,
+      extract: true
+    })
+  },
