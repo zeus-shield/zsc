a346de9286f9df599a673918790ed2e1ad566bbe
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-12-05 18:51:01 +0800

    add interface 'getTrackElementByIndex' for WebTest/sol/logistics/multiple/logistics_database.sol

--- WebTest/sol/logistics/multiple/logistics_database.sol
+++ WebTest/sol/logistics/multiple/logistics_database.sol
@@ -280,6 +280,8 @@ contract LogisticsDatabase is Delegate {
         return str;
     }
 
+    function getTrackElementByIndex(string _num, uint _index, string _elementType) external view returns (string) {
+    }
     function getBrief(string _originalNum, string _num) external view returns (string, string, string, uint16, uint8) {
         return (_originalNum, briefs_[_num].transNum_, briefs_[_num].model_, briefs_[_num].destinationCountry_, briefs_[_num].lastStatus_);
     }
