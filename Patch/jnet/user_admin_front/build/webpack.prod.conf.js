c74850ba6560bfa97734c199f4482901548fe8fb
scy-aojiao <1652336252@qq.com>
2018-12-06 17:08:25 +0800

    add HtmlWebpackPlugin for webpack.prod.conf.js

--- Patch/jnet/user_admin_front/build/webpack.prod.conf.js
+++ Patch/jnet/user_admin_front/build/webpack.prod.conf.js
@@ -49,3 +49,20 @@ var webpackConfig = merge(baseWebpackConfig, {
         safe: true
       }
     }),
+    // generate dist index.html with correct asset hash for caching.
+    // you can customize output by editing /index.html
+    // see https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename: config.build.index,
+      template: 'index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+        // more options:
+        // https://github.com/kangax/html-minifier#options-quick-reference
+      },
+      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+      chunksSortMode: 'dependency'
+    }),
