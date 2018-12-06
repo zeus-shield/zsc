3afd280c61a5c85758e3a13e9a07772f25b550d8
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-12-06 09:15:22 +0800

    add LogisticsCore for WebTest/sol/logistics/multiple/logistics_analytics.sol

--- WebTest/sol/logistics/multiple/logistics_analytics.sol
+++ WebTest/sol/logistics/multiple/logistics_analytics.sol
@@ -10,6 +10,13 @@ import "../../utillib/LibString.sol";
 // import "../../utillib/LibInt.sol";
 // import "../../common/delegate.sol";
 
+contract LogisticsCore {
+    function number() external view returns (uint);
+    function numberOfTracks(string _num) external view returns (uint);
+    function getNumByIndex(uint _index) external view returns (string);
+    function getTrackElementByIndex(string _num, uint _index, string _elementType) external view returns (string);
+    function getBriefByIndex(uint _index) external view returns (string, string, string, uint16, uint8);
+}
 
 contract logisticsAnalytics {
 
