add451bba5e0f423c78e869a7c51e9530e2eca22
scy-aojiao <1652336252@qq.com>
2018-12-07 14:40:48 +0800

    update require for index.js

--- Patch/jnet/user_admin_front/config/index.js
+++ Patch/jnet/user_admin_front/config/index.js
@@ -21,3 +21,10 @@ module.exports = {
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
   },
+  dev: {
+    env: require('./dev.env'),
+    port: 8080,
+    autoOpenBrowser: true,
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {},
