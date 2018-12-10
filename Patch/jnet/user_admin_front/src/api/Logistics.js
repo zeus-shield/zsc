35396af874c8f1c781f786d79c5acf48ad25d60c
scy-aojiao <1652336252@qq.com>
2018-12-10 10:54:39 +0800

    add class 'LogisticsCore' for Logistics.js

--- Patch/jnet/user_admin_front/src/api/Logistics.js
+++ Patch/jnet/user_admin_front/src/api/Logistics.js
@@ -13,3 +13,10 @@ const contractAddress = Symbol('contractAddress');
 
 const web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/0a2f1fc6246240f0b1e12aec6f3d8ed7"));
 window.web3 = web3;
+
+export default class LogisticsCore {
+    constructor(_abi, _contractAddr) {
+        this[account] = "0x6A76fb522F948CfB7d6d929408D6aD5876E7139F";
+        this[contractAbi] = _abi;
+        this[contractAddress] = _contractAddr; 
+    }
