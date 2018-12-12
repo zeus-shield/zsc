3902b6768427437c2946c780db0bf76a4a9dbe67
swedenwolf <xmuguoyang@gmail.com>
2018-12-12 15:24:56 +0800

    added function index() for 'Channel.php'

--- Patch/zscdemo/application/admin/controller/Channel.php
+++ Patch/zscdemo/application/admin/controller/Channel.php
@@ -10,4 +10,29 @@ class Channel extends Admin {
 		parent::_initialize();
 	}
 
+	public function index($type = 0) {
+		/* 获取频道列表 */
+		//$map  = array('status' => array('gt', -1), 'pid'=>$pid);
+		$map  = array('status' => array('gt', -1));
+		if ($type) {
+			$map['type']   = $type;
+		}
+		$list = db('Channel')->where($map)->order('sort asc,id asc')->column('*', 'id');
+
+		if (!empty($list)) {
+			$tree = new \com\Tree();
+			$list = $tree->toFormatTree($list);
+		}
+
+		config('_sys_get_channel_tree_', true);
+
+		$data = array(
+			'tree' => $list,
+			'type' => $type
+		);
+		$this->assign($data);
+		$this->setMeta('导航管理');
+		return $this->fetch();
+	}
+
 }
