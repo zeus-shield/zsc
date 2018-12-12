c27440141cf530103d23d176b2935075bb95672e
swedenwolf <xmuguoyang@gmail.com>
2018-12-12 15:26:10 +0800

    added function add() for 'Channel.php'

--- Patch/zscdemo/application/admin/controller/Channel.php
+++ Patch/zscdemo/application/admin/controller/Channel.php
@@ -42,4 +42,37 @@ class Channel extends Admin {
 		}
 	}
 
+	public function add() {
+		if (IS_POST) {
+			$Channel = model('Channel');
+			$data    = $this->request->post();
+			if ($data) {
+				$id = $Channel->save($data);
+				if ($id) {
+					return $this->success('新增成功', url('index'));
+					//记录行为
+					action_log('update_channel', 'channel', $id, session('user_auth.uid'));
+				} else {
+					return $this->error('新增失败');
+				}
+			} else {
+				$this->error($Channel->getError());
+			}
+		} else {
+			$pid = input('pid', 0);
+			//获取父导航
+			if (!empty($pid)) {
+				$parent = db('Channel')->where(array('id' => $pid))->field('title')->find();
+				$this->assign('parent', $parent);
+			}
+
+			$pnav = db('Channel')->where(array('pid' => '0'))->select();
+			$this->assign('pnav', $pnav);
+			$this->assign('pid', $pid);
+			$this->assign('info', null);
+			$this->setMeta('新增导航');
+			return $this->fetch('edit');
+		}
+	}
+
 }
