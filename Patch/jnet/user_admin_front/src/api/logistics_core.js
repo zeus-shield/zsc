4b7c596bb277d128f45769c9f313b259843844b8
scy-aojiao <1652336252@qq.com>
2018-12-14 11:52:51 +0800

    add function 'updateBriefEx' for logistics_core.js

--- Patch/jnet/user_admin_front/src/api/logistics_core.js
+++ Patch/jnet/user_admin_front/src/api/logistics_core.js
@@ -79,3 +79,23 @@ export default class LogisticsCore {
             }
         });
     }
+
+    updateBriefEx(_account, _key, _num, _brief, _func) {
+        let handler = this;
+        let contractInstance = web3.eth.contract(this[contractAbi]).at(this[contractAddress]);
+        let data = contractInstance.updateBriefEx.getData(_num, _brief);
+
+        contractInstance.updateBriefEx.estimateGas(_num, _brief, {from: _account}, function(error, result) {
+            if (!error) {
+                let transaction = new Transaction(_account, _key);
+                if('undefined' != typeof transaction) {
+                    transaction.do("transaction", data, result, handler[contractAddress], _func);
+                }
+            } else {
+                console.log(error);
+                if (null != _func) {
+                    _func(error);
+                }
+            }
+        });
+    }
