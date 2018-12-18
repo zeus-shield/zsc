159cbb198f89aeaf921f6cc3feca068d5dd0a0ec
swedenwolf <xmuguoyang@gmail.com>
2018-12-18 16:52:54 +0800

    added function edit() for 'Model.php'

--- Patch/zscdemo/application/admin/controller/Model.php
+++ Patch/zscdemo/application/admin/controller/Model.php
@@ -40,4 +40,61 @@ class Model extends Admin {
 		return $this->fetch();
 	}	
 
+	public function edit() {
+		$id = input('id', '', 'trim,intval');
+		if (empty($id)) {
+			return $this->error('参数不能为空！');
+		}
+
+		/*获取一条记录的详细数据*/
+		$model = model('Model');
+		$data  = $model::find($id);
+		if (!$data) {
+			return $this->error($Model->getError());
+		}
+		$data['attribute_list'] = empty($data['attribute_list']) ? '' : explode(",", $data['attribute_list']);
+
+		// 是否继承了其他模型
+		if ($data['extend'] == 1) {
+			$map['model_id'] = array('IN', array($data['id'], $data['extend']));
+		} else {
+			$map['model_id'] = $data['id'];
+		}
+		$map['is_show'] = 1;
+		$fields         = db('Attribute')->where($map)->select();
+
+		// 梳理属性的可见性
+		foreach ($fields as $key => $field) {
+			if (!empty($data['attribute_list']) && !in_array($field['id'], $data['attribute_list'])) {
+				$field['is_show'] = 0;
+			}
+			$field['group']           = -1;
+			$field['sort']            = 0;
+			$fields_tem[$field['id']] = $field;
+		}
+
+		// 获取模型排序字段
+		$field_sort = json_decode($data['field_sort'], true);
+		if (!empty($field_sort)) {
+			foreach ($field_sort as $group => $ids) {
+				foreach ($ids as $key => $value) {
+					if (!empty($fields_tem[$value])) {
+						$fields_tem[$value]['group'] = $group;
+						$fields_tem[$value]['sort']  = $key;
+					}
+				}
+			}
+		}
+
+		if (isset($fields_tem) && $fields_tem) {
+			// 模型字段列表排序
+			$fields = list_sort_by($fields_tem, "sort");
+		}
+
+		$this->assign('fields', $fields);
+		$this->assign('info', $data);
+		$this->setMeta('编辑模型');
+		return $this->fetch();
+	}	
+
 }
