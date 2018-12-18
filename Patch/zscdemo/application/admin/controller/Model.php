1bc7d2eb4dc03b944fdf4bce9ee617f0071a7baf
swedenwolf <xmuguoyang@gmail.com>
2018-12-18 16:50:50 +0800

    added function index() for 'Model.php'

--- Patch/zscdemo/application/admin/controller/Model.php
+++ Patch/zscdemo/application/admin/controller/Model.php
@@ -10,4 +10,24 @@ class Model extends Admin {
 		parent::_initialize();
 		$this->getContentMenu();
 	}
+
+	public function index() {
+		$map = array('status' => array('gt', -1));
+
+		$order = "id desc";
+		$list  = model('Model')->where($map)->order($order)->paginate(10);
+
+		$data = array(
+			'list' => $list,
+			'page' => $list->render(),
+		);
+
+		// 记录当前列表页的cookie
+		Cookie('__forward__', $_SERVER['REQUEST_URI']);
+
+		$this->assign($data);
+		$this->setMeta('模型管理');
+		return $this->fetch();
+	}
+	
 }
