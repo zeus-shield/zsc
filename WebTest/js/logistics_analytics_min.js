f0249fbb2828d6863311f294e5aaf73549a685a8
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-12-19 17:43:49 +0800

    add WebTest/js/logistics_analytics_min.js

---
+++ WebTest/js/logistics_analytics_min.js
@@ -0,0 +1,88 @@
+/*
+ Copyright (c) 2018 ZSC Dev Team
+*/
+
+import Receipt from './receipt.js';
+import Transaction from './transaction_raw.js';
+
+// private member
+const account = Symbol('account');
+
+const contractAbi = Symbol('contractAbi');
+const contractAddress = Symbol('contractAddress');
+
+export default class LogisticsAnalytics {
+    constructor(_abi, _contractAddr) {
+        this[account] = web3.eth.coinbase;
+        this[contractAbi] = _abi;
+        this[contractAddress] = _contractAddr; 
+    }
+
+    update(_account, _key, _num, _parcel, _func) {
+        let handler = this;
+        let contractInstance = web3.eth.contract(this[contractAbi]).at(this[contractAddress]);
+        let data = contractInstance.update.getData(_num, _parcel);
+
+        contractInstance.update.estimateGas(_num, _parcel, {from: _account}, function(error, result) {
+            if (!error) {
+                let transaction = new Transaction(_account, _key);
+                if('undefined' != typeof transaction) {
+                    transaction.do("transaction", data, result, handler[contractAddress], _func);
+                }
+            } else {
+                console.log(error);
+                if (null != _func) {
+                    _func(error);
+                }
+            }
+        });
+    }
+
+    get(_index, _count, _func) {
+        let handler = this;
+        let contractInstance = web3.eth.contract(this[contractAbi]).at(this[contractAddress]);
+
+        // estimate gas
+        // The MetaMask Web3 object does not support synchronous methods without a callback parameter
+        contractInstance.get.estimateGas(_index, _count, {from: this[account]}, function(error, result) {
+            if(!error) {
+                let gasRequired = result;
+                // get gas price
+                // MetaMask Web3 object does not support synchronous methods without a callback parameter
+                web3.eth.getGasPrice(function(error, result) {
+                    if(!error) {
+                        console.log("============= LogisticsAnalytics.get(uint, uint) =============");
+                        console.log("from:    ", handler[account]);
+                        console.log("gas:     ", gasRequired);
+                        console.log("gasPrice:", result.toString(10));
+                        console.log("==============================================================");
+                        // call 'LogisticsCore.get(uint, uint)'
+                        contractInstance.get.call(_index, _count, {from: handler[account], gas: gasRequired, gasPrice: result}, function(error, result) { 
+                            if(!error) {
+                                console.log("[Parcels]: %s", result);
+                                if (null != _func) {
+                                    _func(null, result);
+                                }
+                            } else {
+                                console.log(error);
+                                if (null != _func) {
+                                    _func(error);
+                                }
+                            }
+                        });
+                    } else {
+                        console.log(error);
+                        if (null != _func) {
+                            _func(error);
+                        }
+                    }
+                });
+            } else {
+                console.log(error);
+                if (null != _func) {
+                    _func(error);
+                }
+            }
+        });
+    }
+}
