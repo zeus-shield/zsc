089c6a5cb27ec189fd316bdd3ce19f1fcbaafc0f
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-12-20 18:00:16 +0800

    add function 'swap' for WebTest/sol/common/container.sol

--- WebTest/sol/common/container.sol
+++ WebTest/sol/common/container.sol
@@ -30,6 +30,23 @@ contract Container {
     function update(bytes32 _key, string _data) external {
     }
 
+    function swap(bytes32 _key1, bytes32 _key2) external {
+        uint id1 = 0;
+        uint id2 = 0;
+
+        require(exists_[_key1]);
+        require(exists_[_key2]);
+
+        id1 = ids_[_key1];
+        id2 = ids_[_key2];
+
+        keys_[id1] = _key2;
+        keys_[id2] = _key1;
+
+        ids_[_key1] = id2;
+        ids_[_key2] = id1;
+    }
+
     function remove(bytes32 _key) external {
         bytes32 key2 = bytes32(0);
 
