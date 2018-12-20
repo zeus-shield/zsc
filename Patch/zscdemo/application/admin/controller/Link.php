68faf40f1cb6ee1dfeb6d95c8872a5384c8b0210
swedenwolf <xmuguoyang@gmail.com>
2018-12-20 20:12:52 +0800

    added function add() for 'Link.php'

--- Patch/zscdemo/application/admin/controller/Link.php
+++ Patch/zscdemo/application/admin/controller/Link.php
@@ -20,4 +20,30 @@ class Link extends Admin {
 		$this->setMeta("友情链接");
 		return $this->fetch();
 	}
+
+	
+	public function add() {
+		$link = model('Link');
+		if (IS_POST) {
+			$data = input('post.');
+			if ($data) {
+				unset($data['id']);
+				$result = $link->save($data);
+				if ($result) {
+					return $this->success("添加成功！", url('Link/index'));
+				} else {
+					return $this->error($link->getError());
+				}
+			} else {
+				return $this->error($link->getError());
+			}
+		} else {
+			$data = array(
+				'keyList' => $link->keyList,
+			);
+			$this->assign($data);
+			$this->setMeta("添加友链");
+			return $this->fetch('public/edit');
+		}
+	}
 }
