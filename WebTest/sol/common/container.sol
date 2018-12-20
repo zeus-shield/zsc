dd7bd9d6660344fd8060be1e04204a207f487c3f
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-12-20 13:38:23 +0800

    add WebTest/sol/common/container.sol

---
+++ WebTest/sol/common/container.sol
@@ -0,0 +1,45 @@
+/**
+  Copyright (c) 2018, ZSC Dev Team
+  2018-10-19: v0.00.01
+ */
+
+pragma solidity ^0.4.25;
+// pragma experimental ABIEncoderV2;
+
+contract Container {
+
+    struct Data {
+        uint8 type_;
+        bytes32 dataBytes32_;
+        string dataString_;
+    }
+
+    uint sum_;
+    mapping (uint => bytes32) private keys_;
+    mapping (bytes32 => uint) private ids_;
+    mapping (bytes32 => bool) private exists_;
+    mapping (bytes32 => Data) private datas_;
+
+    constructor() public {
+        sum_ = 0;
+    }
+
+    function update(bytes32 _key, bytes32 _data) external {
+    }
+
+    function update(bytes32 _key, string _data) external {
+    }
+
+    function remove(bytes32 _key) external {
+    }
+
+    function number() external view returns (uint) {
+        return sum_;
+    }
+
+    function get(bytes32 _key) external view returns (bytes32, string) {
+    }
+
+    function get(uint _id) external view returns (bytes32, string) {
+    }
+}
