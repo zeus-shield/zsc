d99786aaa7f8aa365663c5a27f993c4f0ecedbc0
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-12-21 19:18:11 +0800

    add WebTest\run\compile_logistics.sh

---
+++ WebTest/run/compile_logistics.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SFLAGS="--optimize --overwrite --pretty-json --combined-json bin,abi"
+CORE="sol"
+MODULE="logistics"
+# MODE="single"
+MODE="multiple"
+IMPORTPATH="/=/"
+SOURCES=(
+logistics_core
+logistics_analytics
+logistics_analytics_min
+logistics
+)
+BUILDPATH="../build"
+
+for((i=0;i<${#SOURCES[@]};i++));
+do
+SOURCE=${SOURCES[i]}".sol";
+echo "########## compiling "${SOURCE}" ##########"
+
+# Compiling to JSON file(build/xxx/combined.json)
+# solc ${SFLAGS} ${IMPORTPATH} ./${CORE}/${MODULE}/${MODE}/${SOURCE} -o ${BUILDPATH}/${MODULE}/${MODE}/${SOURCES[i]}
+solc ${SFLAGS} ${IMPORTPATH} ../${CORE}/${MODULE}/${MODE}/${SOURCE} -o ${BUILDPATH}/${MODULE}/${MODE}
+mv ${BUILDPATH}/${MODULE}/${MODE}/combined.json ${BUILDPATH}/${MODULE}/${MODE}/${SOURCES[i]}.json
+
+
+# Compiling to JS file(WebAdm/js/compiled_xxx.js)
+# COMPILED="`solc ${SFLAGS} ./${CORE}/${SOURCE}`"
+# echo "########## compiled "${COMPILED}" ##########"
+# echo "var compiled_"${SOURCES[i]}"=${COMPILED}" > ./compiled/"${SOURCES[i]}".js
+done
+
+read -p "Press any key to continue."
