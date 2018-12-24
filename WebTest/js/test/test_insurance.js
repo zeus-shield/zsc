113b5c608d4ebdfd3a4dc9bf74a8f79b7362760a
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-12-24 11:24:03 +0800

    up WebTest/js/test/test_insurance.js

--- WebTest/js/test/test_insurance.js
+++ WebTest/js/test/test_insurance.js
@@ -3,10 +3,10 @@
   2018-10-19: v0.00.01
  */
 
-import Output from '../common/output.js';
+import Output from "../common/output.js";
 import Transaction from '../common/transaction_raw.js';
 import Delegate from '../common/delegate.js';
-import Logistics from '../insurance/insurance_template.js';
+import InsuranceTemplate from '../insurance/insurance_template.js';
 
 //private member
 const compiledJson = Symbol('compiledJson');
@@ -17,6 +17,7 @@ const templateContractAddress = Symbol('templateContractAddress');
 //private function
 const getAccount = Symbol('getAccount');
 const transactionProc = Symbol('transactionProc');
+const hexToString = Symbol('hexToString');
 
 export default class TestInsurance {
 
@@ -25,7 +26,7 @@ export default class TestInsurance {
 
         this[templateAbi] = [];
 
-        this[templateContractAddress] = '';
+        this[templateContractAddress] = "";
     }
 
     [getAccount]() { 
@@ -52,31 +53,50 @@ export default class TestInsurance {
             } else {
                 let status = "Try to get status again!";
                 let string = `[TransactionHash]:${result.transactionHash}</br>[Status]:${status}</br>[Try]:${result.tryTimes}(times)`;
-                Output(output, 'small', 'red', string);
+                Output(output, "small", "red", string);
             }
         } else {
-            Output(output, 'small', 'red', error);
+            Output(output, "small", "red", error);
         }
     }
+
+    [hexToString](str) {
+        let value = "";
+        let byte = "";
+        let code = "";
+
+        str = str.substr(str.indexOf("0x")+2);
+
+        for(let i=0; i<str.length/2; i++) {
+            byte = str.substring(i*2, i*2+2);
+            code = parseInt(byte, 16);
+            if ("0" == code) {
+                break;
+            }
+            value += String.fromCharCode(code);
+        }
+
+        return value;
+  }
 
     setCompiledJson(data) {
         this[compiledJson] = JSON.parse(data);
     }
 
     deploy(contractName) {
-        console.log('TestInsurance.deploy(%s)', contractName);
+        console.log("TestInsurance.deploy(%s)", contractName);
         let elementId;
 
-        if ('InsuranceTemplate' == contractName) {
+        if ("InsuranceTemplate" == contractName) {
             elementId = window.outputDeployTemplateElement;
         } else {
-            console.log('Contract name Error!');
+            console.log("Contract name Error!");
             return;
         }
 
         let tmps = this[getAccount]();
         if (0 == tmps[0]) {
-            Output(elementId, 'small', 'red', "No channnel(idle)!");
+            Output(elementId, "small", "red", "No channnel(idle)!");
             return;
         }
 
@@ -103,21 +123,21 @@ export default class TestInsurance {
         }
 
         if (!found) {
-            Output(elementId, 'small', 'red', "JSON file error！");
+            Output(elementId, "small", "red", "JSON file error！");
             return;
         }
 
         if ('' == this[compiledJson].contracts[fullName].bin) {
-            Output(elementId, 'small', 'red', "Bin is null in json file!");
+            Output(elementId, "small", "red", "Bin is null in json file!");
             return;
         }     
 
-        byteCode = '0x' + this[compiledJson].contracts[fullName].bin;
-        if ('InsuranceTemplate' == contractName) {
+        byteCode = "0x" + this[compiledJson].contracts[fullName].bin;
+        if ("InsuranceTemplate" == contractName) {
             this[templateAbi] = JSON.parse(this[compiledJson].contracts[fullName].abi);
             contract = web3.eth.contract(this[templateAbi]);
         } else {
-            console.log('Contract name Error!');
+            console.log("Contract name Error!");
             return;
         }
 
@@ -128,50 +148,92 @@ export default class TestInsurance {
         web3.eth.estimateGas({data: data}, function(error, result) {
             if (!error) {
                 transaction = new Transaction(account, key);
-                if('undefined' != typeof transaction) {
+                if ("undefined" != typeof transaction) {
                     transaction.do("deploy", data, result, null, function(error, result) {
                         if (!error) {
-                            if ('InsuranceTemplate' == contractName) {
+                            if ("InsuranceTemplate" == contractName) {
                                 handler[templateContractAddress] = result.contractAddress;
                             } else {
-                                console.log('Contract name Error!');
+                                console.log("Contract name Error!");
                                 return;
                             }
                             let string = `[TransactionHash]:${result.transactionHash}</br>[ContractAddress]:${result.contractAddress}</br>[Try]:${result.tryTimes}(times)`;
-                            Output(elementId, 'small', 'red', string);
+                            Output(elementId, "small", "red", string);
                         } else {
-                            Output(elementId, 'small', 'red', error);
+                            Output(elementId, "small", "red", error);
                         }
                     });
                 }
             } else {
-                Output(elementId, 'small', 'red', error);
+                Output(elementId, "small", "red", error);
             }
         });
     }
 
-    create(module, params) {
-        console.log('TestInsurance.create(%s, %s)', module, params);
-    }
+    template(operation, params) {
+        console.log("TestInsurance.template(%s, %s)", operation, params);
 
-    Get(module, params) {
-        console.log('TestInsurance.create(%s, %s)', module, params);
+        // check param
+        if (("" == operation) || ("" == params)) {
+            Output(window.outputTemplateElement, "small", "red", "Please input correct input!");
+            return;
+        }
+
+        let handler = this;
+        let tmps = this[getAccount]();
+        if (0 == tmps[0]) {
+            Output(window.outputDelegateWriteElement, 'small', 'red', "No channnel(idle)!");
+            return;
+        }
+
+        let account = tmps[0];
+        let key = tmps[1];
+
+        let insuranceTemplate;
+        switch (operation) {
+            case "Create":
+                tmps = params.split(",");
+                let name = tmps[0];
+                let data = tmps[1];
+
+                if ((undefined == name) || (undefined == data)) {
+                    Output(window.outputTemplateElement, "small", "red", "Please input correct params!");
+                    return;
+                }
+
+                insuranceTemplate = new InsuranceTemplate(this[templateAbi], this[templateContractAddress]);
+                insuranceTemplate.create(account, key, name, data, function(error, result) {
+                    handler[transactionProc](error, result, window.outputTemplateElement);
+                });
+                break;
+            case "Get":
+                insuranceTemplate = new InsuranceTemplate(this[templateAbi], this[templateContractAddress]);
+                insuranceTemplate.get(params, function(error, result) {
+                    if (!error) {
+                        let name = handler[hexToString](result[0]);
+                        Output(window.outputTemplateElement, "small", "red", `[Template]:</br>${name},${result[1]}`);
+                    } else {
+                        Output(window.outputTemplateElement, "small", "red", error);
+                    }
+                });
+                break;
+            default:
+                Output(window.outputTemplateElement, "small", "red", "Operation Error!");
+                break;
+        }
     }
 
     do(operation, para1, para2) {
-        console.log('TestInsurance.do(%s, %s, %s)', operation, para1, para2);
+        console.log("TestInsurance.do(%s, %s, %s)", operation, para1, para2);
         switch(operation) {
-            case 'Deploy':
+            case "Deploy":
                 this.deploy(para1);
                 break;
-            case 'Create':
-                this.create(para1, para2);
-                break;
-            case 'Get':
-                this.create(para1, para2);
+            case "Template":
+                this.template(para1, para2);
                 break;
             default:
-                console.log('Operation Error!');
+                console.log("Operation Error!");
                 break;
         }
     }
