6cd4eadb4e780a550dcc6fceedb1ead5f9cbd23a
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-12-26 15:51:33 +0800

    up WebTest/js/main.js

--- WebTest/js/main.js
+++ WebTest/js/main.js
@@ -10,10 +10,7 @@ require.config({
 require(['test_search', 'test_analysis'], function (){});
 */
 import TestSearch from './test/test_search.js';
-import TestRaw from './test/test_raw.js';
 import TestLogistics from './test/test_logistics.js';
-import TestLogisticsRaw from './test/test_logistics_raw.js';
-import TestAnalysis from './test/test_analysis.js';
 import TestInsurance from './test/test_insurance.js';
 import TestDemo from './test/test_demo.js';
 
@@ -32,15 +29,6 @@ export default function main(module, param1, param2, param3) {
             }
             test.setContractName('AISearch');
             break;
-        case 'Raw':
-            if('undefined' == typeof window.testRawClass) {
-                test = new TestRaw();
-                window.testRawClass = test;
-            } else {
-                test = window.testRawClass;
-            }
-            test.setContractName('Sample');
-            break;
         case 'Logistics':
             if('undefined' == typeof window.testLogisticsClass) {
                 test = new TestLogistics();
@@ -48,25 +36,7 @@ export default function main(module, param1, param2, param3) {
             } else {
                 test = window.testLogisticsClass;
             }
-            test.setContractName('Logistics');
-            break;
-        case 'LogisticsRaw':
-            if('undefined' == typeof window.testLogisticsRawClass) {
-                test = new TestLogisticsRaw();
-                window.testLogisticsRawClass = test;
-            } else {
-                test = window.testLogisticsRawClass;
-            }
             // test.setContractName('Logistics');
-            break;
-        case 'Analysis':
-            if('undefined' == typeof window.testAnalysisClass) {
-                test = new TestAnalysis();
-                window.testAnalysisClass = test;
-            } else {
-                test = window.testAnalysisClass;
-            }
-            test.setContractName('AIAnalysis');
             break;
         case 'Insurance':
             if('undefined' == typeof window.testInsuranceClass) {
