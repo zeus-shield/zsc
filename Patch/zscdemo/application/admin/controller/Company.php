cbdbce629b144505f6c55830ac4e58ac31bce2ff
swedenwolf <xmuguoyang@gmail.com>
2018-12-26 15:18:30 +0800

    added function index() for 'Comapny.php'

--- Patch/zscdemo/application/admin/controller/Company.php
+++ Patch/zscdemo/application/admin/controller/Company.php
@@ -12,4 +12,19 @@ class Company extends Admin {
     	parent::_initialize();
         $this->model = model('Users');
     }
+
+	public function index($id=null) {
+		$list = $this->model->where('type',1)->paginate(15);
+
+		$data = array(
+			'list' => $list,
+			'page' => $list->render(),
+		);
+
+		$this->assign($data);
+
+		$this->setMeta('公司信息');
+
+		return $this->fetch('index');
+	}    
 }
