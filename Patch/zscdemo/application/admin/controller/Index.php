a060e6fe80f41b5a6f6a183eea9cd9d0df1b12b1
swedenwolf <xmuguoyang@gmail.com>
2018-12-26 15:32:34 +0800

    added function login() for 'Index.php'

--- Patch/zscdemo/application/admin/controller/Index.php
+++ Patch/zscdemo/application/admin/controller/Index.php
@@ -11,4 +11,33 @@ class Index extends Admin {
 		return $this->fetch();
 	}
 
+	public function login($username = '', $password = '', $verify = '') {
+		if (IS_POST) {
+			if (!$username || !$password) {
+				return $this->error('用户名或者密码不能为空！', '');
+			}
+
+			//验证码验证
+			$this->checkVerify($verify);
+
+			$user = model('User');
+			$uid  = $user->login($username, $password);
+			if ($uid > 0) {
+				return $this->success('登录成功！', url('admin/index/index'));
+			} else {
+				switch ($uid) {
+				case -1:$error = '用户不存在或被禁用！';
+					break; //系统级别禁用
+				case -2:$error = '密码错误！';
+					break;
+				default:$error = '未知错误！';
+					break; // 0-接口参数错误（调试阶段使用）
+				}
+				return $this->error($error, '');
+			}
+		} else {
+			return $this->fetch();
+		}
+	}	
+
 }
