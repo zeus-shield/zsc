3761d004fad94aa55cc3e99b1679ee76b93b2e96
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-12-27 16:44:26 +0800

    up WebTest/sol/insurance/insurance_template.sol

--- WebTest/sol/insurance/insurance_template.sol
+++ WebTest/sol/insurance/insurance_template.sol
@@ -32,14 +32,14 @@ contract InsuranceTemplate is Ownable {
     function() external payable { revert(); }
 
     /** [desc] update template.
-      * [param] _name: name of template.
+      * [param] _key: key of template.
       * [param] _data: data of template.
       * [return] none.
       */
-    function update(bytes32 _name, string _data) external _onlyOwner {
+    function update(string _key, string _data) external _onlyOwner {
         // check param
-        require(bytes32(0) != _name);
-        Hashmap(tempMgr_).set(_name, bytes32(0), _data, address(0), uint(0));
+        require(0 != bytes(_key).length);
+        Hashmap(tempMgr_).set(_key, bytes32(0), _data, address(0), uint(0));
     }
 
     /** [desc] Get size of templates.
@@ -52,46 +52,46 @@ contract InsuranceTemplate is Ownable {
 
     /** [desc] Get template by id.
       * [param] _id: _id of template.
-      * [return] error code and template name/data.
+      * [return] error code and template key/data.
       *           0: success
       *          -1: params error
       *          -2: no data
       *          -3: inner error
       */
-    function getById(uint _id) external view returns (int, bytes32, string) {
+    function getById(uint _id) external view returns (int, string, string) {
         // check param
         if (Hashmap(tempMgr_).size() <= _id) {
-            return (-1, bytes32(0), "");
+            return (-1, "", "");
         }
 
         int error = 0;
-        bytes32 name = bytes32(0);
+        string memory key = "";
         bytes32 data0 = bytes32(0);
         string memory data1 = "";
         address data2 = address(0);
         uint data3 = uint(0);
 
-        (error, name, data0, data1, data2, data3) = Hashmap(tempMgr_).get(_id);
+        (error, key, data0, data1, data2, data3) = Hashmap(tempMgr_).get(_id);
         
-        return (error, name, data1);
+        return (error, key, data1);
     }
 
-    /** [desc] Get template by name.
-      * [param] _name: name of template.
+    /** [desc] Get template by key.
+      * [param] _key: key of template.
       * [return] error code and template data.
       *           0: success
       *          -1: params error
       *          -2: no data
       *          -3: inner error
       */
-    function getByName(bytes32 _name) external view returns (int, string) {
+    function getByKey(string _key) external view returns (int, string) {
         int error = 0;
         bytes32 data0 = bytes32(0);
         string memory data1 = "";
         address data2 = address(0);
         uint data3 = uint(0);
 
-        (error, data0, data1, data2, data3) = Hashmap(tempMgr_).get(_name);
+        (error, data0, data1, data2, data3) = Hashmap(tempMgr_).get(_key);
         
         return (error, data1);
     }
