1ed93e2f6c2aa9ae8c791da35d9619e9bee483d1
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-12-27 19:38:44 +0800

    add WebTest/js/insurance/insurance_policy.js

---
+++ WebTest/js/insurance/insurance_policy.js
@@ -0,0 +1,216 @@
+/**
+  Copyright (c) 2018, ZSC Dev Team
+  2018-10-19: v0.00.01
+ */
+
+import Receipt from '../common/receipt.js';
+import Transaction from '../common/transaction_raw.js';
+
+// private member
+const account = Symbol('account');
+const contractAbi = Symbol('contractAbi');
+const contractAddress = Symbol('contractAddress');
+
+//private function
+const transactionProc = Symbol('transactionProc');
+const notifyError = Symbol('notifyError');
+
+export default class InsurancePolicy {
+    constructor(abi, contractAddr) {
+        this[account] = web3.eth.coinbase;
+        this[contractAbi] = abi;
+        this[contractAddress] = contractAddr; 
+    }
+
+    [notifyError](error, func) {
+        console.log(error);
+        if (null != func) {
+            func(error);
+        }
+    }
+
+    [transactionProc](handler, account, privateKey, data, error, gasRequired, func) {
+        if (!error) {
+            let transaction = new Transaction(account, privateKey);
+            if('undefined' != typeof transaction) {
+                transaction.do("transaction", data, gasRequired, handler[contractAddress], func);
+            }
+        } else {
+            handler[notifyError](error, func);
+        }
+    }
+
+    setup(account, privateKey, templateAddr, func) {
+        let handler = this;
+        let contractInstance = web3.eth.contract(this[contractAbi]).at(this[contractAddress]);
+
+        contractInstance.setup.estimateGas(templateAddr, {from: account}, function(error, gasRequired) {
+            handler[transactionProc](handler, account, privateKey, contractInstance.setup.getData(templateAddr), error, gasRequired, func);
+        });
+    }
+
+    submit(account, privateKey, templateKey, data, func) {
+        let handler = this;
+        let contractInstance = web3.eth.contract(this[contractAbi]).at(this[contractAddress]);
+
+        contractInstance.submit.estimateGas(templateKey, data, {from: account}, function(error, gasRequired) {
+            handler[transactionProc](handler, account, privateKey, contractInstance.submit.getData(templateKey, data), error, gasRequired, func);
+        });
+    }
+
+    size(func) {
+        let handler = this;
+        let contractInstance = web3.eth.contract(this[contractAbi]).at(this[contractAddress]);
+
+        // estimate gas
+        // The MetaMask Web3 object does not support synchronous methods without a callback parameter
+        contractInstance.size.estimateGas({from: this[account]}, function(error, result) {
+            if(!error) {
+                let gasRequired = result;
+                // get gas price
+                // MetaMask Web3 object does not support synchronous methods without a callback parameter
+                web3.eth.getGasPrice(function(error, result) {
+                    if(!error) {
+                        console.log("=============== InsurancePolicy.size() ===============");
+                        console.log("from:    ", handler[account]);
+                        console.log("gas:     ", gasRequired);
+                        console.log("gasPrice:", result.toString(10));
+                        console.log("======================================================");
+                        // call 'InsurancePolicy.size()'
+                        contractInstance.size.call({from: handler[account], gas: gasRequired, gasPrice: result}, function(error, result) { 
+                            if(!error) {
+                                console.log("[User]: %s", result.toString(10));
+                                if (null != func) {
+                                    func(null, result);
+                                }
+                            } else {
+                                handler[notifyError](error, func);
+                            }
+                        });
+                    } else {
+                        handler[notifyError](error, func);
+                    }
+                });
+            } else {
+                handler[notifyError](error, func);
+            }
+        });
+    }
+
+    getByKey(type, key, func) {
+        let handler = this;
+        let contractInstance = web3.eth.contract(this[contractAbi]).at(this[contractAddress]);
+
+        // estimate gas
+        // The MetaMask Web3 object does not support synchronous methods without a callback parameter
+        contractInstance.getByKey.estimateGas(type, key, {from: this[account]}, function(error, result) {
+            if(!error) {
+                let gasRequired = result;
+                // get gas price
+                // MetaMask Web3 object does not support synchronous methods without a callback parameter
+                web3.eth.getGasPrice(function(error, result) {
+                    if(!error) {
+                        console.log("=============== InsurancePolicy.getByKey(uint8, string) ===============");
+                        console.log("from:    ", handler[account]);
+                        console.log("gas:     ", gasRequired);
+                        console.log("gasPrice:", result.toString(10));
+                        console.log("=======================================================================");
+                        // call 'InsurancePolicy.getByKey(uint8, string)'
+                        contractInstance.getByKey.call(type, key, {from: handler[account], gas: gasRequired, gasPrice: result}, function(error, result) { 
+                            if(!error) {
+                                console.log("[User]: %s", result);
+                                if (null != func) {
+                                    func(null, result);
+                                }
+                            } else {
+                                handler[notifyError](error, func);
+                            }
+                        });
+                    } else {
+                        handler[notifyError](error, func);
+                    }
+                });
+            } else {
+                handler[notifyError](error, func);
+            }
+        });
+    }
+
+    getById(type, id, func) {
+        let handler = this;
+        let contractInstance = web3.eth.contract(this[contractAbi]).at(this[contractAddress]);
+
+        // estimate gas
+        // The MetaMask Web3 object does not support synchronous methods without a callback parameter
+        contractInstance.getById.estimateGas(type, id, {from: this[account]}, function(error, result) {
+            if(!error) {
+                let gasRequired = result;
+                // get gas price
+                // MetaMask Web3 object does not support synchronous methods without a callback parameter
+                web3.eth.getGasPrice(function(error, result) {
+                    if(!error) {
+                        console.log("=============== InsurancePolicy.getById(uint8, uint) ===============");
+                        console.log("from:    ", handler[account]);
+                        console.log("gas:     ", gasRequired);
+                        console.log("gasPrice:", result.toString(10));
+                        console.log("====================================================================");
+                        // call 'InsurancePolicy.getById(uint8, uint)'
+                        contractInstance.getById.call(type, id, {from: handler[account], gas: gasRequired, gasPrice: result}, function(error, result) { 
+                            if(!error) {
+                                console.log("[User%s]: %s", id, result.toString(10));
+                                if (null != func) {
+                                    func(null, id, result);
+                                }
+                            } else {
+                                handler[notifyError](error, func);
+                            }
+                        });
+                    } else {
+                        handler[notifyError](error, func);
+                    }
+                });
+            } else {
+                handler[notifyError](error, func);
+            }
+        });
+    }
+
+    getTemplateAddr(func) {
+        let handler = this;
+        let contractInstance = web3.eth.contract(this[contractAbi]).at(this[contractAddress]);
+
+        // estimate gas
+        // The MetaMask Web3 object does not support synchronous methods without a callback parameter
+        contractInstance.getTemplateAddr.estimateGas({from: this[account]}, function(error, result) {
+            if(!error) {
+                let gasRequired = result;
+                // get gas price
+                // MetaMask Web3 object does not support synchronous methods without a callback parameter
+                web3.eth.getGasPrice(function(error, result) {
+                    if(!error) {
+                        console.log("=============== InsurancePolicy.getTemplateAddr() ===============");
+                        console.log("from:    ", handler[account]);
+                        console.log("gas:     ", gasRequired);
+                        console.log("gasPrice:", result.toString(10));
+                        console.log("=================================================================");
+                        // call 'InsurancePolicy.getTemplateAddr()'
+                        contractInstance.getTemplateAddr.call({from: handler[account], gas: gasRequired, gasPrice: result}, function(error, result) { 
+                            if(!error) {
+                                console.log("[Address]: %s", result);
+                                if (null != func) {
+                                    func(null, result);
+                                }
+                            } else {
+                                handler[notifyError](error, func);
+                            }
+                        });
+                    } else {
+                        handler[notifyError](error, func);
+                    }
+                });
+            } else {
+                handler[notifyError](error, func);
+            }
+        });
+    }
+}
