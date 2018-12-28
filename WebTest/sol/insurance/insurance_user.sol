1a39c3fdff3a952f5fb191882f1ea1cac03a5df4
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2018-12-28 16:37:18 +0800

    up WebTest/sol/insurance/insurance_user.sol

--- WebTest/sol/insurance/insurance_user.sol
+++ WebTest/sol/insurance/insurance_user.sol
@@ -7,6 +7,7 @@ pragma solidity ^0.4.25;
 // pragma experimental ABIEncoderV2;
 
 import "../utillib/LibString.sol";
+import "../utillib/LibInt.sol";
 import "../common/hashmap.sol";
 
 contract InsuranceTemplate {
@@ -16,6 +17,7 @@ contract InsuranceTemplate {
 contract InsuranceUser is Ownable {
 
     using LibString for *;
+    using LibInt for *;
 
     address private userMgr_;
     address private templateAddr_;
@@ -42,39 +44,52 @@ contract InsuranceUser is Ownable {
 
     /** [desc] Get user detail info.
       * [param] _user: user info address.
-      * [return] user info for json data.
+      * [return] error code and user info for json data.
       */
-    function _getDetailInfo(address _user) private view returns (string) {
+    function _getDetailInfo(address _user) private view returns (int, string) {
         string memory str = "{";
 
         uint len = Hashmap(_user).size();
         for (uint i=0; i<len; i++) {
             int error = 0;
             string memory key = "";
-            string memory value = "";
+            uint8 position = 0;
+            string memory data0 = "";
             address data1 = address(0);
             uint data2 = uint(0);
-            (error, key, value, data1, data2) = Hashmap(_user).get(i);
-            if (0 == error) {
-                if ((len -1) == i) {
-                    str = str.concat(value.toKeyValue(key));
-                } else {
-                    str = str.concat(value.toKeyValue(key), ",");
-                }
+            (error, key, position, data0, data1, data2) = Hashmap(_user).get(i);
+            if (0 != error) {
+                return (error, "{}");
+            }
+
+            if (0 == position) {
+                str = str.concat(data0.toKeyValue(key));
+            } else if (1 == position) {
+                string memory data = "0x";
+                data = data.concat(data1.addrToAsciiString());
+                str = str.concat(data.toKeyValue(key));
+            } else if (2 == position) {
+                str = str.concat(data2.toKeyValue(key));
+            } else {
+                return (-2, "{}");
+            }
+
+            if ((len -1) > i) {
+                str = str.concat(",");
             }
         }
 
         str = str.concat("}");
 
-        return str;
+        return (0, str);
     }
 
     /** [desc] Get user brief info.
       * [param] _key: user key.
       * [param] _user: user info address.
-      * [return] user info for json data.
+      * [return] error code and user info for json data.
       */
-    function _getBriefInfo(string _key, address _user) private pure returns (string) {
+    function _getBriefInfo(string _key, address _user) private pure returns (int, string) {
         string memory str = "{";
         string memory user = "0x";
 
@@ -83,7 +98,7 @@ contract InsuranceUser is Ownable {
         str = str.concat(_key.toKeyValue("Key"), ",");
         str = str.concat(user.toKeyValue("Address"), "}");
 
-        return str;
+        return (0, str);
     }
 
     /** [desc] This unnamed function is called whenever someone tries to send ether to it.
@@ -124,7 +139,7 @@ contract InsuranceUser is Ownable {
         for (uint i=0; i<keys_.length; i++) { 
             if (_data.keyExists(keys_[i])) {
                 string memory value = _data.getStringValueByKey(keys_[i]);
-                Hashmap(user).set(keys_[i], value, address(0), uint(0));
+                Hashmap(user).set(keys_[i], 0, value, address(0), uint(0));
                 
                 if (keys_[i].equals("Key")) {
                     key = value;
@@ -134,9 +149,14 @@ contract InsuranceUser is Ownable {
             }
         }
 
+        address policies = new Hashmap();
+        Hashmap(user).set("Policies", 1, "", policies, uint(0));
+        address receipts = new Hashmap();
+        Hashmap(user).set("Receipts", 1, "", receipts, uint(0)); 
+
         require(valid);
 
-        Hashmap(userMgr_).set(key, "", user, uint(0));
+        Hashmap(userMgr_).set(key, 1, "", user, uint(0));
     }
 
     /** [desc] Get size of users.
@@ -163,22 +183,23 @@ contract InsuranceUser is Ownable {
         }
 
         int error = 0;
+        uint position = 0;
         string memory data0 = "";
         address user = address(0);
         uint data2 = uint(0);
-        (error, data0, user, data2) = Hashmap(userMgr_).get(_key);
+        (error, position, data0, user, data2) = Hashmap(userMgr_).get(_key);
         if (0 != error) {
             return (error, "{}");
         }
-
-        string memory str = "";
-        if (0 == _type) {
-            str = _getDetailInfo(user);
-        } else {
-            str = _getBriefInfo(_key, user);
+        if (1 != position) {
+            return (-2, "{}");
         }
 
-        return (0, str);
+        if (0 == _type) {
+            return _getDetailInfo(user);
+        } else {
+            return _getBriefInfo(_key, user);
+        }
     }
 
     /** [desc] Get user info by id.
@@ -198,26 +219,26 @@ contract InsuranceUser is Ownable {
 
         int error = 0;
         string memory key = "";
+        uint position = 0;
         string memory data0 = "";
         address user = address(0);
         uint data2 = uint(0);
-        (error, key, data0, user, data2) = Hashmap(userMgr_).get(_id);
+        (error, key, position, data0, user, data2) = Hashmap(userMgr_).get(_id);
         if (0 != error) {
             return (error, "{}");
         }
-
-        string memory str = "";
-
-        if (0 == _type) {
-            str = _getDetailInfo(user);
-        } else {
-            str = _getBriefInfo(key, user);
+        if (1 != position) {
+            return (-2, "{}");
         }
 
-        return (0, str);
+        if (0 == _type) {
+            return  _getDetailInfo(user);
+        } else {
+            return _getBriefInfo(key, user);
+        }
     }
 
-    function getTemplateAddr() external view _onlyOwner returns (address) {
+    function getAddr() external view _onlyOwner returns (address) {
         return templateAddr_;
     }
 }
