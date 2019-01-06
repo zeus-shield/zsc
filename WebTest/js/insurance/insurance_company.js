d3d8cbaafb35c6a68fdff59e0968d72a9c8a6557
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-01-06 16:59:31 +0800

    add WebTest/js/insurance/insurance_company.js

---
+++ WebTest/js/insurance/insurance_company.js
@@ -0,0 +1,57 @@
+/**
+  Copyright (c) 2018, ZSC Dev Team
+  2018-10-19: v0.00.01
+ */
+
+import Receipt from '../common/receipt.js';
+import Transaction from '../common/transaction_raw.js';
+
+// private member
+const account = Symbol('account');
+const contractAbi = Symbol('contractAbi');
+const contractAddress = Symbol('contractAddress');
+
+//private function
+const transactionProc = Symbol('transactionProc');
+const notifyError = Symbol('notifyError');
+
+export default class InsuranceCompany {
+    constructor(abi, contractAddr) {
+        this[account] = web3.eth.coinbase;
+        this[contractAbi] = abi;
+        this[contractAddress] = contractAddr; 
+    }
+
+    [notifyError](error, func) {
+        console.log(error);
+        if (null != func) {
+            func(error);
+        }
+    }
+
+    [transactionProc](handler, account, privateKey, data, error, gasRequired, func) {
+        if (!error) {
+            let transaction = new Transaction(account, privateKey);
+            if('undefined' != typeof transaction) {
+                transaction.do("transaction", data, gasRequired, handler[contractAddress], func);
+            }
+        } else {
+            handler[notifyError](error, func);
+        }
+    }
+
+    update(account, privateKey, key, data, func) {
+    }
+
+    remove(account, privateKey, key, func) {
+    }
+
+    size(func) {
+    }
+
+    getById(id, func) {
+    }
+
+    getByKey(key, func) {
+    }
+}
