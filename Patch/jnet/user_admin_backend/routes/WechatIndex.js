04ec104a00a124dc9a211d6fb7584e7597be25f6
scy-aojiao <1652336252@qq.com>
2019-01-11 10:17:53 +0800

    add import & const for WechatIndex.js

---
+++ Patch/jnet/user_admin_backend/routes/WechatIndex.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios');
+var crypto = require('crypto');
+
+import Insurance_user from '../public/js/insurance_user';
+import ContractInfo from '../public/js/ContractInfo';
+
+const userAbi = ContractInfo.userAbi;
+const userAddress = ContractInfo.userAddress;
+
+const account = "";
+const accountkey = "";
+
+const appid = "";
+const secret = "";
+const encryptKey = "";
