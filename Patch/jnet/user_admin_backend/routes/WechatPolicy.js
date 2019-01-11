3ae72c628e4715cf9c302368e252d446af43ce96
scy-aojiao <1652336252@qq.com>
2019-01-11 10:35:16 +0800

    add request 'size' for WechatPolicy.js

--- Patch/jnet/user_admin_backend/routes/WechatPolicy.js
+++ Patch/jnet/user_admin_backend/routes/WechatPolicy.js
@@ -9,3 +9,25 @@ const policyAddress = ContractInfo.policyAddress;
 
 const account = "";
 const accountkey = "";
+
+router.get('/size', function (req, res) {
+    let insurance_policy = new Insurance_policy(policyAbi,policyAddress);
+    insurance_policy.size(function(error, result) {
+        if(error) {
+            res.json({
+                status:"error",
+                code:"-9",
+                msg:"交易报错",
+                error:error.toString(10)
+            })
+        } else {
+            res.json({
+                status:"success",
+                code:"0",
+                msg:"获取成功",
+                data:result
+            })
+        }
+    })
+})
+
