dbbfa833fd8030547c6e45f55f35f269d06212a7
scy-aojiao <1652336252@qq.com>
2019-01-11 10:15:32 +0800

    add request'getByKey' for WechatCompany.js

--- Patch/jnet/user_admin_backend/routes/WechatCompany.js
+++ Patch/jnet/user_admin_backend/routes/WechatCompany.js
@@ -75,3 +75,57 @@ router.post('/getById', function (req, res) {
         }
     })
 })
+
+router.post('/getByKey', function (req, res) {
+    //let Key = "PingAn";
+    let Key = req.body.Key;
+    let insurance_company = new Insurance_company(companyAbi,companyAddress);
+    insurance_company.getByKey(Key,function(error, result) {
+        if(error) {
+            res.json({
+                status:"error",
+                code:"-9",
+                msg:"交易报错",
+                error:error.toString(10)
+            })
+        } else {
+            if(result.status == "0x0") {
+                res.json({
+                    status:"fail",
+                    code:"-6",
+                    msg:"交易失败"
+                })
+            } else {
+                if(result[0] == -3) {//判断状态值//inner error
+                    res.json({
+                        status:"success",
+                        code:"-3",
+                        msg:"合约出现问题，请稍后再试或联系管理员",
+                        data:null
+                    })
+                } else if (result[0] == -1) {//params error
+                    res.json({
+                        status:"success",
+                        code:"-1",
+                        msg:"参数有问题，请检查是否输入正确",
+                        data:null
+                    })
+                } else if (result[0] == -2) {//no data
+                    res.json({
+                        status:"success",
+                        code:"-2",
+                        msg:"该公司没有险种",
+                        data:null
+                    })
+                }  else if (result[0] == 0) {//success
+                    res.json({
+                        status:"success",
+                        code:"0",
+                        msg:"公司所有险种获取成功",
+                        data:result
+                    })
+                }
+            }
+        }
+    })
+})
