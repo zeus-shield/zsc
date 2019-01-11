f57996740c57c50c6195b30e0c57fa064610b0f9
scy-aojiao <1652336252@qq.com>
2019-01-11 10:15:08 +0800

    add request 'getById' for WechatCompany.js

--- Patch/jnet/user_admin_backend/routes/WechatCompany.js
+++ Patch/jnet/user_admin_backend/routes/WechatCompany.js
@@ -29,3 +29,49 @@ router.get('/size', function (req, res) {
         }
     })
 })
+
+router.post('/getById', function (req, res) {
+    //let Id = 0;
+    let Id = req.body.Id;
+    let insurance_company = new Insurance_company(companyAbi,companyAddress);
+    insurance_company.getById(Id,function(error, result) {
+        if(error) {
+            res.json({
+                status:"error",
+                code:"-9",
+                msg:"交易报错",
+                error:error.toString(10)
+            })
+        } else {
+            if(result[0] == -3) {//判断状态值//inner error
+                res.json({
+                    status:"success",
+                    code:"-3",
+                    msg:"合约出现问题，请稍后再试或联系管理员",
+                    data:null
+                })
+            } else if (result[0] == -1) {//params error
+                res.json({
+                    status:"success",
+                    code:"-1",
+                    msg:"参数有问题，请检查是否输入正确",
+                    data:null
+                })
+            } else if (result[0] == -2) {//no data
+                res.json({
+                    status:"success",
+                    code:"-2",
+                    msg:"该公司没有险种",
+                    data:null
+                })
+            }  else if (result[0] == 0) {//success
+                res.json({
+                    status:"success",
+                    code:"0",
+                    msg:"公司所有险种获取成功",
+                    data:result
+                })
+            }
+        }
+    })
+})
