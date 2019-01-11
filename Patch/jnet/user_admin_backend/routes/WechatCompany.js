907e795a732bd5543fcec6f941bbd8de28917a41
scy-aojiao <1652336252@qq.com>
2019-01-11 10:14:43 +0800

    add request'size' for WechatCompany.js

--- Patch/jnet/user_admin_backend/routes/WechatCompany.js
+++ Patch/jnet/user_admin_backend/routes/WechatCompany.js
@@ -9,3 +9,23 @@ const companyAddress = ContractInfo.companyAddress;
 
 const account = "";
 const accountkey = "";
+router.get('/size', function (req, res) {
+    let insurance_company = new Insurance_company(companyAbi,companyAddress);
+    insurance_company.size(function(error, result) {
+        if(error) {
+            res.json({
+                status:"error",
+                code:"-9",
+                msg:"交易报错",
+                error:error.toString(10)
+            })
+        } else {
+            res.json({
+                status:"success",
+                code:"0",
+                msg:"获取成功",
+                data:result
+            })
+        }
+    })
+})
