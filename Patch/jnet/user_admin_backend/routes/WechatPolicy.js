0438f941a14ec2811b25e189e39367a936f43705
scy-aojiao <1652336252@qq.com>
2019-01-11 10:31:40 +0800

    add import & const for WechatPolicy.js

---
+++ Patch/jnet/user_admin_backend/routes/WechatPolicy.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+import Insurance_policy from '../public/js/insurance_policy';
+import ContractInfo from '../public/js/ContractInfo';
+
+const policyAbi = ContractInfo.policyAbi;
+const policyAddress = ContractInfo.policyAddress;
+
+const account = "";
+const accountkey = "";
