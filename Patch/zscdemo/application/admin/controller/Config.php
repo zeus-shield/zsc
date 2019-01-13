9ed19e35f7a1b9a2534fd852dd866272a5a47b32
swedenwolf <xmuguoyang@gmail.com>
2019-01-13 21:44:05 +0800

    added function index() for 'Config.php'

--- Patch/zscdemo/application/admin/controller/Config.php
+++ Patch/zscdemo/application/admin/controller/Config.php
@@ -10,4 +10,33 @@ class Config extends Admin {
 		parent::_initialize();
 		$this->model = model('Config');
 	}
+	public function index() {
+		$group = input('group', 0, 'trim');
+		$name  = input('name', '', 'trim');
+		/* 查询条件初始化 */
+		$map          = array('status' => 1);
+		if ($group) {
+			$map['group'] = $group;
+		}
+
+		if ($name) {
+			$map['name'] = array('like', '%' . $name . '%');
+		}
+
+		$list = $this->model->where($map)->order('id desc')->paginate(25);
+		// 记录当前列表页的cookie
+		Cookie('__forward__', $_SERVER['REQUEST_URI']);
+
+		$data = array(
+			'group'       => config('config_group_list'),
+			'config_type' => config('config_config_list'),
+			'page'        => $list->render(),
+			'group_id'    => $group,
+			'list'        => $list,
+		);
+
+		$this->assign($data);
+		$this->setMeta('配置管理');
+		return $this->fetch();
+	}
 }
