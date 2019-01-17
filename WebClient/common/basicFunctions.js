e358c3e980ffb2520dc8f596b30c75bd0fc8c85f
Eric-Yao <norhustar@hotmail.com>
2019-01-17 10:55:24 +0100

    updated function bF_fixedNumberFromWei()

--- WebClient/common/basicFunctions.js
+++ WebClient/common/basicFunctions.js
@@ -44,7 +44,7 @@ function bF_showHashResult(elementID, hash, func) {
 
 function bF_fixedNumberFromWei(value, n) {
     var num = web3.fromWei(value, 'ether');
-    return Number(num).toFixed(8);
+    return Number(num).toFixed(6);
 }
 
 function bF_secondsToDate(secs) {
