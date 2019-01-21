5816c16870548dd19864ff7b11764c551d98c05d
swedenwolf <xmuguoyang@gmail.com>
2019-01-21 10:53:13 +0800

    added function add() for 'Group.php'

--- Patch/zscdemo/application/admin/controller/Group.php
+++ Patch/zscdemo/application/admin/controller/Group.php
@@ -13,4 +13,19 @@ class Group extends Admin {
 		$this->group = model('AuthGroup');
 		$this->rule  = model('AuthRule');
 	}
+	//会员分组首页控制器
+	public function index($type = 'admin') {
+		$map['module'] = $type;
+
+		$list = db('AuthGroup')->where($map)->order('id desc')->paginate(10);
+
+		$data = array(
+			'list' => $list,
+			'page' => $list->render(),
+			'type' => $type,
+		);
+		$this->assign($data);
+		$this->setMeta('用户组管理');
+		return $this->fetch();
+	}
 }
