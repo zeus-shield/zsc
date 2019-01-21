16757d1edffba6cfaf8308e4cc6b3e793af136cf
swedenwolf <xmuguoyang@gmail.com>
2019-01-21 10:55:10 +0800

    added function index() for 'Group.php'

--- Patch/zscdemo/application/admin/controller/Group.php
+++ Patch/zscdemo/application/admin/controller/Group.php
@@ -28,4 +28,23 @@ class Group extends Admin {
 		$this->setMeta('用户组管理');
 		return $this->fetch();
 	}
+	//会员分组添加控制器
+	public function add($type = 'admin') {
+		if (IS_POST) {
+			$result = $this->group->change();
+			if ($result) {
+				return $this->success("添加成功！", url('admin/group/index'));
+			} else {
+				return $this->error("添加失败！");
+			}
+		} else {
+			$data = array(
+				'info'    => array('module' => $type, 'status' => 1),
+				'keyList' => $this->group->keyList,
+			);
+			$this->assign($data);
+			$this->setMeta('添加用户组');
+			return $this->fetch('public/edit');
+		}
+	}
 }
