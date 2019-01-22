d74444ebabd0424d3a54df4fcc0596bf27bc3017
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-01-22 11:37:42 +0800

    add delegate for WebTest/sol/insurance/insurance_policy.sol

--- WebTest/sol/insurance/insurance_policy.sol
+++ WebTest/sol/insurance/insurance_policy.sol
@@ -30,7 +30,7 @@ contract InsurancePolicy is Delegate {
     }
 
     modifier _onlyAdminOrHigher() {
-        // require(checkDelegate(msg.sender, 2));
+        require(checkDelegate(msg.sender, 2));
         _;
     }
 
