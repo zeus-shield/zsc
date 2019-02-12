7edb809b5605fd1cd15da731588b532aaa1ab6de
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-02-12 11:52:23 +0800

    Patch/minetrack/server/routes/Order.js

---
+++ Patch/minetrack/server/routes/Order.js
@@ -0,0 +1,25 @@
+
+router.post('/getTrack', function (req, res) {
+})
+
+router.post('/getNums', function (req, res) {
+})
+
+//添加订单信息至数据库
+router.post('/add', function (req, res) {
+})
+
+router.get('/getOrder', function (req, res) {
+})
+
+router.get('/getContractInfo', function (req, res) {
+})
+
+router.get('/getContractInfoForAdm', function (req, res) {
+})
+
+router.get('/getContractInfoForAnalytics', function (req, res) {
+})
+
+module.exports = router;
+
