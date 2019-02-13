736bc99a5cf9c516f21d22be6771a0fe820be7fd
scy-aojiao <1652336252@qq.com>
2019-02-13 17:54:30 +0800

    update WechatAnalytics.js

--- Patch/minetrack/server/routes/WechatAnalytics.js
+++ Patch/minetrack/server/routes/WechatAnalytics.js
@@ -6,3 +6,6 @@ import ContractInfo from '../public/js/ContractInfo';
 
 const analyticsAddress = ContractInfo.analyticsAddress;
 const analyticsAbi = ContractInfo.analyticsAbi;
+
+const account = "0x00";
+const accountkey = "0x00";
