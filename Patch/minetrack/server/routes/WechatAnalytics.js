a4169ebda42734496a8f8261f74b4c6446dd6db1
scy-aojiao <1652336252@qq.com>
2019-02-13 17:54:07 +0800

    update WechatAnalytics.js

--- Patch/minetrack/server/routes/WechatAnalytics.js
+++ Patch/minetrack/server/routes/WechatAnalytics.js
@@ -1,2 +1,8 @@
 var express = require('express');
 var router = express.Router();
+
+import Insurance_analytics from '../public/js/insurance_analytics';
+import ContractInfo from '../public/js/ContractInfo';
+
+const analyticsAddress = ContractInfo.analyticsAddress;
+const analyticsAbi = ContractInfo.analyticsAbi;
