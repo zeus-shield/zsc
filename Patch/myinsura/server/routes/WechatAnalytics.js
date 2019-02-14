60c0da40aa521efde7b0448667e16a386d143f67
scy-aojiao <1652336252@qq.com>
2019-02-14 18:10:26 +0800

    update for WechatAnalytics.js

--- Patch/myinsura/server/routes/WechatAnalytics.js
+++ Patch/myinsura/server/routes/WechatAnalytics.js
@@ -9,6 +9,13 @@ const analyticsAbi = ContractInfo.analyticsAbi;
 
 const account = "0x6A76fb522F948CfB7d6d929408D6aD5876E7139F";
 const accountkey = "0x805fd8b8d4aeee085b220b44caa5a73af77aa23fbebfc1a7e09d2150887c6e21";
+
 router.get('/getKeys', function (req, res) {
+    let insurance_analytics = new Insurance_analytics(analyticsAbi,analyticsAddress);
+    let id = 0;
+    let count = 100;
+    insurance_analytics.getKeys(id,count,function(error, result) {
+        if(error) {
+            res.json({
 })
 module.exports = router;
