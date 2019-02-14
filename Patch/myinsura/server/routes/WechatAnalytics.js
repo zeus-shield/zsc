72f1abd55ed58aedd90c947935413c9ad393e2cc
scy-aojiao <1652336252@qq.com>
2019-02-14 18:08:30 +0800

    update for WechatAnalytics.js

--- Patch/myinsura/server/routes/WechatAnalytics.js
+++ Patch/myinsura/server/routes/WechatAnalytics.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+import Insurance_analytics from '../public/js/insurance_analytics';
+import ContractInfo from '../public/js/ContractInfo';
+
+const analyticsAddress = ContractInfo.analyticsAddress;
+const analyticsAbi = ContractInfo.analyticsAbi;
 router.get('/getKeys', function (req, res) {
 })
 module.exports = router;
