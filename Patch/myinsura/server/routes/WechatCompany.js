43c600cc0d665e1293ce8e5f19dae24ca2aef2ae
scy-aojiao <1652336252@qq.com>
2019-02-18 17:40:27 +0800

    update for WechatCompany.js

--- Patch/myinsura/server/routes/WechatCompany.js
+++ Patch/myinsura/server/routes/WechatCompany.js
@@ -1,2 +1,5 @@
 var express = require('express');
 var router = express.Router();
+
+import Insurance_company from '../public/js/insurance_company';
+import ContractInfo from '../public/js/ContractInfo';
