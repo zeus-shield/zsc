d0b545b15ee607a63e6bdf18be79486b57a756e4
scy-aojiao <1652336252@qq.com>
2019-02-18 17:42:40 +0800

    update for WechatCompany.js

--- Patch/myinsura/server/routes/WechatCompany.js
+++ Patch/myinsura/server/routes/WechatCompany.js
@@ -21,3 +21,24 @@ router.get('/size', function (req, res) {
                 error:error.toString(10)
             })
         } else {
+            res.json({
+                status:"success",
+                code:"0",
+                msg:"获取成功",
+                data:result
+            })
+        }
+    })
+})
+
+router.post('/getById', function (req, res) {
+    //let Id = 0;
+    let Id = req.body.Id;
+    let insurance_company = new Insurance_company(companyAbi,companyAddress);
+    insurance_company.getById(Id,function(error, result) {
+        if(error) {
+            res.json({
+                status:"error",
+                code:"-9",
+                msg:"交易报错",
+                error:error.toString(10)
