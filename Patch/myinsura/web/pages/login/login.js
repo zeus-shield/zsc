d912588da2f768bacd1878ac3b0e04a12436d217
swedenwolf <xmuguoyang@gmail.com>
2019-02-18 17:26:10 +0800

    updated 'login.js'

--- Patch/myinsura/web/pages/login/login.js
+++ Patch/myinsura/web/pages/login/login.js
@@ -1,5 +1,12 @@
-
+const app = getApp()
 
 Page({
+  data: {
+    motto: 'µã»÷½øÈëMyinsura',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    isLogining:false
+  },
 
 })
