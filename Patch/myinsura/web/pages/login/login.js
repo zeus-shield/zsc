61a985649b17dc2db5accae3c983e3f8f3604c68
swedenwolf <xmuguoyang@gmail.com>
2019-02-18 17:30:19 +0800

    added function loginIn() for 'login.js'

--- Patch/myinsura/web/pages/login/login.js
+++ Patch/myinsura/web/pages/login/login.js
@@ -51,4 +51,77 @@ Page({
       url: '../companyAndPolicy/companyAndPolicy'
     })
   },
+  loginIn: function () {
+    if(this.data.isLogining == false) {
+      this.setData({
+        isLogining:true
+      })
+      wx.showToast({
+        title: '身份登录中',
+        icon: 'loading',
+        duration: 50000
+      })
+      let handler = this;
+      wx.request({
+        url: 'http://minetrack.io:3001/WechatIndex/login',
+        method: 'post',
+        data: {
+          Code: app.globalData.code,
+          NickName: handler.data.userInfo.nickName
+        },
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        success(res) {
+          if (res.data.code == 0) {//新用户
+            app.globalData.code = res.data.data;
+            app.globalData.isLogin = true;
+            app.globalData.notice = '欢迎新用户'
+            // wx.showToast({
+            //   title: '欢迎新用户',
+            //   icon: 'success',
+            //   duration: 3000,
+            //   success: function () {
+            //     wx.switchTab({
+            //       url: '../index/index',
+            //     })
+            //   }
+            // })
+            wx.switchTab({
+              url: '../companyAndPolicy/companyAndPolicy',
+            })
+
+          } else if (res.data.code == 3) {//老用户，拿data值
+            app.globalData.isLogin = true;
+            app.globalData.code = res.data.data;
+            app.globalData.notice = '欢迎老用户'
+            console.log(app.globalData.isLogin);
+
+            wx.switchTab({
+              url: '../companyAndPolicy/companyAndPolicy',
+            })
+          } else {
+            console.log(res.data)
+            //提示用户身份识别有误
+            wx.showToast({
+              title: '身份识别有误',
+              icon: 'success',
+              duration: 1500
+            })
+          }
+          app.globalData.notice = '身份识别有误'
+        },
+        fail(error) {
+          //console.log(error);
+          //网络延迟，请重输入
+          wx.showToast({
+            title: '打开调试',
+            icon: 'success',
+            duration: 1500
+          })
+
+
+        },
+      })
+    }
+    
+  }
 })
