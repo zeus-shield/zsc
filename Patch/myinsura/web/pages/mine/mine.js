8e2a58e6d6fc53edf18b0bbd1c712b1ea7648ac8
swedenwolf <xmuguoyang@gmail.com>
2019-02-19 09:49:41 +0800

    updated 'mine.js'

--- Patch/myinsura/web/pages/mine/mine.js
+++ Patch/myinsura/web/pages/mine/mine.js
@@ -1,4 +1,18 @@
+const app = getApp()
 
 Page({
 
+  data: {
+    userAllPolicy: [],
+    userAllPolicytemp:[],
+    userAllPolicyHiddenValue:[],
+    isGet:[],
+
+    userInfo: {},
+    code:"",
+    hasPolicy:false,
+    isIdHidden:true,
+    isHashHidden:[],
+
+  },
 })
