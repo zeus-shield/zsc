6f1fd5425890e2ef6eeb6c2a722b7227c819521f
scy-aojiao <1652336252@qq.com>
2019-02-20 17:08:46 +0800

    update code for WechatCompany.js

--- Patch/myinsura/server/routes/WechatCompany.js
+++ Patch/myinsura/server/routes/WechatCompany.js
@@ -17,14 +17,14 @@ router.get('/size', function (req, res) {
             res.json({
                 status:"error",
                 code:"-9",
-                msg:"交易报错",
+                msg:"浜ゆ槗鎶ラ敊",
                 error:error.toString(10)
             })
         } else {
             res.json({
                 status:"success",
                 code:"0",
-                msg:"获取成功",
+                msg:"鑾峰彇鎴愬姛",
                 data:result
             })
         }
@@ -40,39 +40,3 @@ router.post('/getById', function (req, res) {
             res.json({
                 status:"error",
                 code:"-9",
-                msg:"交易报错",
-                error:error.toString(10)
-            })
-        } else {
-            if(result[0] == -3) {//判断状态值//inner error
-                res.json({
-                    status:"success",
-                    code:"-3",
-                    msg:"合约出现问题，请稍后再试或联系管理员",
-                    data:null
-                })
-            } else if (result[0] == -1) {//params error
-                res.json({
-                    status:"success",
-                    code:"-1",
-                    msg:"参数有问题，请检查是否输入正确",
-                    data:null
-                })
-            } else if (result[0] == -2) {//no data
-                res.json({
-                    status:"success",
-                    code:"-2",
-                    msg:"该公司没有险种",
-                    data:null
-                })
-            }  else if (result[0] == 0) {//success
-                res.json({
-                    status:"success",
-                    code:"0",
-                    msg:"公司所有险种获取成功",
-                    data:result
-                })
-            }
-        }
-    })
-})
