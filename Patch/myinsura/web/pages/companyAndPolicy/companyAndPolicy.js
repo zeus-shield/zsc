df265a72181e3cb104d2b9df93fa2df107b89d4d
swedenwolf <xmuguoyang@gmail.com>
2019-02-21 09:46:08 +0800

    updated 'companyAndPolicy.js'

--- Patch/myinsura/web/pages/companyAndPolicy/companyAndPolicy.js
+++ Patch/myinsura/web/pages/companyAndPolicy/companyAndPolicy.js
@@ -1,5 +1,18 @@
-
+const app = getApp();
+const PolicyInfo = require("policyInfo.js");
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
 
 Page({
+  data: {
+    //navbar控制参数
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderLeft: 0,
+    //navbar数据
+    tabs: [],
+    //九宫格数据
+    grids: [],
+    //九宫格被选中选项
+    policyKey: "DB_Policy_中国人保_寿险鑫利年金险",
 
 })
