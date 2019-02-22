6123ffe09b67ce6b65e629685de4ffeaff679855
scy-aojiao <1652336252@qq.com>
2019-02-22 17:46:22 +0800

    add const for WechatIndex.js

--- Patch/myinsura/server/routes/WechatIndex.js
+++ Patch/myinsura/server/routes/WechatIndex.js
@@ -5,3 +5,6 @@ var crypto = require('crypto');
 
 import Insurance_user from '../public/js/insurance_user';
 import ContractInfo from '../public/js/ContractInfo';
+
+const userAbi = ContractInfo.userAbi;
+const userAddress = ContractInfo.userAddress;
