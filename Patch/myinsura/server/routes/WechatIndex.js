20685616f2f6b192e9ec2ae72ff013ba84ecd350
scy-aojiao <1652336252@qq.com>
2019-02-22 17:46:06 +0800

    add import for WechatIndex.js

--- Patch/myinsura/server/routes/WechatIndex.js
+++ Patch/myinsura/server/routes/WechatIndex.js
@@ -2,3 +2,6 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 var crypto = require('crypto');
+
+import Insurance_user from '../public/js/insurance_user';
+import ContractInfo from '../public/js/ContractInfo';
