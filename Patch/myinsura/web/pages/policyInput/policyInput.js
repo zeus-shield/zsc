7d041436ee6fe1b0326793cf9f30a90089132a5d
swedenwolf <xmuguoyang@gmail.com>
2019-02-27 16:57:22 +0800

    updated 'policyInput.js'

--- Patch/myinsura/web/pages/policyInput/policyInput.js
+++ Patch/myinsura/web/pages/policyInput/policyInput.js
@@ -1,5 +1,39 @@
-
+const app = getApp()
 
 Page({
+  data: {
+
+    createTemp: {},
+    companyArray: {},
+    policyInfo: {},
+
+    //保单模板
+    policyTemp: "",
+    policyTempArray: [],
+    policyTempData: {},
+
+    multiArray: [],
+    multiIndex: [0, 0],
+
+    bol: false,
+
+    accounts: [],
+    accountIndex: 0,
+    accounts1: [],
+    accountIndex1: 0,
+    radioText: "",
+    para:"",
+    title:"",
+
+    radio: [
+      { value: "0", name: "年交", checked: 'true' },
+      { value: "1", name: "月交" }
+    ],
+    flag: false,
+    date: "1980-01-01",
+    date1: "1980-01-01"
+
+
+  },
 	
 })
