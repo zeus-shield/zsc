4942722a01744325501dbfec0cd912377de15a5f
scy-aojiao <1652336252@qq.com>
2019-02-27 10:53:18 +0800

    add import for WechatPolicy.js

--- Patch/myinsura/server/routes/WechatPolicy.js
+++ Patch/myinsura/server/routes/WechatPolicy.js
@@ -1,2 +1,8 @@
 var express = require('express');
 var router = express.Router();
+
+import Insurance_policy from '../public/js/insurance_policy';
+import ContractInfo from '../public/js/ContractInfo';
+
+const policyAbi = ContractInfo.policyAbi;
+const policyAddress = ContractInfo.policyAddress;
