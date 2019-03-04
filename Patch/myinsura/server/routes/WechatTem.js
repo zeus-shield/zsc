c3031f35b0c3fe547e4959d1a61ccfd1ad0e8bf7
scy-aojiao <1652336252@qq.com>
2019-03-04 17:32:09 +0800

    update WechatTem.js

--- Patch/myinsura/server/routes/WechatTem.js
+++ Patch/myinsura/server/routes/WechatTem.js
@@ -9,3 +9,14 @@ const temAddress = ContractInfo.temAddress;
 
 const account = "";
 const accountkey = "";
+
+router.get('/size', function (req, res) {
+    let insurance_template = new Insurance_template(temAbi,temAddress);
+    insurance_template.size(function(error, result) {
+        if(error) {
+            res.json({
+                status:"error",
+                code:"-9",
+                msg:"½»Ò×±¨´í",
+                error:error.toString(10)
+            })
