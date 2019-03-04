111d6951c94c85c214a221d5764d044db744ef0b
scy-aojiao <1652336252@qq.com>
2019-03-04 17:31:04 +0800

    add import for WechatTem.js

--- Patch/myinsura/server/routes/WechatTem.js
+++ Patch/myinsura/server/routes/WechatTem.js
@@ -1,2 +1,5 @@
 var express = require('express');
 var router = express.Router();
+
+import Insurance_template from '../public/js/insurance_template';
+import ContractInfo from '../public/js/ContractInfo';
