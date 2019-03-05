7fc4ea76a70c21cc8a9fa78a00b2912f7afe3678
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-03-05 15:45:30 +0800

    add WebTest/sol/token/ERC20/ERC20Pausable.sol

---
+++ WebTest/sol/token/ERC20/ERC20Pausable.sol
@@ -0,0 +1,35 @@
+/**
+  Copyright (c) 2018, ZSC Dev Team
+  2018-10-19: v0.00.01
+ */
+
+pragma solidity ^0.4.25;
+
+import "./ERC20.sol";
+import "../../common/pausable.sol";
+
+/**
+ * @title Pausable token
+ * @dev ERC20 modified with pausable transfers.
+ */
+contract ERC20Pausable is ERC20, Pausable {
+    function transfer(address to, uint256 value) public whenNotPaused returns (bool) {
+        return super.transfer(to, value);
+    }
+
+    function transferFrom(address from, address to, uint256 value) public whenNotPaused returns (bool) {
+        return super.transferFrom(from, to, value);
+    }
+
+    function approve(address spender, uint256 value) public whenNotPaused returns (bool) {
+        return super.approve(spender, value);
+    }
+
+    function increaseAllowance(address spender, uint addedValue) public whenNotPaused returns (bool success) {
+        return super.increaseAllowance(spender, addedValue);
+    }
+
+    function decreaseAllowance(address spender, uint subtractedValue) public whenNotPaused returns (bool success) {
+        return super.decreaseAllowance(spender, subtractedValue);
+    }
+}
