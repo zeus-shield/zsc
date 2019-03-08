92c9a8509633af774fe195f971410e8abecebf83
scy-aojiao <1652336252@qq.com>
2019-03-08 14:52:38 +0800

    add const for WechatUser.js

--- Patch/myinsura/server/routes/WechatUser.js
+++ Patch/myinsura/server/routes/WechatUser.js
@@ -1,2 +1,11 @@
 var express = require('express');
 var router = express.Router();
+
+import Insurance_user from '../public/js/insurance_user';
+import ContractInfo from '../public/js/ContractInfo';
+
+const userAbi = ContractInfo.userAbi;
+const userAddress = ContractInfo.userAddress;
+
+const account = "";
+const accountkey = "";
