b0644410445bd1e4b282127860861fe98d50a205
scy-aojiao <1652336252@qq.com>
2019-03-08 12:16:46 +0800

    update for WechatTem.js

--- Patch/myinsura/server/routes/WechatTem.js
+++ Patch/myinsura/server/routes/WechatTem.js
@@ -119,3 +119,15 @@ router.post('/getByKey', function (req, res) {
                         msg:"该模板不存在",
                         data:null
                     })
+                }  else if (result[0] == 0) {//success
+                    res.json({
+                        status:"success",
+                        code:"0",
+                        msg:"模板获取成功",
+                        data:result[1]
+                    })
+                }
+            }
+        }
+    })
+})
