abfd3f5ffe7432398fa81cbc6cf0978597751a69
scy-aojiao <1652336252@qq.com>
2019-03-08 14:57:48 +0800

    update WechatUser.js

--- Patch/myinsura/server/routes/WechatUser.js
+++ Patch/myinsura/server/routes/WechatUser.js
@@ -9,3 +9,14 @@ const userAddress = ContractInfo.userAddress;
 
 const account = "";
 const accountkey = "";
+
+router.get('/size', function (req, res) {
+    let insurance_user = new Insurance_user(userAbi,userAddress);
+    insurance_user.size(function(error, result) {
+        if(error) {
+            res.json({
+                status:"error",
+                code:"-9",
+                msg:"交易报错",
+                error:error.toString(10)
+            })
