de497cb4142b9f305a6e5f67d6b54fe3d438dcd0
scy-aojiao <1652336252@qq.com>
2019-04-10 18:03:00 +0800

    update index.vue

--- Patch/myinsura/web/views/index.vue
+++ Patch/myinsura/web/views/index.vue
@@ -3,3 +3,15 @@
    <!-- slider ÂÖ²¥-->  
   <section class="wrapper100percent" id="menu1"> 
     <el-carousel trigger="click" :height="imgHeight" >
+      <el-carousel-item v-for="item in 3" :key="item">
+          <img style="width:100%" :src="getImgUrl(item)" alt=""/>
+          <div class="flex-caption flex-caption1" v-if="item == 1">
+            <div class="container">
+              <div class="row">
+                  <div style="text-align:center;">
+                    <h1 style="font-size:35px">{{language.index.menu1.title1}}</h1>
+                    <p>{{language.index.menu1.page1}}</p>
+                  </div>
+              </div>
+            </div>
+          </div>
