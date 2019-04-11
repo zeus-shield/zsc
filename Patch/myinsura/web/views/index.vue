5063826f8712551a277ab23f65d88d2507e4c5b1
scy-aojiao <1652336252@qq.com>
2019-04-11 17:47:22 +0800

    update section for index.vue

--- Patch/myinsura/web/views/index.vue
+++ Patch/myinsura/web/views/index.vue
@@ -111,3 +111,16 @@
                     </div>
                   </div>
                 </div>
+                <div :class="activeIndex == 3?'test':'test-hover'" v-if="item==4">
+                  <div class="col-sm-8 homeblog" style="width:90%;margin-top:10px;padding:0;left:50%">
+                    <div style="position:relative;right:50%">
+                    <figure class="view1 view-first1">
+                      <img src="../assets/images/team5.jpg" alt=""> 
+                    </figure> 
+                    <div class="wrapper100percent">
+                          <h4>{{language.index.menu3.title4}}</h4>
+                          <p class="pageforPhone">{{language.index.menu3.page4}}</p>          
+                    </div> 
+                    </div>
+                  </div>
+                </div>
