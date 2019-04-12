dea33cf253fa5bd5e6a1d14051f37bbade2afa61
scy-aojiao <1652336252@qq.com>
2019-04-12 18:39:50 +0800

    update html for myinsura_faq.vue

--- Patch/myinsura/web/views/myinsura_faq.vue
+++ Patch/myinsura/web/views/myinsura_faq.vue
@@ -3,3 +3,9 @@
     <el-row :gutter="40">
         <el-col :span="24">    
             <div style="font-size:40px;font:'PingFang SC';color:black;">
+                {{language.FAQ.title}}
+            </div>
+            <el-collapse v-model="activeName" accordion>
+                <el-collapse-item name="1">
+                    <template slot="title">
+                    <span class="FAQ-title">{{language.FAQ.question1}}</span>&nbsp;&nbsp;<i class="header-icon el-icon-info"></i>
