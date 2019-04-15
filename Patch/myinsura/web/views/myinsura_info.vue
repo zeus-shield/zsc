76ca469318bb5724a0abad67d06da58f36b6ba8a
scy-aojiao <1652336252@qq.com>
2019-04-15 18:45:55 +0800

    update myinsura_info.vue

---
+++ Patch/myinsura/web/views/myinsura_info.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="page-body">
+        <div>
+        <el-row :gutter="40">
+                <el-col :span="24">
+                    <div class="up-card">
+                        <div class="up-card-title">
+                            <span>{{policyinfo.title}}</span>
+                        </div>
