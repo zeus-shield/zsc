668d815112d72df4963ad6d85d3f5c7b6f51e96e
scy-aojiao <1652336252@qq.com>
2019-04-16 18:02:24 +0800

    update myinsura_mine.vue

---
+++ Patch/myinsura/web/views/myinsura_mine.vue
@@ -0,0 +1,9 @@
+<template>
+<div class="page-body">
+    <div v-loading.fullscreen.lock="loading" :element-loading-text="status == 0 ? '个人信息载入中':(status == 1?'用户签到处理中':(status == 2? '积分赠送处理中':(status == 3?'积分查询处理中':'积分加载中')))">
+       <el-row :gutter="40">
+            <el-col :span="24" :md="8">
+                <div class="left-card" >
+                    <div class="card-title">
+                        <span>{{code}}</span>
+                    </div>
