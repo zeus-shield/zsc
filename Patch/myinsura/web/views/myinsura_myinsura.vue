6b2a0808b5269e83a46cc58c636926a9034fab6d
scy-aojiao <1652336252@qq.com>
2019-05-04 01:53:33 +0800

    update myinsura_myinsura.vue

--- Patch/myinsura/web/views/myinsura_myinsura.vue
+++ Patch/myinsura/web/views/myinsura_myinsura.vue
@@ -3,3 +3,8 @@
     <el-tabs v-loading.fullscreen.lock="loading" :element-loading-text="language.loading.p2" element-loading-spinner="el-icon-loading" v-model="activeName" type="card" @tab-click="handleClick" >
         <el-tab-pane :key="tabitem.name" v-for="(tabitem,tabkey) in companyListShow" :label="generateCompany(tabitem.title)" :id="tabitem.name">
             <div>
+                <el-row :gutter="50" >
+                    <el-col :span="24" :sm="12" :md="8" v-for="(item) in policyListShow[tabkey]" :key="item" :id="item">
+                        <div class="square-card" >
+                            <router-link :to="`/info/${focusCompany}_${item}`">
+                                <div class="square-card-image">
