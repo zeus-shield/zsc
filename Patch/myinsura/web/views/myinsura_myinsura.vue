d7fe596dc4e548d3e9b11ea303819e30246e760b
scy-aojiao <1652336252@qq.com>
2019-05-04 01:53:22 +0800

    update myinsura_myinsura.vue

---
+++ Patch/myinsura/web/views/myinsura_myinsura.vue
@@ -0,0 +1,5 @@
+<template>
+<div class="page-body">
+    <el-tabs v-loading.fullscreen.lock="loading" :element-loading-text="language.loading.p2" element-loading-spinner="el-icon-loading" v-model="activeName" type="card" @tab-click="handleClick" >
+        <el-tab-pane :key="tabitem.name" v-for="(tabitem,tabkey) in companyListShow" :label="generateCompany(tabitem.title)" :id="tabitem.name">
+            <div>
