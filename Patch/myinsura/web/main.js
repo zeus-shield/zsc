c014e2cf206e6608ce8808784601322a611fe8bc
scy-aojiao <1652336252@qq.com>
2019-05-17 04:34:53 +0800

    update main.js

--- Patch/myinsura/web/main.js
+++ Patch/myinsura/web/main.js
@@ -3,3 +3,6 @@ import App from './App'
 import ElementUI from 'element-ui'
 // import 'element-ui/lib/theme-default/index.css'
 // import 'element-theme-default';
+import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/display.css';
+import VueRouter from 'vue-router'
