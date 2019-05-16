43d0d6cead32fc2164bd129c5d5442c2448d82e0
scy-aojiao <1652336252@qq.com>
2019-05-17 04:37:12 +0800

    update main.js

--- Patch/myinsura/web/main.js
+++ Patch/myinsura/web/main.js
@@ -18,6 +18,12 @@ import "./assets/style/style.css"; //引入CSS
 Vue.use(ElementUI)
 Vue.use(VueRouter)
 Vue.use(Vuex)
+Vue.use(Meta, {
+  keyName: 'metaInfo', // the component option name that vue-meta looks for meta info on.
+  attribute: 'data-vue-meta', // the attribute name vue-meta adds to the tags it observes
+  ssrAttribute: 'data-vue-meta-server-rendered', // the attribute name that lets vue-meta know that meta info has already been server-rendered
+  tagIDKeyName: 'vmid' // the property name that vue-meta uses to determine whether to overwrite or append a tag
+})
 router.beforeEach((to, from, next) => {
   // NProgress.start();
   // if (to.path == '/login') {
