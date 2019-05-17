b784ffcbab6e601bda335022236216ce5d5ef38c
scy-aojiao <1652336252@qq.com>
2019-05-18 04:01:50 +0800

    update  routes.js

--- Patch/myinsura/web/routes.js
+++ Patch/myinsura/web/routes.js
@@ -1,6 +1,12 @@
 import NotFound from './views/404.vue'
 
 let routes = [
+    {
+        path: '/404',
+        component: NotFound,
+        name: '',
+        hidden: true
+    },
 ];
 
 export default routes;
