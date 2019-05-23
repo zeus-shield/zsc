30110b2e42860c6a2ac33b933b7e73c8740e9cd2
scy-aojiao <1652336252@qq.com>
2019-05-24 06:18:41 +0800

    update footer.vue

---
+++ Patch/myinsura/web/components/footer.vue
@@ -0,0 +1,11 @@
+<template>
+ <footer class="bottom">
+    <div class="container">
+        <div class="col-md-12">
+          <div class="copyright">
+            <p>Copyright© 2018-2019 MyInsura</p>
+          </div>
+        </div>
+    </div>
+  </footer>
+</template>
