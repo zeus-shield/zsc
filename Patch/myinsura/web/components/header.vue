4911d9cb76e30ec9fa550468334e68808084d98f
scy-aojiao <1652336252@qq.com>
2019-05-28 18:18:14 +0800

    update header.vue

--- Patch/myinsura/web/components/header.vue
+++ Patch/myinsura/web/components/header.vue
@@ -8,3 +8,11 @@
         @select="handleSelect" 
         text-color="#fff"
         active-text-color="#ffd04b">
+        <el-menu-item index="1" v-if="!isphone">{{language.header.p1}}</el-menu-item>
+        <el-menu-item index="2" v-if="!isphone">{{language.header.p2}}</el-menu-item>
+        <el-menu-item index="3" v-if="!isphone">{{language.header.p3}}</el-menu-item>
+        <el-menu-item index="4" v-if="!isphone">{{language.header.p4}}</el-menu-item>
+        <el-menu-item index="5" v-if="!isphone&&isLogin">{{language.header.p5}}</el-menu-item>
+        <el-menu-item index="6" v-if="!isphone&&!isLogin">{{language.header.p6}}</el-menu-item>
+        <el-menu-item index="7" v-if="!isphone">{{language.header.p7}}</el-menu-item></router-link>
+        <el-menu-item index="8" v-if="!isphone" @click="changeLanguage()">{{language.header.p8}}</el-menu-item>
