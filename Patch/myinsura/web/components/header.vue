1d3f86ac6cfe2fafa6fc79a17321f89f63a795b5
scy-aojiao <1652336252@qq.com>
2019-05-28 18:18:30 +0800

    update header.vue

--- Patch/myinsura/web/components/header.vue
+++ Patch/myinsura/web/components/header.vue
@@ -16,3 +16,8 @@
         <el-menu-item index="6" v-if="!isphone&&!isLogin">{{language.header.p6}}</el-menu-item>
         <el-menu-item index="7" v-if="!isphone">{{language.header.p7}}</el-menu-item></router-link>
         <el-menu-item index="8" v-if="!isphone" @click="changeLanguage()">{{language.header.p8}}</el-menu-item>
+        <!-- <el-submenu index="7" v-if="!isphone">
+          <template slot="title">语言</template>
+            <el-menu-item @click="changeLanguage()" index="7-1">中文</el-menu-item>
+            <el-menu-item @click="changeLanguage()" index="7-2">英文</el-menu-item>
+        </el-submenu> -->
