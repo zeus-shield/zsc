64198cfe28fc745b1a9a006e1f04c61dd3a1cb50
scy-aojiao <1652336252@qq.com>
2019-06-13 02:14:44 +0800

    update input.vue

--- Patch/myinsura/web/components/input.vue
+++ Patch/myinsura/web/components/input.vue
@@ -2,3 +2,7 @@
 <div v-loading.fullscreen.lock="loading" :element-loading-text="isInput?language.loading.p1:language.loading.p2 " element-loading-spinner="el-icon-loading" >
   <el-dialog :title="title" :visible.sync="dialogFormVisible" center :width="dialogWidth">
     <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+      <div v-for="(item,key) in policyTemArray" :key="key">
+        <el-form-item v-if="item == '投保人'" :label="language.input.p1" prop="投保人">
+            <el-input v-model="form.投保人"></el-input>
+        </el-form-item>
