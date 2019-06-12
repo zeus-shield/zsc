228c2634cc569932fa49c266d38b6dee74d496c0
scy-aojiao <1652336252@qq.com>
2019-06-13 02:11:06 +0800

    update input.vue

---
+++ Patch/myinsura/web/components/input.vue
@@ -0,0 +1,4 @@
+<template>
+<div v-loading.fullscreen.lock="loading" :element-loading-text="isInput?language.loading.p1:language.loading.p2 " element-loading-spinner="el-icon-loading" >
+  <el-dialog :title="title" :visible.sync="dialogFormVisible" center :width="dialogWidth">
+    <el-form ref="form" :model="form" :rules="rules" label-width="80px">
