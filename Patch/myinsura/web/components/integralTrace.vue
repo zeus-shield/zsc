d3a31b8fd8fac85e2db9e1d9a1fdcfa7d262af72
scy-aojiao <1652336252@qq.com>
2019-06-19 17:39:27 +0800

    update integralTrace.vue

---
+++ Patch/myinsura/web/components/integralTrace.vue
@@ -0,0 +1,3 @@
+<template>
+<div v-loading.fullscreen.lock="loading" :element-loading-text="language.loading.p3" element-loading-spinner="el-icon-loading">
+  <el-dialog :title="language.integralTrace.title" :visible.sync="dialogFormVisible" center :width="dialogWidth">
