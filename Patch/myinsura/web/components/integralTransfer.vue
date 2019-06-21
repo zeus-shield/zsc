8fe5c1c8e5e54d8895b2bdebcfd9b009a8dc4001
scy-aojiao <1652336252@qq.com>
2019-06-21 15:41:19 +0800

    update integralTransfer.vue

--- Patch/myinsura/web/components/integralTransfer.vue
+++ Patch/myinsura/web/components/integralTransfer.vue
@@ -3,3 +3,6 @@
   <el-dialog :title="language.integralTransfer.title" :visible.sync="dialogFormVisible" center :width="dialogWidth">
     <el-form ref="form" :model="form" :rules="rules" label-width="80px">
       <div>
+        <el-form-item :label="language.integralTransfer.p1" prop="code">
+          <el-input v-model="form.code" :placeholder="language.integralTransfer.p3"></el-input>
+        </el-form-item>
