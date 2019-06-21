879beee3fbb6d7aad6f13f62a49221e70fcd8e30
scy-aojiao <1652336252@qq.com>
2019-06-21 15:41:09 +0800

    update integralTransfer.vue

--- Patch/myinsura/web/components/integralTransfer.vue
+++ Patch/myinsura/web/components/integralTransfer.vue
@@ -1,3 +1,5 @@
 <template>
 <div v-loading.fullscreen.lock="loading" :element-loading-text="language.loading.p9" element-loading-spinner="el-icon-loading">
   <el-dialog :title="language.integralTransfer.title" :visible.sync="dialogFormVisible" center :width="dialogWidth">
+    <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+      <div>
