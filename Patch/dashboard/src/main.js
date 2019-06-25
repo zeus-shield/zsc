76de20b461e104b537673cdde57bd38a722306a3
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-06-25 19:10:34 +0800

    add src\main.js

---
+++ Patch/dashboard/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import App from './App';
+
+Vue.use(ElementUI);
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app');
