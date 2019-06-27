5039ac6b3d5ad06267f7e9bc1f5f89ddac75da9a
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-06-27 19:43:18 +0800

    up src/main.js

--- Patch/dashboard/src/main.js
+++ Patch/dashboard/src/main.js
@@ -4,11 +4,13 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 import router from './router';
+import store from './store';
 import App from './App';
 
 Vue.use(ElementUI);
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app');
