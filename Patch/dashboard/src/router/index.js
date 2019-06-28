461b5e54762b2dcfc106d53debd82686e4564735
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-06-28 19:40:01 +0800

    up Patch/dashboard/src/router/index.js

--- Patch/dashboard/src/router/index.js
+++ Patch/dashboard/src/router/index.js
@@ -2,6 +2,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+// Method 3
+const Home = resolve => require.ensure([], () => resolve(require('@/views/Home')), 'lazy');
 Vue.use(Router);
 
 let router = new Router({});
