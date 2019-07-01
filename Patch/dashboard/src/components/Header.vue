47afe0fcb67820866d56dee67612f5a303566438
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-01 21:40:43 +0800

    up Patch/dashboard/src/components/Header.vue

--- Patch/dashboard/src/components/Header.vue
+++ Patch/dashboard/src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="header">
     <el-row class="header-menu" :gutter="0">
+
+      <!-- logo -->
+      <el-col :xs="{span: 4}" :sm="{span: 4}" :md="{span: 4}" :lg="{span: 4}">
+      </el-col>
     </el-row>
   </div>
 </template>
