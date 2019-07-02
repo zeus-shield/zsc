c3fa582bd789d37ba9b518cc10496f7fd54bc320
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-02 19:20:08 +0800

    up Patch/dashboard/src/App.vue

--- Patch/dashboard/src/App.vue
+++ Patch/dashboard/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <el-container>
       <el-header v-if="device == 'pc'">
+        <header-pc :activeIndex="activeIndex"></header-pc>
       </el-header>
     </el-container>
   </div>
