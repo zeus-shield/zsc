cac7c1552de4d82eae0553888c866f1fd69a2a12
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-02 19:20:38 +0800

    up Patch/dashboard/src/router/index.js

--- Patch/dashboard/src/router/index.js
+++ Patch/dashboard/src/router/index.js
@@ -6,6 +6,7 @@ import Router from 'vue-router';
 // Method 3
 const Home = resolve => require.ensure([], () => resolve(require('@/views/Home')), 'lazy');
 const Certificate = resolve => require.ensure([], () => resolve(require('@/views/Certificate')), 'lazy');
+const Analytics = resolve => require.ensure([], () => resolve(require('@/views/Analytics')), 'lazy');
 Vue.use(Router);
 
 let router = new Router({});
