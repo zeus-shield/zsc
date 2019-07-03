a1ce8b9177fbf557d76aed28e21be09f6f100ef7
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-03 19:10:51 +0800

    up Patch/dashboard/src/components/m/Header.vue

--- Patch/dashboard/src/components/m/Header.vue
+++ Patch/dashboard/src/components/m/Header.vue
@@ -2,6 +2,7 @@
   <div class="header">
     <div class="header-top">
       <i class="el-icon-tickets header-logo" @click="hide = !hide"></i>
+      <span class="header-title">Dashboard</span>
     </div>
   </div>
 </template>
