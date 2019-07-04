2d34352cfd030c281f294236d16cc4d6dec992d7
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-04 18:21:18 +0800

    up Patch/dashboard/src/common/utils.js

--- Patch/dashboard/src/common/utils.js
+++ Patch/dashboard/src/common/utils.js
@@ -4,6 +4,7 @@ const utils = {
     cookie: {
       set(key, value, expires) {
         if (expires === 'N/A') {
+          document.cookie = `${key}=${value};path=/;`;
         } else {
         }
       },
