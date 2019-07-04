e7cc6d69f09aa83e4e37be67da7b5cad85e13c4a
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-04 18:17:30 +0800

    up Patch/dashboard/src/components/404.vue

--- Patch/dashboard/src/components/404.vue
+++ Patch/dashboard/src/components/404.vue
@@ -2,6 +2,9 @@
   <div class="not-found">
     <img src="../assets/images/not_found.png">
     <p>您访问的页面不存在...</p>
+    <div>
+      <router-link :to="{path: '/'}"><el-button type="primary" plain>返回首页</el-button></router-link>
+    </div>
   </div>
 </template>
 <style scoped>
