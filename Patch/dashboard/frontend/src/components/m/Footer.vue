3a3d0716b0e6844e99c4fd150e27e8c27844616f
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-08 11:57:32 +0800

    add frontend\src\components\m\Footer.vue

---
+++ Patch/dashboard/frontend/src/components/m/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer">
+    <p>Copyright? 2018-2019 Dashboard(mobile)</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .footer {
+  }
+  .footer p {
+  }
+  .footer a {
+  }
+</style>
+
+<script>
+</script>
