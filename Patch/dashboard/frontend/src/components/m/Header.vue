bd1c9517be12f7c9f6ec4f8afc36a7b873c375aa
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-08 11:58:58 +0800

    add frontend\src\components\m\Header.vue

---
+++ Patch/dashboard/frontend/src/components/m/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="header">
+    <div class="header-top">
+      <i class="el-icon-tickets header-logo" @click="hide = !hide"></i>
+      <span class="header-title">Dashboard</span>
+    </div>
+    <el-collapse-transition>
+    </el-collapse-transition>
+  </div>
+</template>
+<style scoped>
+</style>
