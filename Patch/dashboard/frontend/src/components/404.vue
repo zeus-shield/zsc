ff14b517cce0a4e0e60c6eb0e082571e2a7f5534
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-09 15:43:09 +0800

    add files

---
+++ Patch/dashboard/frontend/src/components/404.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="not-found">
+    <img src="../assets/images/not_found.png">
+    <p>您访问的页面不存在...</p>
+    <div>
+      <router-link :to="{path: '/'}"><el-button type="primary" plain>返回首页</el-button></router-link>
+    </div>
+  </div>
+</template>
+<style scoped>
+</style>
---
+++ Patch/dashboard/frontend/src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="footer">
+    <p>Copyright? 2018-2019 Dashboard</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .footer {
+  }
+  .footer p {
+  }
+</style>
