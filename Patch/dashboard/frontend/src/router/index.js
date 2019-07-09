10f34892a9ef38c56f91ab0c309996e0ddb983d8
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-09 15:45:06 +0800

    add frontend\src\router\index.js

---
+++ Patch/dashboard/frontend/src/router/index.js
@@ -0,0 +1,23 @@
+
+import Vue from 'vue';
+import Router from 'vue-router';
+
+
+// Method 3
+const Home = resolve => require.ensure([], () => resolve(require('@/views/Home')), 'lazy');
+const Certificate = resolve => require.ensure([], () => resolve(require('@/views/Certificate')), 'lazy');
+const Analytics = resolve => require.ensure([], () => resolve(require('@/views/Analytics')), 'lazy');
+const FAQ = resolve => require.ensure([], () => resolve(require('@/views/FAQ')), 'lazy');
+Vue.use(Router);
+
+let router = new Router({
+  routes: [
+    {
+      path: '*',
+      component: NotFound
+    }
+    }
+  ]
+});
+
+export default router;
