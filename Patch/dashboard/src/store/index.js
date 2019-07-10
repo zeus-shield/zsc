5336f810318a2153d56d48547b7800d80f7a08c3
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-10 11:33:32 +0800

    remove src/store/index.js

--- Patch/dashboard/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-
-Vue.use(Vuex);
-
-const debug = process.env.NODE_ENV !== 'production';
-const store = new Vuex.Store({});
-
-export default store;
