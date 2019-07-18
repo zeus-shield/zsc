29a59ce3f94f590fd575bcb7ebab90b744dca910
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-18 15:11:22 +0800

    up Patch/dashboard/backend/config/errorMessage.js

--- Patch/dashboard/backend/config/errorMessage.js
+++ Patch/dashboard/backend/config/errorMessage.js
@@ -4,5 +4,11 @@ module.exports = {
     status: 500,
     errorCode: 10000,
     errorMessage: 'SERVER_ERROR'
+  },
+  'jwt expired': {
+    status: 500,
+    errorCode: 10001,
+    errorMessage: 'jwt expired'
+  }
   }
 }
