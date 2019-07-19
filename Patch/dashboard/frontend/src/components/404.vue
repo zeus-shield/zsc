fafa87e3a1ea049bdb8cbc61707d56117953f369
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-19 09:27:56 +0800

    up Patch/dashboard/frontend/src/components/404.vue

--- Patch/dashboard/frontend/src/components/404.vue
+++ Patch/dashboard/frontend/src/components/404.vue
@@ -1,11 +1,33 @@
 <template>
   <div class="not-found">
     <img src="../assets/images/not_found.png">
-    <p>您访问的页面不存在...</p>
+    <p>?????????...</p>
     <div>
-      <router-link :to="{path: '/'}"><el-button type="primary" plain>返回首页</el-button></router-link>
+      <router-link :to="{path: '/'}"><el-button type="primary" plain>????</el-button></router-link>
     </div>
   </div>
 </template>
 <style scoped>
+/*  html,body {
+    height: 100%;
+  }
+  #app {
+    height: 100%;
+    margin-top: 0;
+  }*/
+  .not-found {
+/*    position: absolute;*/
+/*    top: 20%;*/
+/*    width: 100%;*/
+    text-align: center;
+  }
+/*  .not-found img {
+    max-width: 100%;
+    max-height: 100%;
+  }*/
+  .not-found p {
+    margin: 20px;
+    font-size: 16px;
+    color: #0d3349;
+  }
 </style>
