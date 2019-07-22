ae2728a7037be0ce8cd92035737a450b9974b88e
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-22 17:06:01 +0800

    up Patch/dashboard/frontend/src/apis/user.js

--- Patch/dashboard/frontend/src/apis/user.js
+++ Patch/dashboard/frontend/src/apis/user.js
@@ -1,6 +1,13 @@
 'use strict';
 
 import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'http://localhost:3000/',
+  timeout: 2000
+  // withCredentials: true // bypass cookie from and to server
+});
+
 const user = {
   login(user, password, func) {
     setTimeout(() => {
