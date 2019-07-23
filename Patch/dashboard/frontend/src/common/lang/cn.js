272b8530b476d365905940f2ccb7002c8e363cf1
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-23 17:52:04 +0800

    up Patch/dashboard/frontend/src/common/lang/cn.js

--- Patch/dashboard/frontend/src/common/lang/cn.js
+++ Patch/dashboard/frontend/src/common/lang/cn.js
@@ -1,12 +1,13 @@
 
 const cn = {
   header: {
-    home: '首页', // 1
+    home: '??', // 1
     
-    insurance: '保单', // 2
-    certificate: '录入', // 21
-    analytics: '分析', // 22
+    insurance: '??', // 2
+    certificate: '??', // 21
+    analytics: '??', // 22
 
+    whitePaper: '???', // 3
     whitePaper: '白皮书', // 3
   }
 };
