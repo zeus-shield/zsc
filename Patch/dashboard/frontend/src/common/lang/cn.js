60ba27de010aaa9474d64221035d87c39f2a9141
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-24 17:32:27 +0800

    up Patch/dashboard/frontend/src/common/lang/cn.js

--- Patch/dashboard/frontend/src/common/lang/cn.js
+++ Patch/dashboard/frontend/src/common/lang/cn.js
@@ -1,14 +1,16 @@
 
 const cn = {
   header: {
-    home: '??', // 1
+    home: '首页', // 1
     
-    insurance: '??', // 2
-    certificate: '??', // 21
-    analytics: '??', // 22
+    insurance: '保单', // 2
+    certificate: '录入', // 21
+    analytics: '分析', // 22
 
-    whitePaper: '???', // 3
     whitePaper: '白皮书', // 3
+    faq: '常见问题', // 4
+    login: '登录', // 5
+    signUp: '免费注册', // 6
   }
 };
 export default cn;
