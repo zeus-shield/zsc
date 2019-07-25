c3fa3678a28d3a6a360bbd0bba8720b20ed7c2f8
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-25 09:30:47 +0800

    up Patch/dashboard/frontend/src/common/lang/cn.js

--- Patch/dashboard/frontend/src/common/lang/cn.js
+++ Patch/dashboard/frontend/src/common/lang/cn.js
@@ -11,6 +11,13 @@ const cn = {
     faq: '常见问题', // 4
     login: '登录', // 5
     signUp: '免费注册', // 6
+    
+    // pic 7
+    mine: '个人中心', // 71
+    logout: '注销', // 72
+    
+    lang: '中文（简体）' // 8
+  },
   }
 };
 export default cn;
