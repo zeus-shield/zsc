f3f22964395585e54b2566342119a85bf1c2203f
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-29 18:12:51 +0800

    up Patch/dashboard/frontend/src/components/Header.vue

--- Patch/dashboard/frontend/src/components/Header.vue
+++ Patch/dashboard/frontend/src/components/Header.vue
@@ -5,6 +5,9 @@
       <!-- logo -->
       <el-col :xs="{span: 4}" :sm="{span: 4}" :md="{span: 4}" :lg="{span: 4}">
         <div class="header-logo">
+          <!-- TODO -->
+          <img src="../assets/images/logo.png" style="height: 50px;">
+          <img src="../assets/images/dashboard.png" style="height: 100px; margin-top: 3px;">
         </div>
       </el-col>
 
