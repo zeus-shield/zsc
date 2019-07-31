061945942e35494646b5874b058d5da65827e955
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-07-31 19:13:56 +0800

    up Patch/dashboard/frontend/src/common/lang/cn.js

--- Patch/dashboard/frontend/src/common/lang/cn.js
+++ Patch/dashboard/frontend/src/common/lang/cn.js
@@ -13,12 +13,16 @@ const cn = {
     signUp: '免费注册', // 6
     
     // pic 7
-    mine: '个人中心', // 71
+    user: '个人中心', // 71
     logout: '注销', // 72
     
     lang: '中文（简体）' // 8
   },
   login: {
+    title: '用户登录',
+    account: '请输入手机号或邮箱地址',
+    password: '请输入登录密码',
+    submit: '登录'
   },
   signUp: {
   },
