8a227a66ad4ea2add5235890c0b0b33b81163d41
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-08-06 09:46:27 +0800

    Patch/dashboard/frontend/src/store/index.js

--- Patch/dashboard/frontend/src/store/index.js
+++ Patch/dashboard/frontend/src/store/index.js
@@ -5,6 +5,42 @@ import logColor from './modules/logColor';
 Vue.use(Vuex);
 
 const debug = process.env.NODE_ENV !== 'production';
-const store = new Vuex.Store({});
+
+const store = new Vuex.Store({
+  modules: {
+    logColor,
+    lang
+  },
+});
+
+// if (module.hot) {
+//   // 使 action 和 mutation 成为可热重载模块
+//   module.hot.accept(['./getters', './mutations', './actions'], () => {
+//     // 获取更新后的模块
+
+//     // for 'export default {...}'
+//     // 因为 babel 6 的模块编译格式问题，这里需要加上 `.default`
+//     // 加载新模块
+//     store.hotUpdate({
+//       getters: require('./getters').default,
+//       mutations: require('./mutations').default,
+//       actions: require('./actions').default
+//     });
+
+//     // for 'export const xxx = {...}'
+//     // store.hotUpdate({
+//     //   getters: require('./getters').getters,
+//     //   mutations: require('./mutations').mutations,
+//     //   actions: require('./actions').actions
+//     // })
+
+//     // for 'export const xxx = ...'
+//     // store.hotUpdate({
+//     //   getters: require('./getters'),
+//     //   mutations: require('./mutations'),
+//     //   actions: require('./actions')
+//     // })
+//   });
+// }
 
 export default store;
