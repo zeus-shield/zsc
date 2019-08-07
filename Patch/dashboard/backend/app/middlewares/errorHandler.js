ff516922ad478a73345e5c6d36924c8eab3bb12c
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-08-07 11:15:53 +0800

    up Patch/dashboard/backend/app/middlewares/errorHandler.js

--- Patch/dashboard/backend/app/middlewares/errorHandler.js
+++ Patch/dashboard/backend/app/middlewares/errorHandler.js
@@ -4,4 +4,8 @@ const debug = require('debug')('backend:app:middlewares:errorHandler');
 
 // error handler
 module.exports = (err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.sendErr(err);
 };
