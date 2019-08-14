8974505543c09d02169a5ac7a4bd11a3ef3abc42
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-08-14 09:43:32 +0800

    up Patch/dashboard/backend/app/middlewares/index.js

--- Patch/dashboard/backend/app/middlewares/index.js
+++ Patch/dashboard/backend/app/middlewares/index.js
@@ -4,3 +4,4 @@ const middlewares = {};
 middlewares.resExtend = require('./resExtend');
 middlewares.verifyToken = require('./verifyToken');
 middlewares.notFind = require('./notFind');
+module.exports = middlewares;
