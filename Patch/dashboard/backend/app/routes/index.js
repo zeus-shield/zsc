f1c174a960e3ddef62ae03007ec7faf2179823f0
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-08-20 17:52:31 +0800

    up Patch/dashboard/backend/app/routes/index.js

--- Patch/dashboard/backend/app/routes/index.js
+++ Patch/dashboard/backend/app/routes/index.js
@@ -3,5 +3,9 @@
 const controllers = require('../controllers');
 const middlewares = require('../middlewares');
 const middlewaresArr = [middlewares.verifyToken, middlewares.log];
+
+// const wrap = fn => (...args) => fn(...args).catch(args[2]); // req, res, next
+const wrap = fn => (...args) => fn(...args).catch(err => {args[1].sendErr(err);});
+
 module.exports = (app) => {
 };
