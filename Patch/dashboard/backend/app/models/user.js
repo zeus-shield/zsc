4aaed8e5d0b5b2de6748ffa8530326cf31977d8b
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-08-22 11:39:22 +0800

    up Patch/dashboard/backend/app/models/user.js

--- Patch/dashboard/backend/app/models/user.js
+++ Patch/dashboard/backend/app/models/user.js
@@ -3,6 +3,7 @@
 const mongoose = require('mongoose');
 const moment = require('moment');
 const {settings} = require('../../config');
+UserSchema.set('toJSON', { getters: true, virtuals: true });
 UserSchema.statics = {
 };
 
