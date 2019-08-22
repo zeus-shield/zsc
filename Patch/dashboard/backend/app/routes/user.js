ae2ac5f8ca8935e5243dfbc547407f5c66997bfd
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-08-22 11:40:30 +0800

    up Patch/dashboard/backend/app/routes/user.js

--- Patch/dashboard/backend/app/routes/user.js
+++ Patch/dashboard/backend/app/routes/user.js
@@ -4,3 +4,6 @@ const controllers = require('../controllers');
 
 // const wrap = fn => (...args) => fn(...args).catch(args[2]); // req, res, next
 const wrap = fn => (...args) => fn(...args).catch(err => {args[1].sendErr(err);});
+
+module.exports = (app) => {
+};
