61b15affb9d6155065f25e6c7ca97af192dd4272
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-08-28 15:40:40 +0800

    up Patch/dashboard/backend/app/utils/responseHandler.js

--- Patch/dashboard/backend/app/utils/responseHandler.js
+++ Patch/dashboard/backend/app/utils/responseHandler.js
@@ -5,5 +5,7 @@ const {errMsg} = require('../../config');
 
 module.exports = {
   getErrorRes(err) {
+    const result = errMsg[err.message] ? errMsg[err.message] : errMsg['SERVER_ERROR'];
+    return result;
   };
 }
