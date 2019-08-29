21daa45066e1ce0affe9272f5038cf93000cd7dd
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-08-29 14:55:20 +0800

    up Patch/dashboard/backend/app/routes/user.js

--- Patch/dashboard/backend/app/routes/user.js
+++ Patch/dashboard/backend/app/routes/user.js
@@ -7,4 +7,6 @@ const wrap = fn => (...args) => fn(...args).catch(err => {args[1].sendErr(err);}
 
 module.exports = (app) => {
   app.route('/user').get(wrap(controllers.user.list));
+  app.route('/user/detail')
+    .get(wrap(controllers.user.detail));
 };
