4988bb634f5823b889dd43c3c1d54529a680e5b4
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-09-04 17:32:44 +0800

    up Patch/dashboard/frontend/src/router/index.js

--- Patch/dashboard/frontend/src/router/index.js
+++ Patch/dashboard/frontend/src/router/index.js
@@ -21,7 +21,7 @@ import Router from 'vue-router';
 
 // Method 3
 const Home = resolve => require.ensure([], () => resolve(require('@/views/Home')), 'lazy');
-const Certificate = resolve => require.ensure([], () => resolve(require('@/views/Certificate')), 'lazy');
+const Insurance = resolve => require.ensure([], () => resolve(require('@/views/Insurance')), 'lazy');
 const Analytics = resolve => require.ensure([], () => resolve(require('@/views/Analytics')), 'lazy');
 const FAQ = resolve => require.ensure([], () => resolve(require('@/views/FAQ')), 'lazy');
 const Login = resolve => require.ensure([], () => resolve(require('@/views/Login')), 'lazy');
