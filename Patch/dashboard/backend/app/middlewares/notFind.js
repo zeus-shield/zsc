92f3d30bf9009a3426c0594b48ced3c6a1334215
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-09-04 17:22:29 +0800

    up Patch/dashboard/backend/app/middlewares/notFind.js

--- Patch/dashboard/backend/app/middlewares/notFind.js
+++ Patch/dashboard/backend/app/middlewares/notFind.js
@@ -2,3 +2,7 @@
 
 const debug = require('debug')('backend:app:middlewares:notFind');
 const createError = require('http-errors');
+
+// catch 404 and forward to error handler
+module.exports = (req, res, next) => {
+};
--- Patch/dashboard/backend/app/middlewares/resExtend.js
+++ Patch/dashboard/backend/app/middlewares/resExtend.js
@@ -1,3 +1,6 @@
 'use strict';
 const debug = require('debug')('backend:app:middlewares:resExtend');
 const {resHandler} = require('../utils');
+
+module.exports = (req, res, next) => {
+};
