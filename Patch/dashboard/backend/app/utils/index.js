57f534033d3b7714354881afb240832bd762168f
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-09-12 11:09:55 +0800

    up Patch/dashboard/backend/app/utils/index.js

--- Patch/dashboard/backend/app/utils/index.js
+++ Patch/dashboard/backend/app/utils/index.js
@@ -6,4 +6,5 @@ utils.crypto = require('./crypto');
 utils.paramsHandler = require('./params-handler');
 utils.resHandler = require('./responseHandler');
 utils.format = require('./format');
+utils.auth = require('./authentication');
 module.exports = utils;
