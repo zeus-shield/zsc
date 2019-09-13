d5e1d4abf42f246f6c7e9c7e6ec24f73910b0a40
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-09-13 16:34:27 +0800

    up Patch/dashboard/backend/app/middlewares/resExtend.js

--- Patch/dashboard/backend/app/middlewares/resExtend.js
+++ Patch/dashboard/backend/app/middlewares/resExtend.js
@@ -9,6 +9,15 @@ module.exports = (req, res, next) => {
       errorCode: 0,
       content: data
     };
+    logger.info(`traceId: ${req.headers.traceId}`);
+    logger.info(`method: [${req.method}] req-url: ${req.url}`);
+    // logger.info(`req-headers: ${JSON.stringify(req.headers)}`);
+    logger.info(`req-query: ${JSON.stringify(req.query)}`);
+    logger.info(`req-params: ${JSON.stringify(req.params)}`);
+    // if (req.url !== '/user/login') {
+    logger.info(`req-body: ${JSON.stringify(req.body)}`);
+    // }
+    logger.info(`sendOk: ${JSON.stringify(data)}`);
     res.status(200).send(rst);
   }
   res.sendErr = (err) => {
