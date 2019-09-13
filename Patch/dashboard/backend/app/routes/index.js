6b623ffbd54403ef5ebee1e7845c8372fee62dbb
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-09-13 16:37:31 +0800

    Patch/dashboard/backend/app/routes/index.js

--- Patch/dashboard/backend/app/routes/index.js
+++ Patch/dashboard/backend/app/routes/index.js
@@ -17,12 +17,15 @@ module.exports = (app) => {
   require('./user')(app);
 
   require('./company')(app);
-  require('./category')(app);
-  require('./article')(app);
-  app.route('/upload').post(Controllers.article.upload);
-  app.route('/test').post(Controllers.test.testResponse);
-  app.post(Controllers.test.test);
-  app.use('/test1', Controllers.test.test);
+  // require('./category')(app);
+  // require('./article')(app);
+  // app.route('/upload').post(Controllers.article.upload);
+  // app.route('/test').post(Controllers.test.testResponse);
+
+  // app.post(Controllers.test.test)
+
+  // app.use('/test1', Controllers.test.test);
+
   // catch 404 and forward to error handler
   app.use(middlewares.notFind);
 
