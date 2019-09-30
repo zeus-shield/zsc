bc4f3fefe0be2b98066b9989552aab578adaea20
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-09-30 09:49:17 +0800

    up Patch/dashboard/backend/app/routes/company.js

--- Patch/dashboard/backend/app/routes/company.js
+++ Patch/dashboard/backend/app/routes/company.js
@@ -8,4 +8,5 @@ const wrap = fn => (...args) => fn(...args).catch(err => {args[1].sendErr(err);}
 module.exports = (app) => {
   app.route('/company').get(wrap(controllers.company.list));
   app.route('/company/detail').get(wrap(controllers.company.detail));
+  app.route('/company/update').post(wrap(controllers.company.update));
 };
