75251fa2245ea05380fcb4a6f28edc7fe3d4d7d7
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-09-30 09:44:59 +0800

    up Patch/dashboard/frontend/src/components/m/Footer.vue

--- Patch/dashboard/frontend/src/components/m/Footer.vue
+++ Patch/dashboard/frontend/src/components/m/Footer.vue
@@ -33,8 +33,8 @@ export default {
     return {
     };
   },
-  props: {
-  },
+  // props: {
+  // },
   computed: {
     ...mapState({
       logColor: state => state.logColor.footer
@@ -52,6 +52,7 @@ export default {
   updated() {
     console.log('%c[Footer]updated()', `color:${this.logColor}`);
   },
+  methods: {
   }
 };
 </script>
