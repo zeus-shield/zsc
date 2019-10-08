31b668a5e1e62ac21d6176d741ca84b07a41c8be
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-10-08 08:55:31 +0800

    up Patch/dashboard/backend/app/utils/responseHandler.js

--- Patch/dashboard/backend/app/utils/responseHandler.js
+++ Patch/dashboard/backend/app/utils/responseHandler.js
@@ -8,12 +8,12 @@ module.exports = {
   //   const successRes = successMsg[succMsg] ? successMsg[succMsg] : successMsg['OPTION_SUCCESS']
   //   return successRes
   // },
-  getErrorMsg (error) {
-    const errorMessage = error.message ? error.message : 'serverError'
-    const finalError = errorSystem[errorMessage] ? errorSystem[errorMessage] : 'SERVER_ERROR'
-    const result = errorMsg[finalError]
-    return result
-  },
+  // getErrorMsg (error) {
+  //   const errorMessage = error.message ? error.message : 'serverError'
+  //   const finalError = errorSystem[errorMessage] ? errorSystem[errorMessage] : 'SERVER_ERROR'
+  //   const result = errorMsg[finalError]
+  //   return result
+  // },
   getErrorRes(err) {
     const result = errMsg[err.message] ? errMsg[err.message] : errMsg['SERVER_ERROR'];
     if (err.message !== undefined) {
