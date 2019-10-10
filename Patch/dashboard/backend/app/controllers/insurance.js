c80e1d6546ad4026a7de32ee5f314f2008dd4b28
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-10-10 16:15:28 +0800

    up Patch/dashboard/backend/app/controllers/insurance.js

--- Patch/dashboard/backend/app/controllers/insurance.js
+++ Patch/dashboard/backend/app/controllers/insurance.js
@@ -3,3 +3,5 @@
 const mongoose = require('mongoose');
 const createError = require('http-errors');
 const services = require('../services');
+
+const debug = require('debug')('backend:app:controllers:insurance');
