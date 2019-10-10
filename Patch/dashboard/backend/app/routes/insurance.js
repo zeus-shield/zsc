de4dc6e80faeffe97d581aaddf3bde08ed1ca9ca
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-10-10 16:16:38 +0800

    up Patch/dashboard/backend/app/routes/insurance.js

--- Patch/dashboard/backend/app/routes/insurance.js
+++ Patch/dashboard/backend/app/routes/insurance.js
@@ -7,4 +7,5 @@ const wrap = fn => (...args) => fn(...args).catch(err => {args[1].sendErr(err);}
 
 module.exports = (app) => {
   app.route('/insurance').get(wrap(controllers.insurance.list));
+  app.route('/insurance/count').get(wrap(controllers.insurance.count));
 };
