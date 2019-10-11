82793b563240722736dd44f2d55b9cd04b678ca7
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-10-11 14:15:58 +0800

    up Patch/dashboard/backend/app/controllers/insurance.js

--- Patch/dashboard/backend/app/controllers/insurance.js
+++ Patch/dashboard/backend/app/controllers/insurance.js
@@ -5,3 +5,6 @@ const createError = require('http-errors');
 const services = require('../services');
 
 const debug = require('debug')('backend:app:controllers:insurance');
+
+const add = async (req, res) => {
+};
