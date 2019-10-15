9df0fd1f19b6594fa0cf2aba6ad3c9ec626a333f
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-10-15 10:19:41 +0800

    up Patch/dashboard/backend/config/settings.js

--- Patch/dashboard/backend/config/settings.js
+++ Patch/dashboard/backend/config/settings.js
@@ -4,6 +4,8 @@ module.exports = {
   jwtExpires: 3600 * 24, //s
   port: process.env.PORT || 4000,
   website: '127.0.0.1:4000',
+  dbConfig: {
+  },
   mongooseDebug: true,
   stmpConfig: {
     host: 'smtp.163.com',
