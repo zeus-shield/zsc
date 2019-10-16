61ae0f16f9065c8d57dc1673090414db64646bce
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-10-16 15:36:35 +0800

    up Patch/dashboard/frontend/src/apis/user.js

--- Patch/dashboard/frontend/src/apis/user.js
+++ Patch/dashboard/frontend/src/apis/user.js
@@ -112,6 +112,22 @@ const user = {
     data.append('TOTPOn', on);
 
     let config = {
+      // req.headers
+      headers: {
+        'token': token,
+        'code': code,
+        '_id': id
+      }
+
+      // req.query
+      // `params` are the URL parameters to be sent with the request
+      // Must be a plain object or a URLSearchParams object
+      // params: data
+
+      // req.body
+      // Only applicable for request methods 'PUT', 'POST', and 'PATCH'
+      // - Node only: JSON Object
+      // data: querystring.parse(data.toString())
     };
 
     try {
