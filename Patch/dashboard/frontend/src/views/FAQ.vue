09a2bcc9d0937921d37b1c513df04f90382d5c4f
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-10-23 16:57:31 +0800

    up Patch/dashboard/frontend/src/views/FAQ.vue

--- Patch/dashboard/frontend/src/views/FAQ.vue
+++ Patch/dashboard/frontend/src/views/FAQ.vue
@@ -33,6 +33,8 @@ export default {
   updated() {
     console.log('%c[FAQ]updated()', `color:${this.logColor}`);
   },
+  beforeRouteUpdate(to, from, next) {
+  },
   beforeRouteLeave(to, from, next) {
     console.log('%c[FAQ]beforeRouteLeave(\"%s\" => \"%s\")', `color:${this.logColor}`, from.fullPath, to.fullPath);
     next();
