82df41b706cbb203caa64e533ba70d8718c79dda
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-10-28 10:01:32 +0800

    up Patch/dashboard/frontend/src/apis/index.js

--- Patch/dashboard/frontend/src/apis/index.js
+++ Patch/dashboard/frontend/src/apis/index.js
@@ -6,6 +6,12 @@ const instance = axios.create({
   timeout: 2000
   // withCredentials: true // bypass cookie from and to server
 });
+
+import Wallet from './ethereum/wallet';
+import User from './userClass';
+import Insurance from './insuranceClass';
+import Company from './companyClass';
+
 class APIs {
 }
 
