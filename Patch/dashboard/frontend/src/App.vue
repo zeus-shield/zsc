b9191b1762fb2f78f9d8cee3b487e5fbfd48f84d
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-11-07 17:37:59 +0800

    up Patch/dashboard/frontend/src/App.vue

--- Patch/dashboard/frontend/src/App.vue
+++ Patch/dashboard/frontend/src/App.vue
@@ -75,6 +75,8 @@
  .el-popper[x-placement^=top] {
   margin-bottom:12px
 }
+.el-popper[x-placement^=top] .popper__arrow {
+}
 .el-popper[x-placement^=bottom] {
   // margin-top:12px
   margin-top:5px
