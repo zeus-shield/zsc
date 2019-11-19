0eca2276b2bf10b0337880fca23d210cff854b73
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-11-19 17:18:11 +0800

    up Patch/dashboard/frontend/src/App.vue

--- Patch/dashboard/frontend/src/App.vue
+++ Patch/dashboard/frontend/src/App.vue
@@ -76,6 +76,11 @@
 //   margin-bottom:12px
 // }
 .el-popper[x-placement^=top] .popper__arrow {
+  bottom:-6px;
+  left:50%;
+  margin-right:3px;
+  border-top-color:#EBEEF5;
+  border-bottom-width:0
 }
 .el-popper[x-placement^=top] .popper__arrow::after {
 }
