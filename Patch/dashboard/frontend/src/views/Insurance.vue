24a9efcf348ee432935bdd630be040c769315841
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-12-03 16:14:22 +0800

    up Patch/dashboard/frontend/src/views/Insurance.vue

--- Patch/dashboard/frontend/src/views/Insurance.vue
+++ Patch/dashboard/frontend/src/views/Insurance.vue
@@ -5,7 +5,14 @@
 
 <style lang="scss">
 </style>
+
 <script>
+// import dummyData from '../apis/insuranceDummyData';
+import InsurancePingAnBrief from '@/views/templates/insurances/pingan/brief';
+import InsurancePICCBrief from '@/views/templates/insurances/picc/brief';
+
+import { mapState, mapGetters } from 'vuex';
+
 import utils from '../common/utils';
 import APIs from '../apis';
 
