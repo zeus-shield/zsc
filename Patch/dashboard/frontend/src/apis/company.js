0d089125043170e36a2725471bf037399c76bf05
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-12-04 09:22:45 +0800

    up Patch/dashboard/frontend/src/apis/company.js

--- Patch/dashboard/frontend/src/apis/company.js
+++ Patch/dashboard/frontend/src/apis/company.js
@@ -2,7 +2,11 @@
 
 import axios from 'axios';
 import querystring from 'querystring';
+
 const instance = axios.create({
+  baseURL: 'http://localhost:3000/',
+  timeout: 2000
+  // withCredentials: true // bypass cookie from and to server
 });
 
 const company = {
