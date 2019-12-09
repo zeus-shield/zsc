8f220ed207a6ba892a5d746c2f00f2b9a2983d92
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-12-09 14:11:38 +0800

    up Patch/dashboard/frontend/src/apis/insurance.js

--- Patch/dashboard/frontend/src/apis/insurance.js
+++ Patch/dashboard/frontend/src/apis/insurance.js
@@ -15,6 +15,11 @@ const insurance = {
       let res = await instance.post('insurance/add', null, config);
       return res.data;
     } catch (err) {
+      if (err.response !== undefined) {
+        throw err.response.data;
+      } else {
+        throw err.message;
+      }
     }
   },
 };
