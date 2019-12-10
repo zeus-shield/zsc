8f9beae88f2df40760b180d2c31a34916e3a16fa
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-12-10 11:09:30 +0800

    up Patch/dashboard/backend/app/controllers/company.js

--- Patch/dashboard/backend/app/controllers/company.js
+++ Patch/dashboard/backend/app/controllers/company.js
@@ -11,8 +11,8 @@ const add = async (req, res) => {
   }
 };
 
-const remove = async (req, res) => {
-  debug("remove(%s)", req.body.name);
+const remove = async(req, res) => {
+  debug('remove(%s)', req.body.name);
 
   let session = null;
   try {
@@ -20,8 +20,8 @@ const remove = async (req, res) => {
   }
 };
 
-const removeCategory = async (req, res) => {
-  debug("removeCategory(%s, %s)", req.body.name, req.body.category);
+const removeCategory = async(req, res) => {
+  debug('removeCategory(%s, %s)', req.body.name, req.body.category);
 
   let session = null;
   try {
@@ -58,12 +58,12 @@ const removeCategory = async (req, res) => {
   }
 };
 
-const update = async (req, res) => {
+const update = async(req, res) => {
   const name = req.body.name;
   const newName = req.body.newName;
   const newCategoryNames = JSON.parse(req.body.newCategoryNames);
 
-  debug("update(%s, %s, %s)", name, newName, newCategoryNames);
+  debug('update(%s, %s, %s)', name, newName, newCategoryNames);
 
   let session = null;
   try {
@@ -80,7 +80,7 @@ const update = async (req, res) => {
       throw createError('COMPANY_NOT_EXIST');
     }
 
-    for(let i=0; i<newCategoryNames.length; i++) {
+    for (let i = 0; i < newCategoryNames.length; i++) {
       if (result.categories[i] !== undefined && result.categories[i] !== null) {
         const categoryName = result.categories[i].name;
         const newCategoryName = newCategoryNames[i];
@@ -120,8 +120,8 @@ const update = async (req, res) => {
   }
 };
 
-const list = async (req, res) => {
-  debug("list()");
+const list = async(req, res) => {
+  debug('list()');
   try {
     const result = await services.companies.findAll(null);
     res.sendOk(result);
@@ -130,22 +130,22 @@ const list = async (req, res) => {
   }
 };
 
-const detail = async (req, res) => {
-  debug("detail(%s)", req.query.name);
+const detail = async(req, res) => {
+  debug('detail(%s)', req.query.name);
   try {
     const result = await services.companies.findByName(req.query.name, null);
     if (!result) {
       throw createError('COMPANY_NOT_EXIST');
     }
-    
+
     res.sendOk(result);
   } catch (err) {
     res.sendErr(err);
   }
 };
 
-const groupCategoriesByName = async (req, res) => {
-  debug("groupCategoriesByName()");
+const groupCategoriesByName = async(req, res) => {
+  debug('groupCategoriesByName()');
   try {
     const result = await services.companies.groupCategoriesByName(null);
     res.sendOk(result);
@@ -154,4 +154,4 @@ const groupCategoriesByName = async (req, res) => {
   }
 };
 
-module.exports = {add, remove, removeCategory, update, list, detail, groupCategoriesByName};
+module.exports = { add, remove, removeCategory, update, list, detail, groupCategoriesByName };
