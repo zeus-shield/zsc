f22aff017b8b99ea3f4748bd3521e1d60d95288e
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-12-10 11:10:35 +0800

    up Patch/dashboard/backend/app/routes/user.js

--- Patch/dashboard/backend/app/routes/user.js
+++ Patch/dashboard/backend/app/routes/user.js
@@ -9,8 +9,8 @@ module.exports = (app) => {
   app.route('/user').get(wrap(controllers.user.list));
   app.route('/user/detail')
     .get(wrap(controllers.user.detail));
-    // .put(controllers.user.update)
-    // .delete(controllers.user.destroy)
+  // .put(controllers.user.update)
+  // .delete(controllers.user.destroy)
   app.route('/user/setTOTP').post(wrap(controllers.user.setTOTP));
   app.route('/user/saveTOTP').post(wrap(controllers.user.saveTOTP));
   app.route('/user/updateTOTP').post(wrap(controllers.user.updateTOTP));
