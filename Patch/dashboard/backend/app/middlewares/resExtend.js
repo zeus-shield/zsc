bd9a451180e3ee03eeed694d917b49eaddc02bf0
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-12-10 11:07:18 +0800

    up Patch/dashboard/backend/app/middlewares/resExtend.js

--- Patch/dashboard/backend/app/middlewares/resExtend.js
+++ Patch/dashboard/backend/app/middlewares/resExtend.js
@@ -1,6 +1,6 @@
 'use strict';
-const debug = require('debug')('backend:app:middlewares:resExtend');
-const {resHandler} = require('../utils');
+
+const { resHandler } = require('../utils');
 
 module.exports = (req, res, next) => {
   res.sendOk = (data) => {
@@ -9,27 +9,27 @@ module.exports = (req, res, next) => {
       errorCode: 0,
       content: data
     };
-    logger.info(`traceId: ${req.headers.traceId}`);
-    logger.info(`method: [${req.method}] req-url: ${req.url}`);
-    // logger.info(`req-headers: ${JSON.stringify(req.headers)}`);
-    logger.info(`req-query: ${JSON.stringify(req.query)}`);
-    logger.info(`req-params: ${JSON.stringify(req.params)}`);
+    global.logger.debug(`traceId: ${req.headers.traceId}`);
+    global.logger.info(`method: [${req.method}] req-url: ${req.url}`);
+    // logger.debug(`req-headers: ${JSON.stringify(req.headers)}`);
+    global.logger.debug(`req-query: ${JSON.stringify(req.query)}`);
+    global.logger.debug(`req-params: ${JSON.stringify(req.params)}`);
     // if (req.url !== '/user/login') {
-    logger.info(`req-body: ${JSON.stringify(req.body)}`);
+    global.logger.debug(`req-body: ${JSON.stringify(req.body)}`);
     // }
-    logger.info(`sendOk: ${JSON.stringify(data)}`);
+    global.logger.debug(`sendOk: ${JSON.stringify(data)}`);
     res.status(200).send(rst);
-  }
+  };
   res.sendErr = (err) => {
-    logger.error(`error: [name:${err.name}][message:${err.message}][status:${err.status}]`);
+    global.logger.error(`error: [name:${err.name}][message:${err.message}][status:${err.status}]`);
     const errRes = resHandler.getErrorRes(err);
-    logger.error(`traceId: ${req.headers.traceId}`);
-    logger.error(`method: [${req.method}] req-url: ${req.url}`);
-    logger.error(`req-query: ${JSON.stringify(req.query)}`);
-    logger.error(`req-params: ${JSON.stringify(req.params)}`);
-    logger.error(`req-body: ${JSON.stringify(req.body)}`);
-    logger.error(`sendErr: ${JSON.stringify(errRes)}`);
+    global.logger.error(`traceId: ${req.headers.traceId}`);
+    global.logger.error(`method: [${req.method}] req-url: ${req.url}`);
+    global.logger.error(`req-query: ${JSON.stringify(req.query)}`);
+    global.logger.error(`req-params: ${JSON.stringify(req.params)}`);
+    global.logger.error(`req-body: ${JSON.stringify(req.body)}`);
+    global.logger.error(`sendErr: ${JSON.stringify(errRes)}`);
     res.status(errRes.status || 500).send(errRes);
-  }
+  };
   next();
 };
