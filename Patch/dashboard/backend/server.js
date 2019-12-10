c2d04a78a82a62776d2f5f062f2c6c6eb8988d45
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-12-10 11:15:11 +0800

    up Patch/dashboard/backend/server.js

--- Patch/dashboard/backend/server.js
+++ Patch/dashboard/backend/server.js
@@ -1,4 +1,14 @@
 'use strict';
 
 require('babel-register');
-require('./app');
+const proj = process.argv[2];
+if (proj === undefined) {
+  console.log('Project is undefined, project config error!');
+} else {
+  if (proj === 'dashboard') {
+    console.log('Project(dashboard) config success!');
+    require('./app');
+  } else {
+    console.log('No project(' + proj + '), project config error!');
+  }
+}
