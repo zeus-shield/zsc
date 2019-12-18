4bca7a5647c5227d5303708732bbbfa549c5fc48
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-12-18 09:08:51 +0800

    up Patch/dashboard/frontend/src/store/modules/logColor.js

--- Patch/dashboard/frontend/src/store/modules/logColor.js
+++ Patch/dashboard/frontend/src/store/modules/logColor.js
@@ -17,6 +17,10 @@ const state = () => {
       detail: 'black',
       google: 'black',
       policy: 'black',
+      pc: {
+      },
+      mobile: {
+      }
     },
     insurance: {
       base: 'black',
