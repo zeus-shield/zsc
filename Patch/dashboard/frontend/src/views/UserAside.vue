2bd5ac3d822bb41856964bfdfcba607309d890b2
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-12-19 11:00:31 +0800

    up Patch/dashboard/frontend/src/views/UserAside.vue

--- Patch/dashboard/frontend/src/views/UserAside.vue
+++ Patch/dashboard/frontend/src/views/UserAside.vue
@@ -20,9 +20,9 @@
 .menu .menu-bottom-border {
   border-bottom: 1px solid #e9e9e9;
 }
-.menu-container .menu .menu-item.is-active, .menu-container .menu .menu-item:hover {
-  background: rgba(48,117,238,.05);
-  box-shadow: inset 3px 0 0 0 #409EFF;
+.menu-container .menu .item span:hover {
+  color: #f91;
+  text-decoration: underline;
 }
 </style>
 
