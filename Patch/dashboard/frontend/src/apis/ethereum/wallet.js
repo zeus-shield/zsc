af62dbda1e6f6e9aff1afe607754a11267ecc6c1
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-12-24 10:51:09 +0800

    up Patch/dashboard/frontend/src/apis/ethereum/wallet.js

--- Patch/dashboard/frontend/src/apis/ethereum/wallet.js
+++ Patch/dashboard/frontend/src/apis/ethereum/wallet.js
@@ -11,7 +11,46 @@ class Wallet {
     // 3. enabled
     this.state = 'unstalled';
   };
+
   async enable() {
+    // let web3_ = undefined;
+    // let provider = undefined;
+    // if (typeof window.ethereum !== 'undefined' || (typeof window.web3 !== 'undefined')) {
+    //   // Web3 browser user detected. You can now use the provider.
+    //   // provider = provider = window['ethereum'] || window.web3.currentProvider;
+    //   // web3_ = new Web3(provider);
+
+    //   window.ethereum.enable()
+    //   // provider.enable()
+    //   .then(accounts => {
+    //     // You now have an array of accounts!
+    //     // Currently only ever one:
+    //     // ['0xFDEa65C8e26263F6d9A1B5de9555D2931A33b825']
+    //     // alert(accounts);
+    //     // const message = '就快完成了！作为最后一步，MetaMask会要求您以数字方式登录您的钱包，并将其与谜恋猫链接。单击按钮以继续';
+    //     // utils.notice.confirm(vm, 'warning', vm.langSet.component.alert.warningTitle, message, vm.langSet.component.button.confirm, vm.langSet.component.button.cancel, true, true, () => {
+    //     //   alert('confirm');
+    //     // }, () => {
+    //     //   alert('cancel');
+    //     // });
+    //     if (func !== null) {
+    //       func('enable', accounts[0]);
+    //     }
+    //   })
+    //   .catch(reason => {
+    //     // Handle error. Likely the user rejected the login:
+    //     // alert(reason);
+    //     // console.log(reason === 'User rejected provider access');
+    //     func('enable', accounts[0]);
+    //   });
+    // } else {
+    //   // set the provider you want from Web3.providers
+    //   // provider = new Web3.providers.HttpProvider('http://localhost:8545');
+    //   // web3_ = new Web3(provider);
+    //   alert('By signing up, you agree to our Terms of Service and Privacy Policy');
+    //   func('web3');
+    // }
+
     try {
       // check if wallet install ?
       // if (typeof window.ethereum === 'undefined' && (typeof window.web3 === 'undefined')) {
