4e08f2f3f293e57025a8963a29cefa50eb4d74c2
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2019-12-24 10:54:15 +0800

    up Patch/dashboard/frontend/src/views/Analytics.vue

--- Patch/dashboard/frontend/src/views/Analytics.vue
+++ Patch/dashboard/frontend/src/views/Analytics.vue
@@ -152,8 +152,8 @@ export default {
 
       let option = {
         title: {
-          text: utils.storage.cookie.get('lang') === 'en' ? 'Insurance ranking' : '险种热度排行榜',
-          subtext: utils.storage.cookie.get('lang') === 'en' ? '(TOP10)' : '（排列前十名的险种）',
+          text: utils.storage.cookie.get('lang') === 'en' ? 'Insurance ranking' : '???????',
+          subtext: utils.storage.cookie.get('lang') === 'en' ? '(TOP10)' : '(????????)',
           x: 'center',
           textStyle: {
             color: '#000000',
@@ -170,6 +170,20 @@ export default {
           data: this.pieLegendData
         },
         series: [
+          {
+            name: utils.storage.cookie.get('lang') === 'en' ? 'Policy: ' : '?? :',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: this.pieSeriesData,
+            itemStyle: {
+              emphasis: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
         ]
       };
     },
