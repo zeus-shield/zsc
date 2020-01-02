719d9797a21d655918d833e29f944e3843c03e2a
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-01-02 09:48:59 +0800

    up Patch/dashboard/frontend/src/views/Login.vue

--- Patch/dashboard/frontend/src/views/Login.vue
+++ Patch/dashboard/frontend/src/views/Login.vue
@@ -77,7 +77,13 @@ export default {
     };
     let validateCode = (rule, value, callback) => {
       if (value === '') {
+        callback(new Error(this.langSet['message']['error']['USER_GOOGLE_CODE_NONE']));
       } else {
+        if (value.length !== 6) {
+          callback(new Error(this.langSet['message']['error']['USER_GOOGLE_CODE_LEN_ERR']));
+        } else {
+          callback();
+        }
       }
     };
     return {
