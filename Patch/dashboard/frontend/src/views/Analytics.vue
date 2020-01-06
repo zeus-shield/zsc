fceabe7038d08169c616b1177485a60247b201be
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-01-06 09:37:30 +0800

    up Patch/dashboard/frontend/src/views/Analytics.vue

--- Patch/dashboard/frontend/src/views/Analytics.vue
+++ Patch/dashboard/frontend/src/views/Analytics.vue
@@ -29,6 +29,10 @@
   padding: 1.25rem; // 20px
 }
 .header {
+  font-size: 1.5rem; // 24px;
+  font-weight: 400;
+  padding: 0.5rem 0;
+  border-bottom: 0.0625rem solid #e4e4e4;
 }
 .header .sprite {
 }
