60e50536bed37c7882f818ec63be03a703c139c1
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-01-07 09:42:20 +0800

    up Patch/dashboard/frontend/src/views/Analytics.vue

--- Patch/dashboard/frontend/src/views/Analytics.vue
+++ Patch/dashboard/frontend/src/views/Analytics.vue
@@ -208,12 +208,6 @@ export default {
     },
     drawBar() {
       this.chartBar = echarts.init(document.getElementById('chartBar'));
-
-      // var dataAxis = ['点', '击', '柱', '子', '或', '者', '两', '指', '在', '触', '屏', '上', '滑', '动', '能', '够', '自', '动', '缩', '放'];
-      // var data = [220, 182, 191, 234, 290, 330, 310, 123, 442, 321, 90, 149, 210, 122, 133, 334, 198, 123, 125, 220];
-      // var yMax = 500;
-      // var dataShadow = [];
-
       for (var i = 0; i < this.barData.length; i++) {
         this.barDataShadow.push(20);
       }
