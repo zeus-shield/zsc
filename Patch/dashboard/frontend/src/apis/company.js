e0766d57f53f2a1d6829714596875c7388894ef3
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-01-16 10:29:30 +0800

    up Patch/dashboard/frontend/src/apis/company.js

--- Patch/dashboard/frontend/src/apis/company.js
+++ Patch/dashboard/frontend/src/apis/company.js
@@ -16,6 +16,25 @@ const company = {
     data.append('name', name);
     // categories is array
     data.append('categories', JSON.stringify(categories));
+
+    let config = {
+      // req.headers
+      headers: {
+        'token': token
+        // 'name': name,
+        // 'categories': JSON.stringify(categories)
+      },
+
+      // req.query
+      // `params` are the URL parameters to be sent with the request
+      // Must be a plain object or a URLSearchParams object
+      // params: data
+
+      // req.body
+      // Only applicable for request methods 'PUT', 'POST', and 'PATCH'
+      // - Node only: JSON Object
+      data: querystring.parse(data.toString())
+    };
   },
 
   async remove(token, name) {
