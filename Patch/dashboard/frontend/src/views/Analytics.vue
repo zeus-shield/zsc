4c0e5bd6e7077cfa5845e8139c60d48688144f4c
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-02-17 12:58:13 +0800

    up Patch/dashboard/frontend/src/views/Analytics.vue

--- Patch/dashboard/frontend/src/views/Analytics.vue
+++ Patch/dashboard/frontend/src/views/Analytics.vue
@@ -280,6 +280,12 @@ export default {
             itemStyle: {
               normal: {
                 color: new echarts.graphic.LinearGradient(
+                  0, 0, 0, 1,
+                  [
+                    {offset: 0, color: '#83bff6'},
+                    {offset: 0.5, color: '#188df0'},
+                    {offset: 1, color: '#188df0'}
+                  ]
                 )
               },
               emphasis: {
