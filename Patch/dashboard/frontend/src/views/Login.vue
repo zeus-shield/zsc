c0942d11842a53a74ca448556d7cf654b3657ab8
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-02-20 16:32:12 +0800

    up Patch/dashboard/frontend/src/views/Login.vue

--- Patch/dashboard/frontend/src/views/Login.vue
+++ Patch/dashboard/frontend/src/views/Login.vue
@@ -127,8 +127,8 @@ export default {
       },
       rules: {
         account: [
-          // { required: true, message: '??????/????', trigger: 'blur' },
-          // { min: 6, max: 18, message: '??? 6 ? 18 ???', trigger: 'blur' }
+          // { required: true, message: '请输入手机号/邮箱地址', trigger: 'blur' },
+          // { min: 6, max: 18, message: '长度在 6 到 18 个字符', trigger: 'blur' }
           // { validator: validateAccount, trigger: ['blur', 'change'] },
           { validator: validateAccount, trigger: 'blur' }
         ],
@@ -235,6 +235,9 @@ export default {
         if (valid) {
           vm.errorMessage = '';
           vm.loading = true;
+          // 1. common callback case
+          user.loginCommon(vm.form.account, vm.form.password, (err, data) => {
+          // });
         } else {
           // console.log('error submit!!');
           return false;
