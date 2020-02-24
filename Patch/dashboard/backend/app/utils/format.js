e1ae3409a7a0d11a996834ac9d3e297d6a3b33ed
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-02-24 15:39:31 +0800

    up Patch/dashboard/backend/app/utils/format.js

--- Patch/dashboard/backend/app/utils/format.js
+++ Patch/dashboard/backend/app/utils/format.js
@@ -18,14 +18,14 @@ module.exports = {
     //   data.reSex = enums.user[data.sex];
     // }
     delete data.password;
-    delete data.emailCode;
-    data.createdAt = this.formatDate(data.createdAt);
-    data.updatedAt = this.formatDate(data.updatedAt);
-    if (data.lastLoginAt) {
-      data.lastLoginAt = this.formatDate(data.lastLoginAt);
+    delete data.email_code;
+    data.created_at = this.formatDate(data.created_at);
+    data.updated_at = this.formatDate(data.updated_at);
+    if (data.last_login_at) {
+      data.last_login_at = this.formatDate(data.last_login_at);
     }
-    if (data.activeDateExpiresAt) {
-      data.activeDateExpiresAt = this.formatDate(data.activeDateExpiresAt);
+    if (data.active_expires_at) {
+      data.active_expires_at = this.formatDate(data.active_expires_at);
     }
     return data;
   },
