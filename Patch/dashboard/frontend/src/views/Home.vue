39b462dbeb5239fd39f641aaea6147b5b8393981
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-02-28 23:10:10 +0800

    up Patch/dashboard/frontend/src/views/Home.vue

--- Patch/dashboard/frontend/src/views/Home.vue
+++ Patch/dashboard/frontend/src/views/Home.vue
@@ -42,6 +42,9 @@
           <div class="header">{{ langSet.home.slot4.header }}</div>
           <el-col v-for="(item, index) in 4" :key="item" :xs="{span: 24, offset: 0}" :sm="{span: 24, offset: 0}" :md="{span: 24, offset: 0}" :lg="{span: 6, offset: 0}">
             <el-card class="card" shadow="never" :body-style="{ padding: '1.25rem' }">
+              <div>
+                <span class="content">{{ langSet.home.slot4.content[item-1] }}</span>
+              </div>
             </el-card>
           </el-col>
         </el-row>
