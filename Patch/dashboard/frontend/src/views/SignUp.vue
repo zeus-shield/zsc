3a479510dcae87d77f92a79c2125192ebed273e2
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-02-28 23:08:43 +0800

    up Patch/dashboard/frontend/src/views/SignUp.vue

--- Patch/dashboard/frontend/src/views/SignUp.vue
+++ Patch/dashboard/frontend/src/views/SignUp.vue
@@ -140,10 +140,13 @@ export default {
           { validator: validatePhoneAccount, trigger: 'blur' }
         ],
         emailAccount: [
+          // { required: true, message: '??????/????', trigger: 'blur' },
+          // { min: 6, max: 18, message: '??? 6 ? 18 ???', trigger: 'blur' }
+          { validator: validateEmailAccount, trigger: 'blur' }
         ],
         code: [
-          // { required: true, message: '请输入手机号/邮箱地址', trigger: 'blur' },
-          // { min: 6, max: 18, message: '长度在 6 到 18 个字符', trigger: 'blur' }
+          // { required: true, message: '??????/????', trigger: 'blur' },
+          // { min: 6, max: 18, message: '??? 6 ? 18 ???', trigger: 'blur' }
           { validator: validateCode, trigger: 'blur' }
         ],
         password: [
