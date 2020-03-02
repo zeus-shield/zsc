b7e5bb17b2fd9ac2e7dda596bd69d316412cda5d
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-03-02 12:56:35 +0800

    up Patch/dashboard/frontend/src/views/Home.vue

--- Patch/dashboard/frontend/src/views/Home.vue
+++ Patch/dashboard/frontend/src/views/Home.vue
@@ -42,6 +42,8 @@
           <div class="header">{{ langSet.home.slot4.header }}</div>
           <el-col v-for="(item, index) in 4" :key="item" :xs="{span: 24, offset: 0}" :sm="{span: 24, offset: 0}" :md="{span: 24, offset: 0}" :lg="{span: 6, offset: 0}">
             <el-card class="card" shadow="never" :body-style="{ padding: '1.25rem' }">
+              <img class="img" :src="getImgUrl('card', 2, item)">
+              <span class="title">{{ langSet.home.slot4.title[item-1] }}</span>
               <div>
                 <span class="content">{{ langSet.home.slot4.content[item-1] }}</span>
               </div>
