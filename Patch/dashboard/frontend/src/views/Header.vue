a5069388ff4e48a2098fd5fae0cf3234ff7ac25e
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-03-06 22:32:04 +0800

    up Patch/dashboard/frontend/src/views/Header.vue

--- Patch/dashboard/frontend/src/views/Header.vue
+++ Patch/dashboard/frontend/src/views/Header.vue
@@ -4,6 +4,11 @@
 
       <!-- logo -->
       <el-col :xs="{span: 4}" :sm="{span: 4}" :md="{span: 4}" :lg="{span: 4}">
+        <div class="header-logo">
+        </div>
+      </el-col>
+
+      <el-col :xs="{span: 12}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 12}">
       </el-col>
     </el-row>
   </div>
