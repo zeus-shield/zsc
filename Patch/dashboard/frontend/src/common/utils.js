b26b2bca8c6822f4d59b7b40ba9c40f0adf9142a
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-03-12 09:28:16 +0800

    up Patch/dashboard/frontend/src/common/utils.js

--- Patch/dashboard/frontend/src/common/utils.js
+++ Patch/dashboard/frontend/src/common/utils.js
@@ -68,6 +68,11 @@ const utils = {
   },
   string: {
     omit(str, len) {
+      let reg = /[\u4e00-\u9fa5]/g;
+      let slice = str.substring(0, len);
+      let chineseCharNum = (~~(slice.match(reg) && slice.match(reg).length));
+      let realen = slice.length * 2 - chineseCharNum;
+      return str.substr(0, realen) + (realen < str.length ? '...' : '');
     }
   },
   formatDate(value, type = 's') {
