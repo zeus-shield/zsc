d861ed48279ca1e7acf32f7be29d74bc1709bd7a
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-03-19 09:28:03 +0800

    up Patch/dashboard/frontend/src/apis/insurance.js

--- Patch/dashboard/frontend/src/apis/insurance.js
+++ Patch/dashboard/frontend/src/apis/insurance.js
@@ -5,6 +5,9 @@ import Transaction from './transaction.js';
 class Insurance extends Transaction {
   constructor(instance) {
   };
+
+  async add(token, company, category, brief, detail) {
+  };
 }
 
 export default Insurance;
