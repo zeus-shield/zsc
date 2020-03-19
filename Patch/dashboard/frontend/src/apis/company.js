5ee7d30537b50080192662279b5a14da0d64d918
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-03-19 09:27:31 +0800

    up Patch/dashboard/frontend/src/apis/company.js

--- Patch/dashboard/frontend/src/apis/company.js
+++ Patch/dashboard/frontend/src/apis/company.js
@@ -5,6 +5,9 @@ import Transaction from './transaction.js';
 class Company extends Transaction {
   constructor(instance) {
   };
+
+  async add(token, name, categories) {
+  };
 }
 
 export default Company;
