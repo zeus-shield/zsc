66d248b3c9fb320d9d94b2f1ac37f5ad0c5f608f
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-03-23 11:08:40 +0800

    up Patch/dashboard/frontend/src/apis/company.js

--- Patch/dashboard/frontend/src/apis/company.js
+++ Patch/dashboard/frontend/src/apis/company.js
@@ -4,9 +4,13 @@ import Transaction from './transaction.js';
 
 class Company extends Transaction {
   constructor(instance) {
+    super(instance);
   };
 
   async add(token, name, categories) {
+  };
+
+  async groupCategoriesByName(token) {
   };
 }
 
