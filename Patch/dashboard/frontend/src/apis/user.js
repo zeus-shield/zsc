87b4e579a1f7464ca04bd54e8c98a7fb2281f430
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-03-23 11:12:14 +0800

    up Patch/dashboard/frontend/src/apis/user.js

--- Patch/dashboard/frontend/src/apis/user.js
+++ Patch/dashboard/frontend/src/apis/user.js
@@ -6,6 +6,9 @@ class User extends Transaction {
   constructor(instance) {
     super(instance);
   };
+
+  async statistics(token, company, category, title, sort, limit) {
+  };
 }
 
 export default User;
