e325a11de1304489ff128ad26d89397ae909a517
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-03-24 09:29:14 +0800

    up Patch/dashboard/frontend/src/views/Header.vue

--- Patch/dashboard/frontend/src/views/Header.vue
+++ Patch/dashboard/frontend/src/views/Header.vue
@@ -25,6 +25,15 @@
             <!-- <router-link tag="span" to="/home">{{langSet[lang].header.home}}</router-link> -->
             {{ langSet.header.home }}
           </el-menu-item>
+
+          <!-- insurance -->
+          <el-submenu index="2">
+          </el-submenu>
+
+          <!-- white paper -->
+          <el-menu-item index="3">
+            {{ langSet.header.whitePaper }}
+          </el-menu-item>
         </el-menu>
       </el-col>
 
