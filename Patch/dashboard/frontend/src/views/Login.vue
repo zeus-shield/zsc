98b51418e6cef17ddd879252e15b64afac1d3b72
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-03-25 09:58:10 +0800

    up Patch/dashboard/frontend/src/views/Login.vue

--- Patch/dashboard/frontend/src/views/Login.vue
+++ Patch/dashboard/frontend/src/views/Login.vue
@@ -127,8 +127,8 @@ export default {
       },
       rules: {
         account: [
-          // { required: true, message: '请输入手机号/邮箱地址', trigger: 'blur' },
-          // { min: 6, max: 18, message: '长度在 6 到 18 个字符', trigger: 'blur' }
+          // { required: true, message: '??????/????', trigger: 'blur' },
+          // { min: 6, max: 18, message: '??? 6 ? 18 ???', trigger: 'blur' }
           // { validator: validateAccount, trigger: ['blur', 'change'] },
           { validator: validateAccount, trigger: 'blur' }
         ],
@@ -246,6 +246,9 @@ export default {
             if (data.content.totp_on) {
               vm.dialogVisible = true;
             } else {
+              utils.storage.cookie.set('login_account', data.content.account, 'N/A');
+              utils.storage.cookie.set('login_token', data.content.token, 'N/A');
+              utils.storage.cookie.set('login_id', data.content._id, 'N/A');
               vm.loading = false;
               vm.$router.push({name: 'userDetail'});
             }
