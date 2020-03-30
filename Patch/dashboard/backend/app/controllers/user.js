e55327e18ada6f25d9b2bea4d02fdc4d9ef2d3c0
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-03-30 09:27:44 +0800

    up Patch/dashboard/backend/app/controllers/user.js

--- Patch/dashboard/backend/app/controllers/user.js
+++ Patch/dashboard/backend/app/controllers/user.js
@@ -84,9 +84,15 @@ const addPolicy = async(req, res) => {
     } else {
       throw createError('COMMON_PARAM_ERROR');
     }
+
+    // There is only one database write operation, and session can not be used.
     let policy = JSON.parse(req.body.policy);
     policy.insurance.id = mongoose.Types.ObjectId(policy.insurance.id);
 
+    const update = {
+      $push: {policies: policy},
+      $set: {updated_at: Date.now()}
+    };
     if (!result) {
       throw createError('USER_NOT_EXIST');
     }
