8c886628172168636dbbf97c7fd35b4cb88e40e4
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-04-07 09:18:02 +0800

    up Patch/dashboard/frontend/src/views/templates/insurances/pingan/brief.vue

--- Patch/dashboard/frontend/src/views/templates/insurances/pingan/brief.vue
+++ Patch/dashboard/frontend/src/views/templates/insurances/pingan/brief.vue
@@ -42,6 +42,11 @@
   font-weight: 700;
 }
 .insurance .info .col .corner {
+  position: absolute;
+  width: 61px;
+  height: 61px;
+  left: 20px;
+  top: 20px;
 }
 .insurance .info .col .content {
   margin-top: 20px;
