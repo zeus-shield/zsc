31c1e601ba43f10840b41a919be18060357644d9
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-04-16 14:53:33 +0800

    up Patch/dashboard/frontend/src/views/Header.vue

--- Patch/dashboard/frontend/src/views/Header.vue
+++ Patch/dashboard/frontend/src/views/Header.vue
@@ -98,6 +98,9 @@
           <!-- lang -->
           <el-dropdown class="header-lang" v-if="lang === 'cn'" @command="switchLanguage">
             <div class="header-lang-item">
+              <svg class="iconfont-symbol" aria-hidden="true">
+                <use xlink:href="#if-flag-cn-rect"></use>
+              </svg>
             </div>
           </el-dropdown>
         </div>
