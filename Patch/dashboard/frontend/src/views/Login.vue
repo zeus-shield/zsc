28cd0f1d688828fa7df6a9ec52f693ca92da824d
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-04-20 09:19:36 +0800

    up Patch/dashboard/frontend/src/views/Login.vue

--- Patch/dashboard/frontend/src/views/Login.vue
+++ Patch/dashboard/frontend/src/views/Login.vue
@@ -127,8 +127,8 @@ export default {
       },
       rules: {
         account: [
-          // { required: true, message: '请输入手机号/邮箱地址', trigger: 'blur' },
-          // { min: 6, max: 18, message: '长度在 6 到 18 个字符', trigger: 'blur' }
+          // { required: true, message: '??????/????', trigger: 'blur' },
+          // { min: 6, max: 18, message: '??? 6 ? 18 ???', trigger: 'blur' }
           // { validator: validateAccount, trigger: ['blur', 'change'] },
           { validator: validateAccount, trigger: 'blur' }
         ],
@@ -238,6 +238,13 @@ export default {
           // 1. common callback case
           user.loginCommon(vm.form.account, vm.form.password, (err, data) => {
             if (err === 0 && data.status === 200 && data.errorCode === 0) {
+               console.log('%c[Login]submitForm(%s)', `color:${vm.logColor}`, data);
+               utils.storage.cookie.set('login_account', vm.form.account, 'N/A');
+               utils.storage.cookie.set('login_account', data.content.account, 'N/A');
+               utils.storage.cookie.set('login_token', data.content.token, 'N/A');
+               utils.storage.cookie.set('login_id', data.content._id, 'N/A');
+               vm.loading = false;
+               vm.$router.push({name: 'user'});
              } else {
                console.log('%c[Login]submitForm(%s)', `color:${vm.logColor}`, data);
                vm.errorMessage = data.errorMessage;
