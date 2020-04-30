3f858df230e75ace004910eb58ffc960e9d726f5
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-04-30 09:30:05 +0800

    up Patch/dashboard/frontend/src/apis/dapp/user.js

--- Patch/dashboard/frontend/src/apis/dapp/user.js
+++ Patch/dashboard/frontend/src/apis/dapp/user.js
@@ -2,9 +2,13 @@
 
 import createError from 'http-errors';
 import { ethToObject, strToBytes32, strToHex, hexToStr } from '@/apis/dapp/utils';
+import utils from '@/common/utils';
 import config from '@/apis/dapp/config';
 
 class User {
+
+  async statistics(statId) {
+  }
 }
 
 export default User;
