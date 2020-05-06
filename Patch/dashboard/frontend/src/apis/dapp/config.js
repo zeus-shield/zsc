a09870319a3019bcf85547aefa65c35aa72792f8
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-05-06 11:16:48 +0800

    up Patch/dashboard/frontend/src/apis/dapp/config.js

--- Patch/dashboard/frontend/src/apis/dapp/config.js
+++ Patch/dashboard/frontend/src/apis/dapp/config.js
@@ -5,6 +5,8 @@ const config = {
   userPolicyKeyPrefix: 'POLICY_',
   userInfoKey: 'INFO',
   statIdPrefix: 'STAT_',
+  statCompanyId: 'STAT_COMPANY',
+  statProductId: 'STAT_PRODUCT'
 };
 
 module.exports = config;
