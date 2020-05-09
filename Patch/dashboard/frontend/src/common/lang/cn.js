8e3c25e73251fa9e46a28e79ff14ccbcb4842964
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-05-09 09:54:05 +0800

    up Patch/dashboard/frontend/src/common/lang/cn.js

--- Patch/dashboard/frontend/src/common/lang/cn.js
+++ Patch/dashboard/frontend/src/common/lang/cn.js
@@ -326,6 +326,8 @@ const cn = {
       PICC: '中国人保'
     },
     product: {
+      PINGAN_ZYYL_ESBYLPB: '中国平安e生保医疗险PLUS版',
+      PINGAN_ZYYL_SHYBGMZYZFYL: '中国平安上海医保购买住院自费医疗保险',
     },
     stat: {
       STAT_COMPANY: '公司热度分析',
