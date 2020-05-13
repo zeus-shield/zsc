b4cd1d919a97218cdb685a5e4f960b88fb78987a
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-05-13 20:14:50 +0800

    up Patch/dashboard/backend/app/models/index.js

--- Patch/dashboard/backend/app/models/index.js
+++ Patch/dashboard/backend/app/models/index.js
@@ -5,9 +5,5 @@ mdbs.User = require('./user');
 mdbs.Company = require('./company');
 mdbs.Insurance = require('./insurance');
 mdbs.QA = require('./qa');
-// mdbs.Role = require('./role');
-// mdbs.Right = require('./right');
-// mdbs.Article = require('./article');
-// mdbs.ArticleCategory = require('./category');
 
 module.exports = mdbs;
