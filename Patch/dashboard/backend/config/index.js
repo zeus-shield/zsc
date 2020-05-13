d098f08bc87d3d5626cb30c623d229bde67a123c
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-05-13 20:16:33 +0800

    up Patch/dashboard/backend/config/index.js

--- Patch/dashboard/backend/config/index.js
+++ Patch/dashboard/backend/config/index.js
@@ -10,11 +10,5 @@ const configs = {};
 configs.settings = require(`./${settings}`);
 configs.logConfig = require(`./${log4js}`);
 configs.errMsg = require('./errorMessage');
-// configs.successMsg = require('./success-message');
-// configs.errorSystem = require('./error-system');
-// configs.pageConfig = require('./pagesize');
-// configs.enum = require('./enum');
-// configs.router = require('./router.js');
-// configs.right = require('./right');
 
 module.exports = configs;
