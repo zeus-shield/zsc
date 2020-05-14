83ce7c3e4b4d795e71d0f6f7d790532b450c3434
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-05-14 08:57:08 +0800

    up Patch/dashboard/backend/app/controllers/qa.js

--- Patch/dashboard/backend/app/controllers/qa.js
+++ Patch/dashboard/backend/app/controllers/qa.js
@@ -3,3 +3,8 @@
 // const mongoose = require('mongoose');
 const createError = require('http-errors');
 const services = require('../services');
+
+// const debug = require('debug')('backend:app:controllers:qa');
+
+const add = async(req, res) => {
+};
