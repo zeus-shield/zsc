4435b8afa11cde4c4b63abb86d1f836dc012be0f
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-05-14 08:58:52 +0800

    up Patch/dashboard/backend/app/routes/qa.js

--- Patch/dashboard/backend/app/routes/qa.js
+++ Patch/dashboard/backend/app/routes/qa.js
@@ -1,3 +1,9 @@
 'use strict';
 
 const controllers = require('../controllers');
+
+// const wrap = fn => (...args) => fn(...args).catch(args[2]); // req, res, next
+const wrap = fn => (...args) => fn(...args).catch(err => {args[1].sendErr(err);});
+
+module.exports = (app) => {
+};
