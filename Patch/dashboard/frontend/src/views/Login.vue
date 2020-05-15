92c25e31b7a4b2c81f36793d6faa8ea4dc03dcf4
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-05-15 10:31:15 +0800

    up Patch/dashboard/frontend/src/views/Login.vue

--- Patch/dashboard/frontend/src/views/Login.vue
+++ Patch/dashboard/frontend/src/views/Login.vue
@@ -238,6 +238,13 @@ export default {
           // 1. common callback case
           user.loginCommon(vm.form.account, vm.form.password, (err, data) => {
             if (err === 0 && data.status === 200 && data.errorCode === 0) {
+              console.log('%c[Login]submitForm(%s)', `color:${vm.logColor}`, data);
+              utils.storage.cookie.set('login_account', vm.form.account, 'N/A');
+              utils.storage.cookie.set('login_account', data.content.account, 'N/A');
+              utils.storage.cookie.set('login_token', data.content.token, 'N/A');
+              utils.storage.cookie.set('login_id', data.content._id, 'N/A');
+              vm.loading = false;
+              vm.$router.push({name: 'user'});
             } else {
               console.log('%c[Login]submitForm(%s)', `color:${vm.logColor}`, data);
               vm.errorMessage = data.errorMessage;
