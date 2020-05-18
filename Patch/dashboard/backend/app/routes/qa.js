a1648efb03cdde36f65ceaee801ef86e648d8418
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-05-18 23:08:26 +0800

    up Patch/dashboard/backend/app/routes/qa.js

--- Patch/dashboard/backend/app/routes/qa.js
+++ Patch/dashboard/backend/app/routes/qa.js
@@ -8,4 +8,6 @@ const wrap = fn => (...args) => fn(...args).catch(err => {args[1].sendErr(err);}
 module.exports = (app) => {
   app.route('/qa/add').post(wrap(controllers.qa.add));
   app.route('/qa/remove').post(wrap(controllers.qa.remove));
+  app.route('/qa/removeAll').post(wrap(controllers.qa.removeAll));
+  app.route('/qa/update').post(wrap(controllers.qa.update));
 };
