8f98de7937f0f576107bff8f4dee3726b9f44ab9
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-05-21 11:09:47 +0800

    up Patch/dashboard/backend/app/controllers/company.js

--- Patch/dashboard/backend/app/controllers/company.js
+++ Patch/dashboard/backend/app/controllers/company.js
@@ -145,6 +145,13 @@ const update = async(req, res) => {
 
           // method[2] according to _id for insurances TODO
           for (let k = 0; k < result.categories[i].insurance_ids.length; k++) {
+            const conditions = {
+              _id: result.categories[i].insurance_ids[k]
+            };
+            await services.insurances.remove(conditions, false, session);
+            // if (!resultInner) {
+            //   throw createError('INSURANCE_NOT_EXIST');
+            // }
           }
         }
       }
