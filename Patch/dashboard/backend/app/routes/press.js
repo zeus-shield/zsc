0157a3e1510f32266eef1d0ae046999f7e76b1b8
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-05-25 09:27:06 +0800

    up Patch/dashboard/backend/app/routes/press.js

--- Patch/dashboard/backend/app/routes/press.js
+++ Patch/dashboard/backend/app/routes/press.js
@@ -8,4 +8,6 @@ const wrap = fn => (...args) => fn(...args).catch(err => {args[1].sendErr(err);}
 module.exports = (app) => {
   app.route('/press/add').post(wrap(controllers.press.add));
   app.route('/press/remove').post(wrap(controllers.press.remove));
+  app.route('/press/removeAll').post(wrap(controllers.press.removeAll));
+  app.route('/press/update').post(wrap(controllers.press.update));
 };
