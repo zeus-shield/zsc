65fc9f2670246ede3ecdd1f7e7db0ee26912a25e
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-06-02 09:46:20 +0800

    up Patch/dashboard/frontend/src/apis/dapp/reward.js

---
+++ Patch/dashboard/frontend/src/apis/dapp/reward.js
@@ -0,0 +1,6 @@
+'use strict';
+
+import CryptoJS from 'crypto-js';
+
+import createError from 'http-errors';
+import { ethToObject, toBytes32, strToBytes32, strToHex, hexToStr } from '@/apis/dapp/utils';
