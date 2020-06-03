e42159e4ac4ef267a6e9cd5d7a6f3b1fd8b74774
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-06-03 08:41:08 +0800

    up Patch/dashboard/frontend/src/apis/dapp/reward.js

--- Patch/dashboard/frontend/src/apis/dapp/reward.js
+++ Patch/dashboard/frontend/src/apis/dapp/reward.js
@@ -4,3 +4,8 @@ import CryptoJS from 'crypto-js';
 
 import createError from 'http-errors';
 import { ethToObject, toBytes32, strToBytes32, strToHex, hexToStr } from '@/apis/dapp/utils';
+
+class Reward {
+}
+
+export default Reward;
