3a02fd0b0e834c4a88f904651486e4623e7f6b31
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-06-04 08:24:23 +0800

    up Patch/dashboard/backend/app/apis/scan.js

--- Patch/dashboard/backend/app/apis/scan.js
+++ Patch/dashboard/backend/app/apis/scan.js
@@ -1,9 +1,9 @@
 'use strict';
 
-// const createError = require('http-errors');
+const createError = require('http-errors');
 const services = require('../services');
 
-const debug = require('debug')('backend:app:apis:scan');
+// const debug = require('debug')('backend:app:apis:scan');
 
 const balance = async(req, res) => {
 };
