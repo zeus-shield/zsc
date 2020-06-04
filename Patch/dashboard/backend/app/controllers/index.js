38a72b4ccd3130cd50868bf7b6fcab8294a43491
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-06-04 08:25:14 +0800

    up Patch/dashboard/backend/app/controllers/index.js

--- Patch/dashboard/backend/app/controllers/index.js
+++ Patch/dashboard/backend/app/controllers/index.js
@@ -6,5 +6,6 @@ controllers.company = require('./company');
 controllers.insurance = require('./insurance');
 controllers.qa = require('./qa');
 controllers.press = require('./press');
+controllers.token = require('./token');
 
 module.exports = controllers;
