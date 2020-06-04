4cc6603ac4a89758251f6811502dcdd3fe22792d
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-06-04 08:28:52 +0800

    up Patch/dashboard/frontend/src/apis/dapp/reward.js

--- Patch/dashboard/frontend/src/apis/dapp/reward.js
+++ Patch/dashboard/frontend/src/apis/dapp/reward.js
@@ -6,6 +6,9 @@ import createError from 'http-errors';
 import { ethToObject, toBytes32, strToBytes32, strToHex, hexToStr } from '@/apis/dapp/utils';
 
 class Reward {
+
+  constructor(ewallet, addr, abi) {
+  };
 }
 
 export default Reward;
