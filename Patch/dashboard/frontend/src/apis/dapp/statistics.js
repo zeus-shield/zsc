4d41a2048735e844530b069e63a38548b9127c7b
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-06-04 08:29:36 +0800

    up Patch/dashboard/frontend/src/apis/dapp/statistics.js

--- Patch/dashboard/frontend/src/apis/dapp/statistics.js
+++ Patch/dashboard/frontend/src/apis/dapp/statistics.js
@@ -4,6 +4,9 @@ import createError from 'http-errors';
 import { strToBytes32, hexToStr } from '@/apis/dapp/utils';
 
 class Stat {
+
+  constructor(ewallet, addr, abi) {
+  };
 }
 
 export default Stat;
