d75c201537c921f4197179d805b4ee52f1943819
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-06-05 11:34:52 +0800

    up Patch/dashboard/backend/app/apis/scan.js

--- Patch/dashboard/backend/app/apis/scan.js
+++ Patch/dashboard/backend/app/apis/scan.js
@@ -2,8 +2,11 @@
 
 const createError = require('http-errors');
 const services = require('../services');
-
+const { axios } = require('../utils');
 // const debug = require('debug')('backend:app:apis:scan');
 
 const balance = async(req, res) => {
+};
+
+const token = async(req, res) => {
 };
