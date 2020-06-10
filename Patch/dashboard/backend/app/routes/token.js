21439c65736385910745bb52de800e1dcfcedb9d
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-06-10 09:37:38 +0800

    up Patch/dashboard/backend/app/routes/token.js

--- Patch/dashboard/backend/app/routes/token.js
+++ Patch/dashboard/backend/app/routes/token.js
@@ -6,4 +6,7 @@ const controllers = require('../controllers');
 const wrap = fn => (...args) => fn(...args).catch(err => {args[1].sendErr(err);});
 
 module.exports = (app) => {
+  app.route('/token/add').post(wrap(controllers.token.add));
+  app.route('/token/remove').post(wrap(controllers.token.remove));
+  app.route('/token/update').post(wrap(controllers.token.update));
 };
