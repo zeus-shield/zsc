c27172e8270dea7bee620219cb6054c1888e4942
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-06-11 10:03:11 +0800

    up Patch/dashboard/frontend/src/views/templates/insurances/picc/brief.vue

--- Patch/dashboard/frontend/src/views/templates/insurances/picc/brief.vue
+++ Patch/dashboard/frontend/src/views/templates/insurances/picc/brief.vue
@@ -205,6 +205,10 @@ export default {
       addPolicyToDatabaseDoing: false,
       form: {
         insurance: {
+          id: '',
+          company: '',
+          category: '',
+          title: '',
         },
         user: {
         },
