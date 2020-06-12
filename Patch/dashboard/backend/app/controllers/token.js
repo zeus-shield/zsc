5a5e68af51ed4dd11d7bd0dca614ba227cec1c9d
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-06-12 14:35:58 +0800

    up Patch/dashboard/backend/app/controllers/token.js

--- Patch/dashboard/backend/app/controllers/token.js
+++ Patch/dashboard/backend/app/controllers/token.js
@@ -7,6 +7,11 @@ const services = require('../services');
 // const debug = require('debug')('backend:app:controllers:token');
 
 const add = async(req, res) => {
+  // debug('add(%s)', JSON.stringify(req.body));
+  try {
+  } catch (err) {
+    throw err;
+  }
 };
 
 const remove = async(req, res) => {
