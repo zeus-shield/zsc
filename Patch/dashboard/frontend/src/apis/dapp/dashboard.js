8a0443a6e9ff2237dd9f53115d8c42da0cf0b2cb
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-06-12 14:37:27 +0800

    up Patch/dashboard/frontend/src/apis/dapp/dashboard.js

--- Patch/dashboard/frontend/src/apis/dapp/dashboard.js
+++ Patch/dashboard/frontend/src/apis/dapp/dashboard.js
@@ -1,3 +1,8 @@
 'use strict';
 
 import CryptoJS from 'crypto-js';
+
+import createError from 'http-errors';
+import { ethToObject, toBytes32, strToBytes32, strToHex, hexToStr } from '@/apis/dapp/utils';
+import utils from '@/common/utils';
+import config from '@/apis/dapp/config';
