a792aecbcf6eb66e557e008b5353d47b69292e2b
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-06-16 10:13:34 +0800

    up Patch/dashboard/frontend/src/apis/dapp/dashboard.js

--- Patch/dashboard/frontend/src/apis/dapp/dashboard.js
+++ Patch/dashboard/frontend/src/apis/dapp/dashboard.js
@@ -6,3 +6,8 @@ import createError from 'http-errors';
 import { ethToObject, toBytes32, strToBytes32, strToHex, hexToStr } from '@/apis/dapp/utils';
 import utils from '@/common/utils';
 import config from '@/apis/dapp/config';
+
+class Dashboard {
+}
+
+export default Dashboard;
