70e81a6d74c6817a1a9315597a53f90873204d8a
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-06-29 16:21:07 +0800

    up Patch/dashboard/frontend/projs/all/src/apis/dapp/statistics.js

--- Patch/dashboard/frontend/projs/all/src/apis/dapp/statistics.js
+++ Patch/dashboard/frontend/projs/all/src/apis/dapp/statistics.js
@@ -5,8 +5,8 @@ import { strToBytes32, hexToStr } from '@/apis/dapp/utils';
 
 class Stat {
 
-  constructor(ewallet, addr, abi) {
-    this.ewallet = ewallet;
+  constructor(einstance, addr, abi) {
+    this.einstance = einstance;
     this.addr = addr;
     this.abi = abi;
   };
