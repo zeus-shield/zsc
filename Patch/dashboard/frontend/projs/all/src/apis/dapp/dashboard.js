501a1bf7e45463e7e4ceac8974cc84298db16bf3
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-07-01 09:08:19 +0800

    up Patch/dashboard/frontend/projs/all/src/apis/dapp/dashboard.js

--- Patch/dashboard/frontend/projs/all/src/apis/dapp/dashboard.js
+++ Patch/dashboard/frontend/projs/all/src/apis/dapp/dashboard.js
@@ -3,9 +3,8 @@
 import CryptoJS from 'crypto-js';
 
 import createError from 'http-errors';
-import { ethToObject, toBytes32, strToBytes32, strToHex, hexToStr } from '@/apis/dapp/utils';
-import utils from '@/common/utils';
-import config from '@/apis/dapp/config';
+import { ethToObject, toBytes32, strToBytes32, strToHex, hexToStr, repeatNum } from './utils';
+import config from './config';
 
 class Dashboard {
 
