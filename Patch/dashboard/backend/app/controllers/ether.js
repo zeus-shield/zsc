adcdceec73b75a665c1eacd4545e7da353324252
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-07-14 09:28:29 +0800

    up Patch/dashboard/backend/app/controllers/ether.js

--- Patch/dashboard/backend/app/controllers/ether.js
+++ Patch/dashboard/backend/app/controllers/ether.js
@@ -5,3 +5,8 @@ const userAddPolicy = async(req, res) => {
 
 const userCheckIn = async(req, res) => {
 };
+
+const rewardTransfer = async(req, res) => {
+};
+
+module.exports = { userAddPolicy, userCheckIn, rewardTransfer };
