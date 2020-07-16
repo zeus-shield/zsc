0f35a600f99f3070124469f721acce432304149c
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-07-16 11:58:31 +0800

    up Patch/dashboard/backend/app/controllers/ether.js

--- Patch/dashboard/backend/app/controllers/ether.js
+++ Patch/dashboard/backend/app/controllers/ether.js
@@ -7,6 +7,10 @@ const userAddPolicy = async(req, res) => {
 };
 
 const userCheckIn = async(req, res) => {
+  debug('userCheckIn(%s)', req.body.account);
+  try {
+  } catch (err) {
+  }
 };
 
 const rewardTransfer = async(req, res) => {
