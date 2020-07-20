721767e428ad7035fbf5c4339c9d50bc64ed1ee3
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-07-20 11:52:38 +0800

    Patch/dashboard/frontend/projs/all/src/views/UserPoint.vue

--- Patch/dashboard/frontend/projs/all/src/views/UserPoint.vue
+++ Patch/dashboard/frontend/projs/all/src/views/UserPoint.vue
@@ -3,3 +3,8 @@
 
 <style lang="scss" scoped>
 <script>
+import { mapState, mapGetters } from 'vuex';
+
+import utils from '@/common/utils';
+import APIs from '@/apis';
+import config from '@/apis/dapp/config';
