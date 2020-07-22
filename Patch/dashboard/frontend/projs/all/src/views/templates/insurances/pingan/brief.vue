eb5bec8d8fc81cdf95368862b53d7214c023bafe
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-07-22 10:00:11 +0800

    up Patch/dashboard/frontend/projs/all/src/views/templates/insurances/pingan/brief.vue

--- Patch/dashboard/frontend/projs/all/src/views/templates/insurances/pingan/brief.vue
+++ Patch/dashboard/frontend/projs/all/src/views/templates/insurances/pingan/brief.vue
@@ -242,9 +242,14 @@ export default {
       rules: {
         insurance: {
           company: [
-            // { required: true, message: '请输入公司名称', trigger: 'blur' },
-            // { min: 3, message: '长度大于3个字符', trigger: 'blur' }
+            // { required: true, message: '???????', trigger: 'blur' },
+            // { min: 3, message: '????3???', trigger: 'blur' }
             { required: true, validator: validateCompany, trigger: 'blur' }
+          ],
+          category: [
+            // { required: true, message: '???????', trigger: 'blur' },
+            // { min: 3, message: '????3???', trigger: 'blur' }
+            { required: true, validator: validateCategory, trigger: 'blur' }
           ],
         },
       }
