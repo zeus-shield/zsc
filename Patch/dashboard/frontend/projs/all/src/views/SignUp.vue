c15a31448c99d94761c05f99b033f71c1112607e
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-07-27 09:24:17 +0800

    up Patch/dashboard/frontend/projs/all/src/views/SignUp.vue

--- Patch/dashboard/frontend/projs/all/src/views/SignUp.vue
+++ Patch/dashboard/frontend/projs/all/src/views/SignUp.vue
@@ -130,6 +130,8 @@ export default {
             callback();
           }
         }
+      } else {
+        callback();
       }
     };
     let validateQuickAccount = (rule, value, callback) => {
