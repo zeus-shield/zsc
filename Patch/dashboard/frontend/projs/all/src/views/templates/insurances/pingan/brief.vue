ab0fd3726a3169e441ec4f1b0b0e2f220ef82501
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-07-28 09:44:01 +0800

    up Patch/dashboard/frontend/projs/all/src/views/templates/insurances/pingan/brief.vue

--- Patch/dashboard/frontend/projs/all/src/views/templates/insurances/pingan/brief.vue
+++ Patch/dashboard/frontend/projs/all/src/views/templates/insurances/pingan/brief.vue
@@ -247,17 +247,22 @@ export default {
             { required: true, validator: validateCompany, trigger: 'blur' }
           ],
           category: [
-            // { required: true, message: '请输入产品类型', trigger: 'blur' },
-            // { min: 3, message: '长度大于3个字符', trigger: 'blur' }
+            // { required: true, message: '???????', trigger: 'blur' },
+            // { min: 3, message: '????3???', trigger: 'blur' }
             { required: true, validator: validateCategory, trigger: 'blur' }
           ],
           title: [
-            // { required: true, message: '请输入产品名称', trigger: 'blur' },
-            // { min: 3, message: '长度大于3个字符', trigger: 'blur' }
+            // { required: true, message: '???????', trigger: 'blur' },
+            // { min: 3, message: '????3???', trigger: 'blur' }
             { required: true, validator: validateTitle, trigger: 'blur' }
           ]
         },
         user: {
+          holder: [
+            // { required: true, message: '??????', trigger: 'blur' },
+            // { min: 3, message: '????3???', trigger: 'blur' }
+            { required: true, validator: validateHolder, trigger: 'blur' }
+          ],
         },
       }
     };
