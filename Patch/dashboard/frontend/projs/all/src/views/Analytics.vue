257ce6ef98e2436964fec1a21a276698e6779f34
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-08-06 14:34:57 +0800

    up Patch/dashboard/frontend/projs/all/src/views/Analytics.vue

--- Patch/dashboard/frontend/projs/all/src/views/Analytics.vue
+++ Patch/dashboard/frontend/projs/all/src/views/Analytics.vue
@@ -172,7 +172,7 @@ export default {
         },
         series: [
           {
-            name: utils.storage.cookie.get('lang') === 'en' ? 'Policy: ' : '?? :',
+            name: utils.storage.cookie.get('lang') === 'en' ? 'Policy: ' : '险种 :',
             type: 'pie',
             // radius: '55%',
             // radius: ['50%', '70%'],
@@ -200,8 +200,8 @@ export default {
       this.chartBar = echarts.init(handle);
       let option = {
         title: {
-          text: utils.storage.cookie.get('lang') === 'en' ? 'Company Ranking' : '???????',
-          subtext: utils.storage.cookie.get('lang') === 'en' ? '(TOP10)' : '(????????)',
+          text: utils.storage.cookie.get('lang') === 'en' ? 'Company Ranking' : '公司热度排行榜',
+          subtext: utils.storage.cookie.get('lang') === 'en' ? '(TOP10)' : '（排列前十名的公司）',
           x: 'center',
           textStyle: {
             color: '#444',
