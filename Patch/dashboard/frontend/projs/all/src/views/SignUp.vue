7eced751e38b39c33a0396aba99c83a7459393b6
bakerjiang <36978821+bakerjiang@users.noreply.github.com>
2020-08-10 18:19:04 +0800

    up Patch/dashboard/frontend/projs/all/src/views/SignUp.vue

--- Patch/dashboard/frontend/projs/all/src/views/SignUp.vue
+++ Patch/dashboard/frontend/projs/all/src/views/SignUp.vue
@@ -192,21 +192,21 @@ export default {
       },
       rules: {
         phoneAccount: [
-          // { required: true, message: '请输入手机号/邮箱地址', trigger: 'blur' },
-          // { min: 6, max: 18, message: '长度在 6 到 18 个字符', trigger: 'blur' }
+          // { required: true, message: '??????/????', trigger: 'blur' },
+          // { min: 6, max: 18, message: '??? 6 ? 18 ???', trigger: 'blur' }
           { validator: validatePhoneAccount, trigger: ['blur', 'change'] }
         ],
         emailAccount: [
-          // { required: true, message: '请输入手机号/邮箱地址', trigger: 'blur' },
-          // { min: 6, max: 18, message: '长度在 6 到 18 个字符', trigger: 'blur' }
+          // { required: true, message: '??????/????', trigger: 'blur' },
+          // { min: 6, max: 18, message: '??? 6 ? 18 ???', trigger: 'blur' }
           { validator: validateEmailAccount, trigger: ['blur', 'change'] }
         ],
         quickAccount: [
           { validator: validateQuickAccount, trigger: ['blur', 'change'] }
         ],
         code: [
-          // { required: true, message: '请输入手机号/邮箱地址', trigger: 'blur' },
-          // { min: 6, max: 18, message: '长度在 6 到 18 个字符', trigger: 'blur' }
+          // { required: true, message: '??????/????', trigger: 'blur' },
+          // { min: 6, max: 18, message: '??? 6 ? 18 ???', trigger: 'blur' }
           { validator: validateCode, trigger: ['blur', 'change'] }
         ],
         password: [
@@ -358,7 +358,8 @@ export default {
           });
           // 3. async case
           let account = '';
-          if (vm.tabIndex === '1') {
+          if (vm.tabName === 'signup-tab-phone') {
+            cmd = 'phone';
             account = vm.form.phoneAccount;
           } else if (vm.tabIndex === '0') {
             account = vm.form.emailAccount;
